533c2e14789e770601b39b1d7530580f7ca33425
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-18 18:53:16 +0200

    Initial artifacts layout

---
+++ sandbox-test/src/test/java/de/adorsys/openbankinggateway/BasicTest.java
@@ -0,0 +1,11 @@
+package de.adorsys.openbankinggateway;
+
+import org.junit.jupiter.api.Test;
+
+
+class BasicTest extends WithSandboxSpringBootTest {
+
+    @Test
+    void testEnvStartsUp() {
+    }
+}
