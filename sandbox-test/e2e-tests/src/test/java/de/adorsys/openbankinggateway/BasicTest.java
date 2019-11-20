317d574fe0105995bfed4f7b74bab2bced086adb
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-20 17:47:04 +0200

    Add capability to check that env is ready

--- sandbox-test/e2e-tests/src/test/java/de/adorsys/openbankinggateway/BasicTest.java
+++ sandbox-test/e2e-tests/src/test/java/de/adorsys/openbankinggateway/BasicTest.java
@@ -3,11 +3,15 @@ package de.adorsys.openbankinggateway;
 import de.adorsys.openbankinggateway.sandbox.SandboxAppsStarter;
 import de.adorsys.openbankinggateway.sandbox.internal.SandboxApp;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.awaitility.Duration;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 
+@Slf4j
 class BasicTest extends WithSandboxSpringBootTest {
 
     private final SandboxAppsStarter executor = new SandboxAppsStarter();
@@ -17,8 +21,10 @@ class BasicTest extends WithSandboxSpringBootTest {
     void testEnvStartsUp() {
         executor.runAll();
 
-        Thread.sleep(20000);
+        await().atMost(Duration.FIVE_MINUTES).pollDelay(Duration.ONE_SECOND).until(SandboxApp::allReadyToUse);
+        log.info("ALL READY!");
 
+        Thread.sleep(20000);
         assertThat(SandboxApp.values()).extracting(it -> it.getLoader().get()).isNotNull();
     }
 }
