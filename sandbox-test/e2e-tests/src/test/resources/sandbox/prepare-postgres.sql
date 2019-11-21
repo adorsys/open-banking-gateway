952cf0466f3b54c20603ec59110f94bc408a0d08
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-21 15:40:42 +0200

    Leaner postgres permissions

--- sandbox-test/e2e-tests/src/test/resources/sandbox/prepare-postgres.sql
+++ sandbox-test/e2e-tests/src/test/resources/sandbox/prepare-postgres.sql
@@ -3,16 +3,17 @@ CREATE SCHEMA consent;
 
 CREATE ROLE sandboxrole;
 GRANT ALL ON SCHEMA consent TO sandboxrole;
-GRANT ALL ON SCHEMA ledgers TO sandboxrole;
 
-CREATE USER "test-ledgers-app" WITH ENCRYPTED PASSWORD 'test-ledgers-app' IN ROLE sandboxrole;
+/* Ledgers is has its own database */
+CREATE USER "test-ledgers-app" WITH ENCRYPTED PASSWORD 'test-ledgers-app';
+GRANT ALL ON SCHEMA ledgers TO "test-ledgers-app";
+
+/* This db is shared */
 CREATE USER "test-consentmgmt" WITH ENCRYPTED PASSWORD 'test-consentmgmt' IN ROLE sandboxrole;
 CREATE USER "test-online-banking" WITH ENCRYPTED PASSWORD 'test-online-banking' IN ROLE sandboxrole;
 CREATE USER "test-tpp-rest" WITH ENCRYPTED PASSWORD 'test-tpp-rest' IN ROLE sandboxrole;
 
 /* Necessary, as default privileges apply only from current user context  */
-ALTER DEFAULT PRIVILEGES FOR USER "test-ledgers-app" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
-ALTER DEFAULT PRIVILEGES FOR USER "test-ledgers-app" IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
 ALTER DEFAULT PRIVILEGES FOR USER "test-consentmgmt" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
 ALTER DEFAULT PRIVILEGES FOR USER "test-consentmgmt" IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
 ALTER DEFAULT PRIVILEGES FOR USER "test-online-banking" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
