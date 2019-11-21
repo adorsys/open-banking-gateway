bec425764e88df1235738e4293fb57cf9a6a8d5a
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-21 12:51:45 +0200

    Fixed postgres creation script

--- sandbox-test/e2e-tests/src/test/resources/sandbox/prepare-postgres.sql
+++ sandbox-test/e2e-tests/src/test/resources/sandbox/prepare-postgres.sql
@@ -1,20 +1,21 @@
 CREATE SCHEMA ledgers;
 CREATE SCHEMA consent;
 
-CREATE USER "test-ledgers-app" WITH ENCRYPTED PASSWORD 'test-ledgers-app';
-GRANT ALL ON SCHEMA ledgers TO "test-ledgers-app";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ledgers TO "test-ledgers-app";
-GRANT ALL ON SCHEMA consent TO "test-ledgers-app";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA consent TO "test-ledgers-app";
+CREATE ROLE sandboxrole;
+GRANT ALL ON SCHEMA consent TO sandboxrole;
+GRANT ALL ON SCHEMA ledgers TO sandboxrole;
 
-CREATE USER "test-consentmgmt" WITH ENCRYPTED PASSWORD 'test-consentmgmt';
-GRANT ALL ON SCHEMA consent TO "test-consentmgmt";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA consent TO "test-consentmgmt";
+CREATE USER "test-ledgers-app" WITH ENCRYPTED PASSWORD 'test-ledgers-app' IN ROLE sandboxrole;
+CREATE USER "test-consentmgmt" WITH ENCRYPTED PASSWORD 'test-consentmgmt' IN ROLE sandboxrole;
+CREATE USER "test-online-banking" WITH ENCRYPTED PASSWORD 'test-online-banking' IN ROLE sandboxrole;
+CREATE USER "test-tpp-rest" WITH ENCRYPTED PASSWORD 'test-tpp-rest' IN ROLE sandboxrole;
 
-CREATE USER "test-online-banking" WITH ENCRYPTED PASSWORD 'test-online-banking';
-GRANT ALL ON SCHEMA consent TO "test-online-banking";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA consent TO "test-online-banking";
-
-CREATE USER "test-tpp-rest" WITH ENCRYPTED PASSWORD 'test-tpp-rest';
-GRANT ALL ON SCHEMA consent TO "test-tpp-rest";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA consent TO "test-tpp-rest";
+/* Necessary, as default privileges apply only from current user context  */
+ALTER DEFAULT PRIVILEGES FOR USER "test-ledgers-app" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-ledgers-app" IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-consentmgmt" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-consentmgmt" IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-online-banking" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-online-banking" IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-tpp-rest" IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER "test-tpp-rest" IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
