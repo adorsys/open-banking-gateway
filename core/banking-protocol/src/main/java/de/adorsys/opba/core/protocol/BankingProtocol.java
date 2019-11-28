b73ecadbaddacd96ee484c781c7eb45f48a0f331
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-28 20:47:09 +0200

    JTA for flowable

--- core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/BankingProtocol.java
+++ core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/BankingProtocol.java
@@ -2,12 +2,26 @@ package de.adorsys.opba.core.protocol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+// FIXME - Multiple DataSource requires JTA
+@EnableConfigurationProperties
+@EnableTransactionManagement
 @SpringBootApplication(
         scanBasePackages = {
                 "de.adorsys.opba.core.protocol.config",
                 "de.adorsys.opba.core.protocol.controller",
                 "de.adorsys.opba.core.protocol.service"
+        },
+        // For Atomikos:
+        exclude = {
+            DataSourceAutoConfiguration.class,
+            HibernateJpaAutoConfiguration.class,
+            DataSourceTransactionManagerAutoConfiguration.class
         }
 )
 public class BankingProtocol {
