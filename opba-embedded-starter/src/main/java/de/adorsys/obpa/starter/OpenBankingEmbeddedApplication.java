1214b5ca61e5aedcb50fea0859ea1397203824fc
Denys Golubiev <dgo@adorsys.de>
2019-11-29 13:28:12 +0100

    Add starter module

---
+++ opba-embedded-starter/src/main/java/de/adorsys/obpa/starter/OpenBankingEmbeddedApplication.java
@@ -0,0 +1,13 @@
+package de.adorsys.obpa.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBankingEmbeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBankingEmbeddedApplication.class, args);
+	}
+
+}
