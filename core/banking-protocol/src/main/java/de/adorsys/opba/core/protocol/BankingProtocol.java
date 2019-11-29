ef59161dea46df527fcff4103accf96440d1e7c5
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-29 16:57:30 +0200

    Added dummy certificates and keystore

--- core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/BankingProtocol.java
+++ core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/BankingProtocol.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-// FIXME - Multiple DataSource requires JTA
 @EnableConfigurationProperties
 @EnableTransactionManagement
 @SpringBootApplication(
--- core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/controller/AccountInformation.java
+++ core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/controller/AccountInformation.java
@@ -1,21 +1,25 @@
 package de.adorsys.opba.core.protocol.controller;
 
+import de.adorsys.opba.core.protocol.service.AccountInformationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
-import static de.adorsys.opba.core.protocol.controller.constants.ApiVersion.API_1;
 import static de.adorsys.opba.core.protocol.controller.constants.ApiPaths.TRANSACTIONS;
+import static de.adorsys.opba.core.protocol.controller.constants.ApiVersion.API_1;
 
 @RestController
 @RequestMapping(API_1)
+@RequiredArgsConstructor
 public class AccountInformation {
+
+    private final AccountInformationService ais;
 
     @GetMapping(TRANSACTIONS)
     public List<String> transactions() {
-        return Collections.emptyList();
+        return ais.transactionList();
     }
 }
