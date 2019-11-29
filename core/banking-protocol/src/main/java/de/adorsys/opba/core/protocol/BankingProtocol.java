6d7159f2e79d038a01fd34f0579a14bf4f103cdb
valb3r <valentyn.berezin@adorsys.com.ua>
2019-11-29 10:41:26 +0200

    Single schema BPMN

--- core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/BankingProtocol.java
+++ core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/BankingProtocol.java
@@ -2,9 +2,6 @@ package de.adorsys.opba.core.protocol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,12 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
                 "de.adorsys.opba.core.protocol.config",
                 "de.adorsys.opba.core.protocol.controller",
                 "de.adorsys.opba.core.protocol.service"
-        },
-        // For Atomikos:
-        exclude = {
-            DataSourceAutoConfiguration.class,
-            HibernateJpaAutoConfiguration.class,
-            DataSourceTransactionManagerAutoConfiguration.class
         }
 )
 public class BankingProtocol {
