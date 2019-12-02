282c4afd22ceee03390958ac1876ba83d31ca2e2
Vladimir Nechai <vne@adorsys.de>
2019-12-02 18:45:35 +0100

    add bootstrap dependency

--- banking-gateway-ui/src/app/bank-search/bank-search-routing.module.ts
+++ banking-gateway-ui/src/app/bank-search/bank-search-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BankSearchComponent} from "./components/bank-search/bank-search.component";
 
-import { BankSearchComponent } from './components/bank-search/bank-search.component';
 
 const routes: Routes = [{ path: '', component: BankSearchComponent }];
 
