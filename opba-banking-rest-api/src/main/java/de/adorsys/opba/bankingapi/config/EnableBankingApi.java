ecb64674691586599c5cf49f3b6bfa94f7ec2326
Denys Golubiev <dgo@adorsys.de>
2019-12-06 15:04:39 +0100

    Replace imports policy according setup of the project

--- opba-banking-rest-api/src/main/java/de/adorsys/opba/bankingapi/config/EnableBankingApi.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/bankingapi/config/EnableBankingApi.java
@@ -2,7 +2,12 @@ package de.adorsys.opba.bankingapi.config;
 
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
--- opba-banking-rest-api/src/main/java/de/adorsys/opba/bankingapi/config/swagger/EnableBankingApiSwagger.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/bankingapi/config/swagger/EnableBankingApiSwagger.java
@@ -2,7 +2,13 @@ package de.adorsys.opba.bankingapi.config.swagger;
 
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
