cdfd77d9c8c8dd1d814f77d03bf22d3789be1e78
Collins Kamgaing <cka@adorsys.de>
2019-12-06 14:13:54 +0100

    styling search component

--- banking-gateway-ui/src/app/bank-search/bank-search.module.ts
+++ banking-gateway-ui/src/app/bank-search/bank-search.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { BankSearchRoutingModule } from './bank-search-routing.module';
 import { BankSearchComponent } from './components/bank-search/bank-search.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './common/search/search.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [BankSearchComponent, ProfileComponent],
-  imports: [
-    CommonModule,
-    BankSearchRoutingModule
-  ]
+  declarations: [BankSearchComponent, ProfileComponent, SearchComponent],
+    imports: [
+        CommonModule,
+        BankSearchRoutingModule,
+        FormsModule
+    ]
 })
 export class BankSearchModule { }
