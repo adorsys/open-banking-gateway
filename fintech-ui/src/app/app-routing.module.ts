8db9b1323c2394219352cb4df93292237c0c5561
Sascha Nuissl <snu@adorsys.de>
2019-12-16 14:37:04 +0100

    fintech-ui: reformat files with prettier

--- fintech-ui/src/app/app-routing.module.ts
+++ fintech-ui/src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-    {
-      path: '',
-      redirectTo: 'search',
-      pathMatch: 'full'
-    },
-    {
-      path: 'search',
-      loadChildren: () => import('./bank-search/bank-search.module').then(m => m.BankSearchModule)
-    }
+  {
+    path: '',
+    redirectTo: 'search',
+    pathMatch: 'full'
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./bank-search/bank-search.module').then(m => m.BankSearchModule)
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
