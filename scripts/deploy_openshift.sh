0fefdeae3db3f334915aaa95aac9a29fe5ae940f
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-12-16 16:27:48 +0100

    fix branch name

--- scripts/deploy_openshift.sh
+++ scripts/deploy_openshift.sh
@@ -9,7 +9,7 @@ SERVICE_NAME="tpp-bank-search-api"
 IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME
 
 docker login -u image-pusher -p $OPENSHIFT_TOKEN $REGISTRY_DOMAIN
-JAR_NAME=banking-protokol-$PROJECT_VERSION.jar
+JAR_NAME=banking-protocol-$PROJECT_VERSION.jar
 docker build -t $IMAGE_NAME:"$IMAGE_TAG" --build-arg JAR_FILE="$JAR_NAME" ./core/banking-protocol
 docker push $IMAGE_NAME:"$IMAGE_TAG" && \
 oc tag $PROJECT_NAME/$SERVICE_NAME:"$IMAGE_TAG" $PROJECT_NAME/$SERVICE_NAME:latest
