a722946a487d148b1461900440201c6c5d82ec03
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-12-16 17:31:54 +0100

    install oc

--- scripts/deploy_openshift.sh
+++ scripts/deploy_openshift.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+curl -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > oc-cli.tar.gz
+tar -xzf oc-cli.tar.gz
+sudo mv ./openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/local/bin
+
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-oc login https://openshift.adorsys.de --token=$OPENSHIFT_TOKEN
+oc login https://openshift.adorsys.de --token="$OPENSHIFT_TOKEN"
 
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
 REGISTRY_DOMAIN=openshift-registry.adorsys.de
@@ -8,7 +13,7 @@ PROJECT_NAME=open-banking-gateway-dev
 SERVICE_NAME="tpp-bank-search-api"
 IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME
 
-docker login -u image-pusher -p $OPENSHIFT_TOKEN $REGISTRY_DOMAIN
+docker login -u image-pusher -p "$OPENSHIFT_TOKEN" $REGISTRY_DOMAIN
 JAR_NAME=banking-protocol-$PROJECT_VERSION.jar
 docker build -t $IMAGE_NAME:"$IMAGE_TAG" --build-arg JAR_FILE="$JAR_NAME" ./core/banking-protocol
 docker push $IMAGE_NAME:"$IMAGE_TAG" && \
