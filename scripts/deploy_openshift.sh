940b6e86d789782f9ff6df984ccf82fbf4ffcd72
T. Nein <30799038+tnein@users.noreply.github.com>
2019-12-17 10:22:15 +0100

    reset openshift deployment strategy to default
    docker login without oc
    commented oc for testing

--- scripts/deploy_openshift.sh
+++ scripts/deploy_openshift.sh
@@ -5,16 +5,14 @@ tar -xzf oc-cli.tar.gz
 sudo mv ./openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/local/bin
 
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-oc login https://openshift.adorsys.de --token="$OPENSHIFT_TOKEN"
 
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
-REGISTRY_DOMAIN=openshift-registry.adorsys.de
-PROJECT_NAME=open-banking-gateway-dev
-SERVICE_NAME="tpp-bank-search-api"
-IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME
 
-docker login -u image-pusher -p "$OPENSHIFT_TOKEN" $REGISTRY_DOMAIN
 JAR_NAME=banking-protocol-$PROJECT_VERSION.jar
-docker build -t $IMAGE_NAME:"$IMAGE_TAG" --build-arg JAR_FILE="$JAR_NAME" ./core/banking-protocol
-docker push $IMAGE_NAME:"$IMAGE_TAG" && \
-oc tag $PROJECT_NAME/$SERVICE_NAME:"$IMAGE_TAG" $PROJECT_NAME/$SERVICE_NAME:latest
+
+docker build -t OPENSHIFT_TPP_BANK_SEARCH_API:$IMAGE_TAG --build-arg JAR_FILE=$JAR_NAME ./core/banking-protocol
+docker login -u github-image-pusher -p $OPENSHIFT_TOKEN $OPENSHIFT_REGISTRY
+docker push $IMAGE_NAME:"$IMAGE_TAG"
+docker logout 
+
+#oc tag $PROJECT_NAME/$SERVICE_NAME:"$IMAGE_TAG" $PROJECT_NAME/$SERVICE_NAME:latest
