c23705930925a94e93df87f118adbe9bcad3adb2
Pouatcha,Francis (ext) <fpo@adorsys.de>
2019-12-17 10:24:02 -0500

    Moving to opne api 3.0

---
+++ opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/config/EnableConsentApi.java
@@ -0,0 +1,16 @@
+package de.adorsys.opba.consentapi.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Import(ConsentApiConfig.class)
+public @interface EnableConsentApi {
+}
---
+++ opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/resource/ConsentServiceResource.java
@@ -0,0 +1,7 @@
+package de.adorsys.opba.consentapi.resource;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConsentServiceResource {
+}
