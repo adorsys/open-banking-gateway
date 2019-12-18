f38047bda242e3f1bbefc0759ac5fac714bd803d
Valentyn Berezin <valb3r.ua@gmail.com>
2019-12-18 15:08:43 +0200

    Using host network for linux

--- sandbox-test/e2e-tests/src/test/java/de/adorsys/openbankinggateway/BasicTest.java
+++ sandbox-test/e2e-tests/src/test/java/de/adorsys/openbankinggateway/BasicTest.java
@@ -4,29 +4,51 @@ import de.adorsys.openbankinggateway.sandbox.SandboxAppsStarter;
 import de.adorsys.openbankinggateway.sandbox.internal.SandboxApp;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.awaitility.Duration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.hamcrest.core.StringContains.containsString;
 
 
 @Slf4j
-@EnabledIfEnvironmentVariable(named = "START_SANDBOX", matches = "true")
 class BasicTest extends WithSandboxSpringBootTest {
 
     private final SandboxAppsStarter executor = new SandboxAppsStarter();
 
+    /**
+     * Sanity test that validates E2E-Sandbox can run on platform.
+     */
     @Test
     @SneakyThrows
     void testEnvStartsUp() {
         executor.runAll();
+        executor.awaitForAllStarted();
 
-        await().atMost(Duration.FIVE_MINUTES).pollDelay(Duration.ONE_SECOND).until(SandboxApp::allReadyToUse);
-        log.info("ALL READY!");
-
-        Thread.sleep(Duration.FOREVER.getValueInMS());
         assertThat(SandboxApp.values()).extracting(it -> it.getLoader().get()).isNotNull();
+        // Dockerized UI can reach backend
+        given().when()
+            .post("http://localhost:4400/oba-proxy/ais/FOO=BAR/authorisation/12345/login?pin=12345")
+            .then()
+            .body(containsString("Internal Server Error"))
+            .statusCode(500);
+    }
+
+    /**
+     * Not really a test, but just launches entire sandbox for you.
+     */
+    @Test
+    @SneakyThrows
+    @EnabledIfSystemProperty(named = "START_SANDBOX", matches = "true")
+    void startTheSandbox() {
+        executor.runAll();
+        executor.awaitForAllStarted();
+
+        assertThat(SandboxApp.values()).extracting(it -> it.getLoader().get()).isNotNull();
+
+        // Loop forever.
+        await().forever().until(() -> false);
     }
 }
