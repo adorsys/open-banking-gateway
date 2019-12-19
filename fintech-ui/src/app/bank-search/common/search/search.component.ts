5bfbcb105431295296d06ca259b479af9363ad06
Collins Kamgaing <cka@adorsys.de>
2019-12-19 11:02:54 -0500

    add debounceTime by searching for a bank

--- fintech-ui/src/app/bank-search/common/search/search.component.ts
+++ fintech-ui/src/app/bank-search/common/search/search.component.ts
@@ -1,17 +1,25 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnDestroy {
+  private subscriptions: Subscription;
+
   @Output() keyword = new EventEmitter();
-  constructor() {}
+  searchTerm$ = new Subject<string>();
 
-  ngOnInit() {}
+  constructor() {
+    this.subscriptions = this.searchTerm$.pipe(debounceTime(200), distinctUntilChanged()).subscribe(inputData => {
+      this.keyword.emit(inputData);
+    });
+  }
 
-  onSearch(keyword: string) {
-    this.keyword.emit(keyword);
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
