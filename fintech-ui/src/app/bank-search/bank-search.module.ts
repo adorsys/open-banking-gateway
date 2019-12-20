0d3d833fe673f8ccd2955c13829c6d0fe4d6d715
Collins Kamgaing <cka@adorsys.de>
2019-12-20 08:37:04 -0500

    use reactiveForm for input field

--- fintech-ui/src/app/bank-search/bank-search.module.ts
+++ fintech-ui/src/app/bank-search/bank-search.module.ts
@@ -5,10 +5,10 @@ import { BankSearchRoutingModule } from './bank-search-routing.module';
 import { BankSearchComponent } from './components/bank-search/bank-search.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SearchComponent } from './common/search/search.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BankSearchComponent, ProfileComponent, SearchComponent],
-  imports: [CommonModule, BankSearchRoutingModule, FormsModule]
+  imports: [CommonModule, BankSearchRoutingModule, ReactiveFormsModule]
 })
 export class BankSearchModule {}
