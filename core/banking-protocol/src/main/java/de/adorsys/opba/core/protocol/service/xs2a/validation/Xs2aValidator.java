7d4bd416dcb21878cb9e89d20260e45795e3bc89
valb3r <valentyn.berezin@adorsys.com.ua>
2019-12-21 18:29:45 +0200

    OBG-85. Pre-added validator

--- core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/service/xs2a/validation/Xs2aValidator.java
+++ core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/service/xs2a/validation/Xs2aValidator.java
@@ -1,11 +1,17 @@
 package de.adorsys.opba.core.protocol.service.xs2a.validation;
 
 import de.adorsys.opba.core.protocol.service.xs2a.context.BaseContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.SmartValidator;
 
 @Service
+@RequiredArgsConstructor
 public class Xs2aValidator {
 
+    private final SmartValidator validator;
+
     public void validate(BaseContext context, Object... values) {
+        //for ()
     }
 }
