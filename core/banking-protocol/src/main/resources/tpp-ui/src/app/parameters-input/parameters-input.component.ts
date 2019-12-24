d9875bf9086070d8c5d25ab5e35be2369270b2e4
Valentyn Berezin <valb3r.ua@gmail.com>
2019-12-24 17:29:15 +0200

    OBG-85. Add parameter input component

---
+++ core/banking-protocol/src/main/resources/tpp-ui/src/app/parameters-input/parameters-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parameters-input',
+  templateUrl: './parameters-input.component.html',
+  styleUrls: ['./parameters-input.component.css']
+})
+export class ParametersInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
