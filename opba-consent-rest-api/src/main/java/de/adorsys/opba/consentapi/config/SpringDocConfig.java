c95fc7c697c0d15286482a306e59f9721804bcf2
Pouatcha,Francis (ext) <fpo@adorsys.de>
2019-12-26 08:01:11 -0500

    Implemented recomentation on pull request

---
+++ opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/config/SpringDocConfig.java
@@ -0,0 +1,9 @@
+package de.adorsys.opba.consentapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+}
