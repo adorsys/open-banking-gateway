6b8fdc8a9d21cb46a28b9bfdc0cd00743a2c2e70
Pouatcha,Francis (ext) <fpo@adorsys.de>
2019-12-26 13:24:09 -0500

    moving to open api 3 both banking and consent api

--- opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/resource/ConsentServiceResource.java
+++ opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/resource/ConsentServiceResource.java
@@ -2,6 +2,8 @@ package de.adorsys.opba.consentapi.resource;
 
 import org.springframework.stereotype.Controller;
 
+import de.adorsys.opba.consent.resource.ConsentAuthorizationApi;
+
 @Controller
-public class ConsentServiceResource {
+public class ConsentServiceResource implements ConsentAuthorizationApi {
 }
