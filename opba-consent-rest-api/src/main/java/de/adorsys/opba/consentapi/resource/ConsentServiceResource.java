454f6eafca5e80a14e70f4a7c7f737983519c97b
Pouatcha,Francis (ext) <fpo@adorsys.de>
2019-12-26 19:47:13 -0500

    Normalizing location for checkstyle and pmd config.

--- opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/resource/ConsentServiceResource.java
+++ opba-consent-rest-api/src/main/java/de/adorsys/opba/consentapi/resource/ConsentServiceResource.java
@@ -2,8 +2,6 @@ package de.adorsys.opba.consentapi.resource;
 
 import org.springframework.stereotype.Controller;
 
-import de.adorsys.opba.consent.resource.ConsentAuthorizationApi;
-
 @Controller
 public class ConsentServiceResource implements ConsentAuthorizationApi {
 }
