de7ac84d4c83416ee4724af1c229608c7e0ce2fd
Pouatcha,Francis (ext) <fpo@adorsys.de>
2019-12-30 14:10:56 -0500

    #83 Initial version of tpp bank search api

---
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/bankingapi/search/resource/TppBankSearchResource.java
@@ -0,0 +1,7 @@
+package de.adorsys.opba.bankingapi.search.resource;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TppBankSearchResource implements TppBankSearchApi {
+}
