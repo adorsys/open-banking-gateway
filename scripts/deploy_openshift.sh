ae43615e939136cc610a51b6a5fb897d2b65dbf1
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-01-07 09:49:50 +0100

    changed openshift service name

--- scripts/deploy_openshift.sh
+++ scripts/deploy_openshift.sh
@@ -5,7 +5,7 @@ PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdou
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
 REGISTRY_DOMAIN=openshift-registry.adorsys.de
 PROJECT_NAME=open-banking-gateway-dev
-SERVICE_NAME="tpp-bank-search-api"
+SERVICE_NAME="open-banking-gateway"
 IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME:$IMAGE_TAG
 LATEST_IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME:latest
 
