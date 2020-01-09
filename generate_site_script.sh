bfcae72416c3961dc1790c421e427d0950ee85ce
electronicpeter <github@electronicpeter.de>
2020-01-09 18:29:03 +0100

    OBG-117

--- generate_site_script.sh
+++ generate_site_script.sh
@@ -1,2 +1,1 @@
-# docker run -it --rm -v "$PWD":/src -w /src -u "$(id -u "${USER}"):$(id -g "${USER}")" --env TRAVIS_TAG g0lden/mkdocs make a
 docker run -it --rm -v "$PWD":/src -w /src -u "$(id -u "${USER}"):$(id -g "${USER}")" --env TRAVIS_TAG g0lden/mkdocs make site
