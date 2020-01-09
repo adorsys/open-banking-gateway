75af24971f74997a6f0d9375d152a0fb6f008505
Denys Golubiev <dgo@adorsys.de>
2020-01-09 13:58:35 +0100

    Rename API. Generalise APIs with common elements
    #83

--- opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/search/resource/TppBankingApiBankSearchResource.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/search/resource/TppBankingApiBankSearchResource.java
@@ -14,12 +14,13 @@ import java.util.UUID;
 @Controller
 public class TppBankingApiBankSearchResource implements TppBankSearchApi {
     @Override
-    public ResponseEntity<InlineResponse2001> bankProfileGET(UUID xRequestID, String bankId) {
+    public ResponseEntity<InlineResponse2001> bankProfileGET(String authorization, UUID xRequestID, String bankId) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @Override
-    public ResponseEntity<InlineResponse200> bankSearchGET(UUID xRequestID, SearchInput keyword, SearchStartIndex start, SearchMaxResult max) {
+    @SuppressWarnings("checkstyle:ParameterNumber") // Parameters are provided through auto-generated base class
+    public ResponseEntity<InlineResponse200> bankSearchGET(String authorization, UUID xRequestID, SearchInput keyword, SearchStartIndex start, SearchMaxResult max) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
