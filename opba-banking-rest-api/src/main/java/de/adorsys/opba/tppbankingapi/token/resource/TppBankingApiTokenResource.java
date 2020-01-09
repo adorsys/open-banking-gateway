a9e2cb6dda3d91c99cdcc9aedcb16f80b7cf24ce
Denys Golubiev <dgo@adorsys.de>
2020-01-09 13:58:35 +0100

    Rename API. Generalise APIs with common elements
    #83

--- opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/token/resource/TppBankingApiTokenResource.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/token/resource/TppBankingApiTokenResource.java
@@ -1,7 +1,7 @@
 package de.adorsys.opba.tppbankingapi.token.resource;
 
-import de.adorsys.opba.tppbankingapi.token.model.InlineResponse201;
-import de.adorsys.opba.tppbankingapi.token.model.RedirectCode;
+
+import de.adorsys.opba.tppbankingapi.token.model.PsuConsentSessionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Controller
 public class TppBankingApiTokenResource implements TppTokenApi {
     @Override
-    public ResponseEntity<InlineResponse201> code2TokenGET(UUID xRequestID, RedirectCode redirectCode) {
+    public ResponseEntity<PsuConsentSessionResponse> code2TokenGET(String authorization, UUID xRequestID, String redirectCode) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
