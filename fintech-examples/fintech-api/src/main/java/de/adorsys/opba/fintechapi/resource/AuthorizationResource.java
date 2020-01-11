7df7dd59575d9e3a43bd71121bb5258a0958f864
Pouatcha,Francis (ext) <fpo@adorsys.de>
2020-01-12 00:25:14 +0100

    #136 Refactoring FinTechApi

--- fintech-examples/fintech-api/src/main/java/de/adorsys/opba/fintechapi/resource/AuthorizationResource.java
+++ fintech-examples/fintech-api/src/main/java/de/adorsys/opba/fintechapi/resource/AuthorizationResource.java
@@ -3,5 +3,5 @@ package de.adorsys.opba.fintechapi.resource;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthorizationResource implements AuthorizationApi {
+public class AuthorizationResource implements FinTechAuthorizationApi {
 }
