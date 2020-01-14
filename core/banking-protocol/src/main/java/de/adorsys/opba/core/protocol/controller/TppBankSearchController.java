3c6817e6d53fc0400263764778922b7a4f3cf803
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-01-14 12:07:03 +0100

    OBG-124 adjusting bank search api and impl

--- core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/controller/TppBankSearchController.java
+++ core/banking-protocol/src/main/java/de/adorsys/opba/core/protocol/controller/TppBankSearchController.java
@@ -3,14 +3,13 @@ package de.adorsys.opba.core.protocol.controller;
 import de.adorsys.opba.core.protocol.domain.entity.Bank;
 import de.adorsys.opba.core.protocol.domain.entity.BankProfile;
 import de.adorsys.opba.core.protocol.service.BankService;
+import de.adorsys.opba.tppbankingapi.search.model.BankDescriptor;
 import de.adorsys.opba.tppbankingapi.search.model.BankProfileDescriptor;
 import de.adorsys.opba.tppbankingapi.search.model.InlineResponse200;
 import de.adorsys.opba.tppbankingapi.search.model.InlineResponse2001;
-import de.adorsys.opba.tppbankingapi.search.model.SearchInput;
-import de.adorsys.opba.tppbankingapi.search.model.SearchMaxResult;
-import de.adorsys.opba.tppbankingapi.search.model.SearchStartIndex;
 import de.adorsys.opba.tppbankingapi.search.resource.TppBankSearchApi;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -26,31 +25,27 @@ import java.util.UUID;
 @CrossOrigin(origins = "*") //FIXME move CORS at gateway/load balancer level
 @SuppressWarnings("checkstyle:ParameterNumber") // FIXME
 public class TppBankSearchController implements TppBankSearchApi {
+    private static final int DEFAULT_START = 0;
+    private static final int DEFAULT_MAX = 10;
 
     private final BankService bankService;
 
     @Override
-    public ResponseEntity<InlineResponse200> bankSearchGET(String authorization,
-                                                           UUID xRequestID,
-                                                           SearchInput keyword,
-                                                           SearchStartIndex start,
-                                                           SearchMaxResult max) {
-        int startInt;
-        int maxInt;
-        try {
-            startInt = Integer.parseInt(start.toString());
-            maxInt = Integer.parseInt(max.toString());
-        } catch (NumberFormatException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<InlineResponse200> bankSearchGET(String authorization, UUID xRequestID, String keyword,
+                                                           Integer start, Integer max) {
+        if (start == null) {
+            start = DEFAULT_START;
         }
-
-        List<Bank> banks = bankService.getBanks(keyword.toString(), startInt, maxInt);
+        if (max == null) {
+            max = DEFAULT_MAX;
+        }
+        List<Bank> banks = bankService.getBanks(keyword, start, max);
         if (banks.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         InlineResponse200 inlineResponse200 = new InlineResponse200();
-        banks.forEach(it -> inlineResponse200.addBankDescriptorItem(Bank.TO_BANK_DESCRIPTOR.map(it)));
+        banks.forEach(it -> inlineResponse200.addBankDescriptorItem(toBankDescriptor(it)));
         inlineResponse200.setKeyword(keyword);
         inlineResponse200.setMax(max);
         inlineResponse200.setStart(start);
@@ -66,10 +61,23 @@ public class TppBankSearchController implements TppBankSearchApi {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         InlineResponse2001 inlineResponse2001 = new InlineResponse2001();
-        BankProfileDescriptor bankProfileDescriptor = BankProfile.TO_BANK_PROFILE_DESCRIPTOR.map(bankProfile.get());
-        bankProfileDescriptor.serviceList(Arrays.asList(bankProfile.get().getServices().split(",")));
+        BankProfileDescriptor bankProfileDescriptor = new BankProfileDescriptor();
+        bankProfileDescriptor.setBankName(bankProfile.get().getBank().getName());
+        bankProfileDescriptor.setBic(bankProfile.get().getBank().getBic());
+        if (StringUtils.isNotEmpty(bankProfile.get().getServices())) {
+            bankProfileDescriptor.serviceList(Arrays.asList(bankProfile.get().getServices().split(",")));
+        }
         inlineResponse2001.setBankProfileDescriptor(bankProfileDescriptor);
         return new ResponseEntity<>(inlineResponse2001, HttpStatus.OK);
+    }
+
+    private BankDescriptor toBankDescriptor(Bank bank) {
+        BankDescriptor bankDescriptor = new BankDescriptor();
+        bankDescriptor.setBankCode(bank.getBankCode());
+        bankDescriptor.setBankName(bank.getName());
+        bankDescriptor.setBic(bank.getBic());
+        bankDescriptor.setUuid(bank.getUuid());
+        return bankDescriptor;
     }
 }
 
