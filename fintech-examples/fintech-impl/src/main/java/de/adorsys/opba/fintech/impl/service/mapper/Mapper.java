f957cca5e92790ed5280cc6a8d5f3096fdf1183a
electronicpeter <github@electronicpeter.de>
2020-01-15 16:22:18 +0100

    OBG-120 checkstyle

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/service/mapper/Mapper.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/service/mapper/Mapper.java
@@ -2,8 +2,9 @@ package de.adorsys.opba.fintech.impl.service.mapper;
 
 import de.adorsys.opba.fintech.api.model.BankDescriptor;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor") // Springboot starter class is not an utility class
 public class Mapper {
-    public static BankDescriptor fromTppToFintech (de.adorsys.opba.tpp.bankserach.api.model.BankDescriptor tppBankDescriptor) {
+    public static BankDescriptor fromTppToFintech(de.adorsys.opba.tpp.bankserach.api.model.BankDescriptor tppBankDescriptor) {
         BankDescriptor fintechBankDescrptor = new BankDescriptor();
         fintechBankDescrptor.setBankCode(tppBankDescriptor.getBankCode());
         fintechBankDescrptor.setBankName(tppBankDescriptor.getBankName());
