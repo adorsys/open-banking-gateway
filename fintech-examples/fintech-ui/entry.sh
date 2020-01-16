dfd675391208f5b022d68c4e266d6a25323de57b
Sascha Nuissl <snu@adorsys.de>
2020-01-16 09:43:39 +0100

    remove unnecassary lines from fintech-ui entry.sh

--- fintech-examples/fintech-ui/entry.sh
+++ fintech-examples/fintech-ui/entry.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
-export DOLLAR='$'
-
+# Use erb to use conditions and environment variables in nginx file
 erb /etc/nginx/conf.d/default.conf.erb > /etc/nginx/conf.d/default.conf
 
+# Start nginx to server frontend application
 nginx -g "daemon off;"
