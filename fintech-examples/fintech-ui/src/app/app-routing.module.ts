6b8f5f7d4f358a9528288b6c687ca87833fcd75e
Sascha Nuissl <snu@adorsys.de>
2020-01-17 14:08:52 +0100

    use mocked authguard to protect search route

--- fintech-examples/fintech-ui/src/app/app-routing.module.ts
+++ fintech-examples/fintech-ui/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'search',
-    //   canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () => import('./bank-search/bank-search.module').then(m => m.BankSearchModule)
   }
 ];
