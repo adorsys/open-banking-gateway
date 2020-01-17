78b6ccf75297cf62ecec04d40bc49d813f7f9111
electronicpeter <github@electronicpeter.de>
2020-01-17 15:14:42 +0100

    OBG 120

--- fintech-examples/fintech-server/src/test/java/de/adorsys/obpa/fintech/server/FinTechApplicationTests.java
+++ fintech-examples/fintech-server/src/test/java/de/adorsys/obpa/fintech/server/FinTechApplicationTests.java
@@ -1,9 +1,13 @@
 package de.adorsys.obpa.fintech.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("application-test-one-time-postgres-ramfs")
+@AutoConfigureMockMvc
 class FinTechApplicationTests {
 
 	@Test
