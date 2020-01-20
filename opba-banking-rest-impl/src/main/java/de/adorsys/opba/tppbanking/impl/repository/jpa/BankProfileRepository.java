48b4d861b07129424d2197c8cab5b22618af6625
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-01-20 18:02:14 +0100

    embedded starter without banking-protocol

--- opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbanking/impl/repository/jpa/BankProfileRepository.java
+++ opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbanking/impl/repository/jpa/BankProfileRepository.java
@@ -2,9 +2,11 @@ package de.adorsys.opba.tppbanking.impl.repository.jpa;
 
 import de.adorsys.opba.tppbanking.impl.domain.entity.BankProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BankProfileRepository extends JpaRepository<BankProfile, Long> {
 
     Optional<BankProfile> findByBankUuid(String bankId);
