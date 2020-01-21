e0083d3451dcc35ddbdda25d68cba86fe5cebac8
Denys Golubiev <dgo@adorsys.de>
2020-01-21 15:13:16 +0100

    Replace normal mapper with MapStruct
    #151

--- opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/ais/resource/AccountListMapper.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/ais/resource/AccountListMapper.java
@@ -1,35 +1,16 @@
 package de.adorsys.opba.tppbankingapi.ais.resource;
 
-import de.adorsys.opba.protocol.services.ais.account.Account;
 import de.adorsys.opba.protocol.services.ais.account.AccountsReport;
-import de.adorsys.opba.tppbankingapi.ais.model.AccountDetails;
 import de.adorsys.opba.tppbankingapi.ais.model.AccountList;
 import de.adorsys.opba.tppbankingapi.ais.model.AccountStatus;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
+import org.mapstruct.Mapper;
 
-import java.util.stream.Collectors;
+@Mapper(componentModel = "spring", uses = TppBankingApiAisResource.class)
+public interface AccountListMapper {
+    AccountList toAccountList(AccountsReport accountsReport);
 
-@Service
-public class AccountListMapper {
-    AccountList toAccountList(AccountsReport accountsReport) {
-        AccountList accountList = new AccountList();
-        accountList.setAccounts(accountsReport.getAccounts().stream()
-                    .map(this::toAccountDetails)
-                    .collect(Collectors.toList()));
-        return accountList;
-    }
-
-    private AccountDetails toAccountDetails(Account account) {
-        AccountDetails accountDetails = new AccountDetails();
-        accountDetails.setResourceId(account.getResourceId());
-        accountDetails.setIban(account.getIban());
-        accountDetails.setCurrency(account.getCurrency() != null ? account.getCurrency().toString() : null);
-        accountDetails.setProduct(account.getProduct());
-        accountDetails.setCashAccountType(account.getCashAccountType());
-        accountDetails.setStatus(AccountStatus.valueOf(StringUtils.upperCase(account.getStatus())));
-        accountDetails.setLinkedAccounts(account.getLinkedAccounts());
-        accountDetails.setUsage(AccountDetails.UsageEnum.valueOf(StringUtils.upperCase(account.getUsage())));
-        return accountDetails;
+    default AccountStatus toAccountStatus(String status) {
+        return AccountStatus.valueOf(StringUtils.upperCase(status));
     }
 }
