f07ee0f5c8dd8e845dd6425ac8f8ac3a6ae4d953
Denys Golubiev <dgo@adorsys.de>
2020-01-21 14:20:39 +0100

    Provide more fields to UserAgentContext
    #151

--- opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/HttpHeaders.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/HttpHeaders.java
@@ -7,4 +7,18 @@ import lombok.experimental.UtilityClass;
 public class HttpHeaders {
     public static final String PSU_CONSENT_SESSION = "PSU-Consent-Session";
     public static final String X_REQUEST_ID = "X-Request-ID";
+
+    @UtilityClass
+    public class UserAgentContext {
+        public static final String PSU_IP_ADDRESS = "PSU-IP-Address";
+        public static final String PSU_IP_PORT = "PSU-IP-Port";
+        public static final String PSU_ACCEPT = "PSU-Accept";
+        public static final String PSU_ACCEPT_CHARSET = "PSU-Accept-Charset";
+        public static final String PSU_ACCEPT_ENCODING = "PSU-Accept-Encoding";
+        public static final String PSU_ACCEPT_LANGUAGE = "PSU-Accept-Language";
+        public static final String PSU_DEVICE_ID = "PSU-Device-ID";
+        public static final String PSU_USER_AGENT = "PSU-User-Agent";
+        public static final String PSU_GEO_LOCATION = "PSU-Geo-Location";
+        public static final String PSU_HTTP_METHOD = "PSU-Http-Method";
+    }
 }
--- opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/UserAgentContext.java
+++ opba-banking-rest-api/src/main/java/de/adorsys/opba/tppbankingapi/UserAgentContext.java
@@ -1,8 +1,19 @@
 package de.adorsys.opba.tppbankingapi;
 
+import lombok.Builder;
 import lombok.Value;
 
 @Value
+@Builder
 public class UserAgentContext {
     private final String psuIpAddress;
+    private final String psuIpPort;
+    private final String psuAccept;
+    private final String psuAcceptCharset;
+    private final String psuAcceptEncoding;
+    private final String psuAcceptLanguage;
+    private final String psuDeviceId;
+    private final String psuUserAgent;
+    private final String psuGeoLocation;
+    private final String psuHttpMethod;
 }
