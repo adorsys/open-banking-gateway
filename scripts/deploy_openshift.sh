c12f788df152ef95af7efb79a2ac7b703c4d068c
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-01-22 12:20:06 +0100

    OBG-125 Migration module

--- scripts/deploy_openshift.sh
+++ scripts/deploy_openshift.sh
@@ -5,13 +5,23 @@ PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdou
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
 REGISTRY_DOMAIN=openshift-registry.adorsys.de
 PROJECT_NAME=open-banking-gateway-dev
+
+docker login -u github-image-pusher -p "$OPENSHIFT_TOKEN" $REGISTRY_DOMAIN
+
 SERVICE_NAME="open-banking-gateway"
 IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME:$IMAGE_TAG
 LATEST_IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME:latest
-
-docker login -u github-image-pusher -p "$OPENSHIFT_TOKEN" $REGISTRY_DOMAIN
-JAR_NAME=open-banking-gateway-$PROJECT_VERSION.jar
+JAR_NAME=$SERVICE_NAME-$PROJECT_VERSION.jar
 docker build -t "$IMAGE_NAME" --build-arg JAR_FILE="$JAR_NAME" ./opba-embedded-starter
+docker tag "$IMAGE_NAME" "$LATEST_IMAGE_NAME"
+docker push "$IMAGE_NAME"
+docker push "$LATEST_IMAGE_NAME"
+
+SERVICE_NAME="fintech-server"
+IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME:$IMAGE_TAG
+LATEST_IMAGE_NAME=$REGISTRY_DOMAIN/$PROJECT_NAME/$SERVICE_NAME:latest
+JAR_NAME=$SERVICE_NAME-$PROJECT_VERSION.jar
+docker build -t "$IMAGE_NAME" --build-arg JAR_FILE="$JAR_NAME" ./fintech-examples/fintech-server
 docker tag "$IMAGE_NAME" "$LATEST_IMAGE_NAME"
 docker push "$IMAGE_NAME"
 docker push "$LATEST_IMAGE_NAME"
