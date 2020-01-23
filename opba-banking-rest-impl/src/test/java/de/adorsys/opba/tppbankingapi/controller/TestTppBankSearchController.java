ef305aa6dadc3382f6036e408c22c328e79824cd
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-01-23 15:07:52 +0100

    OBG-125 returning to duplicating application properties

--- opba-banking-rest-impl/src/test/java/de/adorsys/opba/tppbankingapi/controller/TestTppBankSearchController.java
+++ opba-banking-rest-impl/src/test/java/de/adorsys/opba/tppbankingapi/controller/TestTppBankSearchController.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles({ONE_TIME_POSTGRES_RAMFS})
+@ActiveProfiles(ONE_TIME_POSTGRES_RAMFS)
 @AutoConfigureMockMvc
 class TestTppBankSearchController extends BaseMockitoTest {
 
