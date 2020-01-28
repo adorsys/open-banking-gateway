21b038fedfdbd929f8e68b06dffd7797693e33a6
electronicpeter <github@electronicpeter.de>
2020-01-28 14:44:47 +0100

    OBG-184 set all cookie attributes by application.yml

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FinTechImplConfig.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FinTechImplConfig.java
@@ -1,13 +1,18 @@
 package de.adorsys.opba.fintech.impl.config;
 
+import de.adorsys.opba.fintech.impl.properties.CookieConfigProperties;
 import de.adorsys.opba.tpp.bankserach.api.resource.generated.TppBankSearchApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("de.adorsys.opba.fintech.impl")
+@EnableConfigurationProperties({
+        CookieConfigProperties.class
+})
 public class FinTechImplConfig {
 
     @Value("${tpp.url}")
