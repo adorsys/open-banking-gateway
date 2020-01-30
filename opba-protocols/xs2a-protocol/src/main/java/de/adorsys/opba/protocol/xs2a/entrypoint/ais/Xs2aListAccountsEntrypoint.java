18e30d1ee1fc9a66c743118e316e9c5dca589692
valb3r <valentyn.berezin@adorsys.com.ua>
2020-01-30 14:54:11 +0200

    OBG-194. Mapped requests to context

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/ais/Xs2aListAccountsEntrypoint.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/ais/Xs2aListAccountsEntrypoint.java
@@ -10,12 +10,14 @@ import de.adorsys.opba.protocol.api.dto.result.Result;
 import de.adorsys.opba.protocol.api.dto.result.SuccessResult;
 import de.adorsys.opba.protocol.api.dto.result.ValidationErrorResult;
 import de.adorsys.opba.protocol.xs2a.service.eventbus.ProcessEventHandlerRegistrar;
-import de.adorsys.opba.protocol.xs2a.service.xs2a.ContextFactory;
 import de.adorsys.opba.protocol.xs2a.service.xs2a.context.Xs2aContext;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.DtoMapper;
 import de.adorsys.opba.tppbankingapi.ais.model.generated.AccountList;
 import lombok.RequiredArgsConstructor;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -24,19 +26,21 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.CONTEXT;
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.REQUEST_SAGA;
+import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.SPRING_KEYWORD;
+import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.XS2A_MAPPERS_PACKAGE;
 
 @Service("xs2aListAccounts")
 @RequiredArgsConstructor
 public class Xs2aListAccountsEntrypoint implements ListAccounts {
 
     private final RuntimeService runtimeService;
-    private final ContextFactory contextFactory;
     private final Xs2aResultExtractor extractor;
     private final ProcessEventHandlerRegistrar registrar;
+    private final Xs2aListAccountsEntrypoint.FromRequest mapper;
 
     @Override
     public CompletableFuture<Result<AccountList>> list(ListAccountsRequest request) {
-        Xs2aContext context = contextFactory.createContext();
+        Xs2aContext context = mapper.map(request);
         context.setAction(ProtocolAction.LIST_ACCOUNTS);
 
         ProcessInstance instance = runtimeService.startProcessInstanceByKey(
@@ -56,5 +60,14 @@ public class Xs2aListAccountsEntrypoint implements ListAccounts {
         );
 
         return result;
+    }
+
+    @Mapper(componentModel = SPRING_KEYWORD, implementationPackage = XS2A_MAPPERS_PACKAGE)
+    public interface FromRequest extends DtoMapper<ListAccountsRequest, Xs2aContext> {
+
+        @Mapping(source = "bankID", target = "aspspId")
+        @Mapping(source = "uaContext.psuIpAddress", target = "psuIpAddress")
+        @Mapping(source = "uaContext.psuAccept", target = "contentType")
+        Xs2aContext map(ListAccountsRequest ctx);
     }
 }
