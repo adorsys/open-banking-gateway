dadb793bf2145bf2781e68c2a3f1cec7a4f46b10
valb3r <valentyn.berezin@adorsys.com.ua>
2020-01-30 20:56:12 +0200

    OBG-194. Initial service session persistence

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/ServiceContextProvider.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/ServiceContextProvider.java
@@ -1,17 +1,48 @@
 package de.adorsys.opba.protocol.facade.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import de.adorsys.opba.db.domain.entity.sessions.ServiceSession;
+import de.adorsys.opba.db.repository.jpa.ServiceSessionRepository;
 import de.adorsys.opba.protocol.api.dto.context.ServiceContext;
 import de.adorsys.opba.protocol.api.dto.request.FacadeServiceableGetter;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ServiceContextProvider {
 
+    private static final ObjectMapper MAPPER = new ObjectMapper()
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+    private final ServiceSessionRepository serviceSessions;
+
     @Transactional
+    @SneakyThrows
     public <T extends FacadeServiceableGetter> ServiceContext<T> provide(T request) {
-        return null;
+        ServiceSession session;
+
+        if (null != request.getFacadeServiceable().getServiceSessionId()) {
+            UUID sessionId = UUID.fromString(request.getFacadeServiceable().getServiceSessionId());
+            session = serviceSessions.findById(sessionId).
+                    orElseThrow(() -> new IllegalStateException("No service session " + sessionId));
+        } else {
+            session = new ServiceSession();
+            session.setContext(MAPPER.writeValueAsString(request));
+            session = serviceSessions.save(session);
+        }
+
+        ServiceContext<T> context = ServiceContext.<T>builder()
+                .facadeServiceable(request.getFacadeServiceable())
+                .request(request)
+                .build();
+
+        context.getFacadeServiceable().setServiceSessionId(session.getId().toString());
+        return context;
     }
 }
--- opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListAccountsServiceTest.java
+++ opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListAccountsServiceTest.java
@@ -1,6 +1,7 @@
 package de.adorsys.opba.protocol.facade.services.ais;
 
 import de.adorsys.opba.db.config.EnableBankingPersistence;
+import de.adorsys.opba.protocol.api.dto.request.FacadeServiceableRequest;
 import de.adorsys.opba.protocol.api.dto.request.accounts.ListAccountsRequest;
 import de.adorsys.opba.protocol.api.dto.result.ValidationErrorResult;
 import de.adorsys.opba.protocol.xs2a.EnableXs2aProtocol;
@@ -24,7 +25,13 @@ class ListAccountsServiceTest {
     @SneakyThrows
     void testXs2aWired() {
         assertThat(listAccountsService.list(
-                ListAccountsRequest.builder().bankID("53c47f54-b9a4-465a-8f77-bc6cd5f0cf46").build()).get()
+                ListAccountsRequest.builder()
+                        .facadeServiceable(
+                                FacadeServiceableRequest.builder()
+                                        .bankID("53c47f54-b9a4-465a-8f77-bc6cd5f0cf46")
+                                        .build()
+                        ).build()
+                ).get()
         ).isInstanceOf(ValidationErrorResult.class);
     }
 
