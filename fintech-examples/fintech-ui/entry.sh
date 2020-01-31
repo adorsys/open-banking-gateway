26e270bce8cea94cf5e51debffc8fdce922f1f9d
Denys Golubiev <dgo@adorsys.de>
2020-01-31 16:50:50 +0100

    Fix nginx permissions configuration

--- fintech-examples/fintech-ui/entry.sh
+++
@@ -1,5 +0,0 @@
-# Use erb to use conditions and environment variables in nginx file
-erb /etc/nginx/conf.d/default.conf.erb > /etc/nginx/conf.d/default.conf
-
-# Start nginx to server frontend application
-nginx -g "daemon off;"
