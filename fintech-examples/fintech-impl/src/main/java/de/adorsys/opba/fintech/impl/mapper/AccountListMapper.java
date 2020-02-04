85a7b289aaf560fd5bfe73872c3a37ac1c7eea87
electronicpeter <github@electronicpeter.de>
2020-02-04 11:58:16 +0100

    OBG 213 Mapper with generated annotation

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/AccountListMapper.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/AccountListMapper.java
@@ -3,7 +3,7 @@ package de.adorsys.opba.fintech.impl.mapper;
 import de.adorsys.opba.fintech.api.model.generated.AccountList;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(implementationPackage = "de.adorsys.opba.fintech.impl.mapper.generated")
 public interface AccountListMapper {
     AccountList mapFromTppToFintech(de.adorsys.opba.tpp.ais.api.model.generated.AccountList accountList);
 }
--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/BankProfileDescriptorMapper.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/BankProfileDescriptorMapper.java
@@ -5,7 +5,7 @@ import de.adorsys.opba.tpp.banksearch.api.model.generated.BankProfileDescriptor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(implementationPackage = "de.adorsys.opba.fintech.impl.mapper.generated")
 public interface BankProfileDescriptorMapper {
     @Mapping(target = "services", source = "serviceList")
     BankProfile mapFromTppToFintech(BankProfileDescriptor bankProfileDescriptor);
