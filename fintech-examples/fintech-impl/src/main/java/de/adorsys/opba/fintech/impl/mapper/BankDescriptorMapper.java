894f0144c31c83d37d4ae6799054f1b55087b837
electronicpeter <github@electronicpeter.de>
2020-02-04 11:58:16 +0100

    OBG 213 Mapper with generated annotation

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/BankDescriptorMapper.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/BankDescriptorMapper.java
@@ -3,7 +3,7 @@ package de.adorsys.opba.fintech.impl.mapper;
 import de.adorsys.opba.fintech.api.model.generated.BankDescriptor;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(implementationPackage = "de.adorsys.opba.fintech.impl.mapper.generated")
 public interface BankDescriptorMapper {
     BankDescriptor mapFromTppToFintech(de.adorsys.opba.tpp.banksearch.api.model.generated.BankDescriptor bankDescriptor);
 }
