77ac5d8d3f16b5dcc6347029885d7f32823184cf
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-06 19:42:19 +0200

    OBG-194. Embedded consent retrieval now works too

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/authorization/Xs2aUpdateAuthorization.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/authorization/Xs2aUpdateAuthorization.java
@@ -38,7 +38,7 @@ public class Xs2aUpdateAuthorization implements UpdateAuthorization {
 
         CompletableFuture<Result<UpdateAuthBody>> result = new CompletableFuture<>();
         registrar.addHandler(
-                runtimeService.createExecutionQuery().executionId(executionId).singleResult().getProcessInstanceId(),
+                runtimeService.createExecutionQuery().executionId(executionId).singleResult().getRootProcessInstanceId(),
                 new OutcomeMapper<>(result, res -> new UpdateAuthBody())
         );
 
