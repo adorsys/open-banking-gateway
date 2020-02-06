4ae8f2a242c6cb712d3d01de24bafbe19a64a8de
electronicpeter <github@electronicpeter.de>
2020-02-06 14:45:59 +0100

    OBG 240

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/TppAisClient.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/TppAisClient.java
@@ -3,6 +3,6 @@ package de.adorsys.opba.fintech.impl.config;
 import de.adorsys.opba.tpp.ais.api.resource.generated.TppBankingApiAccountInformationServiceAisApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(url = "${tpp.url}", name = "listAccounts")
+@FeignClient(url = "${tpp.url}", name = "tppAisClient")
 public interface TppAisClient extends TppBankingApiAccountInformationServiceAisApi {
 }
