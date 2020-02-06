bce24090d10ca999ee88f0fae7a8fbb43bda9344
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-06 19:42:19 +0200

    OBG-194. Embedded consent retrieval now works too

--- technical-embedded-ui/tpp-ui/src/app/app.component.ts
+++ technical-embedded-ui/tpp-ui/src/app/app.component.ts
@@ -8,3 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tpp-ui';
 }
+
+export class Helpers {
+
+  static uuidv4(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+}
