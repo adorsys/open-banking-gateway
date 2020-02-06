ffe04e11425c2106447a89928cc776427bbed98e
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-06 18:44:11 +0200

    OBG-194. Redirect approach now works

--- technical-embedded-ui/tpp-ui/proxy-conf.js
+++ technical-embedded-ui/tpp-ui/proxy-conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = {
   '/embedded-server/*': {
     target: "http://localhost:8085",
-      pathRewrite: {
+    pathRewrite: {
       "^/embedded-server": ""
     },
     logLevel: "debug",
@@ -9,9 +9,28 @@ const PROXY_CONFIG = {
     changeOrigin: true,
     onProxyRes: (proxyRes, req, res) => {
       if (proxyRes.headers['location']) {
-        proxyRes.headers['location'] = proxyRes.headers['location'] + '&authorizationSessionId=' + proxyRes.headers['authorization-session-id'] + '&redirectCode=' + proxyRes.headers['redirect-code'];
+
+        // -> store auth session
+        if (proxyRes.headers['location'].includes("localhost:5500")) {
+          proxyRes.headers['location'] = proxyRes.headers['location'] + '&authorizationSessionId=' + proxyRes.headers['authorization-session-id'] + '&redirectCode=' + proxyRes.headers['redirect-code'];
+        }
+
+        // -> avoid cors on sandbox
+        if (proxyRes.headers['location'].includes("localhost:4400")) {
+          let orig = proxyRes.headers['location'];
+          proxyRes.headers['location'] = proxyRes.headers['location'].replace('localhost:4400', 'localhost:5500/sandbox') + '&redirToSandbox=' + orig;
+        }
       }
     }
+  },
+  '/sandbox/*': {
+    target: "http://localhost:4400",
+    pathRewrite: {
+      "^/sandbox": ""
+    },
+    logLevel: "debug",
+    secure: false,
+    changeOrigin: true
   }
 };
 
