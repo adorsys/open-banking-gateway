b1d6f8340f5e9ae9b9907e59b3fda6f810ad9b20
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-11 17:03:42 +0200

    OBG-194. Consent reusage done

--- technical-embedded-ui/tpp-ui/src/app/initial-request/initial-request.component.ts
+++ technical-embedded-ui/tpp-ui/src/app/initial-request/initial-request.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {Consts} from '../consts';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-initial-request',
@@ -11,37 +11,42 @@ import {HttpClient} from '@angular/common/http';
 export class InitialRequestComponent implements OnInit {
 
   getUri: string = Consts.API_V1_URL_BASE + 'banking/ais/accounts';
-  form: FormGroup;
-  fintechUserId: string;
-  fintechRedirectUriOk: string;
-  fintechRedirectUriNok: string;
-  authorization: string;
-  requestId: string;
-  bankId: string;
+  form: FormGroup = new FormGroup({});
+  fintechUserId = new FormControl();
+  fintechRedirectUriOk = new FormControl();
+  fintechRedirectUriNok = new FormControl();
+  authorization = new FormControl();
+  requestId = new FormControl();
+  bankId = new FormControl();
+  serviceSessionId = new FormControl();
 
   constructor(private client: HttpClient) {
-    this.fintechRedirectUriOk = 'http://localhost:5500/ok';
-    this.fintechRedirectUriNok = 'http://localhost:5500/nok';
-    this.fintechUserId = 'John Doe';
-    this.authorization = '12345';
-    this.requestId = '43da4e2f-72cb-43bb-8afd-683104de57f9';
-    this.bankId = '53c47f54-b9a4-465a-8f77-bc6cd5f0cf46';
+    this.fintechRedirectUriOk.setValue('http://localhost:5500/ok');
+    this.fintechRedirectUriNok.setValue('http://localhost:5500/nok');
+    this.fintechUserId.setValue('John Doe');
+    this.authorization.setValue('12345');
+    this.requestId.setValue('43da4e2f-72cb-43bb-8afd-683104de57f9');
+    this.bankId.setValue('53c47f54-b9a4-465a-8f77-bc6cd5f0cf46');
   }
 
   ngOnInit() {
   }
 
   submit() {
+    const headerVals = new HttpHeaders({
+      'Fintech-Redirect-URL-OK': this.fintechRedirectUriOk.value,
+      'Fintech-Redirect-URL-NOK': this.fintechRedirectUriNok.value,
+      'Fintech-User-ID': this.fintechUserId.value,
+      'Authorization': this.authorization.value,
+      'X-Request-ID': this.requestId.value,
+      'Bank-ID': this.bankId.value,
+      'Service-Session-ID': this.serviceSessionId.value
+    });
+
     this.client.get(this.getUri, {
-      observe: 'response',
-      headers: {
-        'Fintech-Redirect-URL-OK': this.fintechRedirectUriOk,
-        'Fintech-Redirect-URL-NOK': this.fintechRedirectUriNok,
-        'Fintech-User-ID': this.fintechUserId,
-        'Authorization': this.authorization,
-        'X-Request-ID': this.requestId,
-        'Bank-ID': this.bankId
-    }}).subscribe(res => {
+      headers: headerVals,
+      observe: 'response'
+    }).subscribe(res => {
       window.location.href = res.headers.get('Location');
     });
   }
