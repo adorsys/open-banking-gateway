04e41214c7dfbc2576c36fd205a1d4e2d18af62c
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-02-12 15:46:56 +0100

    OBG-220 fix tests

--- opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListAccountsServiceTest.java
+++ opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListAccountsServiceTest.java
@@ -3,7 +3,7 @@ package de.adorsys.opba.protocol.facade.services.ais;
 import de.adorsys.opba.db.config.EnableBankingPersistence;
 import de.adorsys.opba.protocol.api.dto.request.FacadeServiceableRequest;
 import de.adorsys.opba.protocol.api.dto.request.accounts.ListAccountsRequest;
-import de.adorsys.opba.protocol.api.dto.result.fromprotocol.dialog.ValidationErrorResult;
+import de.adorsys.opba.protocol.facade.dto.result.torest.redirectable.FacadeStartAuthorizationResult;
 import de.adorsys.opba.protocol.xs2a.EnableXs2aProtocol;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
@@ -29,10 +29,13 @@ class ListAccountsServiceTest {
                         .facadeServiceable(
                                 FacadeServiceableRequest.builder()
                                         .bankId("53c47f54-b9a4-465a-8f77-bc6cd5f0cf46")
+                                        .sessionPassword("123")
+                                        .fintechRedirectUrlOk("http://google.com")
+                                        .fintechRedirectUrlNok("http://microsoft.com")
                                         .build()
                         ).build()
                 ).get()
-        ).isInstanceOf(ValidationErrorResult.class);
+        ).isInstanceOf(FacadeStartAuthorizationResult.class);
     }
 
     @EnableXs2aProtocol
