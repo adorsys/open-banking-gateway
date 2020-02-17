48499ec67fbbbc3c33c2605965a8c68fba2caea4
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-17 21:10:08 +0200

    OBG-267. Added basic docker-compose.yml and configuration

--- how-to-start-with-project/technical-ui/01.build.sh
+++ how-to-start-with-project/technical-ui/01.build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 DIR=`pwd`
+PROJECT_ROOT=`pwd`"/../.."
 
-cd .. || exit
+cd "$PROJECT_ROOT" || exit
 
-echo "Build Open Banking Jars"
-./mvnw clean package -DskipTests
+echo ">>> 01. Build Open Banking Jars"
+./mvnw clean package -DskipTests || exit
 
-echo "Build Technical UI"
+echo ">>> 02. Build Technical UI"
+docker run -it --rm --name technical-ui-build -v "$PROJECT_ROOT/technical-embedded-ui/tpp-ui":/usr/src/app -w /usr/src/app node:12.6.0-stretch npm install || exit
 
-echo "Build Technical UI docker image"
+echo ">>> 04. Build Open Banking docker image"
+docker build "$PROJECT_ROOT/opba-embedded-starter" -t open-banking-gateway || exit
 
-echo "Build Open Banking docker image"
+echo ">>> 03. Build Technical UI docker image"
+docker build "$PROJECT_ROOT/technical-embedded-ui/tpp-ui" -t technical-tpp-ui || exit
 
 cd "$DIR" || exit
