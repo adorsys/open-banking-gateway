8bab2147f0f1c0cebffc7e8d1002326693cce36d
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-18 18:32:43 +0200

    OBG-267. Fixed OPBA database creation

--- how-to-start-with-project/technical-ui/01.build.sh
+++ how-to-start-with-project/technical-ui/01.build.sh
@@ -9,14 +9,13 @@ echo ">>> 01. Build Open Banking Jars"
 ./mvnw clean package -DskipTests || exit
 
 echo ">>> 02. Build Technical UI"
+mkdir -p $PROJECT_ROOT/technical-embedded-ui/tpp-ui/node_modules
 # MacOS docker volumes are sloow...
 if [[ "$(uname)" == "Darwin" ]]; then
-    docker run -it --rm --name technical-ui-build -v "$PROJECT_ROOT/technical-embedded-ui/tpp-ui":/usr/src/app:delegated -w /usr/src/app node:12.6.0-stretch npm install || exit
+    docker run -it --rm --name technical-ui-build -v "$PROJECT_ROOT/technical-embedded-ui/tpp-ui":/usr/src/app:delegated -e NG_CLI_ANALYTICS=false -w /usr/src/app node:12.6.0-stretch npm --silent install || exit
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-    docker run -it --rm --name technical-ui-build -v "$PROJECT_ROOT/technical-embedded-ui/tpp-ui":/usr/src/app -w /usr/src/app node:12.6.0-stretch npm install || exiti
+    docker run -it --rm --name technical-ui-build -v "$PROJECT_ROOT/technical-embedded-ui/tpp-ui":/usr/src/app -e NG_CLI_ANALYTICS=false -w /usr/src/app node:12.6.0-stretch npm --silent install || exit
 fi
-
-
 
 echo ">>> 04. Build Open Banking docker image"
 docker build "$PROJECT_ROOT/opba-embedded-starter" -t open-banking-gateway || exit
