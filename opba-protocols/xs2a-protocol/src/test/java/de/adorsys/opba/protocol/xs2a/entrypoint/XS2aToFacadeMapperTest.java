5f392a8512b82fdf02cb6296e95bb6fad02a34bb
electronicpeter <github@electronicpeter.de>
2020-02-19 16:07:26 +0100

    OBG 260 generic mapping

---
+++ opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/entrypoint/XS2aToFacadeMapperTest.java
@@ -0,0 +1,43 @@
+package de.adorsys.opba.protocol.xs2a.entrypoint;
+
+import de.adorsys.opba.protocol.api.dto.result.body.AccountListBody;
+import de.adorsys.xs2a.adapter.service.model.AccountDetails;
+import de.adorsys.xs2a.adapter.service.model.AccountListHolder;
+import de.adorsys.xs2a.adapter.service.model.Amount;
+import de.adorsys.xs2a.adapter.service.model.Balance;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class XS2aToFacadeMapperTest {
+    static String IBAN = "DE23445343434";
+    static String AMOUNTVALUE = "123,32";
+
+    @Test
+    public void testMapping() {
+
+        AccountListHolder xs2aEntity = new AccountListHolder();
+        {
+            Amount amount = new Amount();
+            amount.setAmount(AMOUNTVALUE);
+            Balance balance = new Balance();
+            balance.setBalanceAmount(amount);
+            List<Balance> balances = new ArrayList<>();
+            balances.add(balance);
+
+            AccountDetails account = new AccountDetails();
+            account.setIban(IBAN);
+            account.setBalances(balances);
+
+            List<AccountDetails> accounts = new ArrayList<>();
+            accounts.add(account);
+
+            xs2aEntity.setAccounts(accounts);
+        }
+        AccountListBody facadeEntity = Mappers.getMapper(XS2aToFacadeMapper.class).mapFromXs2aToFacade(xs2aEntity);
+        Assert.assertEquals(IBAN, facadeEntity.getAccounts().get(0).getIban());
+    }
+}
