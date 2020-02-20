092753ec5b837e0700a340bbe344ce1ffc6fde23
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-02-20 16:39:46 +0100

    OPB-241 Fix similarity threshold

--- opba-db/src/main/java/de/adorsys/opba/db/repository/BankRepositoryImpl.java
+++ opba-db/src/main/java/de/adorsys/opba/db/repository/BankRepositoryImpl.java
@@ -3,10 +3,13 @@ package de.adorsys.opba.db.repository;
 import de.adorsys.opba.db.domain.entity.Bank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,16 +17,24 @@ public class BankRepositoryImpl {
 
     private static final BeanPropertyRowMapper<Bank> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Bank.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public List<Bank> getBanks(String query, int startPos, int maxResults) {
-        String sql = "SELECT id, uuid, name, bic, bank_code, "
-                + "word_similarity(?, (name || ' ' || bic || ' ' || bank_code)) as sim "
-                + "FROM opb_bank "
-                + "WHERE ? <% (name || ' ' || bic || ' ' || bank_code)"
-                + "ORDER BY sim DESC "
-                + "LIMIT ? "
-                + "OFFSET ?";
-        return jdbcTemplate.query(sql, ROW_MAPPER, query, query, maxResults, startPos);
+        String subQuery = "SELECT id, uuid, name, bic, bank_code, word_similarity(:keyword, field) as sml "
+                + "FROM opb_bank WHERE :keyword <% field ";
+
+        String sql = Stream.of("name", "bank_code", "bic")
+                .map(s -> subQuery.replaceAll("field", s))
+                .collect(Collectors.joining("UNION "))
+                + "ORDER BY sml DESC "
+                + "LIMIT :max "
+                + "OFFSET :start";
+
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("keyword", query);
+        parameters.addValue("max", maxResults);
+        parameters.addValue("start", startPos);
+
+        return jdbcTemplate.query(sql, parameters, ROW_MAPPER);
     }
 }
