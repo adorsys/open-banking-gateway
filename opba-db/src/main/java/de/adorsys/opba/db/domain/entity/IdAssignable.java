edc594e744f7612bc614d74d23f16779c594db57
Valentyn Berezin <valb3r.ua@gmail.com>
2020-02-21 13:46:40 +0200

    OBG-282. Allow id generation

---
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/IdAssignable.java
@@ -0,0 +1,9 @@
+package de.adorsys.opba.db.domain.entity;
+
+import java.io.Serializable;
+
+public interface IdAssignable<I extends Serializable> {
+
+    I getId();
+    void setId(I id);
+}
---
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/generators/AssignedUuidGenerator.java
@@ -0,0 +1,25 @@
+package de.adorsys.opba.db.domain.generators;
+
+import de.adorsys.opba.db.domain.entity.IdAssignable;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.UUIDGenerator;
+
+import java.io.Serializable;
+
+public class AssignedUuidGenerator extends UUIDGenerator {
+
+    public static final String ASSIGNED_ID_GENERATOR = "assigned-identity";
+    public static final String ASSIGNED_ID_STRATEGY = "de.adorsys.opba.db.domain.generators.AssignedUuidGenerator";
+
+    @Override
+    public Serializable generate(SharedSessionContractImplementor session, Object obj) {
+        if (obj instanceof IdAssignable) {
+            Serializable id = ((IdAssignable<? extends Serializable>) obj).getId();
+            if (id != null) {
+                return id;
+            }
+        }
+
+        return super.generate(session, obj);
+    }
+}
