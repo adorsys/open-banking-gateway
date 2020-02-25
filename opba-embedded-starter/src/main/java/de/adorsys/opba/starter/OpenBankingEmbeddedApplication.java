85b22dd2c426ed35aa6edf7b0f08cc12a07b7820
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-25 14:24:49 +0200

    OBG-296. Wired extracted protocol

--- opba-embedded-starter/src/main/java/de/adorsys/opba/starter/OpenBankingEmbeddedApplication.java
+++ opba-embedded-starter/src/main/java/de/adorsys/opba/starter/OpenBankingEmbeddedApplication.java
@@ -3,6 +3,7 @@ package de.adorsys.opba.starter;
 import de.adorsys.opba.consentapi.config.EnableConsentApi;
 import de.adorsys.opba.db.config.EnableBankingPersistence;
 import de.adorsys.opba.protocol.xs2a.EnableXs2aProtocol;
+import de.adorsys.opba.protocol.xs2a.EnableXs2aSandboxProtocol;
 import de.adorsys.opba.tppbankingapi.config.EnableBankingApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableConsentApi
 @EnableBankingApi
 @EnableXs2aProtocol
+@EnableXs2aSandboxProtocol
 @EnableBankingPersistence
 @SpringBootApplication
 @ComponentScan("de.adorsys.opba.consentapi")
