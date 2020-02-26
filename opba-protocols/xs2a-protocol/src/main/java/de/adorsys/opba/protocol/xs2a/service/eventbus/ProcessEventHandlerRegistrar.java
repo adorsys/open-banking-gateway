5973d6e95b01b1db350cfc9b951b6b93886ee975
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-26 11:57:42 +0200

    OBG-253. Dropped 'obvious' deprecated code

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/eventbus/ProcessEventHandlerRegistrar.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/eventbus/ProcessEventHandlerRegistrar.java
@@ -6,38 +6,13 @@ import de.adorsys.opba.protocol.xs2a.domain.dto.messages.Response;
 import de.adorsys.opba.protocol.xs2a.domain.dto.messages.ValidationIssue;
 import de.adorsys.opba.protocol.xs2a.entrypoint.OutcomeMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 @Service
 @RequiredArgsConstructor
 public class ProcessEventHandlerRegistrar {
 
     private final ProcessResultEventHandler handler;
-
-    @Deprecated // FIXME - kept only for tests using endpoints
-    public <T> void addHandler(String processId,
-                               Consumer<Response> onSuccess,
-                               CompletableFuture<ResponseEntity<T>> result
-    ) {
-        handler.add(
-                processId,
-                procResult -> {
-                    if (procResult instanceof Response) {
-                        doSuccess(onSuccess, (Response) procResult);
-                    } else if (procResult instanceof Redirect) {
-                        doRedirect(result, (Redirect) procResult);
-                    } else if (procResult instanceof ValidationIssue) {
-                        doFixValidation(result, (ValidationIssue) procResult);
-                    }
-                });
-    }
 
     public <T> void addHandler(String processId, OutcomeMapper<T> mapper) {
         handler.add(
@@ -55,22 +30,5 @@ public class ProcessEventHandlerRegistrar {
                         mapper.onError();
                     }
                 });
-    }
-
-    private void doSuccess(Consumer<Response> onSuccess, Response procResult) {
-        onSuccess.accept(procResult);
-    }
-
-    private <T> void doRedirect(CompletableFuture<ResponseEntity<T>> result, Redirect redirResult) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(redirResult.getRedirectUri());
-        result.complete(new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY));
-    }
-
-    @SneakyThrows
-    private <T> void doFixValidation(CompletableFuture<ResponseEntity<T>> result, ValidationIssue validResult) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(validResult.getProvideMoreParamsDialog());
-        result.complete(new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY));
     }
 }
