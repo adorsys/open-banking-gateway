a9ac753c91071a73f56d11e364c1572c6e04e4b6
valb3r <valentyn.berezin@adorsys.com.ua>
2020-02-26 12:12:28 +0200

    OBG-253. Dropped some unused code and dependencies

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/config/SpringDocConfig.java
+++
@@ -1,9 +0,0 @@
-package de.adorsys.opba.protocol.xs2a.config;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import org.springframework.context.annotation.Configuration;
-
-@OpenAPIDefinition
-@Configuration
-public class SpringDocConfig {
-}
