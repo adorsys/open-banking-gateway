24bb3091d83ff3ce88684459320643545941d648
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-02-27 18:12:41 +0100

    OBG-260 xs2a to facade response mapper test

--- opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/entrypoint/Xs2AToFacadeMapperTest.java
+++ opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/entrypoint/Xs2AToFacadeMapperTest.java
@@ -1,46 +1,55 @@
 package de.adorsys.opba.protocol.xs2a.entrypoint;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.common.io.Resources;
 import de.adorsys.opba.protocol.api.dto.result.body.AccountListBody;
+import de.adorsys.opba.protocol.api.dto.result.body.TransactionsResponseBody;
 import de.adorsys.opba.protocol.xs2a.service.mappers.generated.Xs2aResultBodyExtractor$Xs2aToFacadeMapperImpl;
-import de.adorsys.xs2a.adapter.service.model.AccountDetails;
 import de.adorsys.xs2a.adapter.service.model.AccountListHolder;
-import de.adorsys.xs2a.adapter.service.model.Amount;
-import de.adorsys.xs2a.adapter.service.model.Balance;
-import de.adorsys.xs2a.adapter.service.model.CashAccountType;
-import org.junit.Assert;
+import de.adorsys.xs2a.adapter.service.model.TransactionsReport;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URL;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = Xs2aResultBodyExtractor$Xs2aToFacadeMapperImpl.class)
 public class Xs2AToFacadeMapperTest {
-    static String IBAN = "DE23445343434";
-    static String AMOUNTVALUE = "123,32";
+    public static final ObjectMapper JSON_MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    @Autowired
+    private Xs2aResultBodyExtractor.Xs2aToFacadeMapper mapper;
 
     @Test
-    public void testMapping() {
+    @SneakyThrows
+    void accountsMapperTest() {
+        URL inputUrl = Resources.getResource("mapperTestFixtures/xs2a_to_facade_response_mapper_accounts_input.json");
+        AccountListHolder inputObject = JSON_MAPPER.readValue(inputUrl, AccountListHolder.class);
+        AccountListBody outputObject = mapper.map(inputObject);
 
-        AccountListHolder xs2aEntity = new AccountListHolder();
-        {
-            Amount amount = new Amount();
-            amount.setAmount(AMOUNTVALUE);
-            Balance balance = new Balance();
-            balance.setBalanceAmount(amount);
-            List<Balance> balances = new ArrayList<>();
-            balances.add(balance);
+        URL outputUrl = Resources.getResource("mapperTestFixtures/xs2a_to_facade_response_mapper_accounts_output.json");
+        AccountListBody expectedObject = JSON_MAPPER.readValue(outputUrl, AccountListBody.class);
 
-            AccountDetails account = new AccountDetails();
-            account.setIban(IBAN);
-            account.setBalances(balances);
-            account.setCashAccountType(CashAccountType.CASH);
+        assertThat(expectedObject).isEqualToComparingFieldByField(outputObject);
+    }
 
-            List<AccountDetails> accounts = new ArrayList<>();
-            accounts.add(account);
+    @Test
+    @SneakyThrows
+    void transactionsMapperTest() {
+        URL inputUrl = Resources.getResource("mapperTestFixtures/xs2a_to_facade_response_mapper_transactions_input.json");
+        TransactionsReport inputObject = JSON_MAPPER.readValue(inputUrl, TransactionsReport.class);
+        TransactionsResponseBody outputObject = mapper.map(inputObject);
 
-            xs2aEntity.setAccounts(accounts);
-        }
-        AccountListBody facadeEntity = new Xs2aResultBodyExtractor$Xs2aToFacadeMapperImpl().map(xs2aEntity);
-        Assert.assertEquals(IBAN, facadeEntity.getAccounts().get(0).getIban());
-        Assert.assertEquals(CashAccountType.CASH.name(), facadeEntity.getAccounts().get(0).getCashAccountType());
+        URL outputUrl = Resources.getResource("mapperTestFixtures/xs2a_to_facade_response_mapper_transactions_output.json");
+        TransactionsResponseBody expectedObject = JSON_MAPPER.readValue(outputUrl, TransactionsResponseBody.class);
+
+        assertThat(expectedObject).isEqualToComparingFieldByField(outputObject);
     }
 }
