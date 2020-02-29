ea0d909aaac33c17a462df152170b9ca670a3be3
Valentyn Berezin <valb3r.ua@gmail.com>
2020-02-29 19:55:20 +0200

    Drop infinite loop

---
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/consent/ConsentConst.java
@@ -0,0 +1,10 @@
+package de.adorsys.opba.protocol.xs2a.service.xs2a.consent;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor") //Checkstyle doesn't recognise Lombok
+public class ConsentConst {
+
+    public static final String CONSENT_FINALIZED = "finalised";
+}
