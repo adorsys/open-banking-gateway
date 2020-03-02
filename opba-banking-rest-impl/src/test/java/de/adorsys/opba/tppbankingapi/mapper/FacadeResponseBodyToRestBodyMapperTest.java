fee022abb062f23614602d7702dfc448c1f171ce
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-03-02 19:35:21 +0100

    test using json assert

--- opba-banking-rest-impl/src/test/java/de/adorsys/opba/tppbankingapi/mapper/FacadeResponseBodyToRestBodyMapperTest.java
+++ opba-banking-rest-impl/src/test/java/de/adorsys/opba/tppbankingapi/mapper/FacadeResponseBodyToRestBodyMapperTest.java
@@ -9,32 +9,38 @@ import de.adorsys.opba.protocol.api.dto.result.body.AccountListDetailBody;
 import de.adorsys.opba.protocol.api.dto.result.body.TransactionsResponseBody;
 import de.adorsys.opba.tppbankingapi.ais.model.generated.AccountList;
 import de.adorsys.opba.tppbankingapi.ais.model.generated.TransactionsResponse;
+import de.adorsys.opba.tppbankingapi.controller.TppBankingApiAisController;
 import de.adorsys.opba.tppbankingapi.mapper.generated.TppBankingApiAisController$AccountListFacadeResponseBodyToRestBodyMapperImpl;
-import de.adorsys.opba.tppbankingapi.mapper.generated.TppBankingApiAisController$TransactionsFacadeResponseBodyToRestBodyMapperImpl;
 import lombok.SneakyThrows;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest(classes = {TppBankingApiAisController$AccountListFacadeResponseBodyToRestBodyMapperImpl.class,
-                            TppBankingApiAisController$TransactionsFacadeResponseBodyToRestBodyMapperImpl.class})
+@SpringBootTest(classes = FacadeResponseBodyToRestBodyMapperTest.TestConfig.class)
 public class FacadeResponseBodyToRestBodyMapperTest {
     public static final ObjectMapper JSON_MAPPER = new ObjectMapper()
             .registerModule(new JavaTimeModule())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .enable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    public static final String PATH_PREFIX = "mapper-test-fixtures/facade_to_rest_response_";
 
     @Autowired
-    private TppBankingApiAisController$AccountListFacadeResponseBodyToRestBodyMapperImpl accountsRestMapper;
+    private TppBankingApiAisController.AccountListFacadeResponseBodyToRestBodyMapper accountsRestMapper;
 
     @Autowired
-    private TppBankingApiAisController$TransactionsFacadeResponseBodyToRestBodyMapperImpl transactionsRestMapper;
+    private TppBankingApiAisController.TransactionsFacadeResponseBodyToRestBodyMapper transactionsRestMapper;
 
     private static final String IBAN = "DE122342343243";
     private static final String DELETED = "DELETED";
@@ -58,26 +64,41 @@ public class FacadeResponseBodyToRestBodyMapperTest {
     @Test
     @SneakyThrows
     void accountsMapperTest() {
-        URL inputUrl = Resources.getResource("mapperTestFixtures/facade_to_rest_response_accounts_mapper_input.json");
-        AccountListBody inputObject = JSON_MAPPER.readValue(inputUrl, AccountListBody.class);
+        String inputUrl = PATH_PREFIX + "accounts_mapper_input.json";
+        AccountListBody inputObject = getFromFile(inputUrl, AccountListBody.class);
         AccountList outputObject = accountsRestMapper.map(inputObject);
 
-        URL outputUrl = Resources.getResource("mapperTestFixtures/facade_to_rest_response_accounts_mapper_output.json");
-        AccountList expectedObject = JSON_MAPPER.readValue(outputUrl, AccountList.class);
-
-        assertThat(expectedObject).isEqualToComparingFieldByField(outputObject);
+        JSONObject outputJson = new JSONObject(outputObject);
+        JSONObject expected = getRESTData(PATH_PREFIX + "accounts_mapper_output.json");
+        JSONAssert.assertEquals(expected, outputJson, JSONCompareMode.STRICT);
     }
 
     @Test
-    @SneakyThrows
+
     void transactionsMapperTest() {
-        URL inputUrl = Resources.getResource("mapperTestFixtures/facade_to_rest_response_transactions_mapper_input.json");
-        TransactionsResponseBody inputObject = JSON_MAPPER.readValue(inputUrl, TransactionsResponseBody.class);
+        String inputUrl = PATH_PREFIX + "transactions_mapper_input.json";
+        TransactionsResponseBody inputObject = getFromFile(inputUrl, TransactionsResponseBody.class);
         TransactionsResponse outputObject = transactionsRestMapper.map(inputObject);
 
-        URL outputUrl = Resources.getResource("mapperTestFixtures/facade_to_rest_response_transactions_mapper_output.json");
-        TransactionsResponse expectedObject = JSON_MAPPER.readValue(outputUrl, TransactionsResponse.class);
+        JSONObject outputJson = new JSONObject(outputObject);
+        JSONObject expected = getRESTData(PATH_PREFIX + "transactions_mapper_output.json");
+        JSONAssert.assertEquals(expected, outputJson, JSONCompareMode.STRICT);
+    }
 
-        assertThat(expectedObject).isEqualToComparingFieldByField(outputObject);
+    @SneakyThrows
+    private <T> T getFromFile(String path, Class<T> valueType) {
+        return JSON_MAPPER.readValue(Resources.getResource(path), valueType);
+    }
+
+    @SneakyThrows
+    private JSONObject getRESTData(String path) {
+        String file = Resources.getResource(path).getPath();
+        String content = new String(Files.readAllBytes(Paths.get(file)));
+        return new JSONObject(content);
+    }
+
+    @Configuration
+    @ComponentScan(basePackages = "de.adorsys.opba.tppbankingapi.mapper.generated")
+    public static class TestConfig {
     }
 }
