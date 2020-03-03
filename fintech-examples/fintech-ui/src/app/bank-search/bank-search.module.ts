c05ef172450e92bf7bd7e34b4e002b8a68ed3553
Hergy Tchuinkou <htc@adorsys.de>
2020-03-03 15:09:17 +0100

    added services of selected bank

--- fintech-examples/fintech-ui/src/app/bank-search/bank-search.module.ts
+++ fintech-examples/fintech-ui/src/app/bank-search/bank-search.module.ts
@@ -6,9 +6,10 @@ import { SearchComponent } from './common/search/search.component';
 import { ShareModule } from '../common/share.module';
 import { CommonModule } from '@angular/common';
 import { RedirectPageComponent } from './components/redirect-page/redirect-page.component';
+import { ListServicesComponent } from './components/list-services/list-services.component';
 
 @NgModule({
-  declarations: [BankSearchComponent, SearchComponent, RedirectPageComponent],
+  declarations: [BankSearchComponent, SearchComponent, RedirectPageComponent, ListServicesComponent],
   imports: [CommonModule, ShareModule, BankSearchRoutingModule]
 })
 export class BankSearchModule {}
