94111835fc8b84db05625f9902500582783a6939
Vladimir Nechai <vne@adorsys.de>
2020-03-04 13:24:00 +0100

    init ais routing

--- consent-ui/src/app/ais/ais-routing.module.ts
+++ consent-ui/src/app/ais/ais-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EntryPageComponent} from "./entry-page/entry-page.component";
+import {PasswordInputPageComponent} from "./password-input-page/password-input-page.component";
+import {ConfirmConsentPageComponent} from "./confirm-consent-page/confirm-consent-page.component";
+import {TanConfirmPageComponent} from "./tan-confirm-page/tan-confirm-page.component";
+import {ResultPageComponent} from "./result-page/result-page.component";
+import {ScaSelectPageComponent} from "./sca-select-page/sca-select-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EntryPageComponent,
+  },
+  {
+    path: 'authenticate',
+    component: PasswordInputPageComponent,
+  },
+  {
+    path: 'confirm-consent',
+    component: ConfirmConsentPageComponent,
+  },
+  {
+    path: 'select-sca',
+    component: ScaSelectPageComponent,
+  },
+  {
+    path: 'confirm-tan',
+    component: TanConfirmPageComponent,
+  },
+  {
+    path: 'consent-result',
+    component: ResultPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- consent-ui/src/app/app-routing.module.ts
+++ consent-ui/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ais',
+    loadChildren: () => import('./ais/ais.module').then(m => m.AisModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
