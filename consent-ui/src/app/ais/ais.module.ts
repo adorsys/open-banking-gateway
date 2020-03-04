c5aca6719607048e4523592ad1274d2f1640d4e7
Vladimir Nechai <vne@adorsys.de>
2020-03-04 10:40:37 +0100

    add ais module

---
+++ consent-ui/src/app/ais/ais.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AisRoutingModule } from './ais-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AisRoutingModule
+  ]
+})
+export class AisModule { }
