c130e1846d64923fcd2b9c8842d0f283fbf59f4e
electronicpeter <github@electronicpeter.de>
2020-03-05 17:14:27 +0100

    OBG 236

--- fintech-examples/fintech-ui/src/app/bank-search/bank-search-routing.module.ts
+++ fintech-examples/fintech-ui/src/app/bank-search/bank-search-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { BankSearchComponent } from './components/bank-search/bank-search.component';
 import { RedirectPageComponent } from './components/redirect-page/redirect-page.component';
 
--- fintech-examples/fintech-ui/src/app/dashboard/services/ais.service.ts
+++ fintech-examples/fintech-ui/src/app/dashboard/services/ais.service.ts
@@ -1,59 +1,52 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { FinTechAccountInformationService } from '../../api';
+import { AccountDetails, AccountList, FinTechAccountInformationService } from '../../api';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AisService {
+  private okurl = '';
+
   constructor(
     private route: ActivatedRoute,
     private finTechAccountInformationService: FinTechAccountInformationService
-  ) {}
-
-  getAccounts(bankId: string) {
-    //    return this.finTechAccountInformationService
-    //    .aisAccountsGET(bankId, '', '', 'ok-url', 'not-ok-url')
-    //      .pipe(map(response => response.accounts));
-
-    // TODO maybe without protocol
-    const okurl = window.location.protocol + '//' + window.location.host + '/redirectAfterConsent';
-    console.log('redirect url:' + okurl);
-
-    const response = this.finTechAccountInformationService.aisAccountsGET(
-      bankId,
-      '',
-      '',
-      okurl,
-      'not-ok-url',
-      'response'
-    );
-    response.subscribe(r => {
-      console.log('response status is ' + r.status);
-      switch (r.status) {
-        case 202:
-          let locationForRedirect = r.headers.get('location');
-          const additionalParameters = new URLSearchParams({
-            authorizationSessionId: r.headers.get('Authorization-Session-ID'),
-            serviceSessionId: r.headers.get('Service-Session-ID'),
-            redirectCode: r.headers.get('Redirect-Code')
-          });
-          locationForRedirect += '&' + additionalParameters;
-          window.location.href = locationForRedirect;
-          return []; /* this code is never reached */
-        case 200:
-          console.log('I got the accounts and I want to show them ;-)');
-          console.log('I got ', r.body.accounts.length, ' accounts');
-          return r.body.accounts;
-      }
-    });
-    return [];
+  ) {
+    this.okurl = window.location.protocol + '//' + window.location.host + '/redirectAfterConsent';
+    console.log('redirect url:' + this.okurl);
   }
 
-  getTransactions() {
+  getAccounts(bankId: string) {
+    // TODO maybe without protocol
+
     return this.finTechAccountInformationService
-      .aisTransactionsGET('bankId', 'accountId', '', '', 'ok-url', 'not-ok-url')
+      .aisAccountsGET(bankId, '', '', this.okurl, 'not-ok-url', 'response')
+      .pipe(
+        map(response => {
+          switch (response.status) {
+            case 202:
+              const additionalParameters = new URLSearchParams({
+                authorizationSessionId: response.headers.get('Authorization-Session-ID'),
+                serviceSessionId: response.headers.get('Service-Session-ID'),
+                redirectCode: response.headers.get('Redirect-Code')
+              });
+              window.location.href = response.headers.get('location') + '&' + additionalParameters;
+              break;
+            case 200:
+              console.log('I got the accounts and I want to show them ;-)');
+              console.log('I got ', response.body.accounts.length, ' accounts');
+              return response.body.accounts;
+          }
+        })
+      );
+  }
+
+  getTransactions(bankId: string, accountId: string) {
+    return this.finTechAccountInformationService
+      .aisTransactionsGET('bankId', 'accountId', '', '', this.okurl, 'not-ok-url')
       .pipe(map(response => response.transactions));
   }
 }
