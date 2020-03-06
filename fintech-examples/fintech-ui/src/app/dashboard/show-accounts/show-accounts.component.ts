059dce6b9feb9b0e26c777f7b7f6cd181c00302a
electronicpeter <github@electronicpeter.de>
2020-03-06 11:22:49 +0100

    OBG 236 listAccounts Component cares about accounts, not dashboard

--- fintech-examples/fintech-ui/src/app/dashboard/show-accounts/show-accounts.component.ts
+++ fintech-examples/fintech-ui/src/app/dashboard/show-accounts/show-accounts.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { AccountDetails } from '../../api';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AisService } from '../services/ais.service';
 
 @Component({
   selector: 'app-show-accounts',
   templateUrl: './show-accounts.component.html',
   styleUrls: ['./show-accounts.component.scss']
 })
-export class ShowAccountsComponent implements OnInit {
-  @Input()
-  bankId = '';
+export class ShowAccountsComponent implements OnInit, OnDestroy {
+  private accountsSubscription: Subscription;
+  accounts: AccountDetails[];
 
   config = {
     headline: 'small',
@@ -17,9 +20,24 @@ export class ShowAccountsComponent implements OnInit {
   };
 
   @Input()
-  accounts: AccountDetails[];
+  showAccounts = false;
 
-  constructor() {}
+  @Input()
+  bankId = '';
 
-  ngOnInit() {}
+  constructor(private route: ActivatedRoute, private aisService: AisService) {}
+
+  ngOnInit() {
+    if (this.showAccounts) {
+      this.route.params.forEach(param => {
+        this.aisService.getAccounts(param.id).subscribe(accounts => {
+          this.accounts = accounts;
+        });
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.accountsSubscription.unsubscribe();
+  }
 }
