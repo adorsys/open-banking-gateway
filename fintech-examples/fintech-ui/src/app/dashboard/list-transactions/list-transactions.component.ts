c36c3b8764e6c6161711b30e3a2c2f5889c9e824
electronicpeter <github@electronicpeter.de>
2020-03-09 12:00:07 +0100

    OBG 236 final optimazation

--- fintech-examples/fintech-ui/src/app/dashboard/list-transactions/list-transactions.component.ts
+++ fintech-examples/fintech-ui/src/app/dashboard/list-transactions/list-transactions.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AisService } from '../services/ais.service';
 import { Subscription } from 'rxjs';
-import { AccountReport } from '../../api';
+import { AccountReport, TransactionDetails } from '../../api';
 
 @Component({
   selector: 'app-list-transactions',
@@ -16,8 +16,9 @@ export class ListTransactionsComponent implements OnInit, OnDestroy {
   @Input()
   bankId = '';
 
-  transactions: AccountReport;
   makeVisible = false;
+  transactionsLists: Array<Array<TransactionDetails>>;
+  transactionsListNames = ['Pending', 'Booked'];
 
   constructor(private route: ActivatedRoute, private aisService: AisService) {}
 
@@ -25,11 +26,11 @@ export class ListTransactionsComponent implements OnInit, OnDestroy {
 
   selectAccount(id) {
     this.makeVisible = false;
-    this.transactions = null;
+    this.transactionsLists = [];
     this.accountId = id;
     console.log('ask for transactions for ', this.accountId);
     this.accountsSubscription = this.aisService.getTransactions(this.bankId, this.accountId).subscribe(transactions => {
-      this.transactions = transactions;
+      this.transactionsLists = [transactions.pending, transactions.booked];
       this.makeVisible = true;
     });
   }
