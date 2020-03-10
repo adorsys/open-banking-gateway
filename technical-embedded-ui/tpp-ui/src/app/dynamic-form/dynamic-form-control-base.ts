e38b537f6a14485f6c3023b3d93b6169f5f6b8d8
Valentyn Berezin <valb3r.ua@gmail.com>
2020-03-10 13:08:05 +0200

    OBG-352. Accommodate technical UI

--- technical-embedded-ui/tpp-ui/src/app/dynamic-form/dynamic-form-control-base.ts
+++ technical-embedded-ui/tpp-ui/src/app/dynamic-form/dynamic-form-control-base.ts
@@ -1,5 +1,4 @@
-const KnownControlTypes = ['textbox', 'dropdown', 'boolean', 'accountaccess', 'date'];
-const KnownTypeValidations = ['string', 'ipaddr', 'class', 'boolean', 'integer'];
+const KnownTypes = ['BOOLEAN', 'INTEGER', 'STRING', 'DATE', 'OBJECT'];
 
 // Validations are defined in format:
 // <controlType>.<valueType>
@@ -12,11 +11,10 @@ export class DynamicFormControlBase<T> {
   value: T;
   id: string;
   code: string;
-  message: string;
   type: string;
+  message: string;
   required: boolean;
   order: number;
-  controlType: string;
 
   // TODO Move to children:
   options: {key: string, value: string}[] = [];
@@ -24,40 +22,28 @@ export class DynamicFormControlBase<T> {
   constructor(
     id: string,
     code: string,
+    type: string,
+    scope: string,
     message: string,
     public target: Target
   ) {
     this.id = id;
     this.code = code || '';
+    this.type = type;
     this.message = message || '';
     this.order = 1;
-    this.parseAndValidateCode(this.code);
-  }
-
-  private parseAndValidateCode(code: string) {
-    let parts = code.split('.');
-
-    if (parts.length < 2) {
-      throw new SyntaxError("Wrong code " + code);
-    }
-
-    this.controlType = parts[0];
-    this.type = parts[1];
     this.validation();
   }
 
   private validation() {
 
-    if (KnownControlTypes.indexOf(this.controlType) < 0) {
-      throw new SyntaxError("Wrong control type " + this.controlType);
-    }
-    if (KnownTypeValidations.indexOf(this.type) < 0) {
-      throw new SyntaxError("Wrong type validator " + this.type);
+    if (KnownTypes.indexOf(this.type) < 0) {
+      throw new SyntaxError("Wrong control type " + this.type);
     }
   }
 }
 
 export enum Target {
-  CONTEXT = 'CONTEXT',
+  GENERAL = 'GENERAL',
   AIS_CONSENT = 'AIS_CONSENT'
 }
