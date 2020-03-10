69bc32d45878937bf2d0d505588465bb73286f27
romanyelizarov <rye@adorsys.com.ua>
2020-03-10 14:50:21 +0200

    refactored RedirectHandlerService

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FinTechImplConfig.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FinTechImplConfig.java
@@ -1,12 +1,7 @@
 package de.adorsys.opba.fintech.impl.config;
 
-import de.adorsys.opba.fintech.impl.database.repositories.RedirectUrlRepository;
-import de.adorsys.opba.fintech.impl.service.AuthorizeService;
-import de.adorsys.opba.fintech.impl.service.RedirectHandlerService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,18 +12,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("de.adorsys.opba.fintech.impl")
 @EntityScan("de.adorsys.opba.fintech.impl.database.entities")
 public class FinTechImplConfig {
-
-    @Value("${fintech-ui.nok-url}")
-    private String notOkUrl;
-
-    @Value("${fintech-ui.nok-url}")
-    private String okUrl;
-
-    @Value("${fintech-ui.nok-url}")
-    private String exceptionUrl;
-
-    @Bean
-    public RedirectHandlerService redirectHandlerService(RedirectUrlRepository redirectUrlRepository, AuthorizeService authorizeService) {
-        return new RedirectHandlerService(notOkUrl, okUrl, exceptionUrl, redirectUrlRepository, authorizeService);
-    }
 }
