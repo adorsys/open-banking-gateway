13123429e0213ff597d89071ee94a3261841003d
electronicpeter <github@electronicpeter.de>
2020-03-10 11:00:56 +0100

    OBG 236

--- fintech-examples/fintech-ui/src/app/common/share.module.ts
+++ fintech-examples/fintech-ui/src/app/common/share.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ListItemComponent } from './list-item/list-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InfoModule } from '../errorsHandler/info/info.module';
 import { RedirectCardComponent } from './redirect-card/redirect-card.component';
 
 @NgModule({
-  declarations: [ListItemComponent, RedirectCardComponent],
+  declarations: [RedirectCardComponent],
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule],
-  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, ListItemComponent, RedirectCardComponent]
+  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, RedirectCardComponent]
 })
 export class ShareModule {}
