9787a91c4ba483400dd0c0d23713073a5ca1199d
Vladimir Nechai <vne@adorsys.de>
2020-03-11 12:15:20 +0100

    password input layout

--- consent-ui/src/app/ais/password-input-page/password-input-page.component.ts
+++ consent-ui/src/app/ais/password-input-page/password-input-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'consent-app-password-input-page',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-input-page.component.scss']
 })
 export class PasswordInputPageComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      pin: ['', Validators.required]
+    });
   }
 
 }
