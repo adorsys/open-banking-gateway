f93ba8f41bd8dadebbf27d68e04bc92fde4a67e0
Vladimir Nechai <vne@adorsys.de>
2020-03-11 15:34:03 +0100

    add updateConsent method to consent-auth.service

--- consent-ui/src/app/ais/password-input-page/password-input-page.component.ts
+++ consent-ui/src/app/ais/password-input-page/password-input-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Consts} from "../../../../../technical-embedded-ui/tpp-ui/src/app/consts";
-import {Helpers} from "../../../../../technical-embedded-ui/tpp-ui/src/app/app.component";
-import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
+import {ConsentAuthorizationService} from "../../api";
+import {Helpers} from "../../../../../technical-embedded-ui/tpp-ui/src/app/app.component";
 
 @Component({
   selector: 'consent-app-password-input-page',
@@ -11,10 +11,14 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./password-input-page.component.scss']
 })
 export class PasswordInputPageComponent implements OnInit {
-  submissionUri: string = Consts.API_V1_URL_BASE + 'consent/';
   passwordForm: FormGroup;
+  private authorizationSessionId: string;
+  private redirectCode: string;
 
-  constructor(private client: HttpClient, private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) { }
+  constructor(private consentAuthorizationService: ConsentAuthorizationService,
+              private activatedRoute: ActivatedRoute,
+              private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     this.passwordForm = this.formBuilder.group({
@@ -23,23 +27,25 @@ export class PasswordInputPageComponent implements OnInit {
 
     this.activatedRoute.queryParams.subscribe(
       params => {
-        this.submissionUri = this.submissionUri + params['authorizationSessionId'] + '/embedded?redirectCode=' + params['redirectCode'];
+        this.authorizationSessionId = params['authorizationSessionId'];
+        this.redirectCode = params['redirectCode'];
       }
     );
   }
 
-
-  submit() {
-    this.client.post(
-      this.submissionUri,
-      {scaAuthenticationData: {PSU_PASSWORD: this.passwordForm.get('pin').value}}, // scaAuthenticationData is not really correct
-      {headers: {
-          'X-Request-ID': Helpers.uuidv4(),
-          'X-XSRF-TOKEN': Helpers.uuidv4(),
-        }}
-    ).subscribe(res => {
-    }, error => {
-      window.location.href = error.url;
-    });
+  submit(): void {
+    this.consentAuthorizationService.embeddedUsingPOST(this.authorizationSessionId,
+      Helpers.uuidv4(),
+      Helpers.uuidv4(),
+      this.redirectCode,
+      {scaAuthenticationData: {PSU_PASSWORD: this.passwordForm.get('pin').value}})
+      .subscribe(
+        res => {
+          // TODO: forward to the next page
+        },
+        error => {
+          window.location.href = error.url;
+        });
   }
+
 }
