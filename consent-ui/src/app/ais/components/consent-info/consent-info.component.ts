059942b22616c47aa61f0e87cca3fe56ce6f1d56
Valentyn Berezin <valb3r.ua@gmail.com>
2020-03-12 21:08:51 +0200

    OBG-374. Added ais access info screen

---
+++ consent-ui/src/app/ais/components/consent-info/consent-info.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {AccountAccessLevel, AisConsentToGrant} from "../../common/dto/ais-consent";
+import {StubUtil} from "../../common/stub-util";
+import {ActivatedRoute, Router} from "@angular/router";
+import {SessionService} from "../../../common/session.service";
+import {ConsentUtil} from "../../common/consent-util";
+
+@Component({
+  selector: 'consent-app-consent-info',
+  templateUrl: './consent-info.component.html',
+  styleUrls: ['./consent-info.component.scss']
+})
+export class ConsentInfoComponent implements OnInit {
+  accountAccessLevel = AccountAccessLevel;
+
+  public finTechName = StubUtil.FINTECH_NAME;
+  public aspspName = StubUtil.ASPSP_NAME;
+
+  private authorizationId: string;
+  private aisConsent: AisConsentToGrant;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private sessionService: SessionService
+  ) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(res => {
+      this.authorizationId = res.authId;
+      this.aisConsent = ConsentUtil.getOrDefault(this.authorizationId, this.sessionService);
+    });
+
+    this.router.events.subscribe(evt => {
+      this.aisConsent = ConsentUtil.getOrDefault(this.authorizationId, this.sessionService);
+    });
+  }
+}
