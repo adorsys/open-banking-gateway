4b179b6deb627b98a6f9ee03846aad5b81478f32
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-03-12 09:57:20 +0100

    OBG-274 tests

--- opba-protocols/xs2a-protocol-tests/xs2a-bdd-wiremock/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/wiremock/mocks/WiremockAccountInformationRequest.java
+++ opba-protocols/xs2a-protocol-tests/xs2a-bdd-wiremock/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/wiremock/mocks/WiremockAccountInformationRequest.java
@@ -9,14 +9,27 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.awaitility.Durations;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.X_XSRF_TOKEN;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.ResourceUtil.readResource;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.AIS_ACCOUNTS_ENDPOINT;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.ANTON_BRUECKNER;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.AUTHORIZE_CONSENT_ENDPOINT;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.withHeadersWithoutIpAddress;
+import static de.adorsys.opba.restapi.shared.HttpHeaders.COMPUTE_PSU_IP_ADDRESS;
+import static de.adorsys.opba.restapi.shared.HttpHeaders.SERVICE_SESSION_ID;
+import static de.adorsys.opba.restapi.shared.HttpHeaders.UserAgentContext.PSU_IP_ADDRESS;
+import static de.adorsys.opba.restapi.shared.HttpHeaders.X_REQUEST_ID;
 import static de.adorsys.xs2a.adapter.service.RequestHeaders.TPP_REDIRECT_URI;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.springframework.http.HttpHeaders.LOCATION;
 
 @JGivenStage
 @SuppressWarnings("checkstyle:MethodName") // Jgiven prettifies snake-case names not camelCase
@@ -39,7 +52,71 @@ public class WiremockAccountInformationRequest<SELF extends WiremockAccountInfor
                     .statusCode(HttpStatus.SEE_OTHER.value())
                     .extract();
 
-        assertThat(response.header(HttpHeaders.LOCATION)).contains("localhost");
+        assertThat(response.header(LOCATION)).contains("localhost");
+
+        return self();
+    }
+
+    public SELF fintech_calls_list_accounts_for_anton_brueckner_ip_address_compute() {
+        ExtractableResponse<Response> response = withHeadersWithoutIpAddress(ANTON_BRUECKNER)
+                .header(SERVICE_SESSION_ID, UUID.randomUUID().toString())
+                .header(COMPUTE_PSU_IP_ADDRESS, true)
+                .when()
+                .get(AIS_ACCOUNTS_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.ACCEPTED.value())
+                .extract();
+        updateServiceSessionId(response);
+        updateRedirectCode(response);
+        updateNextConsentAuthorizationUrl(response);
+        return self();
+    }
+
+    public SELF fintech_calls_list_accounts_for_anton_brueckner_no_ip_address() {
+        ExtractableResponse<Response> response = withHeadersWithoutIpAddress(ANTON_BRUECKNER)
+                .header(SERVICE_SESSION_ID, UUID.randomUUID().toString())
+                .header(COMPUTE_PSU_IP_ADDRESS, true)
+                .when()
+                .get(AIS_ACCOUNTS_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.ACCEPTED.value())
+                .extract();
+        updateServiceSessionId(response);
+        updateRedirectCode(response);
+        updateNextConsentAuthorizationUrl(response);
+        return self();
+    }
+
+    public SELF user_anton_brueckner_provided_initial_parameters_to_list_accounts_with_all_accounts_consent_with_ip_address_check() {
+        String body = readResource("restrecord/tpp-ui-input/params/anton-brueckner-account-all-accounts-consent.json");
+
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .header(X_XSRF_TOKEN, UUID.randomUUID().toString())
+                .header(X_REQUEST_ID, UUID.randomUUID().toString())
+                .queryParam(REDIRECT_CODE_QUERY, redirectCode)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when()
+                .post(AUTHORIZE_CONSENT_ENDPOINT, serviceSessionId)
+                .then()
+                .statusCode(HttpStatus.SEE_OTHER.value())
+                .extract();
+
+        LoggedRequest loggedRequest = await().atMost(Durations.TEN_SECONDS)
+                .until(() ->
+                        wireMock.findAll(postRequestedFor(urlMatching("/v1/consents.*"))), it -> !it.isEmpty()
+                ).get(0);
+
+        assertThat(loggedRequest.getHeader(PSU_IP_ADDRESS)).isNotEmpty();
+
+        this.responseContent = response.body().asString();
+        this.redirectUriToGetUserParams = response.header(LOCATION);
+        updateRedirectCode(response);
+
+
+        updateServiceSessionId(response);
+        updateRedirectCode(response);
 
         return self();
     }
