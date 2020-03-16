3b17cfd4f8a832a381834f42dc8086c9caf7251f
Valentyn Berezin <valb3r.ua@gmail.com>
2020-03-16 11:26:48 +0200

    OBG-375. Use generated API

--- consent-ui/src/environments/environment.ts
+++ consent-ui/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_BASE_PATH: 'http://localhost:4200/embedded-server/'
 };
 
 /*
