5f5390569146d5c2d3b87b67ed50d3fb3208d352
electronicpeter <github@electronicpeter.de>
2020-03-16 17:19:14 +0100

    OBG 373 merge sidebar duplicat raus

--- fintech-examples/fintech-ui/src/app/bank/bank.module.ts
+++ fintech-examples/fintech-ui/src/app/bank/bank.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { BankComponent } from './bank.component';
 import { BankRoutingModule } from './bank-routing.module';
 import { ShareModule } from '../common/share.module';
 import { CommonModule } from '@angular/common';
 import { ListAccountsModule } from './list-accounts/list-accounts.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [SidebarComponent, BankComponent, SidebarComponent],
+  declarations: [SidebarComponent, BankComponent],
   imports: [CommonModule, ShareModule, BankRoutingModule, ListAccountsModule]
 })
 export class BankModule {}
