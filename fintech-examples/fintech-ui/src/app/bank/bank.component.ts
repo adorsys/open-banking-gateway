966262ab3c2924140da0f50bf2d8722cb10be883
Hergy Tchuinkou <htc@adorsys.de>
2020-03-17 12:22:18 +0100

    commit

--- fintech-examples/fintech-ui/src/app/bank/bank.component.ts
+++ fintech-examples/fintech-ui/src/app/bank/bank.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bank.component.scss']
 })
 export class BankComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  backToPreviousPage() {
+    this.router.navigate(['/search']);
+  }
 }
