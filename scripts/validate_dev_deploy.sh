569bf231391f1e56e0fcbc35cb5c1f1ce7afbed8
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-18 19:18:39 +0200

    OBG-408. Added scheduled run after deploy

---
+++ scripts/validate_dev_deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_REPO_SLUG != "adorsys/open-banking-gateway"
+    || $TRAVIS_PULL_REQUEST != "false"
+    || -z "$TRAVIS_COMMIT" ]];
+then
+  echo "ERROR: Deployment validation not allowed"
+  exit 1
+fi
+
+COMMIT="$TRAVIS_COMMIT"
+OPBA_URL=https://obg-dev-openbankinggateway.cloud.adorsys.de
+
+# Wait for deployment to happen
+timeout 10s bash -c 'while [[ $(curl -s "'"$OPBA_URL"'"/actuator/info | grep -c '"$COMMIT"') != 1 ]]; do sleep 10; done' || echo "Failed waiting for deploy $COMMIT"; exit 1
+
+# Run smoke tests:
+export ENABLE_SMOKE_TESTS=true
+./mvnw test -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.SmokeE2ETest
