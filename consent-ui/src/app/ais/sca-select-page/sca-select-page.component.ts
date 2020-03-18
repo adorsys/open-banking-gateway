d217593c531c7e1e9f39e0a77b30ed5b11098b67
Collins Kamgaing <cka@adorsys.de>
2020-03-18 16:47:47 +0100

    reformat code

--- consent-ui/src/app/ais/sca-select-page/sca-select-page.component.ts
+++ consent-ui/src/app/ais/sca-select-page/sca-select-page.component.ts
@@ -1,71 +1,77 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {ConsentAuthorizationService, ScaUserData} from "../../api";
-import {SessionService} from "../../common/session.service";
-import {ApiHeaders} from "../../api/api.headers";
-import {StubUtil} from "../common/stub-util";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConsentAuthorizationService, ScaUserData } from '../../api';
+import { SessionService } from '../../common/session.service';
+import { ApiHeaders } from '../../api/api.headers';
+import { StubUtil } from '../common/stub-util';
 
 @Component({
-    selector: 'consent-app-sca-select-page',
-    templateUrl: './sca-select-page.component.html',
-    styleUrls: ['./sca-select-page.component.scss']
+  selector: 'consent-app-sca-select-page',
+  templateUrl: './sca-select-page.component.html',
+  styleUrls: ['./sca-select-page.component.scss']
 })
 export class ScaSelectPageComponent implements OnInit {
+  authorizationSessionId = '';
+  redirectCode = '';
+  scaMethodForm: FormGroup;
+  scaMethods: ScaUserData[] = [];
 
-    authorizationSessionId: string = '';
-    redirectCode: string = '';
-    scaMethodForm: FormGroup;
-    scaMethods: ScaUserData[];
+  constructor(
+    private sessionService: SessionService,
+    private consentAuthorizationService: ConsentAuthorizationService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
-    constructor(private sessionService: SessionService,
-                private consentAuthorizationService: ConsentAuthorizationService,
-                private route: ActivatedRoute,
-                private formBuilder: FormBuilder) {
-    }
+  ngOnInit() {
+    this.initialScaMethodForm();
+    this.authorizationSessionId = this.route.parent.snapshot.paramMap.get('authId');
+    this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
+    this.loadAvailableMethods();
+  }
 
-    ngOnInit() {
-        this.initialScaMethodForm();
-        this.authorizationSessionId = this.route.parent.snapshot.paramMap.get("authId");
+  onSubmit(): void {
+    this.consentAuthorizationService
+      .embeddedUsingPOST(
+        this.authorizationSessionId,
+        StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
+        StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
+        this.redirectCode,
+        { scaAuthenticationData: { SCA_CHALLENGE_ID: this.scaMethodForm.get('selectedMethodValue').value } },
+        'response'
+      )
+      .subscribe(
+        res => {
+          // redirect to the provided location
+          console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
+          this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
+          window.location.href = res.headers.get(ApiHeaders.LOCATION);
+        },
+        error => {
+          console.log(error);
+          // window.location.href = error.url;
+        }
+      );
+  }
+
+  private loadAvailableMethods(): void {
+    this.consentAuthorizationService
+      .authUsingGET(this.authorizationSessionId, this.redirectCode, 'response')
+      .subscribe(consentAuth => {
+        this.sessionService.setRedirectCode(
+          this.authorizationSessionId,
+          consentAuth.headers.get(ApiHeaders.REDIRECT_CODE)
+        );
         this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
-        this.loadAvailableMethods();
-    }
+        this.scaMethods = consentAuth.body.consentAuth.scaMethods;
+        this.scaMethodForm.get('selectedMethodValue').setValue(this.scaMethods[0].id);
+      });
+  }
 
-    onSubmit(): void {
-        this.consentAuthorizationService.embeddedUsingPOST(
-            this.authorizationSessionId,
-            StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
-            StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
-            this.redirectCode,
-            {scaAuthenticationData: {SCA_CHALLENGE_ID: this.scaMethodForm.get('selectedMethodValue').value}},
-            "response"
-        ).subscribe(
-            res => {
-                // redirect to the provided location
-                console.log("REDIRECTING TO: " + res.headers.get(ApiHeaders.LOCATION));
-                this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
-                window.location.href = res.headers.get(ApiHeaders.LOCATION);
-            },
-            error => {
-                console.log(error);
-                // window.location.href = error.url;
-            });
-
-    }
-
-    private loadAvailableMethods(): void {
-        this.consentAuthorizationService.authUsingGET(this.authorizationSessionId, this.redirectCode, 'response')
-            .subscribe(consentAuth => {
-                this.sessionService.setRedirectCode(this.authorizationSessionId, consentAuth.headers.get(ApiHeaders.REDIRECT_CODE));
-                this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
-                this.scaMethods = consentAuth.body.consentAuth.scaMethods;
-                this.scaMethodForm.get('selectedMethodValue').setValue(this.scaMethods[0].id);
-            });
-    }
-
-    private initialScaMethodForm(): void {
-        this.scaMethodForm = this.formBuilder.group({
-            selectedMethodValue: [new FormControl(), Validators.required],
-        });
-    }
+  private initialScaMethodForm(): void {
+    this.scaMethodForm = this.formBuilder.group({
+      selectedMethodValue: [new FormControl(), Validators.required]
+    });
+  }
 }
