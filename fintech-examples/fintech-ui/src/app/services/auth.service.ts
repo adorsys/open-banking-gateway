55f942d9ab37ed6181c7d8447c2f0ba3d01bfc57
electronicpeter <github@electronicpeter.de>
2020-03-19 11:20:30 +0100

    OBG-426 proprietary cookie service

--- fintech-examples/fintech-ui/src/app/services/auth.service.ts
+++ fintech-examples/fintech-ui/src/app/services/auth.service.ts
@@ -6,17 +6,23 @@ import { FinTechAuthorizationService } from '../api';
 import { Credentials } from '../models/credentials.model';
 import { Consts } from '../common/consts';
 import * as uuid from 'uuid';
+import { DocumentCookieService } from './document-cookie.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private router: Router, private finTechAuthorizationService: FinTechAuthorizationService) {}
+  constructor(
+    private router: Router,
+    private finTechAuthorizationService: FinTechAuthorizationService,
+    private cookieService: DocumentCookieService
+  ) {}
 
   login(credentials: Credentials): Observable<boolean> {
     return this.finTechAuthorizationService.loginPOST(uuid.v4(), credentials, 'response').pipe(
       map(response => {
-        console.log('cookies:' + document.cookie);
+        this.cookieService.getAll().forEach(cookie => console.log('cookie after login :' + cookie));
+        localStorage.setItem(Consts.USERNAME, credentials.username);
         return response.ok;
       })
     );
@@ -24,8 +30,8 @@ export class AuthService {
 
   logout(): void {
     localStorage.clear();
-    document.cookie = Consts.XSRF_TOKEN + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = Consts.SESSION_COOKIE + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    this.cookieService.delete(Consts.XSRF_TOKEN);
+    this.cookieService.delete(Consts.SESSION_COOKIE);
     this.openLoginPage();
   }
 
@@ -34,16 +40,10 @@ export class AuthService {
   }
 
   isLoggedIn(): boolean {
-    return document.cookie.split(';').some(item => item.trim().startsWith(Consts.XSRF_TOKEN + '='));
+    return this.cookieService.exists(Consts.XSRF_TOKEN);
   }
 
   getX_XSRF_TOKEN(): string {
-    for (const cookie of document.cookie.split(';')) {
-      if (cookie.trim().startsWith(Consts.XSRF_TOKEN + '=')) {
-        return cookie.trim().substr(Consts.XSRF_TOKEN.length + 1);
-      }
-    }
-    // throw "did not find Cookie for " + Consts.XSRF_TOKEN;
-    console.error('did not find Cookie for ' + Consts.XSRF_TOKEN);
+    return this.cookieService.find(Consts.XSRF_TOKEN);
   }
 }
