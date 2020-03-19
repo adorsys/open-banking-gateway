7de2b07fc049b56fa07383cd52c5de6063d53de1
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-19 12:55:30 +0200

    OBG-428. Clearer messages

--- scripts/validate_dev_deploy.sh
+++ scripts/validate_dev_deploy.sh
@@ -13,9 +13,14 @@ fi
 COMMIT="$TRAVIS_COMMIT"
 OPBA_URL=https://obg-dev-openbankinggateway.cloud.adorsys.de
 
+echo "Waiting for deployment"
 # Wait for deployment to happen
 timeout 600s bash -c 'while [[ $(curl -s "'"$OPBA_URL"'"/actuator/info | grep -c '"$COMMIT"') != 1 ]]; do sleep 10; done' || echo "Failed waiting for deploy $COMMIT"; exit 1
 
+echo "Run smoke tests"
 # Run smoke tests:
 export ENABLE_SMOKE_TESTS=true
+echo "Run API smoke tests"
 ./mvnw test -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.OpbaApiSmokeE2ETest
+echo "Run Consent UI and API smoke tests"
+./mvnw test -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.OpbaApiWithConsentUiSmokeE2ETest
