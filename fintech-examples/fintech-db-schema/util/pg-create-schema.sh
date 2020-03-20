57d313023880c2cc7a899a4fdc6f2705fa6f2b49
romanyelizarov <rye@adorsys.com.ua>
2020-03-20 16:23:42 +0200

    fixed liquibase migrations

--- fintech-examples/fintech-db-schema/util/pg-create-schema.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# create fintech schema and give permissions to postgres user. Needed for
-# docker-compose so  that we can start the DB with the schema already being
-# present (rat)
-set -e
-echo "Create schema='fintech' for local postgres installation"
-psql -U postgres -d fintech -c 'CREATE SCHEMA IF NOT EXISTS fintech AUTHORIZATION postgres;'
