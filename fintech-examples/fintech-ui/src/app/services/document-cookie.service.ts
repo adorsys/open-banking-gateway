a1d7721f4f44d0802251de4a76e9113a089ac4c1
Hergy Tchuinkou <htc@adorsys.de>
2020-03-23 09:35:57 +0100

    resolved conflict

---
+++ fintech-examples/fintech-ui/src/app/services/document-cookie.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { LogException } from '../common/LogException';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentCookieService {
+  constructor() {}
+
+  public getAll(): string[] {
+    return document.cookie.split(';').map(cookie => cookie.trim());
+  }
+
+  public find(name: string): string {
+    for (const cookie of this.getAll()) {
+      if (cookie.startsWith(name + '=')) {
+        const value = cookie.trim().substr(name.length + 1);
+        if (value.length > 0) {
+          // console.log('found cookie ' + name);
+          return value;
+        }
+      }
+    }
+    // console.log('did not find cookie ' + name);
+  }
+
+  public delete(name: string): void {
+    // console.log('delete cookie ' + name);
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    this.find(name); // this call is important. due to date cookie will be deleted
+  }
+
+  public exists(name: string): boolean {
+    return this.find(name) !== undefined;
+  }
+
+  public get(name: string): string {
+    if (!this.exists(name)) {
+      throw new LogException('DID NOT FIND COOKIE FOR ' + name);
+    }
+    return this.find(name);
+  }
+}
