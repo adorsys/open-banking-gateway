864b0d521eeefffbed3f0454f3aa8c1d6a2cb122
Vladimir Nechai <vne@adorsys.de>
2020-03-23 17:22:41 +0100

    add snapshot to ActivatedRoute mock

--- consent-ui/karma.conf.js
+++ consent-ui/karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function(config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      random: false,
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
