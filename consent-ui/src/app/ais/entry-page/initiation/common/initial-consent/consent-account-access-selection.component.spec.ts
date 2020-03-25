9e8396aab204682e996275fa150fc42fb488341a
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-25 19:55:25 +0200

    OBG-452. Fix test

--- consent-ui/src/app/ais/entry-page/initiation/common/initial-consent/consent-account-access-selection.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/common/initial-consent/consent-account-access-selection.component.spec.ts
@@ -1,17 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { Access, ConsentAccountAccessSelectionComponent } from './consent-account-access-selection.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
-import { SessionService } from '../../../../../common/session.service';
-import { AccountAccessLevel } from '../../../../common/dto/ais-consent';
-import { AuthConsentState } from '../../../../common/dto/auth-state';
-import { AccountsConsentReviewComponent } from '../../accounts/accounts-consent-review/accounts-consent-review.component';
-import { DedicatedAccessComponent } from '../dedicated-access/dedicated-access.component';
-import { StubUtilTests } from '../../../../common/stub-util-tests';
+import {Access, ConsentAccountAccessSelectionComponent} from './consent-account-access-selection.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ActivatedRoute} from '@angular/router';
+import {of} from 'rxjs';
+import {SessionService} from '../../../../../common/session.service';
+import {AccountAccessLevel, AisConsentToGrant} from '../../../../common/dto/ais-consent';
+import {AuthConsentState} from '../../../../common/dto/auth-state';
+import {AccountsConsentReviewComponent} from '../../accounts/accounts-consent-review/accounts-consent-review.component';
+import {DedicatedAccessComponent} from '../dedicated-access/dedicated-access.component';
+import {StubUtilTests} from '../../../../common/stub-util-tests';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ConsentAccountAccessSelectionComponent', () => {
   let component: ConsentAccountAccessSelectionComponent;
@@ -20,7 +21,7 @@ describe('ConsentAccountAccessSelectionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ConsentAccountAccessSelectionComponent],
-      imports: [ReactiveFormsModule, RouterTestingModule],
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
       providers: [
         {
           provide: ActivatedRoute,
@@ -32,6 +33,7 @@ describe('ConsentAccountAccessSelectionComponent', () => {
         {
           provide: SessionService,
           useValue: {
+            getConsentObject: () => new AisConsentToGrant(),
             hasAisViolation: () => false,
             hasGeneralViolation: () => false,
             getConsentState: () => new AuthConsentState([])
