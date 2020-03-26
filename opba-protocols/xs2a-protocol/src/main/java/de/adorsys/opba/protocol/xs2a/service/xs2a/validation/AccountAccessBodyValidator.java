7c5a32c554624d194470b4b7f096e349256cd96c
iiuex <iiu@adorsys.com.ua>
2020-03-26 10:04:26 +0200

    OBG-297: Validator fully implemented, unit tests added

---
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/validation/AccountAccessBodyValidator.java
@@ -0,0 +1,59 @@
+package de.adorsys.opba.protocol.xs2a.service.xs2a.validation;
+
+import de.adorsys.opba.protocol.xs2a.service.xs2a.annotations.ValidAisConsent;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.consent.AisConsentInitiateBody;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class AccountAccessBodyValidator implements ConstraintValidator<ValidAisConsent, AisConsentInitiateBody.AccountAccessBody> {
+
+    @Override
+    public void initialize(ValidAisConsent constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(AisConsentInitiateBody.AccountAccessBody body, ConstraintValidatorContext context) {
+        if (body == null) {
+            return true;
+        }
+
+        return isValidAisConsentStructure(body);
+    }
+
+    private boolean isValidAisConsentStructure(AisConsentInitiateBody.AccountAccessBody body) {
+        return isValidDedicatedConsent(body) || isValidGlobalConsent(body) || isValidBankOfferedConsent(body);
+    }
+
+    private boolean isValidDedicatedConsent(AisConsentInitiateBody.AccountAccessBody body) {
+        boolean validDedicatedWithAccounts = !isEmptyAccountInfo(body)
+                                                     && StringUtils.isEmpty(body.getAllPsd2())
+                                                     && StringUtils.isEmpty(body.getAvailableAccounts());
+
+        boolean validDedicatedWithoutAccounts = isEmptyAccountInfo(body)
+                                                        && StringUtils.isEmpty(body.getAllPsd2())
+                                                        && "allAccounts".equals(body.getAvailableAccounts());
+
+        return validDedicatedWithAccounts || validDedicatedWithoutAccounts;
+    }
+
+    private boolean isValidGlobalConsent(AisConsentInitiateBody.AccountAccessBody body) {
+        return isEmptyAccountInfo(body)
+                       && "allAccounts".equals(body.getAllPsd2())
+                       && StringUtils.isEmpty(body.getAvailableAccounts());
+    }
+
+    private boolean isValidBankOfferedConsent(AisConsentInitiateBody.AccountAccessBody body) {
+        return isEmptyAccountInfo(body)
+                       && StringUtils.isEmpty(body.getAllPsd2())
+                       && StringUtils.isEmpty(body.getAvailableAccounts());
+    }
+
+    private boolean isEmptyAccountInfo(AisConsentInitiateBody.AccountAccessBody body) {
+        return CollectionUtils.isEmpty(body.getAccounts())
+                       && CollectionUtils.isEmpty(body.getBalances())
+                       && CollectionUtils.isEmpty(body.getTransactions());
+    }
+}
