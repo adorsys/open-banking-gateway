7fdf32a0d91092b97fa731761787d03b0c23afbd
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-26 15:52:00 +0200

    OBG-475. Hide PSU ip address (make Consent API to compute it)

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/EnableFinTechImplConfig.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/EnableFinTechImplConfig.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@Import(FinTechImplConfig.class)
+@Import({FinTechImplConfig.class, FeignConfig.class})
 public @interface EnableFinTechImplConfig {
 }
 
