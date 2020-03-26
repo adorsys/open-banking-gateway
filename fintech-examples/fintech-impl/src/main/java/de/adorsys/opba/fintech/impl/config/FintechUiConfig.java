5f8efd904eb219d06d2bbe335a886352f61c4ae5
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-26 19:12:23 +0200

    OBG-475. Fixed test

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FintechUiConfig.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/FintechUiConfig.java
@@ -1,11 +1,15 @@
 package de.adorsys.opba.fintech.impl.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
+@NoArgsConstructor
+@AllArgsConstructor
 @ConfigurationProperties("fintech-ui")
 public class FintechUiConfig {
 
