ea5f15f3017fd444c5474d2eae87361105b42374
iiuex <iiu@adorsys.com.ua>
2020-03-26 10:04:26 +0200

    OBG-297: Validator fully implemented, unit tests added

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/annotations/ValidAisConsent.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/annotations/ValidAisConsent.java
@@ -1,6 +1,6 @@
 package de.adorsys.opba.protocol.xs2a.service.xs2a.annotations;
 
-import de.adorsys.opba.protocol.xs2a.service.xs2a.validation.AisConsentValidator;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.validation.AccountAccessBodyValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
@@ -8,16 +8,17 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE, TYPE_USE})
+@Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
-@Constraint(validatedBy = AisConsentValidator.class)
+@Constraint(validatedBy = AccountAccessBodyValidator.class)
 @Documented
 public @interface ValidAisConsent {
 
-    String message() default "{hibernate-validation.invalid-consent-mesage";
+    String message() default "{hibernate-validation.invalid-consent-message";
 
     Class<?>[] groups() default {};
 
