8eafe0944fd6cf0e8edba7513471b6eede01cf1d
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-27 09:12:51 +0200

    OBG-475. Fix tests and make checkstyle happy

--- fintech-examples/fintech-server/src/test/java/de/adorsys/opba/fintech/server/FinTechListAccountsTest.java
+++ fintech-examples/fintech-server/src/test/java/de/adorsys/opba/fintech/server/FinTechListAccountsTest.java
@@ -1,6 +1,8 @@
 package de.adorsys.opba.fintech.server;
 
 import de.adorsys.opba.fintech.impl.config.EnableFinTechImplConfig;
+import de.adorsys.opba.fintech.impl.database.entities.SessionEntity;
+import de.adorsys.opba.fintech.impl.database.repositories.UserRepository;
 import de.adorsys.opba.fintech.server.config.TestConfig;
 import de.adorsys.opba.fintech.server.feignmocks.TppAisClientFeignMock;
 import de.adorsys.opba.tpp.ais.api.model.generated.AccountList;
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,7 +25,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.*;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.AUTHORIZATION_SESSION_ID;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.PSU_CONSENT_SESSION;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.REDIRECT_CODE;
+import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.SERVICE_SESSION_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -46,10 +52,14 @@ public class FinTechListAccountsTest extends FinTechBankSearchApiTest {
     @MockBean
     protected TppBankingApiAccountInformationServiceAisApi mockedTppBankingApiAccountInformationServiceAisApi;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     @SneakyThrows
     public void testListAccountsFor200() {
         BankProfileTestResult result = getBankProfileTestResult();
+        setServiceSessionId(result, UUID.randomUUID());
         List<String> accountIDs = listAccountsForOk(result);
         assertTrue(accountIDs.containsAll(Arrays.asList(new String[]{"12345", "67890"})));
     }
@@ -74,7 +84,7 @@ public class FinTechListAccountsTest extends FinTechBankSearchApiTest {
     @Test
     @SneakyThrows
     public void testListAccountsFor303() {
-        ResponseEntity<AccountList> accepted = ResponseEntity.accepted()
+        ResponseEntity accepted = ResponseEntity.accepted()
                 .header(AUTHORIZATION_SESSION_ID, "1")
                 .header(REDIRECT_CODE, "redirectCode")
                 .header(PSU_CONSENT_SESSION, "2")
@@ -82,8 +92,9 @@ public class FinTechListAccountsTest extends FinTechBankSearchApiTest {
                 .location(new URI("affe"))
                 .build();
         BankProfileTestResult result = getBankProfileTestResult();
-        when(tppAisClientFeignMock.getAccounts(any(), any(), any(), any(), any(), any(), any(), any(), any()))
-                .thenReturn(accepted);
+        setServiceSessionId(result, null);
+        when(tppAisClientFeignMock.getTransactions(any(), any(), any(), any(), any(), any(), any(), any(), any(), any(),
+                any(), any(), any(), any(), any())).thenReturn(accepted);
 
         MvcResult mvcResult = plainListAccounts(result.getXsrfToken(), result.getBankUUID());
         assertEquals(ACCEPTED.value(), mvcResult.getResponse().getStatus());
@@ -124,4 +135,9 @@ public class FinTechListAccountsTest extends FinTechBankSearchApiTest {
     public void loginPostUnAuthorized() {
     }
 
+    protected void setServiceSessionId(BankProfileTestResult result, UUID serviceSessionId) {
+        SessionEntity session = userRepository.findByXsrfToken(result.getXsrfToken()).get();
+        session.setServiceSessionId(serviceSessionId);
+        userRepository.save(session);
+    }
 }
