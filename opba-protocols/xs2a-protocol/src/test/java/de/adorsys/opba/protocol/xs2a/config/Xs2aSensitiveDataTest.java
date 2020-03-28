0f65eb88583795acebca215e3c6fd67f29d1ed76
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-28 14:14:06 +0200

    Add tests that validate sensitive data is not serialized

---
+++ opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/config/Xs2aSensitiveDataTest.java
@@ -0,0 +1,86 @@
+package de.adorsys.opba.protocol.xs2a.config;
+
+import de.adorsys.opba.protocol.xs2a.config.flowable.FlowableConfig;
+import de.adorsys.opba.protocol.xs2a.config.flowable.Xs2aFlowableProperties;
+import de.adorsys.opba.protocol.xs2a.config.flowable.Xs2aObjectMapper;
+import de.adorsys.opba.protocol.xs2a.service.storage.TransientDataStorage;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.context.Xs2aContext;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+
+/**
+ * Validates only module configuration.
+ */
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@SpringBootTest(classes = {FlowableConfig.class, Xs2aSensitiveDataTest.TestConfig.class}, webEnvironment = NONE)
+public class Xs2aSensitiveDataTest {
+
+    @Autowired
+    private Xs2aObjectMapper mapper;
+
+    @Autowired
+    private TransientDataStorage dataStorage;
+
+    @Test
+    @SneakyThrows
+    void testPsuPasswordNotSerialized() {
+        Xs2aContext context = new Xs2aContext();
+        context.setTransientStorage(dataStorage);
+        context.setSagaId("12345");
+        context.setPsuPassword("PASSWORD");
+
+        assertThat(mapper.writeValueAsString(context))
+                .isEqualTo("{\"flowByAction\":"
+                        + "{\"LIST_ACCOUNTS\":\"xs2a-list-accounts\",\"LIST_TRANSACTIONS\":\"xs2a-list-transactions\"},"
+                        + "\"sagaId\":\"12345\","
+                        + "\"violations\":[],"
+                        + "\"contentType\":\"application/json\","
+                        + "\"redirectConsentOk\":false}"
+                );
+    }
+
+    @Test
+    @SneakyThrows
+    void testLastScaChallengeNotSerialized() {
+        Xs2aContext context = new Xs2aContext();
+        context.setTransientStorage(dataStorage);
+        context.setSagaId("123456");
+        context.setLastScaChallenge("Challenge!");
+
+        assertThat(mapper.writeValueAsString(context))
+                .isEqualTo("{\"flowByAction\":"
+                        + "{\"LIST_ACCOUNTS\":\"xs2a-list-accounts\",\"LIST_TRANSACTIONS\":\"xs2a-list-transactions\"},"
+                        + "\"sagaId\":\"123456\","
+                        + "\"violations\":[],"
+                        + "\"contentType\":\"application/json\","
+                        + "\"redirectConsentOk\":false}"
+                );
+    }
+
+    @Configuration
+    public static class TestConfig {
+
+        @Bean
+        TransientDataStorage dataStorage() {
+            return new TransientDataStorage(new HashMap<>());
+        }
+
+        @Bean
+        Xs2aFlowableProperties flowableProperties() {
+            return new Xs2aFlowableProperties();
+        }
+    }
+}
+
+
