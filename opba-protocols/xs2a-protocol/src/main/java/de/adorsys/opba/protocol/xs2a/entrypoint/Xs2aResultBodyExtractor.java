f094f4e104f9be9c5650294682328c74ed84fa07
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-28 12:32:48 +0200

    Sensitive data is not persisted anymore

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/Xs2aResultBodyExtractor.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/Xs2aResultBodyExtractor.java
@@ -2,42 +2,28 @@ package de.adorsys.opba.protocol.xs2a.entrypoint;
 
 import de.adorsys.opba.protocol.api.dto.result.body.AccountListBody;
 import de.adorsys.opba.protocol.api.dto.result.body.TransactionsResponseBody;
-import de.adorsys.opba.protocol.xs2a.domain.dto.messages.InternalProcessResult;
+import de.adorsys.opba.protocol.xs2a.domain.dto.messages.ProcessResponse;
 import de.adorsys.xs2a.adapter.service.model.AccountListHolder;
 import de.adorsys.xs2a.adapter.service.model.TransactionsReport;
 import lombok.RequiredArgsConstructor;
-import org.flowable.engine.RuntimeService;
-import org.flowable.engine.impl.persistence.entity.ExecutionEntity;
-import org.flowable.engine.runtime.ProcessInstance;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Service;
 
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.SPRING_KEYWORD;
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.XS2A_MAPPERS_PACKAGE;
-import static de.adorsys.opba.protocol.xs2a.service.ContextUtil.getResult;
 
 @Service
 @RequiredArgsConstructor
 public class Xs2aResultBodyExtractor {
 
-    private final RuntimeService runtimeService;
     private final Xs2aToFacadeMapper mapper;
 
-    public AccountListBody extractAccountList(InternalProcessResult result) {
-        ProcessInstance updated =
-                runtimeService.createProcessInstanceQuery()
-                        .processInstanceId(result.getProcessId())
-                        .singleResult();
-        ExecutionEntity exec = (ExecutionEntity) updated;
-
-        return mapper.map(getResult(exec, AccountListHolder.class));
+    public AccountListBody extractAccountList(ProcessResponse result) {
+        return mapper.map((AccountListHolder) result.getResult());
     }
 
-    public TransactionsResponseBody extractTransactionsReport(InternalProcessResult result) {
-        ProcessInstance updated = runtimeService.createProcessInstanceQuery()
-                .processInstanceId(result.getProcessId()).singleResult();
-        ExecutionEntity exec = (ExecutionEntity) updated;
-        return mapper.map(getResult(exec, TransactionsReport.class));
+    public TransactionsResponseBody extractTransactionsReport(ProcessResponse result) {
+        return mapper.map((TransactionsReport) result.getResult());
     }
 
     @Mapper(componentModel = SPRING_KEYWORD, implementationPackage = XS2A_MAPPERS_PACKAGE)
