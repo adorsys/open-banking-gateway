a5eb90daa7f5e1d55d0de7ca9990b56e4f194f16
Valentyn Berezin <valb3r.ua@gmail.com>
2020-03-30 13:54:54 +0300

    Revert "Fixed tests"
    This reverts commit e188ac792a6f095baf93e3d222eab29c2be14385.

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/context/Xs2aContext.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/context/Xs2aContext.java
@@ -47,25 +47,21 @@ public class Xs2aContext extends BaseContext implements NeedsTransientStorage {
 
     private boolean redirectConsentOk;
 
-    @JsonIgnore
     public String getPsuPassword() {
-        TransientDataStorage.DataEntry entry = this.transientStorage.get(this);
-        return null != entry ? this.transientStorage.get(this).getPsuPassword() : null;
+        TransientDataStorage.DataEntry entry = transientStorage.get(this);
+        return null != entry ? transientStorage.get(this).getPsuPassword() : null;
     }
 
-    @JsonIgnore
     public String getLastScaChallenge() {
-        TransientDataStorage.DataEntry entry = this.transientStorage.get(this);
-        return null != entry ? this.transientStorage.get(this).getScaChallengeResult() : null;
+        TransientDataStorage.DataEntry entry = transientStorage.get(this);
+        return null != entry ? transientStorage.get(this).getScaChallengeResult() : null;
     }
 
-    @JsonIgnore
     public void setPsuPassword(String psuPassword) {
-        this.transientStorage.set(this, new TransientDataStorage.DataEntry(psuPassword, null));
+        transientStorage.set(this, new TransientDataStorage.DataEntry(psuPassword, null));
     }
 
-    @JsonIgnore
     public void setLastScaChallenge(String scaChallengeResult) {
-        this.transientStorage.set(this, new TransientDataStorage.DataEntry(null, scaChallengeResult));
+        transientStorage.set(this, new TransientDataStorage.DataEntry(null, scaChallengeResult));
     }
 }
