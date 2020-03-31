1c58d4d99b462c4ab89cec1a1a579159f677681e
valb3r <valentyn.berezin@adorsys.com.ua>
2020-03-31 20:43:19 +0300

    OBG-501. Fix ui tests

--- consent-ui/src/app/ais/sca-result-page/sca-result-page.component.spec.ts
+++ consent-ui/src/app/ais/sca-result-page/sca-result-page.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ReportScaResultComponent } from './sca-result-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
-import { StubUtilTests } from '../common/stub-util-tests';
+import {ReportScaResultComponent} from './sca-result-page.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
+import {StubUtilTests} from '../common/stub-util-tests';
 
 describe('ReportScaResultComponent', () => {
   let component: ReportScaResultComponent;
@@ -18,7 +18,7 @@ describe('ReportScaResultComponent', () => {
       providers: [
         {
           provide: ActivatedRoute,
-          useValue: { parent: { snapshot: { paramMap: convertToParamMap({ authId: StubUtilTests.AUTH_ID }) } } }
+          useValue: { snapshot: {queryParamMap: convertToParamMap({})}, parent: { snapshot: { paramMap: convertToParamMap({ authId: StubUtilTests.AUTH_ID }) } } }
         }
       ]
     }).compileComponents();
