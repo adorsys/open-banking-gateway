b554fa0af931d4e1c787e19a1a0056dc97c2ba93
Hergy Tchuinkou <htc@adorsys.de>
2020-04-01 16:40:07 +0200

    smoke test setup for fintech

---
+++ fintech-examples/fintech-example-tests/src/main/java/de/adorsys/fintech/tests/e2e/stages/UserInformationRequest.java
@@ -0,0 +1,10 @@
+package de.adorsys.fintech.tests.e2e.stages;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@JGivenStage
+public class UserInformationRequest<SELF extends UserInformationRequest<SELF>> extends Stage <SELF>{
+}
