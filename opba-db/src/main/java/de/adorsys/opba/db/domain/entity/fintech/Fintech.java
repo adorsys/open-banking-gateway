9ae4099b21bf1de26cd28ffacce9d35867f70794
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-01 12:55:56 +0300

    OBG-509. Cleaned up initial schema

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/fintech/Fintech.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/fintech/Fintech.java
@@ -6,14 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
 
 @Getter
@@ -28,9 +24,4 @@ public class Fintech {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fintech_id_generator")
     @SequenceGenerator(name = "fintech_id_generator", sequenceName = "fintech_id_sequence")
     private Long id;
-
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
-    @Column(nullable = false)
-    private byte[] data;
 }
