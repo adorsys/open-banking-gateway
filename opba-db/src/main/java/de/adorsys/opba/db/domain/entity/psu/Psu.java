6552b01535193c3bb536a6ef3dce5a9363bf7699
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-01 11:56:30 +0300

    OBG-509. Initial tables and schema

---
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/psu/Psu.java
@@ -0,0 +1,36 @@
+package de.adorsys.opba.db.domain.entity.psu;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Psu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "psu_id_generator")
+    @SequenceGenerator(name = "psu_id_generator", sequenceName = "psu_id_sequence")
+    private Long id;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private byte[] data;
+}
