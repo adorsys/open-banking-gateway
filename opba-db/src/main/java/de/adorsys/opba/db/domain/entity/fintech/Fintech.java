306baf354cc94f1e83c21cf0fd46d1db95affd4b
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-01 19:51:09 +0300

    OBG-509. Initial working datasafe integration for Fintech

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/fintech/Fintech.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/fintech/Fintech.java
@@ -6,11 +6,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
+import java.util.Collection;
 
 @Getter
 @Setter
@@ -24,4 +30,18 @@ public class Fintech {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fintech_id_generator")
     @SequenceGenerator(name = "fintech_id_generator", sequenceName = "fintech_id_sequence")
     private Long id;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] keystore;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] pubKeys;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "fintech")
+    private Collection<FintechInbox> inbox;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "fintech")
+    private Collection<FintechPrivate> privateStore;
 }
