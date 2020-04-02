31951cfc3fdae96570a1a7280a11999f28642587
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-02 12:27:33 +0300

    OBG-509. Added PSU support too

--- opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/config/encryption/DatasafeStorageTest.java
+++ opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/config/encryption/DatasafeStorageTest.java
@@ -3,11 +3,13 @@ package de.adorsys.opba.protocol.facade.config.encryption;
 import com.google.common.io.ByteStreams;
 import de.adorsys.datasafe.encrypiton.api.types.UserIDAuth;
 import de.adorsys.datasafe.types.api.actions.ReadRequest;
-import de.adorsys.datasafe.types.api.actions.WriteRequest;
 import de.adorsys.opba.db.domain.entity.fintech.Fintech;
+import de.adorsys.opba.db.domain.entity.psu.Psu;
 import de.adorsys.opba.db.repository.jpa.fintech.FintechRepository;
+import de.adorsys.opba.db.repository.jpa.psu.PsuRepository;
 import de.adorsys.opba.protocol.facade.config.ApplicationTest;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.fintech.FintechSecureStorage;
+import de.adorsys.opba.protocol.facade.config.encryption.impl.psu.PsuSecureStorage;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -17,62 +19,76 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.UUID;
+import java.util.function.Supplier;
 
+import static de.adorsys.datasafe.types.api.actions.WriteRequest.forDefaultPrivate;
+import static de.adorsys.datasafe.types.api.actions.WriteRequest.forDefaultPublic;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @ActiveProfiles("test")
 @SpringBootTest(classes = ApplicationTest.class)
-class DatasafeStorageTest {
+class DatasafeStorageIntegrationCheckTest {
+
+    private static final Supplier<char[]> PASSWORD = "A very secret password"::toCharArray;
+    private static final String DATA_TO_STORE = "This is a test!";
 
     @Autowired
-    private FintechSecureStorage datasafe;
+    private FintechSecureStorage fintechSecureStorage;
 
     @Autowired
     private FintechRepository fintechs;
 
+    @Autowired
+    private PsuSecureStorage psuSecureStorage;
+
+    @Autowired
+    private PsuRepository psus;
 
     @Test
     @SneakyThrows
-    void test() {
-        log.info("BEGIN");
+    void testFintechDatasafeIntegration() {
         Fintech fintech = fintechs.save(Fintech.builder().build());
-        UserIDAuth idAuth = new UserIDAuth(fintech.getId().toString(), "aaaa"::toCharArray);
-        datasafe.registerFintech(idAuth);
-        log.info("CREATE DONE");
+        UserIDAuth idAuth = new UserIDAuth(fintech.getId().toString(), PASSWORD);
+        fintechSecureStorage.registerFintech(idAuth);
 
-        UUID inbox = UUID.randomUUID();
-        try (OutputStream os = datasafe.inboxService().write(
-                WriteRequest.forDefaultPublic(Collections.singleton(idAuth.getUserID()), "./" + inbox)
-        )) {
-            os.write("This is a test!".getBytes());
+        String inboxFile = UUID.randomUUID().toString();
+        try (OutputStream os = fintechSecureStorage.inboxService().write(forDefaultPublic(Collections.singleton(idAuth.getUserID()), inboxFile))) {
+            os.write(DATA_TO_STORE.getBytes(StandardCharsets.UTF_8));
         }
-        log.info("INBOX WRITE DONE");
 
         byte[] message;
-        try (InputStream is = datasafe.inboxService().read(
-                ReadRequest.forDefaultPrivate(idAuth, "./" + inbox)
-        )) {
+        try (InputStream is = fintechSecureStorage.inboxService().read(ReadRequest.forDefaultPrivate(idAuth, inboxFile))) {
             message = ByteStreams.toByteArray(is);
         }
-        log.info("INBOX READ DONE");
 
-        UUID privateId = UUID.randomUUID();
-        try (OutputStream os = datasafe.privateService().write(
-                WriteRequest.forDefaultPrivate(idAuth, "./" + privateId)
-        )) {
+        String privateFile = UUID.randomUUID().toString();
+        try (OutputStream os = fintechSecureStorage.privateService().write(forDefaultPrivate(idAuth, privateFile))) {
             os.write(message);
         }
-        log.info("PRIVATE WRITE DONE");
 
-        try (InputStream is = datasafe.privateService().read(
-                ReadRequest.forDefaultPrivate(idAuth, "./" + privateId)
-        )) {
-            assertThat(is).hasContent("This is a test!");
+        try (InputStream is = fintechSecureStorage.privateService().read(ReadRequest.forDefaultPrivate(idAuth, privateFile))) {
+            assertThat(is).hasContent(DATA_TO_STORE);
         }
-        log.info("PRIVATE READ DONE");
-        log.info("END");
+    }
+
+    @Test
+    @SneakyThrows
+    void testPsuDatasafeIntegration() {
+        Psu psu = psus.save(Psu.builder().build());
+        UserIDAuth idAuth = new UserIDAuth(psu.getId().toString(), PASSWORD);
+        psuSecureStorage.registerPsu(idAuth);
+
+        String privateFile = UUID.randomUUID().toString();
+        try (OutputStream os = psuSecureStorage.privateService().write(forDefaultPrivate(idAuth, privateFile))) {
+            os.write(DATA_TO_STORE.getBytes(StandardCharsets.UTF_8));
+        }
+
+        try (InputStream is = psuSecureStorage.privateService().read(ReadRequest.forDefaultPrivate(idAuth, privateFile))) {
+            assertThat(is).hasContent(DATA_TO_STORE);
+        }
     }
 }
