08a457daace4e5e97df86bcc417af67cd8e060ba
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-04-03 16:50:21 +0200

    resolve conflict

--- fintech-examples/fintech-ui/src/app/bank/list-transactions/list-transactions.component.ts
+++ fintech-examples/fintech-ui/src/app/bank/list-transactions/list-transactions.component.ts
@@ -6,7 +6,8 @@ import { RedirectStruct } from '../redirect-page/redirect-struct';
 
 @Component({
   selector: 'app-list-transactions',
-  templateUrl: './list-transactions.component.html'
+  templateUrl: './list-transactions.component.html',
+  styleUrls: ['./list-transactions.component.scss']
 })
 export class ListTransactionsComponent implements OnInit {
   accountId = '';
