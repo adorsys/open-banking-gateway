3c7f4f7c16f9e8f39338bb3d655bd3f39ec7e743
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-03 18:32:34 +0300

    OBG-509. Forcefully drop sensitive data after it is not needed

---
+++ opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/service/xs2a/consent/authenticate/embedded/Xs2aReportScaChallengeClearsScaTest.java
@@ -0,0 +1,86 @@
+package de.adorsys.opba.protocol.xs2a.service.xs2a.consent.authenticate.embedded;
+
+import de.adorsys.opba.protocol.xs2a.BaseMockitoTest;
+import de.adorsys.opba.protocol.xs2a.service.dto.ValidatedPathHeadersBody;
+import de.adorsys.opba.protocol.xs2a.service.storage.TransientDataStorage;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.context.Xs2aContext;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.Xs2aAuthorizedConsentParameters;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.Xs2aStandardHeaders;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.validation.Xs2aValidator;
+import de.adorsys.xs2a.adapter.service.AccountInformationService;
+import de.adorsys.xs2a.adapter.service.Response;
+import de.adorsys.xs2a.adapter.service.model.ScaStatus;
+import de.adorsys.xs2a.adapter.service.model.ScaStatusResponse;
+import de.adorsys.xs2a.adapter.service.model.TransactionAuthorisation;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.HashMap;
+
+import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.CONTEXT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class Xs2aReportScaChallengeClearsScaTest extends BaseMockitoTest {
+
+    private static final String SCA_VALUE = "123456";
+
+    @Mock
+    private Xs2aReportScaChallenge.Extractor extractor;
+
+    @Mock
+    private Xs2aValidator validator;
+
+    @Mock
+    private AccountInformationService ais;
+
+    @Mock
+    private DelegateExecution delegateExecution;
+
+    @Mock
+    private ValidatedPathHeadersBody<Xs2aAuthorizedConsentParameters, Xs2aStandardHeaders, TransactionAuthorisation> mockParams;
+
+    @Mock
+    private Response<ScaStatusResponse> scaStatus;
+
+    @Mock
+    private ScaStatusResponse responseBody;
+
+    @InjectMocks
+    private Xs2aReportScaChallengeTestable tested;
+
+    @Test
+    void scaChallengeCleaned() {
+        Xs2aContext context = new Xs2aContext();
+        context.setTransientStorage(new TransientDataStorage(new HashMap<>()));
+        when(mockParams.getHeaders()).thenReturn(new Xs2aStandardHeaders());
+        when(mockParams.getBody()).thenReturn(new TransactionAuthorisation());
+        when(mockParams.getPath()).thenReturn(new Xs2aAuthorizedConsentParameters());
+        when(delegateExecution.getVariable(CONTEXT)).thenReturn(context);
+        context.setPsuPassword(SCA_VALUE);
+        when(extractor.forExecution(context)).thenReturn(mockParams);
+        when(ais.updateConsentsPsuData(any(), any(), any(), any(TransactionAuthorisation.class))).thenReturn(scaStatus);
+        when(scaStatus.getBody()).thenReturn(responseBody);
+        when(scaStatus.getBody().getScaStatus()).thenReturn(ScaStatus.EXEMPTED);
+
+        tested.doRealExecution(delegateExecution, context);
+
+        verify(delegateExecution).setVariable(CONTEXT, context);
+        assertThat(context.getPsuPassword()).isNull();
+    }
+
+    public static class Xs2aReportScaChallengeTestable extends Xs2aReportScaChallenge {
+        public Xs2aReportScaChallengeTestable(Extractor extractor, Xs2aValidator validator, AccountInformationService ais) {
+            super(extractor, validator, ais);
+        }
+
+        @Override
+        public void doRealExecution(DelegateExecution execution, Xs2aContext context) {
+            super.doRealExecution(execution, context);
+        }
+    }
+}
