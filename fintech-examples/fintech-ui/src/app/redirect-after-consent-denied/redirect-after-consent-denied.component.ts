863fd47289a6927bdf706d27b85412117d4e4599
aos <aos@adorsys.de>
2020-04-03 17:30:23 +0200

    changes are done in scss and html

--- fintech-examples/fintech-ui/src/app/redirect-after-consent-denied/redirect-after-consent-denied.component.ts
+++ fintech-examples/fintech-ui/src/app/redirect-after-consent-denied/redirect-after-consent-denied.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-redirect-after-consent-denied",
-  templateUrl: "./redirect-after-consent-denied.component.html",
-  styleUrls: ["./redirect-after-consent-denied.component.scss"]
+  selector: 'app-redirect-after-consent-denied',
+  templateUrl: './redirect-after-consent-denied.component.html',
+  styleUrls: ['./redirect-after-consent-denied.component.scss']
 })
 export class RedirectAfterConsentDeniedComponent implements OnInit {
-
-  constructor(private router: Router) {
-  }
+  constructor(private router: Router) {}
 
   // TODO: call to fintech-server to reset redirect cookie and to consent initiation data. Not available now in the Fintech-server
   ngOnInit() {}
 
   toBankSearch(): void {
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
   }
-
 }
