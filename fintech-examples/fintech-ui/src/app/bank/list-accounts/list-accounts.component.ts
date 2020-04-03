5808431f073a88e474c35c4405fb4c1a2dece70f
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-04-03 16:50:21 +0200

    resolve conflict

--- fintech-examples/fintech-ui/src/app/bank/list-accounts/list-accounts.component.ts
+++ fintech-examples/fintech-ui/src/app/bank/list-accounts/list-accounts.component.ts
@@ -6,7 +6,8 @@ import { RedirectStruct } from '../redirect-page/redirect-struct';
 
 @Component({
   selector: 'app-list-accounts',
-  templateUrl: './list-accounts.component.html'
+  templateUrl: './list-accounts.component.html',
+  styleUrls: ['./list-accounts.component.scss']
 })
 export class ListAccountsComponent implements OnInit {
   accounts: AccountDetails[];
