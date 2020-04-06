81f32c86d2c3e6fd1e8ac2b046cdab693bea0c51
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-06 17:26:47 +0200

    OBG-507 auth api impl

--- opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/controller/PsuAuthController.java
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/controller/PsuAuthController.java
@@ -1,26 +1,63 @@
 package de.adorsys.opba.tppauthapi.controller;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import de.adorsys.opba.db.domain.entity.psu.Psu;
+import de.adorsys.opba.tppauthapi.model.generated.PsuAuthBody;
 import de.adorsys.opba.tppauthapi.resource.generated.PsuAuthApi;
 import de.adorsys.opba.tppauthapi.service.PsuAuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.time.Duration;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAmount;
+import java.util.Date;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class PsuAuthController implements PsuAuthApi {
-
     private final PsuAuthService psuAuthService;
 
     @Override
-    public CompletableFuture login(String authorization, UUID xRequestID, String psuId, String password) {
+    public ResponseEntity<String> login(PsuAuthBody psuAuthBody, UUID xRequestID) {
         return null;
     }
 
     @Override
-    public CompletableFuture registration(String authorization, UUID xRequestID, String psuId, String password) {
+    @SneakyThrows
+    public ResponseEntity<Void> registration(PsuAuthBody psuAuthDto, UUID xRequestID) {
+        Psu psu = psuAuthService.createPsuIfNotExist(psuAuthDto.getId(), psuAuthDto.getPassword());
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("AES");
+        KeyPair keyPair = generator.generateKeyPair();
+        generateToken(psu.getUserId(), keyPair.getPrivate(), Duration.ofDays(7));
         return null;
+        // return jwttoken as httpOnly cookie,  xsrftoken in response body
+    }
+
+    @SneakyThrows
+    private String generateToken(String id, PrivateKey privateKey, TemporalAmount validity) {
+        ZonedDateTime currentTime = ZonedDateTime.now(ZoneOffset.UTC);
+        JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                .expirationTime(Date.from(currentTime.plus(validity).toInstant()))
+                .issueTime(Date.from(currentTime.toInstant()))
+                .subject(String.valueOf(id))
+                .build();
+        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
+        SignedJWT signedJWT = new SignedJWT(jwsHeader, claims);
+        signedJWT.sign(new RSASSASigner(privateKey));
+        return signedJWT.serialize();
     }
 }
