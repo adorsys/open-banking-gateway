9ba67631987d3ed5597a4aaba0914fd017059799
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-06 15:57:20 +0300

    Fixed dependencies

--- fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/config/SmokeConfig.java
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/config/SmokeConfig.java
@@ -1,9 +1,7 @@
 package de.adorsys.fintech.tests.e2e.config;
 
-import de.adorsys.opba.fintech.impl.database.repositories.UserRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -13,7 +11,4 @@ public class SmokeConfig {
     @Getter
     @Value("${test.smoke.fintech.server-uri}")
     private String fintechServerUri;
-
-    @MockBean
-    private UserRepository userRepository;
 }
