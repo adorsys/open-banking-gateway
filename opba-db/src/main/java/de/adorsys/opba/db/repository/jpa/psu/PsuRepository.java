31165a01236705288d51f5047a6c76cf3fd4f6ef
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-06 17:26:47 +0200

    OBG-507 auth api impl

--- opba-db/src/main/java/de/adorsys/opba/db/repository/jpa/psu/PsuRepository.java
+++ opba-db/src/main/java/de/adorsys/opba/db/repository/jpa/psu/PsuRepository.java
@@ -4,6 +4,9 @@ import de.adorsys.opba.db.domain.entity.psu.Psu;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PsuRepository extends CrudRepository<Psu, Long> {
+    Optional<Psu> findByUserId(String id);
 }
