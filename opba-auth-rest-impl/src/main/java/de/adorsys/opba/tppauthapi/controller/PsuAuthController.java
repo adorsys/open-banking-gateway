293b3265c6f9919c3561b360b0fcc961d2a77102
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-07 19:53:16 +0200

    OBG-507 added tpp key pair, extracted application properties

--- opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/controller/PsuAuthController.java
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/controller/PsuAuthController.java
@@ -7,29 +7,28 @@ import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import de.adorsys.opba.db.domain.entity.psu.Psu;
 import de.adorsys.opba.tppauthapi.config.CookieProperties;
+import de.adorsys.opba.tppauthapi.config.TppProperties;
 import de.adorsys.opba.tppauthapi.model.generated.PsuAuthBody;
 import de.adorsys.opba.tppauthapi.resource.generated.PsuAuthApi;
 import de.adorsys.opba.tppauthapi.service.PsuAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.util.encoders.Hex;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
-import java.nio.charset.StandardCharsets;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.Duration;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
@@ -43,10 +42,11 @@ import static org.springframework.http.HttpHeaders.SET_COOKIE;
 @Controller
 @RequiredArgsConstructor
 public class PsuAuthController implements PsuAuthApi {
-    public static final Duration WEEK = Duration.ofDays(7);
 
+    public static final Base64.Encoder ENCODER = Base64.getEncoder();
     private final PsuAuthService psuAuthService;
     private final CookieProperties cookieProperties;
+    private final TppProperties tppProperties;
 
     @Override
     @SneakyThrows
@@ -56,7 +56,7 @@ public class PsuAuthController implements PsuAuthApi {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        String jwtToken = generateToken(psu.get().getUserId(), getPrivateKey());
+        String jwtToken = generateToken(psu.get().getLogin());
 
         String sessionCookieString = ResponseCookie.from(AUTHORIZATION_SESSION_ID, jwtToken)
                 .httpOnly(cookieProperties.isHttpOnly())
@@ -70,7 +70,7 @@ public class PsuAuthController implements PsuAuthApi {
                 .status(HttpStatus.ACCEPTED)
                 .header(X_REQUEST_ID, xRequestID.toString())
                 .header(SET_COOKIE, sessionCookieString)
-                .header(X_XSRF_TOKEN, getHash(jwtToken))
+                .header(X_XSRF_TOKEN, ENCODER.encodeToString(jwtToken.getBytes()))
                 .body(jwtToken);
     }
 
@@ -81,34 +81,38 @@ public class PsuAuthController implements PsuAuthApi {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(HttpHeaders.LOCATION, "/login");
+        responseHeaders.add(HttpHeaders.LOCATION, tppProperties.getLoginUrl());
         return new ResponseEntity<>(responseHeaders, HttpStatus.CREATED);
     }
 
     @SneakyThrows
-    private String generateToken(String id, PrivateKey privateKey) {
+    public String generateToken(String id) {
         ZonedDateTime currentTime = ZonedDateTime.now(ZoneOffset.UTC);
+        Duration duration = Duration.ofSeconds(tppProperties.getKeyValidity());
         JWTClaimsSet claims = new JWTClaimsSet.Builder()
-                .expirationTime(Date.from(currentTime.plus(WEEK).toInstant()))
+                .expirationTime(Date.from(currentTime.plus(duration).toInstant()))
                 .issueTime(Date.from(currentTime.toInstant()))
                 .subject(String.valueOf(id))
                 .build();
-        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
+        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.parse(tppProperties.getJwsAlgo())).build();
         SignedJWT signedJWT = new SignedJWT(jwsHeader, claims);
-        signedJWT.sign(new RSASSASigner(privateKey));
+        signedJWT.sign(new RSASSASigner(loadPrivateKey()));
         return signedJWT.serialize();
     }
 
-    private PrivateKey getPrivateKey() throws NoSuchAlgorithmException {
-        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-        KeyPair keyPair = generator.generateKeyPair();
-        return keyPair.getPrivate();
+    @SneakyThrows
+    public PrivateKey loadPrivateKey() {
+        byte[] privateKeyBytes = Base64.getDecoder().decode(tppProperties.getPrivateKey());
+        PKCS8EncodedKeySpec ks = new PKCS8EncodedKeySpec(privateKeyBytes);
+        KeyFactory kf = KeyFactory.getInstance(tppProperties.getSignAlgo());
+        return kf.generatePrivate(ks);
     }
 
-    @NotNull
-    private String getHash(String jwtToken) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(jwtToken.getBytes(StandardCharsets.UTF_8));
-        return new String(Hex.encode(hash));
+    @SneakyThrows
+    public PublicKey loadPublicKey() {
+        byte[] publicKeyBytes = Base64.getDecoder().decode(tppProperties.getPublicKey());
+        X509EncodedKeySpec ks = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory kf = KeyFactory.getInstance(tppProperties.getSignAlgo());
+        return kf.generatePublic(ks);
     }
 }
