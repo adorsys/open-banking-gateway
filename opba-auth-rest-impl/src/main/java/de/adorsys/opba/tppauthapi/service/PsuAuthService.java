4711c2402faec0aeb1f5efaeaa53889f659fbaf7
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-08 11:43:28 +0200

    OBG-507 auth config

--- opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/service/PsuAuthService.java
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/service/PsuAuthService.java
@@ -1,16 +1,26 @@
 package de.adorsys.opba.tppauthapi.service;
 
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import de.adorsys.datasafe.encrypiton.api.types.UserIDAuth;
 import de.adorsys.opba.db.domain.entity.psu.Psu;
 import de.adorsys.opba.db.repository.jpa.psu.PsuRepository;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.psu.PsuSecureStorage;
+import de.adorsys.opba.tppauthapi.config.TppProperties;
 import de.adorsys.opba.tppauthapi.exceptions.PsuAuthenticationException;
-import de.adorsys.opba.tppauthapi.exceptions.PsuRegisterException;
 import de.adorsys.opba.tppauthapi.exceptions.PsuAuthorizationException;
+import de.adorsys.opba.tppauthapi.exceptions.PsuRegisterException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -19,6 +29,9 @@ public class PsuAuthService {
 
     private final PsuRepository psuRepository;
     private final PsuSecureStorage psuSecureStorage;
+    private final JWSHeader jwsHeader;
+    private final RSASSASigner rsassaSigner;
+    private final TppProperties tppProperties;
 
     @Transactional(readOnly = true)
     public Psu tryAuthenticateUser(String psuId, String password) throws PsuAuthorizationException {
@@ -45,5 +58,19 @@ public class PsuAuthService {
         UserIDAuth idAuth = new UserIDAuth(newPsu.getId().toString(), password::toCharArray);
         psuSecureStorage.registerPsu(idAuth);
         return newPsu;
+    }
+
+    @SneakyThrows
+    public String generateToken(String id) {
+        ZonedDateTime currentTime = ZonedDateTime.now(ZoneOffset.UTC);
+        Duration duration = Duration.ofSeconds(tppProperties.getKeyValidityDays());
+        JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                .expirationTime(Date.from(currentTime.plus(duration).toInstant()))
+                .issueTime(Date.from(currentTime.toInstant()))
+                .subject(String.valueOf(id))
+                .build();
+        SignedJWT signedJWT = new SignedJWT(jwsHeader, claims);
+        signedJWT.sign(rsassaSigner);
+        return signedJWT.serialize();
     }
 }
