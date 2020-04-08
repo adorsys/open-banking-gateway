5c1aecfcd1891b475c555ecd0ea55e9b2796a7cf
Hergy Tchuinkou <htc@adorsys.de>
2020-04-08 09:51:17 +0200

    refactor tests

--- fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/steps/FintechStagesUtils.java
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/steps/FintechStagesUtils.java
@@ -10,15 +10,15 @@ public class FintechStagesUtils {
     public static final String X_REQUEST_ID = "X_REQUEST_ID";
     public static final String PIN_VALUE = "1234";
     public static final String USERNAME = "tom";
-    public static final String X_XSRF_TOKEN = "X_XSRF_TOKEN";
+    public static final String X_XSRF_TOKEN = "xsrfToken";
     public static final String X_XSRF_TOKEN_VALUE = "441c1f6f-1bda-43c0-91d2-4b41a5d53a9c";
-    public static final String SESSION_COOKIE = "SESSION_COOKIE";
+    public static final String SESSION_COOKIE = "sessionCookie";
     public static final String SESSION_COOKIE_VALUE = "%7B%22fintechUserId%22%3A%226164%22%2C%22hashedXsrfToken%22%3A1713814359%7D";
     public static final String BANK_ID = "BANK_ID";
     public static final String BANK_ID_VALUE = "53c47f54-b9a4-465a-8f77-bc6cd5f0cf46";
-    public static final String FINTECH_LOGIN_ENDPOINT = "/v1/login";
+    public static final String FINTECH_LOGIN_ENDPOINT = "login";
     public static final String BANKSEARCH = "adorsys xs2a";
-    public static final String BANKSEARCH_ENDPOINT = "/v1/search/bankSearch";
+    public static final String BANKSEARCH_ENDPOINT = "search";
 
 
     public static RequestSpecification withDefaultHeaders() {
