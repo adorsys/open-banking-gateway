8eeb9d22f17cda838916439f14d4674a0dcbe82b
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-08 11:01:58 +0200

    OBG-507 password checking, exceptions handling

---
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/controller/GenericControllerAdvice.java
@@ -0,0 +1,52 @@
+package de.adorsys.opba.tppauthapi.controller;
+
+import de.adorsys.opba.tppauthapi.exceptions.PsuAuthenticationException;
+import de.adorsys.opba.tppauthapi.exceptions.PsuAuthorizationException;
+import de.adorsys.opba.tppauthapi.exceptions.PsuRegisterException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+import java.util.List;
+
+@ControllerAdvice(basePackageClasses = {
+        PsuAuthController.class
+})
+@Slf4j
+public class GenericControllerAdvice {
+
+    @ExceptionHandler({PsuRegisterException.class})
+    public ResponseEntity<List<String>> handleUserExistsException(PsuRegisterException ex) {
+        log.debug("User exists exception: {}", ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Collections.singletonList(ex.getMessage()));
+    }
+
+    @ExceptionHandler({PsuAuthenticationException.class})
+    public ResponseEntity<List<String>> handleUserDoesNotExistException(PsuAuthenticationException ex) {
+        log.debug("User does not exist exception: {}", ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Collections.singletonList(ex.getMessage()));
+    }
+
+    @ExceptionHandler({PsuAuthorizationException.class})
+    public ResponseEntity<List<String>> handleUnauthorizedException(Exception ex) {
+        log.debug("Unauthorized exception: {}", ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(Collections.singletonList(ex.getMessage()));
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<List<String>> handleException(Exception ex) {
+        log.debug("Unhandled exception: {}", ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Collections.singletonList(ex.getMessage()));
+    }
+}
