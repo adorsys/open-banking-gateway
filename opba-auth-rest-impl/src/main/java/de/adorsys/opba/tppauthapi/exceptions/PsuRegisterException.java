1dce8d64e8554f8704befc033e8937c68708cbad
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-08 11:01:58 +0200

    OBG-507 password checking, exceptions handling

---
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/exceptions/PsuRegisterException.java
@@ -0,0 +1,8 @@
+package de.adorsys.opba.tppauthapi.exceptions;
+
+public class PsuRegisterException extends RuntimeException {
+
+    public PsuRegisterException(String message) {
+        super(message);
+    }
+}
