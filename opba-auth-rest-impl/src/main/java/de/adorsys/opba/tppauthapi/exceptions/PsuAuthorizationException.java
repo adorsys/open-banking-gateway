2daf73a318e20e9b2deb7b1996b619730c519986
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-08 11:01:58 +0200

    OBG-507 password checking, exceptions handling

---
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/exceptions/PsuAuthorizationException.java
@@ -0,0 +1,7 @@
+package de.adorsys.opba.tppauthapi.exceptions;
+
+public class PsuAuthorizationException extends Exception {
+    public PsuAuthorizationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/service/PsuAuthService.java
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/service/PsuAuthService.java
@@ -4,10 +4,13 @@ import de.adorsys.datasafe.encrypiton.api.types.UserIDAuth;
 import de.adorsys.opba.db.domain.entity.psu.Psu;
 import de.adorsys.opba.db.repository.jpa.psu.PsuRepository;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.psu.PsuSecureStorage;
+import de.adorsys.opba.tppauthapi.exceptions.PsuAuthenticationException;
+import de.adorsys.opba.tppauthapi.exceptions.PsuRegisterException;
+import de.adorsys.opba.tppauthapi.exceptions.PsuAuthorizationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
@@ -17,19 +20,30 @@ public class PsuAuthService {
     private final PsuRepository psuRepository;
     private final PsuSecureStorage psuSecureStorage;
 
-    public Optional<Psu> getPsu(String psuId) {
-        return psuRepository.findByUserId(psuId);
+    @Transactional(readOnly = true)
+    public Psu tryAuthenticateUser(String psuId, String password) throws PsuAuthorizationException {
+        Optional<Psu> psu = psuRepository.findByUserId(psuId);
+        if (!psu.isPresent()) {
+            throw new PsuAuthenticationException("User not found: " + psuId);
+        }
+        UserIDAuth idAuth = new UserIDAuth(psu.get().getId().toString(), password::toCharArray);
+        try {
+            psuSecureStorage.userProfile().privateProfile(idAuth).getKeystore();
+        } catch (Exception e) {
+            throw new PsuAuthorizationException(e.getMessage(), e);
+        }
+        return psu.get();
     }
 
     @Transactional
-    public Optional<Psu> createPsuIfNotExist(String id, String password) {
+    public Psu createPsuIfNotExist(String id, String password) {
         Optional<Psu> psu = psuRepository.findByUserId(id);
         if (psu.isPresent()) {
-            return Optional.empty();
+            throw new PsuRegisterException("Psu already exist");
         }
         Psu newPsu = psuRepository.save(Psu.builder().build());
         UserIDAuth idAuth = new UserIDAuth(newPsu.getId().toString(), password::toCharArray);
         psuSecureStorage.registerPsu(idAuth);
-        return Optional.of(newPsu);
+        return newPsu;
     }
 }
