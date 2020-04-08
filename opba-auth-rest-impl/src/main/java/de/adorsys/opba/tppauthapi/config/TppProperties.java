fbee270e494cabbfbfb3937ca0a6a9c331c85b2d
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-08 11:01:58 +0200

    OBG-507 password checking, exceptions handling

--- opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/config/TppProperties.java
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/config/TppProperties.java
@@ -3,7 +3,10 @@ package de.adorsys.opba.tppauthapi.config;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DurationUnit;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.temporal.ChronoUnit;
 
 @Data
 @Configuration
@@ -15,6 +18,7 @@ public class TppProperties {
     private String privateKey;
     private String publicKey;
     private String signAlgo;
-    private Long keyValidity;
+    @DurationUnit(ChronoUnit.DAYS)
+    private Long keyValidityDays;
     private String jwsAlgo;
 }
---
+++ opba-auth-rest-impl/src/main/java/de/adorsys/opba/tppauthapi/exceptions/PsuAuthenticationException.java
@@ -0,0 +1,8 @@
+package de.adorsys.opba.tppauthapi.exceptions;
+
+public class PsuAuthenticationException extends RuntimeException {
+
+    public PsuAuthenticationException(String message) {
+        super(message);
+    }
+}
