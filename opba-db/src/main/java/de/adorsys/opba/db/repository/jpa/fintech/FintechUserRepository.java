98bc3281612f1167834c293de758610292a31867
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-10 16:56:33 +0300

    OBG-547. Implemented very basic fintech-user consent requirements storage

--- opba-db/src/main/java/de/adorsys/opba/db/repository/jpa/fintech/FintechUserRepository.java
+++ opba-db/src/main/java/de/adorsys/opba/db/repository/jpa/fintech/FintechUserRepository.java
@@ -1,13 +1,14 @@
 package de.adorsys.opba.db.repository.jpa.fintech;
 
 import de.adorsys.opba.db.domain.entity.fintech.Fintech;
+import de.adorsys.opba.db.domain.entity.fintech.FintechUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface FintechRepository extends CrudRepository<Fintech, Long> {
+public interface FintechUserRepository extends CrudRepository<FintechUser, Long> {
 
-    Optional<Fintech> findByGlobalId(String globalId);
+    Optional<FintechUser> findByPsuFintechIdAndFintech(String psuFintechId, Fintech fintech);
 }
