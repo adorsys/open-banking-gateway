476cdaff366f462f5515b11cedc1a3250ce8aa18
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-13 16:44:11 +0300

    OBG-549. Added consent spec encryption

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/authorization/internal/SecretKeyForAuthorizationExchangeService.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/authorization/internal/SecretKeyForAuthorizationExchangeService.java
@@ -1,10 +1,16 @@
 package de.adorsys.opba.protocol.facade.services.authorization.internal;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.opba.db.domain.entity.sessions.AuthSession;
+import de.adorsys.opba.db.repository.jpa.AuthorizationSessionRepository;
+import de.adorsys.opba.protocol.api.services.EncryptionService;
+import de.adorsys.opba.protocol.api.services.EncryptionServiceProvider;
 import de.adorsys.opba.protocol.facade.config.encryption.KeyGeneratorConfig;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.fintech.FintechUserSecureStorage;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.crypto.SecretKey;
 
@@ -13,9 +19,17 @@ import javax.crypto.SecretKey;
 public class SecretKeyForAuthorizationExchangeService {
 
     private final KeyGeneratorConfig.ConsentSpecSecretKeyGenerator specSecretKeyGenerator;
+    private final AuthorizationSessionRepository authSessions;
+    private final EncryptionServiceProvider provider;
+    private final ObjectMapper mapper;
 
+    @SneakyThrows
+    @Transactional
     public SecretKey encryptAndStoreForFuture(AuthSession session, FintechUserSecureStorage.FinTechUserInboxData data) {
         SecretKey key = specSecretKeyGenerator.generate();
+        EncryptionService service = provider.forSecretKey(key);
+        session.setConsentSpec(service, mapper.writeValueAsString(data));
+        authSessions.save(session);
         return key;
     }
 }
--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/EncryptionServiceProvider.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/EncryptionServiceProvider.java
@@ -1,6 +1,9 @@
 package de.adorsys.opba.protocol.api.services;
 
+import javax.crypto.SecretKey;
+
 public interface EncryptionServiceProvider {
 
     EncryptionService getEncryptionById(String id);
+    EncryptionService forSecretKey(SecretKey key);
 }
