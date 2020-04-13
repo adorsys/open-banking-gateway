61dcbc182d7fb5092be41eee7374dbfd815c1bb2
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-13 16:44:11 +0300

    OBG-549. Added consent spec encryption

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/config/encryption/EncryptionProviderConfig.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/config/encryption/EncryptionProviderConfig.java
@@ -1,9 +1,12 @@
 package de.adorsys.opba.protocol.facade.config.encryption;
 
+import de.adorsys.opba.protocol.api.services.EncryptionService;
 import de.adorsys.opba.protocol.api.services.EncryptionServiceProvider;
 import de.adorsys.opba.protocol.facade.services.NoEncryptionServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
 
 // FIXME - replace with normal encryption
 @Configuration
@@ -11,6 +14,16 @@ public class EncryptionProviderConfig {
 
     @Bean
     EncryptionServiceProvider dummyEncryption() {
-        return id -> new NoEncryptionServiceImpl();
+        return new EncryptionServiceProvider() {
+            @Override
+            public EncryptionService getEncryptionById(String id) {
+                return new NoEncryptionServiceImpl();
+            }
+
+            @Override
+            public EncryptionService forSecretKey(SecretKey key) {
+                return new NoEncryptionServiceImpl();
+            }
+        };
     }
 }
