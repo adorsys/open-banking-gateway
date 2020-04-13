d54a48690f2c87c331273636c6ae4d44cc44a67d
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-13 16:32:26 +0300

    OBG-549. Fix merge issues

--- fintech-examples/fintech-server/src/test/java/de/adorsys/opba/fintech/server/feignmocks/TppAisClientFeignMock.java
+++ fintech-examples/fintech-server/src/test/java/de/adorsys/opba/fintech/server/feignmocks/TppAisClientFeignMock.java
@@ -1,12 +1,15 @@
 package de.adorsys.opba.fintech.server.feignmocks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.opba.fintech.impl.tppclients.TppAisClient;
 import de.adorsys.opba.tpp.ais.api.model.generated.AccountList;
 import de.adorsys.opba.tpp.ais.api.model.generated.TransactionsResponse;
 import org.springframework.http.ResponseEntity;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
 public class TppAisClientFeignMock implements TppAisClient {
@@ -19,5 +22,17 @@ public class TppAisClientFeignMock implements TppAisClient {
     @Override
     public ResponseEntity<TransactionsResponse> getTransactions(String accountId, String authorization, String serviceSessionPassword, String fintechUserID, String fintechRedirectURLOK, String fintechRedirectURLNOK, UUID xRequestID, String bankID, String psUConsentSession, UUID serviceSessionID, @Valid LocalDate dateFrom, @Valid LocalDate dateTo, @Valid String entryReferenceFrom, @Valid String bookingStatus, @Valid Boolean deltaList) {
         return null;
+    }
+
+    // TODO: https://github.com/adorsys/open-banking-gateway/issues/559
+    @Override
+    public Optional<ObjectMapper> getObjectMapper() {
+        return Optional.empty();
+    }
+
+    // TODO: https://github.com/adorsys/open-banking-gateway/issues/559
+    @Override
+    public Optional<HttpServletRequest> getRequest() {
+        return Optional.empty();
     }
 }
