a2c32f30692ba49fd0ea90fe7b28a157b184ad74
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-13 16:44:11 +0300

    OBG-549. Added consent spec encryption

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/sessions/AuthSession.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/sessions/AuthSession.java
@@ -3,21 +3,25 @@ package de.adorsys.opba.db.domain.entity.sessions;
 import de.adorsys.opba.db.domain.entity.BankProtocol;
 import de.adorsys.opba.db.domain.entity.fintech.FintechUser;
 import de.adorsys.opba.db.domain.entity.psu.Psu;
+import de.adorsys.opba.protocol.api.services.EncryptionService;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Version;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Getter
@@ -49,6 +53,19 @@ public class AuthSession {
     private String redirectCode;
     private String aspspRedirectCode;
 
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private byte[] encConsentSpec;
+
     @Version
     private int version;
+
+    public String getConsentSpec(EncryptionService encryption) {
+        return new String(encryption.decrypt(encConsentSpec), StandardCharsets.UTF_8);
+    }
+
+    public void setConsentSpec(EncryptionService encryption, String context) {
+        this.encConsentSpec = encryption.encrypt(context.getBytes(StandardCharsets.UTF_8));
+    }
 }
