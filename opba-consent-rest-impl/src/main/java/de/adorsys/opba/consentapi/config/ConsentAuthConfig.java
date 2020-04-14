998239aecfc04c93849dd1654f2ddffb3cbcd939
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-14 18:58:05 +0300

    OBG-549. Fixed issue with same fintech-user ID stuck in authorization session

--- opba-consent-rest-impl/src/main/java/de/adorsys/opba/consentapi/config/ConsentAuthConfig.java
+++ opba-consent-rest-impl/src/main/java/de/adorsys/opba/consentapi/config/ConsentAuthConfig.java
@@ -1,6 +1,9 @@
 package de.adorsys.opba.consentapi.config;
 
+import de.adorsys.opba.api.security.EnableTokenBasedApiSecurity;
+import de.adorsys.opba.api.security.service.TokenBasedAuthService;
 import de.adorsys.opba.protocol.api.dto.request.FacadeServiceableRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -14,19 +17,25 @@ import java.util.Arrays;
 import static de.adorsys.opba.restapi.shared.HttpHeaders.AUTHORIZATION_SESSION_KEY;
 
 @Configuration
-public class FacadeServiceableProviderConfig {
+@EnableTokenBasedApiSecurity
+@RequiredArgsConstructor
+public class ConsentAuthConfig {
+
+    private final TokenBasedAuthService authService;
 
     @Bean
     @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public FacadeServiceableRequest provideCurrentFacadeServiceable(HttpServletRequest httpServletRequest) {
+        String authCookieValue = Arrays.stream(httpServletRequest.getCookies())
+                .filter(it -> AUTHORIZATION_SESSION_KEY.equals(it.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
+
+        String subjectValue = authService.validateTokenAndGetSubject(authCookieValue);
+
         return FacadeServiceableRequest.builder()
-                .authorizationKey(
-                        Arrays.stream(httpServletRequest.getCookies())
-                                .filter(it -> AUTHORIZATION_SESSION_KEY.equals(it.getName()))
-                                .findFirst()
-                                .map(Cookie::getValue)
-                                .orElse(null)
-                )
+                .authorizationKey(subjectValue)
                 .build();
     }
 }
