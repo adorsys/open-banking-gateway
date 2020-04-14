0f435b6646ac7cbaddea87366d723be41a146217
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-14 16:06:01 +0300

    OBG-549. Added cookie to the flow

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/consent/ConsentSearchService.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/consent/ConsentSearchService.java
@@ -2,6 +2,7 @@ package de.adorsys.opba.protocol.facade.services.consent;
 
 import de.adorsys.opba.protocol.api.dto.consent.ConsentResult;
 import de.adorsys.opba.protocol.api.search.FindConsent;
+import de.adorsys.opba.protocol.facade.config.encryption.SecretKeyWithIv;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.fintech.FintechUserSecureStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ public class ConsentSearchService {
     private final Map<String, ? extends FindConsent> matchers;
 
     @Transactional
-    public Optional<ConsentResult> findConsent(FintechUserSecureStorage.FinTechUserInboxData forData) {
+    public Optional<ConsentResult> findConsent(SecretKeyWithIv psuAspspKey, FintechUserSecureStorage.FinTechUserInboxData forData) {
         return Optional.empty();
     }
 }
