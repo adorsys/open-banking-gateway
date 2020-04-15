8f8d0ea401995f079322ce52081c9233dc1fa815
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-15 12:57:17 +0200

    OBG-434 cleaned up consent confirm api, removed redirectCode

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/service/ConsentService.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/service/ConsentService.java
@@ -1,9 +1,9 @@
 package de.adorsys.opba.fintech.impl.service;
 
 import de.adorsys.opba.fintech.impl.tppclients.TppConsenClient;
-import de.adorsys.opba.tpp.token.api.model.generated.PsuConsentSessionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,7 +14,9 @@ import java.util.UUID;
 public class ConsentService {
     private final TppConsenClient tppConsenClient;
 
-    public PsuConsentSessionResponse confirmConsent(String authId, UUID xRequestId, String redirectCode) {
-        return tppConsenClient.confirmConsent(authId, xRequestId, redirectCode).getBody();
+    public boolean confirmConsent(String authId, UUID xRequestId) {
+        HttpStatus statusCode = tppConsenClient.confirmConsent(authId, xRequestId).getStatusCode();
+        log.debug("consent confirmation response code: {}", statusCode);
+        return statusCode.is2xxSuccessful();
     }
 }
