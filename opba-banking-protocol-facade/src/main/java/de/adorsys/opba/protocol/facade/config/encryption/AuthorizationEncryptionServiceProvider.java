7743c4ee65a3d85b6728a8699421215edf1c5b9b
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-15 16:47:23 +0300

    OBG-550. Cleaned up facade

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/config/encryption/AuthorizationEncryptionServiceProvider.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/config/encryption/AuthorizationEncryptionServiceProvider.java
@@ -1,32 +1,14 @@
 package de.adorsys.opba.protocol.facade.config.encryption;
 
-import de.adorsys.opba.protocol.api.services.EncryptionService;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 public abstract class AuthorizationEncryptionServiceProvider {
 
-    private final Map<String, EncryptionServiceWithKey> cachedServices;
     private final EncryptionWithInitVectorOper oper;
 
-    public void remove(EncryptionService service) {
-        if (null == service) {
-            return;
-        }
-
-        cachedServices.remove(service.getId());
-    }
-
-    public EncryptionServiceWithKey getEncryptionById(String id) {
-        return cachedServices.get(id);
-    }
-
-    public EncryptionServiceWithKey forSecretKey(SecretKeyWithIv key) {
-        EncryptionServiceWithKey service = oper.encryptionService(key);
-        cachedServices.put(service.getId(), service);
-        return service;
+    public EncryptionServiceWithKey forSecretKey(String keyId, SecretKeyWithIv key) {
+        return oper.encryptionService(keyId, key);
     }
 
     public SecretKeyWithIv generateKey() {
