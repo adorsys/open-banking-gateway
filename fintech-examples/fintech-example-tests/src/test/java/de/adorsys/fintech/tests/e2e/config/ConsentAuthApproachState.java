c7accf3a0cbed1e7c05b9e201fb077fc24c139bf
Hergy Tchuinkou <htc@adorsys.de>
2020-04-15 17:01:51 +0200

    consent auth for anton.brueckner

---
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/config/ConsentAuthApproachState.java
@@ -0,0 +1,46 @@
+package de.adorsys.fintech.tests.e2e.config;
+
+import io.restassured.RestAssured;
+import io.restassured.mapper.TypeRef;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ConsentAuthApproachState {
+    private final String aspspProfileServerUri;
+
+    private List<String> memoizedApproaches;
+
+    public void memoize() {
+        ExtractableResponse<Response> response = RestAssured
+                                                         .when()
+                                                         .get(aspspProfileServerUri + "/api/v1/aspsp-profile/sca-approaches")
+                                                         .then()
+                                                         .statusCode(HttpStatus.OK.value())
+                                                         .extract();
+
+        this.memoizedApproaches = response.body().as(new TypeRef<List<String>>() {});
+    }
+
+    public void restore() {
+        if (null == this.memoizedApproaches) {
+            return;
+        }
+
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(memoizedApproaches)
+                .when()
+                .put(aspspProfileServerUri + "/api/v1/aspsp-profile/for-debug/sca-approaches")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        this.memoizedApproaches = null;
+    }
+}
