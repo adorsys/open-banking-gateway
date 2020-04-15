04549feb69d30669ff7c0db72c115e6d6e4a48f3
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-15 12:57:17 +0200

    OBG-434 cleaned up consent confirm api, removed redirectCode

--- opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/controller/TppBankingApiTokenController.java
+++ opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/controller/TppBankingApiTokenController.java
@@ -1,7 +1,5 @@
 package de.adorsys.opba.tppbankingapi.controller;
 
-import de.adorsys.opba.tppbankingapi.token.model.generated.PsuConsentSession;
-import de.adorsys.opba.tppbankingapi.token.model.generated.PsuConsentSessionResponse;
 import de.adorsys.opba.tppbankingapi.token.resource.generated.ConsentConfirmationApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,11 +12,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TppBankingApiTokenController implements ConsentConfirmationApi {
     @Override
-    public ResponseEntity<PsuConsentSessionResponse> confirmConsent(String authorization, UUID xRequestID, String redirectCode) {
-        PsuConsentSession psuConsentSession = new PsuConsentSession();
-        psuConsentSession.setValue("Confirmed");
-        PsuConsentSessionResponse psuConsentSessionResponse = new PsuConsentSessionResponse();
-        psuConsentSessionResponse.setPsuConsentSession(psuConsentSession);
-        return new ResponseEntity<>(psuConsentSessionResponse, HttpStatus.OK);
+    public ResponseEntity<Void> confirmConsent(String authorization, UUID xRequestID) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
