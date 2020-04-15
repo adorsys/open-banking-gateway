7d1e98a0199a60ce24f75960477bbec6365ead8c
electronicpeter <github@electronicpeter.de>
2020-04-15 21:39:53 +0200

    OBG-442 authid is now comming from ui

--- fintech-examples/fintech-ui/src/app/bank/list-accounts/list-accounts.component.ts
+++ fintech-examples/fintech-ui/src/app/bank/list-accounts/list-accounts.component.ts
@@ -3,6 +3,8 @@ import { AccountDetails } from '../../api';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AisService } from '../services/ais.service';
 import { RedirectStruct } from '../redirect-page/redirect-struct';
+import {HeaderConfig} from "../../models/consts";
+import {StorageService} from "../../services/storage.service";
 
 @Component({
   selector: 'app-list-accounts',
@@ -14,7 +16,7 @@ export class ListAccountsComponent implements OnInit {
   selectedAccount: string;
   bankId = '';
 
-  constructor(private router: Router, private route: ActivatedRoute, private aisService: AisService) {}
+  constructor(private router: Router, private route: ActivatedRoute, private aisService: AisService, private storageService: StorageService) {}
 
   ngOnInit() {
     this.bankId = this.route.snapshot.paramMap.get('bankid');
@@ -33,7 +35,8 @@ export class ListAccountsComponent implements OnInit {
     this.aisService.getAccounts(this.bankId).subscribe(response => {
       switch (response.status) {
         case 202:
-          const location = encodeURIComponent(response.headers.get('location'));
+          const location = encodeURIComponent(response.headers.get(HeaderConfig.HEADER_FIELD_LOCATION));
+          this.storageService.setAuthId(response.headers.get(HeaderConfig.HEADER_FIELD_AUTH_ID));
           const r = new RedirectStruct();
           r.okUrl = location;
           r.cancelUrl = this.router.url.replace('/account', '');
