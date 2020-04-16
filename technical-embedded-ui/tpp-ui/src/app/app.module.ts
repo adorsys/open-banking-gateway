a5dca4bfebca1ccbd9e2669a03e58e39ac4312ad
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-16 13:00:43 +0300

    OBG-550. Restored technical-ui

--- technical-embedded-ui/tpp-ui/src/app/app.module.ts
+++ technical-embedded-ui/tpp-ui/src/app/app.module.ts
@@ -1,35 +1,37 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ParametersInputComponent} from './parameters-input/parameters-input.component';
-import {DynamicFormComponent} from "./dynamic-form/dynamic-form.component";
-import {DynamicFormFactory} from "./dynamic-form/dynamic-form-factory";
-import {DynamicFormControlComponent} from "./dynamic-form/dynamic-form-control.component";
-import {HttpClientModule} from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ParametersInputComponent } from './parameters-input/parameters-input.component';
+import { DynamicFormComponent } from "./dynamic-form/dynamic-form.component";
+import { DynamicFormFactory } from "./dynamic-form/dynamic-form-factory";
+import { DynamicFormControlComponent } from "./dynamic-form/dynamic-form-control.component";
+import { HttpClientModule } from "@angular/common/http";
 import { ProvidePsuPasswordComponent } from './provide-psu-password/provide-psu-password.component';
 import { SelectScaMethodComponent } from './select-sca-method/select-sca-method.component';
 import { ReportScaResultComponent } from './report-sca-result/report-sca-result.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from "@angular/material/core";
 import { AccountSelectorComponent } from './account-selector/account-selector.component';
-import {MatRadioModule} from "@angular/material/radio";
+import { MatRadioModule } from "@angular/material/radio";
 import { AccountReferenceComponent } from './account-reference-selector/account-reference.component';
-import {MatInputModule} from "@angular/material/input";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatMomentDateModule, MomentDateAdapter} from '@angular/material-moment-adapter';
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatMomentDateModule, MomentDateAdapter } from '@angular/material-moment-adapter';
 import { ThreeStateCheckboxComponent } from './three-state-checkbox/three-state-checkbox.component';
 import { InitialRequestComponent } from './initial-request/initial-request.component';
 import { FromAspspComponent } from './from-aspsp/from-aspsp.component';
 import { EmbeddedStartComponent } from './embedded-start/embedded-start.component';
 import { FintechCallbackOkComponent } from './fintech-callback-ok/fintech-callback-ok.component';
-import {Globals} from "./globals";
+import { Globals } from "./globals";
 import { AppAisConsentComponent } from './app-ais-consent/app-ais-consent.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const AppDateFormats = {
   parse: {
@@ -59,7 +61,9 @@ export const AppDateFormats = {
     FromAspspComponent,
     EmbeddedStartComponent,
     FintechCallbackOkComponent,
-    AppAisConsentComponent
+    AppAisConsentComponent,
+    LoginComponent,
+    RegisterComponent
   ],
     imports: [
         BrowserModule,
