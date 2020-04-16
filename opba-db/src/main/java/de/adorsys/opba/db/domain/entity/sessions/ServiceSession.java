388386d9ad128df1ccc18df69586c18f035ad013
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-16 10:29:05 +0300

    OBG-550. Added audit too

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/sessions/ServiceSession.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/sessions/ServiceSession.java
@@ -10,9 +10,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +25,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Version;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.UUID;
 
@@ -30,6 +35,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ServiceSession implements IdAssignable<UUID>  {
 
     @Id
@@ -63,4 +69,10 @@ public class ServiceSession implements IdAssignable<UUID>  {
 
     @Version
     private int version;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant modifiedAt;
 }
