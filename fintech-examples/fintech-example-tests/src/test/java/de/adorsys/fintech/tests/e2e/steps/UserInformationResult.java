27cd1d7a6de40fba8073a8472ff4d91617baceed
Hergy Tchuinkou <htc@adorsys.de>
2020-04-16 12:05:50 +0200

    redirect and embedded

--- fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/steps/UserInformationResult.java
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/steps/UserInformationResult.java
@@ -9,9 +9,10 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 
+import static de.adorsys.fintech.tests.e2e.steps.FintechStagesUtils.withDefaultHeaders;
 import static de.adorsys.fintech.tests.e2e.steps.FintechStagesUtils.*;
-import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.ANTON_BRUECKNER;
 import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.withDefaultHeaders;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.*;
 
 
 @JGivenStage
@@ -29,6 +30,30 @@ public class UserInformationResult extends AccountInformationResult {
                                                          .header(SESSION_COOKIE, SESSION_COOKIE_VALUE)
                                                          .when()
                                                          .get(BANKPROFILE_ENDPOINT + BANK_ID_VALUE + ACCOUNT + ANTON_BRUECKNER_ID)
+                                                         .then()
+                                                         .statusCode(HttpStatus.OK.value())
+                                                         .extract();
+        this.respContent = response.body().asString();
+        return (UserInformationResult) self();
+    }
+
+    public UserInformationResult fintech_can_read_max_musterman_accounts_and_transactions() {
+        ExtractableResponse<Response> response = withDefaultHeaders(MAX_MUSTERMAN)
+                                                         .header(X_XSRF_TOKEN, X_XSRF_TOKEN_VALUE)
+                                                         .header(SESSION_COOKIE, SESSION_COOKIE_VALUE)
+                                                         .when()
+                                                         .get(BANKPROFILE_ENDPOINT + BANK_ID_VALUE + ACCOUNT + MAX_MUSTERMAN_ID)
+                                                         .then()
+                                                         .statusCode(HttpStatus.OK.value())
+                                                         .extract();
+        this.respContent = response.body().asString();
+        return (UserInformationResult) self();
+    }
+
+    public UserInformationResult fintech_navigates_back_to_login_after_user_logs_out() {
+        ExtractableResponse<Response> response = withDefaultHeaders()
+                                                         .when()
+                                                         .get(BANKSEARCH_LOGIN)
                                                          .then()
                                                          .statusCode(HttpStatus.OK.value())
                                                          .extract();
