14d431e46bed64211d649a9a749fff068c145f99
aos <aos@adorsys.de>
2020-04-16 12:52:19 +0200

    add login and register components. note: run npm scirpt in consent-ui: openapi-auth-gen

---
+++ consent-ui/src/app/utilities/helperfunctions.spec.ts
@@ -0,0 +1,7 @@
+import { Helperfunctions } from './helperfunctions';
+
+describe('Helperfunctions', () => {
+  it('should create an instance', () => {
+    expect(new Helperfunctions()).toBeTruthy();
+  });
+});
