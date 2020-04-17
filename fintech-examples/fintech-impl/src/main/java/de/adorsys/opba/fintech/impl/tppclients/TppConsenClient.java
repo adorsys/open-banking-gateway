bad084f35588a59fb5695794bd9ba860eb8306c0
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-17 16:02:50 +0200

    fintech server feign client fixes

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/tppclients/TppConsenClient.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/tppclients/TppConsenClient.java
@@ -1,8 +1,23 @@
 package de.adorsys.opba.fintech.impl.tppclients;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.opba.tpp.token.api.resource.generated.ConsentConfirmationApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @FeignClient(url = "${tpp.url}", name = "tppConsentClient")
 public interface TppConsenClient extends ConsentConfirmationApi {
+    default Optional<ObjectMapper> getObjectMapper() {
+        return Optional.empty();
+    }
+
+    default Optional<HttpServletRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    default Optional<String> getAcceptHeader() {
+        return getRequest().map(r -> r.getHeader("Accept"));
+    }
 }
