b5fda4276a2651f2a5031950f26d40c3e2dd8291
romanyelizarov <rye@adorsys.com.ua>
2020-04-17 10:53:16 +0300

    refactored signing/verifying configuration

--- opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/config/RequestVerifyingConfig.java
+++ opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/config/RequestVerifyingConfig.java
@@ -1,23 +1,23 @@
 package de.adorsys.opba.tppbankingapi.config;
 
+import de.adorsys.opba.api.security.EnableVerifySignatureBasedApiSecurity;
 import de.adorsys.opba.api.security.filter.RequestSignatureValidationFilter;
 import de.adorsys.opba.api.security.service.RequestVerifyingService;
 import de.adorsys.opba.api.security.service.impl.RsaJwtsVerifyingServiceImpl;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 import java.time.Duration;
 
+
 @Data
-@Configuration
-//@EnableVerifySignatureBasedApiSecurity
+@EnableVerifySignatureBasedApiSecurity
 @PropertySource("classpath:fintech-db.yml")
 @ConfigurationProperties(prefix = "fintech.verification")
-public class FinTechRequestVerificationConfig {
+public class RequestVerifyingConfig {
     private Duration requestTimeLimit;
 
     @Bean
