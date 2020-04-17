d6f664cf59e7cbfaf053e722391be8d4d505368c
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-17 20:21:55 +0300

    OBG-552. Fixed CPD/checkstyle issues

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/fintech/FintechPsuAspspPrvKeyInbox.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/fintech/FintechPsuAspspPrvKeyInbox.java
@@ -1,69 +1,19 @@
 package de.adorsys.opba.db.domain.entity.fintech;
 
-import de.adorsys.opba.db.domain.entity.Bank;
-import de.adorsys.opba.db.domain.entity.IdAssignable;
-import de.adorsys.opba.db.domain.entity.psu.Psu;
-import de.adorsys.opba.db.domain.generators.AssignedUuidGenerator;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import java.time.Instant;
-import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
-@Builder
+@SuperBuilder
 @AllArgsConstructor
-@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class FintechPsuAspspPrvKeyInbox implements IdAssignable<UUID> {
-
-    @Id
-    @GenericGenerator(
-            name = AssignedUuidGenerator.ASSIGNED_ID_GENERATOR,
-            strategy = AssignedUuidGenerator.ASSIGNED_ID_STRATEGY
-    )
-    @GeneratedValue(
-            generator = AssignedUuidGenerator.ASSIGNED_ID_GENERATOR,
-            strategy = GenerationType.AUTO
-    )
-    private UUID id;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Fintech fintech;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Psu psu;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Bank aspsp;
-
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
-    @Column(nullable = false)
-    private byte[] encData;
-
-    @CreatedDate
-    private Instant createdAt;
-
-    @LastModifiedDate
-    private Instant modifiedAt;
+public class FintechPsuAspspPrvKeyInbox extends FintechPsuAspspKey {
 }
