1e7eb88ca9d0306e381226b70b173e674d16f755
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-17 16:02:50 +0200

    fintech server feign client fixes

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/tppclients/TppAisClient.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/tppclients/TppAisClient.java
@@ -1,8 +1,23 @@
 package de.adorsys.opba.fintech.impl.tppclients;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.opba.tpp.ais.api.resource.generated.TppBankingApiAccountInformationServiceAisApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @FeignClient(url = "${tpp.url}", name = "tppAisClient")
 public interface TppAisClient extends TppBankingApiAccountInformationServiceAisApi {
+    default Optional<ObjectMapper> getObjectMapper() {
+        return Optional.empty();
+    }
+
+    default Optional<HttpServletRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    default Optional<String> getAcceptHeader() {
+        return getRequest().map(r -> r.getHeader("Accept"));
+    }
 }
--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/tppclients/TppBankSearchClient.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/tppclients/TppBankSearchClient.java
@@ -1,8 +1,23 @@
 package de.adorsys.opba.fintech.impl.tppclients;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.opba.tpp.banksearch.api.resource.generated.TppBankSearchApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @FeignClient(url = "${tpp.url}", name = "tppSearch")
 public interface TppBankSearchClient extends TppBankSearchApi {
+    default Optional<ObjectMapper> getObjectMapper() {
+        return Optional.empty();
+    }
+
+    default Optional<HttpServletRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    default Optional<String> getAcceptHeader() {
+        return getRequest().map(r -> r.getHeader("Accept"));
+    }
 }
