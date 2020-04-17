c2aa9294942afd120ebf74594d3bc8b4418de380
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-04-17 14:21:58 +0200

    format code

--- consent-ui/src/app/guards/auth.guard.ts
+++ consent-ui/src/app/guards/auth.guard.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { AuthService } from "../common/auth.service";
+import { AuthService } from '../common/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
 
-  constructor(
-    private authService: AuthService
-  ) {
-  }
-
-  canActivate():boolean {
+  canActivate(): boolean {
     return this.authService.isLoggedIn();
   }
-
 }
