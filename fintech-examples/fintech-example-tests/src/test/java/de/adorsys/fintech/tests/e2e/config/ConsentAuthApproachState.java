f9ae30a1820dd864519a1b8e04298e5482e93e4d
Hergy Tchuinkou <htc@adorsys.de>
2020-04-17 16:26:05 +0200

    comment resolved

--- fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/config/ConsentAuthApproachState.java
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/config/ConsentAuthApproachState.java
@@ -12,17 +12,18 @@ import java.util.List;
 
 @RequiredArgsConstructor
 public class ConsentAuthApproachState {
+
     private final String aspspProfileServerUri;
 
     private List<String> memoizedApproaches;
 
     public void memoize() {
         ExtractableResponse<Response> response = RestAssured
-                                                         .when()
-                                                         .get(aspspProfileServerUri + "/api/v1/aspsp-profile/sca-approaches")
-                                                         .then()
-                                                         .statusCode(HttpStatus.OK.value())
-                                                         .extract();
+                .when()
+                    .get(aspspProfileServerUri + "/api/v1/aspsp-profile/sca-approaches")
+                .then()
+                    .statusCode(HttpStatus.OK.value())
+                .extract();
 
         this.memoizedApproaches = response.body().as(new TypeRef<List<String>>() {});
     }
@@ -34,12 +35,12 @@ public class ConsentAuthApproachState {
 
         RestAssured
                 .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(memoizedApproaches)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(memoizedApproaches)
                 .when()
-                .put(aspspProfileServerUri + "/api/v1/aspsp-profile/for-debug/sca-approaches")
+                    .put(aspspProfileServerUri + "/api/v1/aspsp-profile/for-debug/sca-approaches")
                 .then()
-                .statusCode(HttpStatus.OK.value());
+                    .statusCode(HttpStatus.OK.value());
 
         this.memoizedApproaches = null;
     }
