4f4bff0fe666272722e6b5a23f4a60765d61a748
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-21 13:33:06 +0300

    OBG-557. Fixed UI tests + prod build

--- consent-ui/src/app/ais/entry-page/initiation/consent-sharing/consent-sharing.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/consent-sharing/consent-sharing.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConsentSharingComponent } from './consent-sharing.component';
+import { ActivatedRoute } from '@angular/router';
+import { StubUtilTests } from '../../../common/stub-util-tests';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ConsentSharingComponent', () => {
   let component: ConsentSharingComponent;
@@ -8,7 +11,20 @@ describe('ConsentSharingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ConsentSharingComponent]
+      declarations: [ConsentSharingComponent],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            parent: {
+              snapshot: { params: { authId: StubUtilTests.AUTH_ID } }
+            },
+            snapshot: {
+              queryParams: {}
+            }
+          }
+        }]
     }).compileComponents();
   }));
 
