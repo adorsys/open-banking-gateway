1b0872f3b53f9b4fb5a8c2ec45615f8b4021a4e9
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-04-21 09:04:26 +0200

    resolve conflicts

--- consent-ui/src/app/api-auth/encoder.ts
+++ consent-ui/src/app/api-auth/encoder.ts
@@ -5,17 +5,16 @@ import { HttpParameterCodec } from '@angular/common/http';
  * Workaround for https://github.com/angular/angular/issues/18261
  */
 export class CustomHttpParameterCodec implements HttpParameterCodec {
-    encodeKey(k: string): string {
-        return encodeURIComponent(k);
-    }
-    encodeValue(v: string): string {
-        return encodeURIComponent(v);
-    }
-    decodeKey(k: string): string {
-        return decodeURIComponent(k);
-    }
-    decodeValue(v: string): string {
-        return decodeURIComponent(v);
-    }
+  encodeKey(k: string): string {
+    return encodeURIComponent(k);
+  }
+  encodeValue(v: string): string {
+    return encodeURIComponent(v);
+  }
+  decodeKey(k: string): string {
+    return decodeURIComponent(k);
+  }
+  decodeValue(v: string): string {
+    return decodeURIComponent(v);
+  }
 }
-
