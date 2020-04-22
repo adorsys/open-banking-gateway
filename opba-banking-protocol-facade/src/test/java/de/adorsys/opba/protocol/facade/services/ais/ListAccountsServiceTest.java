ab1fc06bac7e74bb6eebc239adb88f2fa8102839
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-22 14:12:28 +0300

    OBG-599. Drop dependecy on xs2a protocol

--- opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListAccountsServiceTest.java
+++ opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListAccountsServiceTest.java
@@ -1,39 +1,51 @@
 package de.adorsys.opba.protocol.facade.services.ais;
 
 import de.adorsys.opba.db.config.EnableBankingPersistence;
+import de.adorsys.opba.protocol.api.ais.ListAccounts;
+import de.adorsys.opba.protocol.api.dto.context.ServiceContext;
 import de.adorsys.opba.protocol.api.dto.context.UserAgentContext;
 import de.adorsys.opba.protocol.api.dto.request.FacadeServiceableRequest;
 import de.adorsys.opba.protocol.api.dto.request.accounts.ListAccountsRequest;
+import de.adorsys.opba.protocol.api.dto.result.fromprotocol.dialog.AuthorizationRequiredResult;
 import de.adorsys.opba.protocol.facade.dto.result.torest.redirectable.FacadeStartAuthorizationResult;
-import de.adorsys.opba.protocol.xs2a.EnableXs2aProtocol;
+import de.adorsys.opba.protocol.facade.services.DbDropper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
+import java.net.URI;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * Note: This test keeps DB in dirty state - doesn't cleanup after itself.
  */
 @SuppressWarnings("CPD-START") // Same steps are used, but that's fine for readability
 @ActiveProfiles("test")
-@Sql(statements = "TRUNCATE opb_fintech CASCADE;")
 @SpringBootTest(classes = ListAccountsServiceTest.TestConfig.class)
-class ListAccountsServiceTest {
+class ListAccountsServiceTest extends DbDropper {
 
     @Autowired
     private ListAccountsService listAccountsService;
+
+    @MockBean(name = "xs2aListAccounts")
+    private ListAccounts listAccounts;
 
     @SuppressWarnings("PMD.AvoidUsingHardCodedIP") // This is a test
     @Test
     @SneakyThrows
     void testXs2aWired() {
+        when(listAccounts.execute(any(ServiceContext.class)))
+                .thenReturn(CompletableFuture.completedFuture(new AuthorizationRequiredResult<>(URI.create("http://example.com"), null)));
+
         assertThat(listAccountsService.execute(
                 ListAccountsRequest.builder()
                         .facadeServiceable(
@@ -52,7 +64,6 @@ class ListAccountsServiceTest {
         ).isInstanceOf(FacadeStartAuthorizationResult.class);
     }
 
-    @EnableXs2aProtocol
     @EnableBankingPersistence
     @SpringBootApplication(scanBasePackages = "de.adorsys.opba.protocol.facade")
     public static class TestConfig {
--- opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListTransactionsServiceTest.java
+++ opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/services/ais/ListTransactionsServiceTest.java
@@ -1,22 +1,30 @@
 package de.adorsys.opba.protocol.facade.services.ais;
 
 import de.adorsys.opba.db.config.EnableBankingPersistence;
+import de.adorsys.opba.protocol.api.ais.ListTransactions;
+import de.adorsys.opba.protocol.api.dto.context.ServiceContext;
 import de.adorsys.opba.protocol.api.dto.context.UserAgentContext;
 import de.adorsys.opba.protocol.api.dto.request.FacadeServiceableRequest;
 import de.adorsys.opba.protocol.api.dto.request.transactions.ListTransactionsRequest;
+import de.adorsys.opba.protocol.api.dto.result.fromprotocol.dialog.AuthorizationRequiredResult;
 import de.adorsys.opba.protocol.facade.dto.result.torest.redirectable.FacadeStartAuthorizationResult;
-import de.adorsys.opba.protocol.xs2a.EnableXs2aProtocol;
+import de.adorsys.opba.protocol.facade.services.DbDropper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.net.URI;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * Note: This test keeps DB in dirty state - doesn't cleanup after itself.
@@ -27,18 +35,23 @@ As we redefine list accounts for adorsys-sandbox bank to sandbox customary one
 (and it doesn't make sense to import sandbox module here) moving it back to plain xs2a bean:
  */
 @SuppressWarnings("CPD-START") // Same steps are used, but that's fine for readability
-@Sql(statements = "TRUNCATE opb_fintech CASCADE;")
 @Sql(statements = "UPDATE opb_bank_protocol SET protocol_bean_name = 'xs2aListTransactions' WHERE protocol_bean_name = 'xs2aSandboxListTransactions'")
 @SpringBootTest(classes = ListTransactionsServiceTest.TestConfig.class)
-class ListTransactionsServiceTest {
+class ListTransactionsServiceTest extends DbDropper {
 
     @Autowired
     private ListTransactionsService listTransactionsService;
+
+    @MockBean(name = "xs2aListTransactions")
+    private ListTransactions listTransactions;
 
     @SuppressWarnings("PMD.AvoidUsingHardCodedIP") // This is a test
     @Test
     @SneakyThrows
     void testXs2aWired() {
+        when(listTransactions.execute(any(ServiceContext.class)))
+                .thenReturn(CompletableFuture.completedFuture(new AuthorizationRequiredResult<>(URI.create("http://example.com"), null)));
+
         assertThat(listTransactionsService.execute(
                 ListTransactionsRequest.builder()
                         .facadeServiceable(
@@ -57,7 +70,6 @@ class ListTransactionsServiceTest {
         ).isInstanceOf(FacadeStartAuthorizationResult.class);
     }
 
-    @EnableXs2aProtocol
     @EnableBankingPersistence
     @SpringBootApplication(scanBasePackages = "de.adorsys.opba.protocol.facade")
     public static class TestConfig {
