27d50cb2fa397b6d26433b5ceb6a5dcf65bdd4c6
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-22 10:39:15 +0300

    OBG-599. Fixed API test

--- opba-protocols/xs2a-protocol-tests/xs2a-bdd-wiremock/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/wiremock/mocks/WiremockAccountInformationRequest.java
+++ opba-protocols/xs2a-protocol-tests/xs2a-bdd-wiremock/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/wiremock/mocks/WiremockAccountInformationRequest.java
@@ -46,7 +46,8 @@ public class WiremockAccountInformationRequest<SELF extends WiremockAccountInfor
                         wireMock.findAll(postRequestedFor(urlMatching("/v1/consents.*"))), it -> !it.isEmpty()
                 ).get(0);
 
-        // no consent UI for embedded test
+        // no consent UI for embedded test - calling backend directly, but because application is configured to use
+        // embedded-server as the request path (for consistent cookie paths) - replacing that URL segment
         this.redirectOkUri = consentInitiateRequest.getHeader(TPP_REDIRECT_URI).replace("/embedded-server/", "/");
         ExtractableResponse<Response> response = RestAssured
                 .given()
