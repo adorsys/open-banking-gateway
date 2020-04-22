79eaabed5f7d009aa6b62eadafbaa6d21e7273af
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-22 18:34:17 +0300

    OBG-415. Upgraded Xs2a adapter version

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/Xs2aResultBodyExtractor.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/Xs2aResultBodyExtractor.java
@@ -1,9 +1,11 @@
 package de.adorsys.opba.protocol.xs2a.entrypoint;
 
+import com.google.common.base.Strings;
 import de.adorsys.opba.protocol.api.dto.result.body.AccountListBody;
 import de.adorsys.opba.protocol.api.dto.result.body.TransactionsResponseBody;
 import de.adorsys.opba.protocol.xs2a.domain.dto.messages.ProcessResponse;
 import de.adorsys.xs2a.adapter.service.model.AccountListHolder;
+import de.adorsys.xs2a.adapter.service.model.RemittanceInformationStructured;
 import de.adorsys.xs2a.adapter.service.model.TransactionsReport;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
@@ -30,5 +32,26 @@ public class Xs2aResultBodyExtractor {
     public interface Xs2aToFacadeMapper {
         AccountListBody map(AccountListHolder accountList);
         TransactionsResponseBody map(TransactionsReport transactions);
+
+        default String map(RemittanceInformationStructured value) {
+            if (null == value) {
+                return null;
+            }
+
+            StringBuilder builder = new StringBuilder();
+            append(builder, value.getReferenceType());
+            append(builder, value.getReferenceIssuer());
+            append(builder, value.getReference());
+            return builder.toString();
+        }
+
+        default void append(StringBuilder builder, String referenceType) {
+            if (!Strings.isNullOrEmpty(referenceType)) {
+               return;
+            }
+
+            builder.append(referenceType);
+            builder.append(":");
+        }
     }
 }
