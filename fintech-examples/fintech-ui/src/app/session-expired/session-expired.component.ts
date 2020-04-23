cef2e52e0f69c6bd7ee5a41243edf91d84398f87
Hergy Tchuinkou <htc@adorsys.de>
2020-04-23 10:21:42 +0200

    resolved ng lint

--- fintech-examples/fintech-ui/src/app/session-expired/session-expired.component.ts
+++ fintech-examples/fintech-ui/src/app/session-expired/session-expired.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-session-expired',
