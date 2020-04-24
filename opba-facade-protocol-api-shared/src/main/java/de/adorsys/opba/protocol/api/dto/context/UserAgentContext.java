227728294d3f7a4d36453d959efccdf7efbf8583
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-24 19:14:48 +0300

    OBG-609. Added more JavaDocs to API side

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/context/UserAgentContext.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/context/UserAgentContext.java
@@ -12,14 +12,49 @@ import java.util.Map;
 @AllArgsConstructor
 public class UserAgentContext {
 
+    /**
+     * The IP address of PSUs browser, mobile phone, etc. (request originator).
+     */
     private final String psuIpAddress;
+
+    /**
+     * The port of PSUs browser, mobile phone, etc. (request originator).
+     */
     private final String psuIpPort;
+
+    /**
+     * Accepted MIME type by PSU. Currently application/json is implied.
+     */
     private final String psuAccept;
+
+    /**
+     * PSUs' accepted charset. Currently UTF-8 implied.
+     */
     private final String psuAcceptCharset;
+
+    /**
+     * PSUs' accepted encoding.
+     */
     private final String psuAcceptEncoding;
+
+    /**
+     * PSUs' language of communication. Currently English is implied.
+     */
     private final String psuAcceptLanguage;
+
+    /**
+     * The ID of PSUs' device (i.e. can be IMEI for mobile device)
+     */
     private final String psuDeviceId;
+
+    /**
+     * PSUs' User-Agent string.
+     */
     private final String psuUserAgent;
+
+    /**
+     * PSU's geo location.
+     */
     private final String psuGeoLocation;
     private final String psuHttpMethod;
 
--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/context/UserAgentExtras.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/context/UserAgentExtras.java
@@ -1,4 +1,7 @@
 package de.adorsys.opba.protocol.api.dto.context;
 
+/**
+ * Extra information about PSUs UserAgent. Is used to extend {@link UserAgentContext} with custom values.
+ */
 public enum UserAgentExtras {
 }
