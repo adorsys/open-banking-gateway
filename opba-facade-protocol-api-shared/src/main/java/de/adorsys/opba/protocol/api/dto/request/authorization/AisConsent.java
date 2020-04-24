0c026c9450f277c77c350bd3b453e27f11edf4c1
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-24 20:06:25 +0300

    OBG-609. Added more JavaDocs to API for facade serviceable

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/request/authorization/AisConsent.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/request/authorization/AisConsent.java
@@ -4,13 +4,34 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
+/**
+ * AIS services access consent.
+ */
 @Data
 public class AisConsent {
 
+    /**
+     * Account, transactions, balances access specification.
+     */
     private AisAccountAccess access;
+
+    /**
+     * How frequent per day this consent can be used.
+     */
     private Integer frequencyPerDay;
+
+    /**
+     * Is this consent for recurring access.
+     */
     private Boolean recurringIndicator;
+
+    /**
+     * Is this consent for combined service.
+     */
     private Boolean combinedServiceIndicator;
-    private String psuId;
+
+    /**
+     * Due date of this consent.
+     */
     private LocalDate validUntil;
 }
