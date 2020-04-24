c67e892e98002c3b2da9333596189bbf87819055
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-24 20:54:34 +0300

    OBG-609. Added more JavaDocs to API for facade shared

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/result/body/TransactionsResponseBody.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/result/body/TransactionsResponseBody.java
@@ -3,10 +3,21 @@ package de.adorsys.opba.protocol.api.dto.result.body;
 import lombok.Builder;
 import lombok.Value;
 
+/**
+ * Transaction result list transactions result from protocol.
+ */
 @Value
 @Builder
 public class TransactionsResponseBody implements ResultBody {
+
+  /**
+   * Account on which the transactions happened.
+   */
   private AccountReference account;
+
+  /**
+   * List of the transactions.
+   */
   private AccountReport transactions;
 
   @Override
--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/result/fromprotocol/ok/SuccessResult.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/result/fromprotocol/ok/SuccessResult.java
@@ -4,9 +4,15 @@ import de.adorsys.opba.protocol.api.dto.result.fromprotocol.Result;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Protocol result that represents operation was OK and contains result as a body (i.e. PSU account list).
+ */
 @Data
 @AllArgsConstructor
 public class SuccessResult<T> implements Result<T> {
 
+    /**
+     * Operation result (i.e. account list).
+     */
     private T body;
 }
