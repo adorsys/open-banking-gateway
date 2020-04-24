e89ee6afbaa591b4231d6063eef5968a3fd7bfef
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-24 19:14:48 +0300

    OBG-609. Added more JavaDocs to API side

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/common/CurrentBankProfile.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/common/CurrentBankProfile.java
@@ -2,12 +2,38 @@ package de.adorsys.opba.protocol.api.common;
 
 import java.util.List;
 
+/**
+ * View of current bank that is provided to protocol.
+ */
 public interface CurrentBankProfile {
 
+    /**
+     * Bank protocol ID.
+     */
     Long getId();
+
+    /**
+     * ASPSP API endpoint URL for this protocol.
+     */
     String getUrl();
+
+    /**
+     * The ID of ASPSP API adapter to use (if defined).
+     */
     String getAdapterId();
+
+    /**
+     * Identity provider URL for ASPSP API.
+     */
     String getIdpUrl();
+
+    /**
+     * Supported SCA authorization approaches by this ASPSP.
+     */
     List<Approach> getScaApproaches();
+
+    /**
+     * Preferred SCA approach for this ASPSP.
+     */
     Approach getPreferredApproach();
 }
