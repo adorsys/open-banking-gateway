d836903c5c9516a8e1d274d72d95d034c9381f69
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-04-24 08:37:29 +0200

    OBG-285 fix mocked json

--- opba-protocols/xs2a-protocol-tests/xs2a-bdd-tests-common/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/stages/AccountInformationRequestCommon.java
+++ opba-protocols/xs2a-protocol-tests/xs2a-bdd-tests-common/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/stages/AccountInformationRequestCommon.java
@@ -9,6 +9,7 @@ import com.tngtech.jgiven.integration.spring.JGivenStage;
 import de.adorsys.opba.api.security.external.service.RequestSigningService;
 import de.adorsys.opba.consentapi.model.generated.InlineResponse200;
 import de.adorsys.opba.consentapi.model.generated.ScaUserData;
+import de.adorsys.opba.protocol.api.common.Approach;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -174,12 +175,11 @@ public class AccountInformationRequestCommon<SELF extends AccountInformationRequ
         return self();
     }
 
-    public SELF user_anton_brueckner_provided_initial_parameters_to_list_accounts_with_all_accounts_consent_without_psu_id() {
-        startInitialInternalConsentAuthorization(
-                AUTHORIZE_CONSENT_ENDPOINT,
-                "restrecord/tpp-ui-input/params/anton-brueckner-account-all-accounts-consent-without-psu-id.json"
-        );
-
+    public SELF user_anton_brueckner_provided_initial_parameters_to_list_accounts_with_all_accounts_consent_without_psu_id(Approach expectedApproach) {
+        String resource = expectedApproach == Approach.EMBEDDED
+                ? "restrecord/tpp-ui-input/params/anton-brueckner-account-all-accounts-consent.json"
+                : "restrecord/tpp-ui-input/params/anton-brueckner-account-all-accounts-consent-without-psu-id.json";
+        startInitialInternalConsentAuthorization(AUTHORIZE_CONSENT_ENDPOINT, resource);
         return self();
     }
 
