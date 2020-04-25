67635d697f17aba7c8ebe6171869b06ae5c98795
peter <peter@admins-MBP-2.fritz.box>
2020-04-25 19:48:14 +0200

    obg 442 fix ui test

--- fintech-examples/fintech-ui/src/app/bank/redirect-page/redirect-page.component.spec.ts
+++ fintech-examples/fintech-ui/src/app/bank/redirect-page/redirect-page.component.spec.ts
@@ -1,10 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ActivatedRoute} from '@angular/router';
+import {of} from 'rxjs';
 
-import { RedirectPageComponent } from './redirect-page.component';
-import { RedirectCardComponent } from '../redirect-card/redirect-card.component';
+
+import {RedirectPageComponent} from './redirect-page.component';
+import {RedirectCardComponent} from '../redirect-card/redirect-card.component';
+import {StorageService} from '../../services/storage.service';
+import {ConsentAuthorizationService} from '../services/consent-authorization.service';
 
 describe('RedirectPageComponent', () => {
   let component: RedirectPageComponent;
@@ -21,13 +24,25 @@ describe('RedirectPageComponent', () => {
             {
               provide: ActivatedRoute,
               useValue: {
-                params: of({ location: 'adorsys.de' }),
+                params: of({location: 'adorsys.de'}),
                 paramMap: {
                   subscribe(location: string): string {
                     return 'adorsys.de';
                   }
                 }
               }
+            },
+            {
+              provide: StorageService,
+              useValue: {
+                getBankName(): string {
+                  return 'peters bank';
+
+                }
+              }
+            },
+            {
+              provide: ConsentAuthorizationService
             }
           ]
         }
