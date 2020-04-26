fd25f26eb4bec8d4899e0a0c69c8c7559a3567bf
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-26 11:40:26 +0300

    OBG-609. Added more JavaDoc to facade shared package

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/scoped/aspsp/UsesCurrentAspspProfile.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/scoped/aspsp/UsesCurrentAspspProfile.java
@@ -2,7 +2,14 @@ package de.adorsys.opba.protocol.api.services.scoped.aspsp;
 
 import de.adorsys.opba.protocol.api.common.CurrentBankProfile;
 
+/**
+ * Protocol facing current ASPSP profile access object. Provides access to bank profile that is used for execution.
+ */
 public interface UsesCurrentAspspProfile {
 
+    /**
+     * @return Bank profile (ASPSP profile {@code de.adorsys.opba.db.domain.entity.BankProfile}) that was selected for
+     * current protocol execution.
+     */
     CurrentBankProfile aspspProfile();
 }
--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/scoped/encryption/UsesEncryptionService.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/scoped/encryption/UsesEncryptionService.java
@@ -2,7 +2,14 @@ package de.adorsys.opba.protocol.api.services.scoped.encryption;
 
 import de.adorsys.opba.protocol.api.services.EncryptionService;
 
+/**
+ * Protocol facing encryption access to persist intermediate data that is necessary to execute the protocol.
+ */
 public interface UsesEncryptionService {
 
+    /**
+     * Encryption service to encrypt and decrypt intermediate data that protocol needs to store. For example, protocol
+     * execution context like next step, next redirection URL, etc.
+     */
     EncryptionService encryption();
 }
