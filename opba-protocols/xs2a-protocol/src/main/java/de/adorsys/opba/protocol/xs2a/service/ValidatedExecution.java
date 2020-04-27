99bfa33eb1c5d3e033f7150e8ad3e40fb47088ee
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-27 09:09:50 +0300

    OBG-609. Expanded JavaDoc in xs2a protocol

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/ValidatedExecution.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/ValidatedExecution.java
@@ -8,9 +8,17 @@ import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.JavaDelegate;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Class that provides generic functionality for Services that can be called in Validation({@link ContextMode#MOCK_REAL_CALLS}
+ * execution mode and Real ASPSP API calls ({@link ContextMode#REAL_CALLS}.
+ * @param <T> Context type
+ */
 @RequiredArgsConstructor
 public abstract class ValidatedExecution<T extends BaseContext> implements JavaDelegate {
 
+    /**
+     * Entrypoint for Flowable BPMN to call the service.
+     */
     @Override
     @Transactional(noRollbackFor = BpmnError.class)
     public void execute(DelegateExecution execution) {
@@ -27,17 +35,34 @@ public abstract class ValidatedExecution<T extends BaseContext> implements JavaD
         doAfterCall(execution, context);
     }
 
+    /**
+     * Used to update BPMN context before any execution.
+     */
     protected void doPrepareContext(DelegateExecution execution, T context) {
     }
 
+    /**
+     * Validation function template for BPMN context. Should check (if necessary) that are parameters
+     * required to call ASPSP are present in context.
+     */
     protected void doValidate(DelegateExecution execution, T context) {
     }
 
+    /**
+     * Real ASPSP call function template.
+     */
     protected abstract void doRealExecution(DelegateExecution execution, T context);
 
+    /**
+     * Mock ASPSP API function call template. Used within validation process to imitate ASPSP responses, so
+     * that certain internal parameters can be provided to context as the API input.
+     */
     protected void doMockedExecution(DelegateExecution execution, T context) {
     }
 
+    /**
+     * Is called as a finishing operation to i.e. validate context after method call.
+     */
     protected void doAfterCall(DelegateExecution execution, T context) {
     }
 }
