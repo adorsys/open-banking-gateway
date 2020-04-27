2888cc8d9fa4d3db02555fa90240d4e9f12d2edd
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-27 09:09:50 +0300

    OBG-609. Expanded JavaDoc in xs2a protocol

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/validation/ValidationErrors.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/validation/ValidationErrors.java
@@ -4,6 +4,10 @@ import de.adorsys.opba.protocol.xs2a.service.xs2a.context.BaseContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Checker to validate that current context is validation-error free (all necessary parameters are in place)
+ * and we can proceed with ASPSP API calls in same order as it was for validation.
+ */
 @Service("validationErrors")
 @RequiredArgsConstructor
 public class ValidationErrors {
