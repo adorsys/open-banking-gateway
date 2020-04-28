6422599783398a16d68d16886593e8af26f7686a
aos <aos@adorsys.de>
2020-04-28 11:11:12 +0200

    solve problems with conflicts

--- consent-ui/src/app/api-auth/api.module.ts
+++ consent-ui/src/app/api-auth/api.module.ts
@@ -1,33 +1,35 @@
-import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
 import { Configuration } from './configuration';
 import { HttpClient } from '@angular/common/http';
+
 
 import { PsuAuthenticationService } from './api/psuAuthentication.service';
 import { PsuAuthenticationAndConsentApprovalService } from './api/psuAuthenticationAndConsentApproval.service';
 
 @NgModule({
-  imports: [],
+  imports:      [],
   declarations: [],
-  exports: [],
-  providers: [PsuAuthenticationService, PsuAuthenticationAndConsentApprovalService]
+  exports:      [],
+  providers: [
+    PsuAuthenticationService,
+    PsuAuthenticationAndConsentApprovalService ]
 })
 export class ApiModule {
-  public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
-    return {
-      ngModule: ApiModule,
-      providers: [{ provide: Configuration, useFactory: configurationFactory }]
-    };
-  }
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
 
-  constructor(@Optional() @SkipSelf() parentModule: ApiModule, @Optional() http: HttpClient) {
-    if (parentModule) {
-      throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
     }
-    if (!http) {
-      throw new Error(
-        'You need to import the HttpClientModule in your AppModule! \n' +
-          'See also https://github.com/angular/angular/issues/20575'
-      );
-    }
-  }
 }
