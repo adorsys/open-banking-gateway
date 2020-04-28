a33858d2abaa279fd20635ac791eee5eae74597f
Hergy Tchuinkou <htc@adorsys.de>
2020-04-28 09:49:06 +0200

    made id dynamically mapped

--- fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/steps/FintechStagesUtils.java
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/steps/FintechStagesUtils.java
@@ -12,11 +12,9 @@ public class FintechStagesUtils {
     public static String USERNAME = "bob";
     public static final String X_XSRF_TOKEN = "xsrfToken";
     public static final String SESSION_COOKIE = "sessionCookie";
-    public static final String BANK_ID_VALUE = "53c47f54-b9a4-465a-8f77-bc6cd5f0cf46";
     public static final String ACCOUNT = "account/";
     public static final String BANKPROFILE_ENDPOINT = "/bank/";
     public static final String BANKSEARCH_LOGIN = "/login";
-    public static final String BANKSEARCH = "/search";
 
     public static RequestSpecification withDefaultHeaders() {
         return RestAssured.given().header(X_REQUEST_ID, UUID.randomUUID().toString());
