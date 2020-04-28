3551d5f31c8bdcd5ef2bb85b8322c358f3a2afa6
aos <aos@adorsys.de>
2020-04-28 11:11:12 +0200

    solve problems with conflicts

--- consent-ui/src/app/api-auth/api/api.ts
+++ consent-ui/src/app/api-auth/api/api.ts
@@ -1,6 +1,5 @@
 export * from './psuAuthentication.service';
 import { PsuAuthenticationService } from './psuAuthentication.service';
-import { PsuAuthenticationAndConsentApprovalService } from './psuAuthenticationAndConsentApproval.service';
-
 export * from './psuAuthenticationAndConsentApproval.service';
+import { PsuAuthenticationAndConsentApprovalService } from './psuAuthenticationAndConsentApproval.service';
 export const APIS = [PsuAuthenticationService, PsuAuthenticationAndConsentApprovalService];
--- consent-ui/src/app/api-auth/encoder.ts
+++ consent-ui/src/app/api-auth/encoder.ts
@@ -5,16 +5,17 @@ import { HttpParameterCodec } from '@angular/common/http';
  * Workaround for https://github.com/angular/angular/issues/18261
  */
 export class CustomHttpParameterCodec implements HttpParameterCodec {
-  encodeKey(k: string): string {
-    return encodeURIComponent(k);
-  }
-  encodeValue(v: string): string {
-    return encodeURIComponent(v);
-  }
-  decodeKey(k: string): string {
-    return decodeURIComponent(k);
-  }
-  decodeValue(v: string): string {
-    return decodeURIComponent(v);
-  }
+    encodeKey(k: string): string {
+        return encodeURIComponent(k);
+    }
+    encodeValue(v: string): string {
+        return encodeURIComponent(v);
+    }
+    decodeKey(k: string): string {
+        return decodeURIComponent(k);
+    }
+    decodeValue(v: string): string {
+        return decodeURIComponent(v);
+    }
 }
+
