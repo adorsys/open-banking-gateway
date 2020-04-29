d6db7b0a7943df6134536f638f324f9950b311d9
Vladimir Nechai <vne@adorsys.de>
2020-04-29 13:39:56 +0200

    add modal service

--- fintech-examples/fintech-ui/src/app/common/share.module.ts
+++ fintech-examples/fintech-ui/src/app/common/share.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { InfoModule } from '../errorsHandler/info/info.module';
-import { SearchComponent } from './search/search.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { InfoModule } from "../errorsHandler/info/info.module";
+import { SearchComponent } from "./search/search.component";
+import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule],
-  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, SearchComponent]
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, NgbModalModule],
+  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, SearchComponent, NgbModalModule]
 })
-export class ShareModule {}
+export class ShareModule {
+}
