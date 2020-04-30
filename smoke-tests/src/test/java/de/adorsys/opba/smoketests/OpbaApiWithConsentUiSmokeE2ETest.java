8cd3feb6032eecba5e07dfe78ec1f18ebe465f6b
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-30 08:44:00 +0300

    OBG-643. Fixed heavy tests and added profile to smoke tests for local execution

--- smoke-tests/src/test/java/de/adorsys/opba/smoketests/OpbaApiWithConsentUiSmokeE2ETest.java
+++ smoke-tests/src/test/java/de/adorsys/opba/smoketests/OpbaApiWithConsentUiSmokeE2ETest.java
@@ -39,6 +39,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @EnabledIfEnvironmentVariable(named = ENABLE_SMOKE_TESTS, matches = TRUE_BOOL)
 @ExtendWith({SeleniumExtension.class, WebDriverErrorReportAspectAndWatcher.class})
 @SpringBootTest(classes = {JGivenConfig.class, SmokeConfig.class, FintechRequestSigningTestConfig.class}, webEnvironment = NONE)
+// Use @ActiveProfiles(profiles = {SMOKE_TEST, "test-smoke-local"}) to run the test on local env.
 @ActiveProfiles(profiles = {SMOKE_TEST})
 class OpbaApiWithConsentUiSmokeE2ETest extends SpringScenarioTest<SandboxServers, WebDriverBasedAccountInformation<? extends WebDriverBasedAccountInformation<?>>, AccountInformationResult> {
 
