e4521f6d25410435879b820e8e8a5ff342fa4a48
valb3r <valentyn.berezin@adorsys.com.ua>
2020-04-30 11:18:25 +0300

    Configured upload flags

--- scripts/upload_coverage_to_codecov.sh
+++ scripts/upload_coverage_to_codecov.sh
@@ -7,5 +7,6 @@ if [[ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]]; then
 fi
 
 echo "Sending test results to codecov using $BRANCH"
-bash <(curl -s https://codecov.io/bash) -f '!*datasafe-business*' -F backend -B "$BRANCH";
-bash <(curl -s https://codecov.io/bash) -s '*datasafe-business*' -F frontend  -B "$BRANCH";
+bash <(curl -s https://codecov.io/bash) -s './last-module-codecoverage*' -F backend  -B "$BRANCH";
+bash <(curl -s https://codecov.io/bash) -s './fintech-examples/fintech-ui*' -F frontend -B "$BRANCH";
+bash <(curl -s https://codecov.io/bash) -s './consent-ui*' -F frontend -B "$BRANCH";
