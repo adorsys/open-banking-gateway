2d5ca91d4c1b7da08ef8bc43fc62bf8903617849
Hergy Tchuinkou <htc@adorsys.de>
2020-04-30 18:09:53 +0200

    added test-config

--- fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/FintechConsentUiSmokeE2ETest.java
+++ fintech-examples/fintech-example-tests/src/test/java/de/adorsys/fintech/tests/e2e/FintechConsentUiSmokeE2ETest.java
@@ -1,12 +1,15 @@
 package de.adorsys.fintech.tests.e2e;
 
-import com.tngtech.jgiven.integration.spring.JGivenStage;
+import com.jayway.jsonpath.JsonPath;
 import com.tngtech.jgiven.integration.spring.junit5.SpringScenarioTest;
 import de.adorsys.fintech.tests.e2e.config.ConsentAuthApproachState;
 import de.adorsys.fintech.tests.e2e.config.SmokeConfig;
 import de.adorsys.fintech.tests.e2e.steps.FintechServer;
 import de.adorsys.fintech.tests.e2e.steps.UserInformationResult;
 import de.adorsys.fintech.tests.e2e.steps.WebDriverBasedUserInfoFintech;
+import de.adorsys.opba.api.security.config.TppTokenProperties;
+import de.adorsys.opba.api.security.service.RequestSigningService;
+import de.adorsys.opba.protocol.xs2a.tests.e2e.JGivenConfig;
 import io.github.bonigarcia.seljup.SeleniumExtension;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.SneakyThrows;
@@ -14,20 +17,23 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.net.URI;
 
+import static de.adorsys.opba.protocol.xs2a.tests.Const.ENABLE_SMOKE_TESTS;
+import static de.adorsys.opba.protocol.xs2a.tests.Const.TRUE_BOOL;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AisStagesCommonUtil.ANTON_BRUECKNER;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
-@JGivenStage
-@ActiveProfiles("test-mocked-fintech")
-@ExtendWith(SeleniumExtension.class)
-@SpringBootTest(classes = JGivenConfig.class, webEnvironment = NONE)
+@EnabledIfEnvironmentVariable(named = ENABLE_SMOKE_TESTS, matches = TRUE_BOOL)
+@ExtendWith({SeleniumExtension.class})
+@SpringBootTest(classes = {JGivenConfig.class, SmokeConfig.class}, webEnvironment = NONE)
 public class FintechConsentUiSmokeE2ETest extends SpringScenarioTest<FintechServer, WebDriverBasedUserInfoFintech<? extends WebDriverBasedUserInfoFintech<?>>, UserInformationResult> {
 
     @Autowired
@@ -35,6 +41,12 @@ public class FintechConsentUiSmokeE2ETest extends SpringScenarioTest<FintechServ
 
     @Autowired
     private ConsentAuthApproachState state;
+
+    @MockBean
+    private RequestSigningService requestSigningService;
+
+    @MockBean
+    private TppTokenProperties tppTokenProperties;
 
     @BeforeEach
     void memoizeConsentAuthorizationPreference() {
@@ -91,11 +103,14 @@ public class FintechConsentUiSmokeE2ETest extends SpringScenarioTest<FintechServ
                 .and()
                 .user_sees_account_and_list_transactions(firefoxDriver);
 
-        String userId = getInfoId(firefoxDriver);
-        String bankId = getBankInfoId(firefoxDriver);
+        String  accountResourceId  = JsonPath.parse(afterLoginAndAuthorizeAccountGetAccountId(firefoxDriver, ANTON_BRUECKNER))
+                                             .read("$.accounts[0].resourceId");
+
+        String bankId = JsonPath.parse(loginAndGetBankProfile(firefoxDriver, ANTON_BRUECKNER))
+                                 .read("$(bankDescriptor[0].uuid)");
 
         then()
-                .fintech_can_read_anton_brueckner_accounts_and_transactions(userId, bankId);
+                .fintech_can_read_anton_brueckner_accounts_and_transactions( accountResourceId , bankId);
     }
 
     @Test
@@ -242,4 +257,57 @@ public class FintechConsentUiSmokeE2ETest extends SpringScenarioTest<FintechServ
         String path = uri.getPath();
         return path.substring(path.lastIndexOf("/bank/") + 1);
     }
+
+
+    private String loginAndGetBankProfile(FirefoxDriver firefoxDriver, String username) {
+        given().enabled_redirect_sandbox_mode(smokeConfig.getAspspProfileServerUri())
+                .fintech_points_to_fintechui_login_page(smokeConfig.getFintechServerUri());
+        when().user_opens_fintechui_login_page(firefoxDriver)
+                .and()
+                .user_login_with_its_credentials(firefoxDriver)
+                .and()
+                .user_confirm_login(firefoxDriver)
+                .and()
+                .user_navigates_to_page(firefoxDriver)
+                .and()
+                .user_looks_for_a_bank_in_the_bank_search_input_place(firefoxDriver)
+                .and()
+                .user_wait_for_the_result_in_bank_search(firefoxDriver);
+
+        UserInformationResult result = then().fintech_get_bank_infos(username);
+
+        return result.getRespContent();
+    }
+
+    private String afterLoginAndAuthorizeAccountGetAccountId(FirefoxDriver firefoxDriver, String username) {
+        given().enabled_redirect_sandbox_mode(smokeConfig.getAspspProfileServerUri())
+                .fintech_points_to_fintechui_login_page(smokeConfig.getFintechServerUri());
+
+        when().user_opens_fintechui_login_page(firefoxDriver)
+                .and()
+                .user_login_with_its_credentials(firefoxDriver)
+                .and()
+                .user_confirm_login(firefoxDriver)
+                .and()
+                .user_navigates_to_page(firefoxDriver)
+                .and()
+                .user_looks_for_a_bank_in_the_bank_search_input_place(firefoxDriver)
+                .and()
+                .user_wait_for_the_result_in_bank_search(firefoxDriver)
+                .and()
+                .user_navigates_to_page(firefoxDriver)
+                .and()
+                .user_select_account_button(firefoxDriver)
+                .and()
+                .user_sees_account_and_list_transactions(firefoxDriver);
+        UserInformationResult result = then().fintech_get_user_infos(username);
+
+        return result.getRespContent();
+    }
+
+
+
+
+
+
 }
