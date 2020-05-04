942fdcbe9a2ef8512fe04739d8a9fcb217af5d86
Vladimir Nechai <vne@adorsys.de>
2020-05-04 10:53:21 +0200

    fix lint issues

--- fintech-examples/fintech-ui/src/app/common/share.module.ts
+++ fintech-examples/fintech-ui/src/app/common/share.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { InfoModule } from "../errorsHandler/info/info.module";
-import { SearchComponent } from "./search/search.component";
-import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InfoModule } from '../errorsHandler/info/info.module';
+import { SearchComponent } from './search/search.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [SearchComponent],
