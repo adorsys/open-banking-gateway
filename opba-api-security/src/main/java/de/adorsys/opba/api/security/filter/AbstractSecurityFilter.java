8c2eb229462333e38b1f8e93b5ccb96c79920313
iiuex <iiu@adorsys.com.ua>
2020-05-05 09:03:07 +0300

    OBG:630 Operation type validation filter added

---
+++ opba-api-security/src/main/java/de/adorsys/opba/api/security/filter/AbstractSecurityFilter.java
@@ -0,0 +1,19 @@
+package de.adorsys.opba.api.security.filter;
+
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.http.HttpServletRequest;
+
+public abstract class AbstractSecurityFilter extends OncePerRequestFilter {
+    public static final String OPBA_BANKING_PATH = "/v1/banking/**";
+
+    private final AntPathMatcher matcher = new AntPathMatcher();
+    private final UrlPathHelper pathHelper = new UrlPathHelper();
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !matcher.match(OPBA_BANKING_PATH, pathHelper.getPathWithinApplication(request));
+    }
+}
