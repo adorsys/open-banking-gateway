bdacb1c275a460c7403cfe6bdc068b80ef8566fb
electronicpeter <github@electronicpeter.de>
2020-05-08 09:37:32 +0200

    OBG 632 timer logic cleared

--- consent-ui/src/app/ais/common/cookie-renewal/CookieRenewalService.ts
+++ consent-ui/src/app/ais/common/cookie-renewal/CookieRenewalService.ts
@@ -1,60 +1,48 @@
-import {Injectable} from '@angular/core';
-import {SimpleTimer} from 'ng2-simple-timer';
-import {PsuAuthenticationService} from '../../../api-auth';
+import { Injectable } from '@angular/core';
+import { SimpleTimer } from 'ng2-simple-timer';
+import { PsuAuthenticationService } from '../../../api-auth';
 import * as uuid from 'uuid';
-import {ApiHeaders} from '../../../api/api.headers';
+import { ApiHeaders } from '../../../api/api.headers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CookieRenewalService {
-
   public static TIMER_NAME = 'cookie-renewal-timer';
   public static ROUTE = 'login';
 
-  constructor(private simpleTimer: SimpleTimer, private psuAuthService: PsuAuthenticationService) {
-  }
-
+  constructor(private simpleTimer: SimpleTimer, private psuAuthService: PsuAuthenticationService) {}
 
   activate(authid): void {
-    this.simpleTimer.newTimerCD(CookieRenewalService.TIMER_NAME, 1, 1);
+    const timer = this.getTimer();
+    console.log(new Date().toLocaleString() + ' activate timer in ' + timer);
+    this.simpleTimer.newTimerCD(CookieRenewalService.TIMER_NAME, timer, timer);
     this.simpleTimer.subscribe(CookieRenewalService.TIMER_NAME, () => this.cookieRenewal(authid));
   }
 
   cookieRenewal(authid): void {
+    console.log(new Date().toLocaleString() + ' timer rings, request for cookie renewal');
 
-    const ttl = this.getTTL();
-    if (ttl === 0) {
-      return;
-    }
-
-    console.log(new Date().toLocaleString() + ' delete old timer');
+    // delete old timer
     this.simpleTimer.unsubscribe(CookieRenewalService.TIMER_NAME);
     this.simpleTimer.delTimer(CookieRenewalService.TIMER_NAME);
 
     // timer is deleted. If following call fails due to whatever reason, session cookie is not valid but
     // timer does not retry to renew it, which is fine, so error handling of call is not needed
-    this.psuAuthService.renewalAuthorizationSessionKey('' + uuid.v4(), authid, 'response')
-      .subscribe(res => {
-        console.log('got new cookie from server ', res.status);
-        localStorage.setItem(ApiHeaders.COOKIE_TTL, res.headers.get(ApiHeaders.COOKIE_TTL));
-        console.log(new Date().toLocaleString() + ' next time should be in  ' + localStorage.getItem(ApiHeaders.COOKIE_TTL));
+    this.psuAuthService.renewalAuthorizationSessionKey('' + uuid.v4(), authid, 'response').subscribe(res => {
+      console.log(new Date().toLocaleString() + ' got new cookie from server ', res.status);
+      localStorage.setItem(ApiHeaders.COOKIE_TTL, res.headers.get(ApiHeaders.COOKIE_TTL));
+      const timer = this.getTimer();
 
-        console.log(new Date().toLocaleString() + ' activate new timer');
-        this.simpleTimer.newTimerCD(CookieRenewalService.TIMER_NAME, ttl-2, ttl-2);
-        this.simpleTimer.subscribe(CookieRenewalService.TIMER_NAME, () => this.cookieRenewal(authid));
-
-      });
+      console.log(new Date().toLocaleString() + ' activate next timer in ' + timer);
+      this.simpleTimer.newTimerCD(CookieRenewalService.TIMER_NAME, timer, timer);
+      this.simpleTimer.subscribe(CookieRenewalService.TIMER_NAME, () => this.cookieRenewal(authid));
+    });
   }
 
-  getTTL() : number {
-    const ttlstring = localStorage.getItem(ApiHeaders.COOKIE_TTL);
-    if (ttlstring === null || ttlstring === '0') {
-      console.log(new Date().toLocaleString() + ' timer is idle');
-      return 0;
-    }
-
-    return parseInt(ttlstring,0);
+  getTimer(): number {
+    const ttl = parseInt(localStorage.getItem(ApiHeaders.COOKIE_TTL), 0);
+    // backend minimum for ttl is 60 secs
+    return isNaN(ttl) ? 58 : ttl - 2;
   }
-
 }
