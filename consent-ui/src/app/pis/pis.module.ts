23afa943d9ebf658bba2b856f2ab19085e62e9ff
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-15 14:06:51 +0200

    Implement enter-sca UI in pis module

--- consent-ui/src/app/pis/pis.module.ts
+++ consent-ui/src/app/pis/pis.module.ts
@@ -4,9 +4,10 @@ import { PaymentInitiateComponent } from './initiation/payment-initiate.componen
 import { PisRoutingModule } from './pis-routing.module';
 import { EntryPageComponent } from './entry-page/entry-page.component';
 import { EnterPinPageComponent } from './enter-pin-page/enter-pin-page.component';
+import { EnterScaPageComponent } from './enter-sca-page/enter-sca-page.component';
 
 @NgModule({
-  declarations: [PaymentInitiateComponent, EntryPageComponent, EnterPinPageComponent],
+  declarations: [PaymentInitiateComponent, EntryPageComponent, EnterPinPageComponent, EnterScaPageComponent],
   imports: [SharedModule, PisRoutingModule]
 })
 export class PisModule {}
