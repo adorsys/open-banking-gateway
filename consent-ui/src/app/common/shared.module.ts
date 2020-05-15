8fcf53a116902f0e4dd6c394e9e592bbacd52863
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-15 14:54:24 +0200

    extract sca-select page from ais and refactor

--- consent-ui/src/app/common/shared.module.ts
+++ consent-ui/src/app/common/shared.module.ts
@@ -5,10 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { InfoModule } from '../errorsHandler/info/info.module';
 import { EnterScaComponent } from './enter-sca/enter-sca.component';
 import { EnterPinComponent } from './enter-pin/enter-pin.component';
+import { SelectScaComponent } from './select-sca/select-sca.component';
 
 @NgModule({
-  declarations: [EnterScaComponent, EnterPinComponent],
+  declarations: [EnterScaComponent, EnterPinComponent, SelectScaComponent],
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule],
-  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, EnterScaComponent, EnterPinComponent]
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    InfoModule,
+    EnterScaComponent,
+    EnterPinComponent,
+    SelectScaComponent
+  ]
 })
 export class SharedModule {}
