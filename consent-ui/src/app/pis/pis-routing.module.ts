c24ac8407d79355553f8a3f6d6dd69374617a81a
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-15 12:39:56 +0200

    Implement enter-pin UI in pis module

--- consent-ui/src/app/pis/pis-routing.module.ts
+++ consent-ui/src/app/pis/pis-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaymentInitiateComponent } from './initiation/payment-initiate.component';
 import { EntryPageComponent } from './entry-page/entry-page.component';
+import { EnterPinPageComponent } from './enter-pin-page/enter-pin-page.component';
 
 const routes: Routes = [
   {
     path: ':authId',
     component: EntryPageComponent,
-    children: [{ path: '', component: PaymentInitiateComponent }]
+    children: [
+      { path: '', component: PaymentInitiateComponent },
+      { path: EnterPinPageComponent.ROUTE, component: EnterPinPageComponent }
+    ]
   }
 ];
 
--- consent-ui/src/app/pis/pis.module.ts
+++ consent-ui/src/app/pis/pis.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '../common/shared.module';
 import { PaymentInitiateComponent } from './initiation/payment-initiate.component';
 import { PisRoutingModule } from './pis-routing.module';
 import { EntryPageComponent } from './entry-page/entry-page.component';
+import { EnterPinPageComponent } from './enter-pin-page/enter-pin-page.component';
 
 @NgModule({
-  declarations: [PaymentInitiateComponent, EntryPageComponent],
+  declarations: [PaymentInitiateComponent, EntryPageComponent, EnterPinPageComponent],
   imports: [SharedModule, PisRoutingModule]
 })
 export class PisModule {}
