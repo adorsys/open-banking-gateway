72b109a7d96707ac381153bc502dbc9067c60ca0
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-15 12:25:13 +0200

    extract password-input page from ais and refactor

--- consent-ui/src/app/ais/common/stub-util-tests.ts
+++ consent-ui/src/app/ais/common/stub-util-tests.ts
@@ -2,4 +2,5 @@ export class StubUtilTests {
   public static AUTH_ID = 'AUTH_ID';
   public static REDIRECT_ID = 'REDIRECT_ID';
   public static SCA_METHOD_VALUE = '1234';
+  public static DUMMY_STRING = '12345';
 }
