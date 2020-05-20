56882986c661f09bca2f275f81a37fa9d4353f99
valb3r <valentyn.berezin@adorsys.com.ua>
2020-05-20 12:48:22 +0300

    OBG-653. Updated retrieval flow

--- opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/service/consent/HbciAisConsentService.java
+++ opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/service/consent/HbciAisConsentService.java
@@ -1,17 +1,19 @@
 package de.adorsys.opba.protocol.hbci.service.consent;
 
-import de.adorsys.opba.protocol.bpmnshared.dto.messages.ValidationProblem;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
+import de.adorsys.opba.protocol.api.dto.ValidationIssue;
+import de.adorsys.opba.protocol.api.dto.codes.FieldCode;
+import de.adorsys.opba.protocol.api.dto.codes.ScopeObject;
+import de.adorsys.opba.protocol.api.dto.codes.TypeCode;
 import de.adorsys.opba.protocol.bpmnshared.service.context.ContextUtil;
 import de.adorsys.opba.protocol.bpmnshared.service.exec.ValidatedExecution;
-import de.adorsys.opba.protocol.hbci.config.HbciProtocolConfiguration;
 import de.adorsys.opba.protocol.hbci.context.AccountListHbciContext;
+import de.adorsys.opba.protocol.hbci.context.HbciContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.flowable.engine.delegate.DelegateExecution;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-
-import java.net.URI;
 
 /**
  * Initiates Account list consent. Forcefully redirects user to consent initiation screen.
@@ -21,21 +23,24 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class HbciAisConsentService extends ValidatedExecution<AccountListHbciContext> {
 
-    private final ApplicationEventPublisher eventPublisher;
-    private final HbciProtocolConfiguration configuration;
-
     @Override
     protected void doRealExecution(DelegateExecution execution, AccountListHbciContext context) {
-        eventPublisher.publishEvent(ValidationProblem.builder()
-                .processId(context.getSagaId())
-                .executionId(execution.getId())
-                .provideMoreParamsDialog(
-                        ContextUtil.evaluateSpelForCtx(
-                                configuration.getRedirect().getParameters().getProvideMore(),
-                                execution,
-                                context,
-                                URI.class)
-                )
-                .build());
+        ContextUtil.getAndUpdateContext(execution, (HbciContext ctx) -> ctx.setViolations(ImmutableSet.of()));
+    }
+
+    @Override
+    protected void doMockedExecution(DelegateExecution execution, AccountListHbciContext context) {
+        if (!Strings.isNullOrEmpty(context.getPsuId())) {
+            return;
+        }
+
+        ValidationIssue issue = ValidationIssue.builder()
+                .code(FieldCode.PSU_ID)
+                .scope(ScopeObject.GENERAL)
+                .type(TypeCode.STRING)
+                .captionMessage("Your login to bank account")
+                .build();
+
+        ContextUtil.getAndUpdateContext(execution, (HbciContext ctx) -> ctx.setViolations(ImmutableSet.of(issue)));
     }
 }
