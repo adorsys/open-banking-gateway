66a68826b98f3a6c0b73eadd4848a96f1ce46481
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-22 13:29:35 +0200

    Extract to-aspsp-layout from ais and implement to-aspsp-page in PIS module

--- consent-ui/src/app/common/shared.module.ts
+++ consent-ui/src/app/common/shared.module.ts
@@ -6,10 +6,9 @@ import { InfoModule } from '../errorsHandler/info/info.module';
 import { EnterTanComponent } from './enter-tan/enter-tan.component';
 import { EnterPinComponent } from './enter-pin/enter-pin.component';
 import { SelectScaComponent } from './select-sca/select-sca.component';
-import { ResultComponent } from './result/result.component';
 
 @NgModule({
-  declarations: [EnterTanComponent, EnterPinComponent, SelectScaComponent, ResultComponent],
+  declarations: [EnterTanComponent, EnterPinComponent, SelectScaComponent],
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule],
   exports: [
     CommonModule,
@@ -18,6 +17,8 @@ import { ResultComponent } from './result/result.component';
     InfoModule,
     EnterTanComponent,
     EnterPinComponent,
+    SelectScaComponent,
+    ToAspspComponent
     SelectScaComponent,
     ResultComponent
   ]
