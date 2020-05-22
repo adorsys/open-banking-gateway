9e84f1eba428812ebb765b52a4fbef8b4a1e3fe7
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-22 13:29:35 +0200

    Extract to-aspsp-layout from ais and implement to-aspsp-page in PIS module

--- consent-ui/src/app/ais/to-aspsp-page/to-aspsp-redirection.component.spec.ts
+++ consent-ui/src/app/ais/to-aspsp-page/to-aspsp-redirection.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ToAspspRedirectionComponent } from './to-aspsp-redirection.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 import { of } from 'rxjs';
 import { StubUtilTests } from '../common/stub-util-tests';
+import { ToAspspRedirectionComponent } from './to-aspsp-redirection.component';
 
 describe('ToAspspRedirectionComponent', () => {
   let component: ToAspspRedirectionComponent;
@@ -14,6 +15,7 @@ describe('ToAspspRedirectionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ToAspspRedirectionComponent],
+      schemas: [NO_ERRORS_SCHEMA],
       imports: [RouterTestingModule, HttpClientTestingModule],
       providers: [
         {
