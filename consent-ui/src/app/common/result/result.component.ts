d0b6f6a705ccdf91d151e0e5e2b9bc9a5ec77cb9
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-05-22 12:29:59 +0200

    Extract result-layout from ais and implement result-page in PIS module

---
+++ consent-ui/src/app/common/result/result.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'consent-app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.scss']
+})
+export class ResultComponent implements OnInit {
+  @Input() redirectTo: string;
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() showDeleteButton: boolean;
+  @Input() finTechName: string;
+  @Output() confirm = new EventEmitter<boolean>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onDeny() {
+    this.confirm.emit(false);
+  }
+
+  onConfirm() {
+    this.confirm.emit(true);
+  }
+}
