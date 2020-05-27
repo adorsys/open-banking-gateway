6e54f902ed573618693133c14b657e80a94defaa
iiuex <iiu@adorsys.com.ua>
2020-05-27 15:49:06 +0300

    Fixed NPE in mapping

--- opba-api-security/src/main/java/de/adorsys/opba/api/security/external/mapper/FeignTemplateToDataToSignMapper.java
+++ opba-api-security/src/main/java/de/adorsys/opba/api/security/external/mapper/FeignTemplateToDataToSignMapper.java
@@ -8,6 +8,7 @@ import de.adorsys.opba.api.security.external.domain.signdata.AisListTransactions
 import de.adorsys.opba.api.security.external.domain.signdata.BankProfileDataToSign;
 import de.adorsys.opba.api.security.external.domain.signdata.BankSearchDataToSign;
 import de.adorsys.opba.api.security.external.domain.signdata.ConfirmConsentDataToSign;
+import feign.RequestTemplate;
 
 import java.time.Instant;
 import java.util.Collection;
@@ -17,7 +18,9 @@ import java.util.UUID;
 public class FeignTemplateToDataToSignMapper {
     private static final String MISSING_HEADER_ERROR_MESSAGE = " header is missing";
 
-    public AisListAccountsDataToSign mapToListAccounts(Map<String, Collection<String>> headers, Instant instant) {
+    public AisListAccountsDataToSign mapToListAccounts(RequestTemplate requestTemplate, Instant instant) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+
         String operationType = extractRequiredValue(headers, HttpHeaders.X_OPERATION_TYPE);
         String xRequestId = extractRequiredValue(headers, HttpHeaders.X_REQUEST_ID);
         String bankId = extractNonRequiredValue(headers, HttpHeaders.BANK_ID);
@@ -28,60 +31,70 @@ public class FeignTemplateToDataToSignMapper {
         return new AisListAccountsDataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType), bankId, fintechUserId, redirectOkUrl, redirectNokUrl);
     }
 
-    public AisListTransactionsDataToSign mapToListTransactions(Map<String, Collection<String>> headers, Map<String, String> queries, Instant instant) {
+    public AisListTransactionsDataToSign mapToListTransactions(RequestTemplate requestTemplate, Instant instant) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+        Map<String, Collection<String>> queries = requestTemplate.queries();
+
         String operationType = extractRequiredValue(headers, HttpHeaders.X_OPERATION_TYPE);
         String xRequestId = extractRequiredValue(headers, HttpHeaders.X_REQUEST_ID);
         String bankId = extractNonRequiredValue(headers, HttpHeaders.BANK_ID);
         String fintechUserId = extractRequiredValue(headers, HttpHeaders.FINTECH_USER_ID);
         String redirectOkUrl = extractRequiredValue(headers, HttpHeaders.FINTECH_REDIRECT_URL_OK);
         String redirectNokUrl = extractRequiredValue(headers, HttpHeaders.FINTECH_REDIRECT_URL_NOK);
-        String dateFrom = queries.get(QueryParams.DATE_FROM);
-        String dateTo = queries.get(QueryParams.DATE_TO);
-        String entryReferenceFrom = queries.get(QueryParams.ENTRY_REFERENCE_FROM);
-        String bookingStatus = queries.get(QueryParams.BOOKING_STATUS);
-        String deltaList = queries.get(QueryParams.DELTA_LIST);
+        String dateFrom = extractNonRequiredValue(queries, QueryParams.DATE_FROM);
+        String dateTo = extractNonRequiredValue(queries, QueryParams.DATE_TO);
+        String entryReferenceFrom = extractNonRequiredValue(queries, QueryParams.ENTRY_REFERENCE_FROM);
+        String bookingStatus = extractNonRequiredValue(queries, QueryParams.BOOKING_STATUS);
+        String deltaList = extractNonRequiredValue(queries, QueryParams.DELTA_LIST);
 
         return new AisListTransactionsDataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType), bankId, fintechUserId,
-                                                 redirectOkUrl, redirectNokUrl, dateFrom, dateTo, entryReferenceFrom, bookingStatus, deltaList
+                redirectOkUrl, redirectNokUrl, dateFrom, dateTo, entryReferenceFrom, bookingStatus, deltaList
         );
     }
 
-    public BankProfileDataToSign mapToBankProfile(Map<String, Collection<String>> headers, Instant instant) {
+    public BankProfileDataToSign mapToBankProfile(RequestTemplate requestTemplate, Instant instant) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+
         String operationType = extractRequiredValue(headers, HttpHeaders.X_OPERATION_TYPE);
         String xRequestId = extractRequiredValue(headers, HttpHeaders.X_REQUEST_ID);
 
         return new BankProfileDataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType));
     }
 
-    public BankSearchDataToSign mapToBankSearch(Map<String, Collection<String>> headers, Map<String, String> queries, Instant instant) {
+    public BankSearchDataToSign mapToBankSearch(RequestTemplate requestTemplate, Instant instant) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+        Map<String, Collection<String>> queries = requestTemplate.queries();
+
         String operationType = extractRequiredValue(headers, HttpHeaders.X_OPERATION_TYPE);
         String xRequestId = extractRequiredValue(headers, HttpHeaders.X_REQUEST_ID);
-        String keyword = queries.get(QueryParams.KEYWORD);
+        String keyword = extractRequiredValue(queries, QueryParams.KEYWORD);
 
         return new BankSearchDataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType), keyword);
     }
 
-    public ConfirmConsentDataToSign mapToConfirmConsent(Map<String, Collection<String>> headers, Instant instant) {
+    public ConfirmConsentDataToSign mapToConfirmConsent(RequestTemplate requestTemplate, Instant instant) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+
         String operationType = extractRequiredValue(headers, HttpHeaders.X_OPERATION_TYPE);
         String xRequestId = extractRequiredValue(headers, HttpHeaders.X_REQUEST_ID);
 
         return new ConfirmConsentDataToSign(UUID.fromString(xRequestId), instant, OperationType.valueOf(operationType));
     }
 
-    private String extractRequiredValue(Map<String, Collection<String>> values, String valueName) {
-        if (values.get(valueName) == null) {
+    private String extractRequiredValue(Map<String, Collection<String>> headers, String valueName) {
+        if (headers.get(valueName) == null) {
             throw new IllegalStateException(valueName + MISSING_HEADER_ERROR_MESSAGE);
         }
 
-        return values.get(valueName).stream().findFirst()
+        return headers.get(valueName).stream().findFirst()
                        .orElseThrow(() -> new IllegalStateException(valueName + MISSING_HEADER_ERROR_MESSAGE));
     }
 
-    private String extractNonRequiredValue(Map<String, Collection<String>> values, String valueName) {
-        if (values.get(valueName) == null) {
+    private String extractNonRequiredValue(Map<String, Collection<String>> headers, String valueName) {
+        if (headers.get(valueName) == null) {
             return null;
         }
 
-        return values.get(valueName).stream().findFirst().orElse(null);
+        return headers.get(valueName).stream().findFirst().orElse(null);
     }
 }
