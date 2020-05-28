8936115ea83cb43eed041b916479693833ecf232
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-05-28 09:59:42 +0200

    OBG-705 payment info fix

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/request/payments/PaymentStatusBody.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/dto/request/payments/PaymentStatusBody.java
@@ -1,6 +1,12 @@
 package de.adorsys.opba.protocol.api.dto.request.payments;
 
 import de.adorsys.opba.protocol.api.dto.result.body.ResultBody;
+import lombok.Data;
 
+/**
+ * Pis payment status body
+ */
+@Data
 public class PaymentStatusBody implements ResultBody {
+    private String transactionStatus;
 }
