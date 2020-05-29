941ac46c355fe9519c9c763035397814d2cee593
Hergy Tchuinkou <htc@adorsys.de>
2020-05-29 12:46:28 +0200

    added test for consent-sharing

--- consent-ui/src/app/ais/entry-page/initiation/consent-sharing/consent-sharing.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/consent-sharing/consent-sharing.component.spec.ts
@@ -1,13 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
 
-import { ConsentSharingComponent } from './consent-sharing.component';
-import { ActivatedRoute } from '@angular/router';
-import { StubUtilTests } from '../../../common/stub-util-tests';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {ConsentSharingComponent} from './consent-sharing.component';
+import {ActivatedRoute} from '@angular/router';
+import {StubUtilTests} from '../../../common/stub-util-tests';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {of} from "rxjs";
+import {ConsentAuthorizationService} from "../../../../api";
 
-describe('ConsentSharingComponent', () => {
+fdescribe('ConsentSharingComponent', () => {
   let component: ConsentSharingComponent;
+  let consentAuthorizationService: ConsentAuthorizationService;
   let fixture: ComponentFixture<ConsentSharingComponent>;
+  let consentAuthorizationServiceSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +26,7 @@ describe('ConsentSharingComponent', () => {
           provide: ActivatedRoute,
           useValue: {
             parent: {
-              snapshot: { params: { authId: StubUtilTests.AUTH_ID } }
+              snapshot: {params: {authId: StubUtilTests.AUTH_ID}}
             },
             snapshot: {
               queryParams: {}
@@ -32,10 +40,17 @@ describe('ConsentSharingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ConsentSharingComponent);
     component = fixture.componentInstance;
+    consentAuthorizationService = fixture.debugElement.injector.get(ConsentAuthorizationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call denyUsingPOST', () => {
+    consentAuthorizationServiceSpy = spyOn(consentAuthorizationService, 'denyUsingPOST').and.returnValue(of());
+    component.onDeny();
+    expect(consentAuthorizationServiceSpy).toHaveBeenCalled();
   });
 });
