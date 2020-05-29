a99a37b05612bbe7ed4f91f471993c18da6e22a8
Hergy Tchuinkou <htc@adorsys.de>
2020-05-29 10:30:00 +0200

    reformatting

--- consent-ui/src/app/ais/entry-page/initiation/common/accounts-reference/accounts-reference.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/common/accounts-reference/accounts-reference.component.spec.ts
@@ -1,45 +1,55 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
 
-import {AccountReference, AccountsReferenceComponent} from './accounts-reference.component';
-import {FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {
+  AccountReference,
+  AccountsReferenceComponent
+} from './accounts-reference.component';
+import {
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 describe('AccountsReferenceComponent', () => {
-    let component: AccountsReferenceComponent;
-    let fixture: ComponentFixture<AccountsReferenceComponent>;
+  let component: AccountsReferenceComponent;
+  let fixture: ComponentFixture<AccountsReferenceComponent>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [AccountsReferenceComponent],
-            imports: [ReactiveFormsModule]
-        }).compileComponents();
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AccountsReferenceComponent],
+      imports: [ReactiveFormsModule]
+    }).compileComponents();
+  }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(AccountsReferenceComponent);
-        component = fixture.componentInstance;
-        component.targetForm = new FormGroup({});
-        component.accounts = [];
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AccountsReferenceComponent);
+    component = fixture.componentInstance;
+    component.targetForm = new FormGroup({});
+    component.accounts = [];
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should call addAccount', () => {
-        component.addAccount();
-        expect(component.accounts.length).toEqual(1);
-    });
+  it('should call addAccount', () => {
+    component.addAccount();
+    expect(component.accounts.length).toEqual(1);
+  });
 
-    it('should call removeAccount', () => {
-        const account: AccountReference = {
-            id: "12345",
-            iban: "DE12344313232222"
-        };
+  it('should call removeAccount', () => {
+    const account: AccountReference = {
+      id: "12345",
+      iban: "DE12344313232222"
+    };
 
-        const removeAccountSpy = spyOn(component, 'removeAccount');
-        component.removeAccount(account);
-        expect(removeAccountSpy).toHaveBeenCalledWith(account);
-        expect(component.accounts.length).toEqual(0);
-    });
+    const removeAccountSpy = spyOn(component, 'removeAccount');
+    component.removeAccount(account);
+    expect(removeAccountSpy).toHaveBeenCalledWith(account);
+    expect(component.accounts.length).toEqual(0);
+  });
 });
