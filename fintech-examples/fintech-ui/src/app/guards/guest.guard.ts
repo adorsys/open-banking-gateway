a383683e85a1363eec4a16a58918bfec6b72ac44
electronicpeter <github@electronicpeter.de>
2020-06-02 13:36:13 +0200

    666 authService for all auth belongings

--- fintech-examples/fintech-ui/src/app/guards/guest.guard.ts
+++ fintech-examples/fintech-ui/src/app/guards/guest.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { StorageService } from '../services/storage.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuestGuard implements CanActivate {
-  constructor(private storageService: StorageService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    const isLoggedIn = this.storageService.isLoggedIn();
+    const isLoggedIn = this.authService.isLoggedIn();
 
     if (isLoggedIn) {
       this.router.navigate(['/search']);
