49aaa18e648c943ef45e5dc1c9910bfe44d8083c
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-02 18:44:45 +0200

    OBG-705 added mappers

---
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/dto/payment/PaymentStateParameters.java
@@ -0,0 +1,21 @@
+package de.adorsys.opba.protocol.xs2a.service.xs2a.dto.payment;
+
+import de.adorsys.opba.protocol.xs2a.context.pis.Xs2aPisContext;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.DtoMapper;
+import de.adorsys.xs2a.adapter.service.RequestParams;
+import org.mapstruct.Mapper;
+
+import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.SPRING_KEYWORD;
+import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.XS2A_MAPPERS_PACKAGE;
+
+public class PaymentStateParameters {
+
+    public RequestParams toParameters() {
+        return RequestParams.builder().build();
+    }
+
+    @Mapper(componentModel = SPRING_KEYWORD, implementationPackage = XS2A_MAPPERS_PACKAGE)
+    public interface FromCtx extends DtoMapper<Xs2aPisContext, PaymentStateParameters> {
+        PaymentStateParameters map(Xs2aPisContext ctx);
+    }
+}
