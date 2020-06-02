0ea58e27532bcab45a53da87654b573be70f1ae6
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-02 18:44:45 +0200

    OBG-705 added mappers

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/pis/Xs2aGetPaymentInfoEntrypoint.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/pis/Xs2aGetPaymentInfoEntrypoint.java
@@ -1,5 +1,6 @@
 package de.adorsys.opba.protocol.xs2a.entrypoint.pis;
 
+import de.adorsys.opba.protocol.api.common.ProtocolAction;
 import de.adorsys.opba.protocol.api.dto.context.ServiceContext;
 import de.adorsys.opba.protocol.api.dto.request.payments.PaymentInfoBody;
 import de.adorsys.opba.protocol.api.dto.request.payments.PaymentInfoRequest;
@@ -7,16 +8,22 @@ import de.adorsys.opba.protocol.api.dto.result.fromprotocol.Result;
 import de.adorsys.opba.protocol.api.dto.result.fromprotocol.ok.SuccessResult;
 import de.adorsys.opba.protocol.api.pis.GetPaymentInfoState;
 import de.adorsys.opba.protocol.api.services.scoped.consent.ProtocolFacingConsent;
+import de.adorsys.opba.protocol.xs2a.context.pis.Xs2aPisContext;
+import de.adorsys.opba.protocol.xs2a.entrypoint.ExtendWithServiceContext;
+import de.adorsys.opba.protocol.xs2a.entrypoint.helpers.UuidMapper;
+import de.adorsys.opba.protocol.xs2a.service.dto.ValidatedQueryHeaders;
+import de.adorsys.opba.protocol.xs2a.service.mapper.QueryHeadersMapperTemplate;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.DtoMapper;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.payment.PaymentInfoHeaders;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.payment.PaymentInfoParameters;
 import de.adorsys.xs2a.adapter.service.PaymentInitiationService;
-import de.adorsys.xs2a.adapter.service.RequestHeaders;
-import de.adorsys.xs2a.adapter.service.RequestParams;
 import de.adorsys.xs2a.adapter.service.Response;
 import de.adorsys.xs2a.adapter.service.model.SinglePaymentInitiationInformationWithStatusResponse;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.SPRING_KEYWORD;
@@ -28,26 +35,64 @@ import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.XS2A_MAPPERS_PA
 @Service("xs2aGetPaymentInfoState")
 @RequiredArgsConstructor
 public class Xs2aGetPaymentInfoEntrypoint implements GetPaymentInfoState {
+    public static final String SEPA_CREDIT_TRANSFERS = "sepa-credit-transfers";
     private final PaymentInitiationService pis;
     private final PaymentInformationToBodyMapper mapper;
+    private final Xs2aGetPaymentInfoEntrypoint.Extractor extractor;
+    private final Xs2aGetPaymentInfoEntrypoint.FromRequest request2ContextMapper;
+    private final ExtendWithServiceContext extender;
 
     @Override
-    public CompletableFuture<Result<PaymentInfoBody>> execute(ServiceContext<PaymentInfoRequest> serviceContext) {
-        ProtocolFacingConsent consent = serviceContext.getRequestScoped().consentAccess().findByCurrentServiceSession()
+    public CompletableFuture<Result<PaymentInfoBody>> execute(ServiceContext<PaymentInfoRequest> context) {
+        ProtocolFacingConsent consent = context.getRequestScoped().consentAccess().findByCurrentServiceSession()
                 .orElseThrow(() -> new IllegalStateException("Context not found"));
 
+        ValidatedQueryHeaders<PaymentInfoParameters, PaymentInfoHeaders> params = extractor.forExecution(prepareContext(context));
+
         Response<SinglePaymentInitiationInformationWithStatusResponse> paymentInformation = pis.getSinglePaymentInformation(
-                "sepa-credit-transfers",
+                SEPA_CREDIT_TRANSFERS,
                 consent.getConsentId(),
-                RequestHeaders.fromMap(new HashMap<>()),
-                RequestParams.fromMap(new HashMap<>()));
+                params.getHeaders().toHeaders(),
+                params.getQuery().toParameters()
+        );
 
         Result<PaymentInfoBody> result = new SuccessResult<>(mapper.map(paymentInformation.getBody()));
         return CompletableFuture.completedFuture(result);
     }
 
+    protected Xs2aPisContext prepareContext(ServiceContext<PaymentInfoRequest> serviceContext) {
+        Xs2aPisContext context = request2ContextMapper.map(serviceContext.getRequest());
+        context.setAction(ProtocolAction.GET_PAYMENT_INFORMATION);
+        extender.extend(context, serviceContext);
+        return context;
+    }
+
+    /**
+     * Mapper to convert incoming user request to processable request context.
+     */
+    @Mapper(componentModel = SPRING_KEYWORD, uses = UuidMapper.class, implementationPackage = XS2A_MAPPERS_PACKAGE)
+    public interface FromRequest extends DtoMapper<PaymentInfoRequest, Xs2aPisContext> {
+
+        @Mapping(source = "facadeServiceable.bankId", target = "aspspId")
+        @Mapping(source = "facadeServiceable.requestId", target = "requestId")
+        Xs2aPisContext map(PaymentInfoRequest ctx);
+    }
+
     @Mapper(componentModel = SPRING_KEYWORD, implementationPackage = XS2A_MAPPERS_PACKAGE)
     public interface PaymentInformationToBodyMapper {
         PaymentInfoBody map(SinglePaymentInitiationInformationWithStatusResponse paymentInformation);
+    }
+
+    @Service
+    public static class Extractor extends QueryHeadersMapperTemplate<
+            Xs2aPisContext,
+            PaymentInfoParameters,
+            PaymentInfoHeaders> {
+
+        public Extractor(
+                DtoMapper<Xs2aPisContext, PaymentInfoHeaders> toHeaders,
+                DtoMapper<Xs2aPisContext, PaymentInfoParameters> toQuery) {
+            super(toHeaders, toQuery);
+        }
     }
 }
