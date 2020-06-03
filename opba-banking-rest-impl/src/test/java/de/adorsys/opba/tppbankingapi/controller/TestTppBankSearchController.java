bf5c1c813d442c8e6db9778e65cd822a4fd0cd77
Veronika Sedlackova <vse@adorsys.de>
2020-06-03 13:11:21 +0200

    obg-540 changed bank Id and BIC in bankprofiletest()

--- opba-banking-rest-impl/src/test/java/de/adorsys/opba/tppbankingapi/controller/TestTppBankSearchController.java
+++ opba-banking-rest-impl/src/test/java/de/adorsys/opba/tppbankingapi/controller/TestTppBankSearchController.java
@@ -1,27 +1,13 @@
 package de.adorsys.opba.tppbankingapi.controller;
 
-import de.adorsys.opba.api.security.external.domain.signdata.BankProfileDataToSign;
-import de.adorsys.opba.api.security.external.domain.signdata.BankSearchDataToSign;
-import de.adorsys.opba.api.security.external.service.RequestSigningService;
-import de.adorsys.opba.api.security.external.domain.OperationType;
 import de.adorsys.opba.tppbankingapi.BaseMockitoTest;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
-import java.time.Instant;
-import java.util.UUID;
-
-import static de.adorsys.opba.api.security.external.domain.HttpHeaders.FINTECH_ID;
-import static de.adorsys.opba.api.security.external.domain.HttpHeaders.X_REQUEST_ID;
-import static de.adorsys.opba.api.security.external.domain.HttpHeaders.X_REQUEST_SIGNATURE;
-import static de.adorsys.opba.api.security.external.domain.HttpHeaders.X_TIMESTAMP_UTC;
-import static de.adorsys.opba.api.security.external.domain.HttpHeaders.X_OPERATION_TYPE;
 import static de.adorsys.opba.tppbankingapi.TestProfiles.ONE_TIME_POSTGRES_RAMFS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -35,15 +21,19 @@ class TestTppBankSearchController extends BaseMockitoTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private RequestSigningService requestSigningService;
     @Test
     void testBankSearch() throws Exception {
-        UUID xRequestId = UUID.randomUUID();
-        Instant xTimestampUtc = Instant.now();
-        String keyword = "commerz";
-
-        performBankSearchRequest(xRequestId, xTimestampUtc, keyword)
+        mockMvc.perform(
+                get("/v1/banking/search/bank-search")
+                        .header("X-Request-ID", "3ab706f2-8cc8-462e-8393-a43f6ee87e53")
+                        .header("Compute-PSU-IP-Address", "true")
+                        .header("X-Timestamp-UTC", "2020-04-17T13:45:17.069Z")
+                        .header("X-Request-Signature", "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJmaW50ZWNoQGF3ZXNvbWUtZmludGVjaC5jb20iLCJpc3MiOiJmaW50ZWNoLmNvbSIsInNpZ24tZGF0YSI6IjNhYjcwNmYyLThjYzgtNDYyZS04MzkzLWE0M2Y2ZWU4N2U1MzIwMjAtMDQtMTdUMTM6NDU6MTcuMDY5WiJ9.S3L4XdAhlzJBXYHTXMXVNlLmABBkUvYqF03znEmzKQU9vOF-n0cT6yWWjvm6T82ISzZ5OYrJaA2QJekFsw78vraY-t7vxhWVn9hO_C1tJR_rV3SFWi6mtZeuSCGDSJxEB_8gmMqFomQs0sEdBayiC1mkW9R3TQGhmLkXyM4GHGR_rHL1oLFjG3Ueo0tYmLVIJDyQ6oqFHhDdNro41O2E1S9BOOVLbANLU7r_jN8KIuujmFIBF3S7L0P2yvIHQ3Sme3W2550m-LdPI3f2SFD4ZRLG6Xsc8LyrDuXtEuk9H3nHqPenbhQnMPHK7OUcsEN2VFqvUQ9SWTgUz4P9nuU2ng")
+                        .header("Fintech-ID", "MY-SUPER-FINTECH-ID")
+                        .param("keyword", "commerz")
+                        .param("max", "10")
+                        .param("start", "0"))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.bankDescriptor.length()").value("10"))
                 .andExpect(jsonPath("$.bankDescriptor[0].bankName").value("Commerzbank"))
                 .andExpect(jsonPath("$.bankDescriptor[0].bic").value("COBADEFFXXX"))
@@ -54,19 +44,13 @@ class TestTppBankSearchController extends BaseMockitoTest {
 
     @Test
     void testBankProfile() throws Exception {
-        UUID xRequestId = UUID.randomUUID();
-        Instant xTimestampUtc = Instant.now();
-
         mockMvc.perform(
                 get("/v1/banking/search/bank-profile")
+                        .header("X-Request-ID", "3ab706f2-8cc8-462e-8393-a43f6ee87e53")
                         .header("Compute-PSU-IP-Address", "true")
-
-                        .header(X_REQUEST_ID, xRequestId)
-                        .header(X_TIMESTAMP_UTC, xTimestampUtc)
-                        .header(X_OPERATION_TYPE, OperationType.BANK_SEARCH)
-                        .header(X_REQUEST_SIGNATURE, requestSigningService.signature(new BankProfileDataToSign(xRequestId, xTimestampUtc, OperationType.BANK_SEARCH)))
-                        .header(FINTECH_ID, "MY-SUPER-FINTECH-ID")
-
+                        .header("X-Timestamp-UTC", "2020-04-17T13:45:17.069Z")
+                        .header("X-Request-Signature", "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJmaW50ZWNoQGF3ZXNvbWUtZmludGVjaC5jb20iLCJpc3MiOiJmaW50ZWNoLmNvbSIsInNpZ24tZGF0YSI6IjNhYjcwNmYyLThjYzgtNDYyZS04MzkzLWE0M2Y2ZWU4N2U1MzIwMjAtMDQtMTdUMTM6NDU6MTcuMDY5WiJ9.S3L4XdAhlzJBXYHTXMXVNlLmABBkUvYqF03znEmzKQU9vOF-n0cT6yWWjvm6T82ISzZ5OYrJaA2QJekFsw78vraY-t7vxhWVn9hO_C1tJR_rV3SFWi6mtZeuSCGDSJxEB_8gmMqFomQs0sEdBayiC1mkW9R3TQGhmLkXyM4GHGR_rHL1oLFjG3Ueo0tYmLVIJDyQ6oqFHhDdNro41O2E1S9BOOVLbANLU7r_jN8KIuujmFIBF3S7L0P2yvIHQ3Sme3W2550m-LdPI3f2SFD4ZRLG6Xsc8LyrDuXtEuk9H3nHqPenbhQnMPHK7OUcsEN2VFqvUQ9SWTgUz4P9nuU2ng")
+                        .header("Fintech-ID", "MY-SUPER-FINTECH-ID")
                         .param("bankId", "4eee696c-b2d2-45ac-86c7-b77a810a261b"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.bankProfileDescriptor.bankName").value("DZ BANK D3000"))
@@ -77,29 +61,18 @@ class TestTppBankSearchController extends BaseMockitoTest {
 
     @Test
     void testSimilarityThreshold() throws Exception {
-        UUID xRequestId = UUID.randomUUID();
-        Instant xTimestampUtc = Instant.now();
-        String keyword = "Sandbox";
-
-        performBankSearchRequest(xRequestId, xTimestampUtc, keyword)
-                .andExpect(jsonPath("$.bankDescriptor.length()").value("0"))
-                .andReturn();
-    }
-
-    @NotNull
-    private ResultActions performBankSearchRequest(UUID xRequestId, Instant xTimestampUtc, String keyword) throws Exception {
-        return mockMvc.perform(
+        mockMvc.perform(
                 get("/v1/banking/search/bank-search")
+                        .header("X-Request-ID", "3ab706f2-8cc8-462e-8393-a43f6ee87e53")
                         .header("Compute-PSU-IP-Address", "true")
-
-                        .header(X_REQUEST_ID, xRequestId)
-                        .header(X_TIMESTAMP_UTC, xTimestampUtc)
-                        .header(X_OPERATION_TYPE, OperationType.BANK_SEARCH)
-                        .header(X_REQUEST_SIGNATURE, requestSigningService.signature(new BankSearchDataToSign(xRequestId, xTimestampUtc, OperationType.BANK_SEARCH, keyword)))
-                        .header(FINTECH_ID, "MY-SUPER-FINTECH-ID")
-                        .param("keyword", keyword)
+                        .header("X-Timestamp-UTC", "2020-04-17T13:45:17.069Z")
+                        .header("X-Request-Signature", "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJmaW50ZWNoQGF3ZXNvbWUtZmludGVjaC5jb20iLCJpc3MiOiJmaW50ZWNoLmNvbSIsInNpZ24tZGF0YSI6IjNhYjcwNmYyLThjYzgtNDYyZS04MzkzLWE0M2Y2ZWU4N2U1MzIwMjAtMDQtMTdUMTM6NDU6MTcuMDY5WiJ9.S3L4XdAhlzJBXYHTXMXVNlLmABBkUvYqF03znEmzKQU9vOF-n0cT6yWWjvm6T82ISzZ5OYrJaA2QJekFsw78vraY-t7vxhWVn9hO_C1tJR_rV3SFWi6mtZeuSCGDSJxEB_8gmMqFomQs0sEdBayiC1mkW9R3TQGhmLkXyM4GHGR_rHL1oLFjG3Ueo0tYmLVIJDyQ6oqFHhDdNro41O2E1S9BOOVLbANLU7r_jN8KIuujmFIBF3S7L0P2yvIHQ3Sme3W2550m-LdPI3f2SFD4ZRLG6Xsc8LyrDuXtEuk9H3nHqPenbhQnMPHK7OUcsEN2VFqvUQ9SWTgUz4P9nuU2ng")
+                        .header("Fintech-ID", "MY-SUPER-FINTECH-ID")
+                        .param("keyword", "Sandbox")
                         .param("max", "10")
                         .param("start", "0"))
-                       .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.bankDescriptor.length()").value("0"))
+                .andReturn();
     }
 }
