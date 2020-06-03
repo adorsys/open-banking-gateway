c015d08f6d259b1e74abeda79300a86844668006
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-06-03 09:46:19 +0200

    implement initiate payment UI

---
+++ fintech-examples/fintech-ui/src/app/payment/payment.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.scss']
+})
+export class PaymentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
