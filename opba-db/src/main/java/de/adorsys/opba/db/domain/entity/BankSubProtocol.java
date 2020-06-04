bc97a8709c4efa7f0648b14f5db27db5a03a9e38
Veronika Sedlackova <vse@adorsys.de>
2020-06-04 15:22:55 +0200

    obg-540 changed bank_protocol to bank_action

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/BankSubProtocol.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/BankSubProtocol.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
