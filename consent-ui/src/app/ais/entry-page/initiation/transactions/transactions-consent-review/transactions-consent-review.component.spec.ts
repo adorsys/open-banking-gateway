989490b3cc217b33650c96c5e06f6a937651d3ca
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-05 14:12:45 +0200

    #705 after rabase fixes

--- consent-ui/src/app/ais/entry-page/initiation/transactions/transactions-consent-review/transactions-consent-review.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/transactions/transactions-consent-review/transactions-consent-review.component.spec.ts
@@ -1,20 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransactionsConsentReviewComponent } from './transactions-consent-review.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
 import { of } from 'rxjs';
 import { StubUtilTests } from '../../../../common/stub-util-tests';
 import { SessionService } from '../../../../../common/session.service';
-import { ConsentAuthorizationService } from '../../../../../api';
-import { TransactionsConsentReviewComponent } from './transactions-consent-review.component';
+import { UpdateConsentAuthorizationService } from '../../../../../api';
+import { Location } from '@angular/common';
 
 describe('TransactionsConsentReviewComponent', () => {
   let component: TransactionsConsentReviewComponent;
   let fixture: ComponentFixture<TransactionsConsentReviewComponent>;
   let consentAuthorizationServiceSpy;
-  let consentAuthorizationService: ConsentAuthorizationService;
+  let consentAuthorizationService: UpdateConsentAuthorizationService;
 
   const locationStub = {
     back: jasmine.createSpy('onBack')
@@ -26,7 +27,7 @@ describe('TransactionsConsentReviewComponent', () => {
       imports: [RouterTestingModule, ReactiveFormsModule, HttpClientTestingModule],
       providers: [
         SessionService,
-        ConsentAuthorizationService,
+        UpdateConsentAuthorizationService,
         FormBuilder,
         { provide: Location, useValue: locationStub },
         {
@@ -41,7 +42,7 @@ describe('TransactionsConsentReviewComponent', () => {
     fixture = TestBed.createComponent(TransactionsConsentReviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    consentAuthorizationService = TestBed.get(ConsentAuthorizationService);
+    consentAuthorizationService = fixture.debugElement.injector.get(UpdateConsentAuthorizationService);
   });
 
   it('should create', () => {
--- consent-ui/src/app/pis/select-sca-page/select-sca-page.component.ts
+++ consent-ui/src/app/pis/select-sca-page/select-sca-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ScaUserData } from '../../api';
+import {AuthStateConsentAuthorizationService, ScaUserData, UpdateConsentAuthorizationService} from '../../api';
 import { FormControl } from '@angular/forms';
+import {SessionService} from "../../common/session.service";
+import {ActivatedRoute} from "@angular/router";
+import {StubUtil} from "../../common/utils/stub-util";
+import {ApiHeaders} from "../../api/api.headers";
 
 @Component({
   selector: 'consent-app-select-sca-page',
@@ -10,12 +14,53 @@ import { FormControl } from '@angular/forms';
 export class SelectScaPageComponent implements OnInit {
   public static ROUTE = 'select-sca';
 
+  authorizationSessionId = '';
+  redirectCode = '';
   scaMethods: ScaUserData[] = [];
   selectedMethod = new FormControl();
 
-  constructor() {}
+  constructor(
+    private sessionService: SessionService,
+    private updateConsentAuthorizationService: UpdateConsentAuthorizationService,
+    private authStateConsentAuthorizationService: AuthStateConsentAuthorizationService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authorizationSessionId = this.route.parent.snapshot.paramMap.get('authId');
+    this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
+    this.loadAvailableMethods();
+  }
 
-  onSubmit(selectedMethodValue: string): void {}
+  onSubmit(selectedMethodValue: string): void {
+    this.updateConsentAuthorizationService
+      .embeddedUsingPOST(
+        this.authorizationSessionId,
+        StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
+        StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
+        this.redirectCode,
+        { scaAuthenticationData: { SCA_CHALLENGE_ID: selectedMethodValue } },
+        'response'
+      )
+      .subscribe(res => {
+        // redirect to the provided location
+        console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
+        this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
+        window.location.href = res.headers.get(ApiHeaders.LOCATION);
+      });
+  }
+
+  private loadAvailableMethods(): void {
+    this.authStateConsentAuthorizationService
+      .authUsingGET(this.authorizationSessionId, this.redirectCode, 'response')
+      .subscribe(consentAuth => {
+        this.sessionService.setRedirectCode(
+          this.authorizationSessionId,
+          consentAuth.headers.get(ApiHeaders.REDIRECT_CODE)
+        );
+        this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
+        this.scaMethods = consentAuth.body.consentAuth.scaMethods;
+        this.selectedMethod.setValue(this.scaMethods[0].id);
+      });
+  }
 }
