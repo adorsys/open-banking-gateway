83f85b34d614fa9de861f7eba9c864e313f2e4bd
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-05 14:35:36 +0200

    #705 consent-ui tests fixes

--- consent-ui/src/app/pis/enter-tan-page/enter-tan-page.component.spec.ts
+++ consent-ui/src/app/pis/enter-tan-page/enter-tan-page.component.spec.ts
@@ -2,25 +2,51 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { EnterTanPageComponent } from './enter-tan-page.component';
+import {UpdateConsentAuthorizationService} from "../../api";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {ActivatedRoute, convertToParamMap} from "@angular/router";
+import {StubUtilTests} from "../../ais/common/stub-util-tests";
+import {of} from "rxjs";
 
-describe('EnterScaPageComponent', () => {
+describe('EnterTanPageComponent', () => {
   let component: EnterTanPageComponent;
   let fixture: ComponentFixture<EnterTanPageComponent>;
+  let consentAuthorizationService: UpdateConsentAuthorizationService;
+  let consentAuthorizationServiceSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EnterTanPageComponent],
-      schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { queryParamMap: convertToParamMap({}) },
+            parent: { snapshot: { paramMap: convertToParamMap({ authId: StubUtilTests.AUTH_ID }) } }
+          }
+        }
+      ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EnterTanPageComponent);
     component = fixture.componentInstance;
+    consentAuthorizationService = fixture.debugElement.injector.get(UpdateConsentAuthorizationService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call method embeddedUsingPOST', () => {
+    consentAuthorizationServiceSpy = spyOn(consentAuthorizationService, 'embeddedUsingPOST').and.returnValue(of());
+    component.onSubmit(StubUtilTests.DUMMY_INPUT);
+    expect(consentAuthorizationServiceSpy).toHaveBeenCalled();
   });
 });
--- consent-ui/src/app/pis/initiation/payment-initiate.component.spec.ts
+++ consent-ui/src/app/pis/initiation/payment-initiate.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaymentInitiateComponent } from './payment-initiate.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {ActivatedRoute} from "@angular/router";
+import {StubUtilTests} from "../../ais/common/stub-util-tests";
 
 describe('InitiationComponent', () => {
   let component: PaymentInitiateComponent;
@@ -8,7 +13,19 @@ describe('InitiationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PaymentInitiateComponent]
+      declarations: [PaymentInitiateComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { authId: StubUtilTests.AUTH_ID },
+              queryParams: { redirectCode: StubUtilTests.REDIRECT_ID }
+            }
+          }
+        }
+      ]
     }).compileComponents();
   }));
 
