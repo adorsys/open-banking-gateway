c6d4a0a8363270af7ee8a1619f05ff918aaac694
Hergy Tchuinkou <htc@adorsys.de>
2020-06-05 10:39:44 +0200

    refactoring

--- consent-ui/src/app/ais/entry-page/initiation/consent-sharing/consent-sharing.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/consent-sharing/consent-sharing.component.spec.ts
@@ -1,10 +1,10 @@
-import { async,ComponentFixture,  TestBed } from '@angular/core/testing';
+import {async,ComponentFixture,  TestBed} from '@angular/core/testing';
 import {ConsentSharingComponent} from './consent-sharing.component';
 import {ActivatedRoute} from '@angular/router';
 import {StubUtilTests} from '../../../common/stub-util-tests';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {of} from "rxjs";
-import {ConsentAuthorizationService} from "../../../../api";
+import {of} from 'rxjs';
+import {ConsentAuthorizationService} from '../../../../api';
 
 describe('ConsentSharingComponent', () => {
   let component: ConsentSharingComponent;
