77a9fcc62211976cb7caa652d3616119a9133c85
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-06-05 14:42:02 +0200

    update dependencies

--- fintech-examples/fintech-ui/src/app/common/share.module.ts
+++ fintech-examples/fintech-ui/src/app/common/share.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularIbanModule } from 'angular-iban';
 import { InfoModule } from '../errorsHandler/info/info.module';
 import { SearchComponent } from './search/search.component';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, NgbModalModule],
-  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, SearchComponent, NgbModalModule]
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, InfoModule, NgbModalModule, AngularIbanModule],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    InfoModule,
+    SearchComponent,
+    NgbModalModule,
+    AngularIbanModule
+  ]
 })
 export class ShareModule {
 }
