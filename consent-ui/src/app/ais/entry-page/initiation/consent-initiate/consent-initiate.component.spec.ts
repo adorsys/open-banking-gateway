1ef7bff20f5b2667e87d041177b8ca15c981ddbf
Hergy Tchuinkou <htc@adorsys.de>
2020-06-05 13:11:45 +0200

    format

--- consent-ui/src/app/ais/entry-page/initiation/consent-initiate/consent-initiate.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/consent-initiate/consent-initiate.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ConsentInitiateComponent } from './consent-initiate.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
-import { StubUtilTests } from '../../../common/stub-util-tests';
+import {ConsentInitiateComponent} from './consent-initiate.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ActivatedRoute} from '@angular/router';
+import {StubUtilTests} from '../../../common/stub-util-tests';
 
 describe('ConsentInitiateComponent', () => {
   let component: ConsentInitiateComponent;
@@ -20,8 +20,8 @@ describe('ConsentInitiateComponent', () => {
           provide: ActivatedRoute,
           useValue: {
             snapshot: {
-              params: { authId: StubUtilTests.AUTH_ID },
-              queryParams: { redirectCode: StubUtilTests.REDIRECT_ID }
+              params: {authId: StubUtilTests.AUTH_ID},
+              queryParams: {redirectCode: StubUtilTests.REDIRECT_ID}
             }
           }
         }
