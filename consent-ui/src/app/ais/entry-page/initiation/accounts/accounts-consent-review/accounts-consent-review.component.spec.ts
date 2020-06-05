bf0e7783ce83adcec5536b74f9d6bf63bb5d8b62
Hergy Tchuinkou <htc@adorsys.de>
2020-06-05 09:55:43 +0200

    added test for account-consent

--- consent-ui/src/app/ais/entry-page/initiation/accounts/accounts-consent-review/accounts-consent-review.component.spec.ts
+++ consent-ui/src/app/ais/entry-page/initiation/accounts/accounts-consent-review/accounts-consent-review.component.spec.ts
@@ -1,26 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
 
-import { AccountsConsentReviewComponent } from './accounts-consent-review.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
-import { of } from 'rxjs';
-import { StubUtilTests } from '../../../../common/stub-util-tests';
+import {AccountsConsentReviewComponent} from './accounts-consent-review.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {
+  ActivatedRoute,
+  convertToParamMap
+} from '@angular/router';
+import {of} from 'rxjs';
+import {StubUtilTests} from '../../../../common/stub-util-tests';
+import {SessionService} from "../../../../../common/session.service";
+import {ConsentAuthorizationService} from "../../../../../api";
+import {Location} from "@angular/common";
 
 describe('AccountsConsentReviewComponent', () => {
   let component: AccountsConsentReviewComponent;
   let fixture: ComponentFixture<AccountsConsentReviewComponent>;
+  let consentAuthorizationService: ConsentAuthorizationService;
+
+  const locationStub = {
+    back: jasmine.createSpy('onBack')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AccountsConsentReviewComponent],
       imports: [RouterTestingModule, ReactiveFormsModule, HttpClientTestingModule],
       providers: [
+        SessionService,
+        ConsentAuthorizationService,
+        {provide: Location, useValue: locationStub},
         {
           provide: ActivatedRoute,
           useValue: {
-            parent: { parent: { params: of(convertToParamMap({ authId: StubUtilTests.AUTH_ID })) } }
+            parent: {parent: {params: of(convertToParamMap({authId: StubUtilTests.AUTH_ID}))}}
           }
         }
       ]
@@ -31,9 +49,24 @@ describe('AccountsConsentReviewComponent', () => {
     fixture = TestBed.createComponent(AccountsConsentReviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    consentAuthorizationService = fixture.debugElement.injector.get(ConsentAuthorizationService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go back after back button is pressed', () => {
+    component.onBack();
+    const location = fixture.debugElement.injector.get(Location);
+    expect(location.back).toHaveBeenCalled();
+  });
+
+  it('should call onConfirm', () => {
+    const consentAuthorizationServiceSpy = spyOn(consentAuthorizationService, 'embeddedUsingPOST').and.returnValue(of());
+    component.onConfirm();
+    fixture.detectChanges();
+    expect(consentAuthorizationServiceSpy).toHaveBeenCalled();
+  })
+
 });
