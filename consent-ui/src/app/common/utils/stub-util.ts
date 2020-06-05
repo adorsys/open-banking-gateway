720424f025a40e92bbd82116cda537fa2afe132e
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-06-05 12:52:14 +0200

    define action as enum

--- consent-ui/src/app/common/utils/stub-util.ts
+++ consent-ui/src/app/common/utils/stub-util.ts
@@ -2,8 +2,6 @@
 export class StubUtil {
   public static FINTECH_NAME = 'Awesome FinTech';
   public static ASPSP_NAME = 'adorsys Sandbox';
-  public static PAYMENT = 'PAYMENT';
-  public static CONSENT = 'consent';
   public static X_XSRF_TOKEN = '12b34483-242a-428b-8295-2f4805bb0a30';
   public static X_REQUEST_ID = 'a8b24683-242a-428b-8295-2f4805bb0a30';
 }
