a37dccd7c149f4e9820f05aaf7b2823459ccb341
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-06-10 12:07:54 +0200

    implement payment confirmation UI

--- fintech-examples/fintech-ui/src/app/bank/payment/payment.module.ts
+++ fintech-examples/fintech-ui/src/app/bank/payment/payment.module.ts
@@ -4,9 +4,10 @@ import { ShareModule } from '../../common/share.module';
 import { CommonModule } from '@angular/common';
 import { PaymentComponent } from './payment.component';
 import { InitiateComponent } from './initiate/initiate.component';
+import { ConfirmComponent } from './confirm/confirm.component';
 
 @NgModule({
-  declarations: [PaymentComponent, InitiateComponent],
+  declarations: [PaymentComponent, InitiateComponent, ConfirmComponent],
   imports: [CommonModule, ShareModule, PaymentRoutingModule]
 })
 export class PaymentModule {}
