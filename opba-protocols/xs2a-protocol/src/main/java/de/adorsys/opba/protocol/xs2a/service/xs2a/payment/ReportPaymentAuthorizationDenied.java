a2bcd66c16ae806dc871aeb53e96d4ad76d5d1ec
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-11 22:14:35 +0300

    OBG-653. Resolve merge with develop issues

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/payment/ReportPaymentAuthorizationDenied.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/payment/ReportPaymentAuthorizationDenied.java
@@ -2,8 +2,8 @@ package de.adorsys.opba.protocol.xs2a.service.xs2a.payment;
 
 import de.adorsys.opba.protocol.bpmnshared.dto.messages.PaymentAcquired;
 import de.adorsys.opba.protocol.bpmnshared.service.exec.ValidatedExecution;
-import de.adorsys.opba.protocol.xs2a.service.xs2a.RedirectExecutor;
 import de.adorsys.opba.protocol.xs2a.context.pis.Xs2aPisContext;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.Xs2aRedirectExecutor;
 import lombok.RequiredArgsConstructor;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReportPaymentAuthorizationDenied extends ValidatedExecution<Xs2aPisContext> {
 
-    private final RedirectExecutor redirectExecutor;
+    private final Xs2aRedirectExecutor redirectExecutor;
 
     @Override
     protected void doRealExecution(DelegateExecution execution, Xs2aPisContext context) {
