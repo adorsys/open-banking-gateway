41683abbc429c3f1232f1120277f5c3aebee7f7f
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-12 09:14:06 +0200

    old sandbox configs for basic test

--- opba-protocols/sandboxes/xs2a-sandbox/src/main/java/de/adorsys/opba/protocol/xs2a/testsandbox/internal/SandboxApp.java
+++ opba-protocols/sandboxes/xs2a-sandbox/src/main/java/de/adorsys/opba/protocol/xs2a/testsandbox/internal/SandboxApp.java
@@ -64,13 +64,13 @@ import static org.awaitility.Awaitility.await;
 @Getter
 public enum SandboxApp {
 
-    ONLINE_BANKING_UI("adorsys/xs2a-online-banking-ui:2.9", true, false), // adorsys/xs2a-online-banking-ui
+    ONLINE_BANKING_UI("adorsys/xs2a-online-banking-ui:2.9", true), // adorsys/xs2a-online-banking-ui
     LEDGERS_APP("ledgers-app-2.8.jar"), // adorsys/ledgers
-    LEDGERS_GATEWAY("gateway-app-5.10.jar", false, ImmutableSet.of(ONLINE_BANKING_UI, LEDGERS_APP), true), // adorsys/xs2a-connector-examples
-    ASPSP_PROFILE("aspsp-profile-server-5.10-exec.jar", false, true), // adorsys/xs2a-aspsp-profile
-    CONSENT_MGMT("cms-standalone-service-5.10.jar", false, true), // adorsys/xs2a-consent-management
-    ONLINE_BANKING("online-banking-app-2.9.jar", false, ImmutableSet.of(ONLINE_BANKING_UI, LEDGERS_APP), true), // adorsys/xs2a-online-banking
-    TPP_REST("tpp-rest-server-2.9.jar", false, ImmutableSet.of(ONLINE_BANKING_UI, LEDGERS_APP), false), // adorsys/xs2a-tpp-rest-server
+    LEDGERS_GATEWAY("gateway-app-5.10.jar", false, ImmutableSet.of(ONLINE_BANKING_UI, LEDGERS_APP)), // adorsys/xs2a-connector-examples
+    ASPSP_PROFILE("aspsp-profile-server-5.10-exec.jar"), // adorsys/xs2a-aspsp-profile
+    CONSENT_MGMT("cms-standalone-service-5.10.jar"), // adorsys/xs2a-consent-management
+    ONLINE_BANKING("online-banking-app-2.9.jar", false, ImmutableSet.of(ONLINE_BANKING_UI, LEDGERS_APP)), // adorsys/xs2a-online-banking
+    TPP_REST("tpp-rest-server-2.9.jar", false, ImmutableSet.of(ONLINE_BANKING_UI, LEDGERS_APP)), // adorsys/xs2a-tpp-rest-server
     CERT_GENERATOR("certificate-generator-2.9.jar"); // adorsys/xs2a-certificate-generator
 
 
@@ -91,7 +91,6 @@ public enum SandboxApp {
     private final String jarOrDockerFile;
     private final String mainClass;
     private final Set<SandboxApp> dependsOn;
-    private boolean useOldProperties;
 
     SandboxApp(String jarOrDockerFile) {
         this.jarOrDockerFile = jarOrDockerFile;
@@ -107,20 +106,18 @@ public enum SandboxApp {
         this.dependsOn = null;
     }
 
-    SandboxApp(String jarOrDockerImage, boolean dockerized, boolean useOldProperties) {
+    SandboxApp(String jarOrDockerImage, boolean dockerized) {
         this.jarOrDockerFile = jarOrDockerImage;
         this.mainClass = null;
         this.dockerized = dockerized;
         this.dependsOn = null;
-        this.useOldProperties = useOldProperties;
     }
 
-    SandboxApp(String jarOrDockerImage, boolean dockerized, Set<SandboxApp> dependsOn, boolean useOldProperties) {
+    SandboxApp(String jarOrDockerImage, boolean dockerized, Set<SandboxApp> dependsOn) {
         this.jarOrDockerFile = jarOrDockerImage;
         this.mainClass = null;
         this.dockerized = dockerized;
         this.dependsOn = dependsOn;
-        this.useOldProperties = useOldProperties;
     }
 
     @SneakyThrows
@@ -137,7 +134,7 @@ public enum SandboxApp {
             return false;
         }
 
-        JsonNode tree = YML.readTree(Resources.getResource("sandbox/application-test-common.yml"));
+        JsonNode tree = YML.readTree(Resources.getResource("sandbox-old/application-test-common.yml"));
         String pointer = "/common/apps/local/" + name().toLowerCase().replaceAll("_", "") + "/port";
         JsonNode port = tree.at(pointer);
 
@@ -208,11 +205,10 @@ public enum SandboxApp {
         try {
             Map<String, String> envVars = new HashMap<>();
             // sandbox/application-test-common.yml
-            JsonNode commonConfig = YML.readTree(Resources.getResource("sandbox/application-test-common.yml"));
+            JsonNode commonConfig = YML.readTree(Resources.getResource("sandbox-old/application-test-common.yml"));
             // sandbox/application-test-${appName}.yml
             String pointer = "/env";
-            String resourceName = "sandbox/application-" + testProfileName() + (useOldProperties ? "-old" : "") + ".yml";
-            JsonNode appConfig = YML.readTree(Resources.getResource(resourceName));
+            JsonNode appConfig = YML.readTree(Resources.getResource("sandbox-old/application-" + testProfileName() + ".yml"));
             JsonNode declaredVars = appConfig.at(pointer);
 
             declaredVars.fields().forEachRemaining(entry -> readYamlVariableToMap(entry, commonConfig, envVars));
@@ -310,14 +306,14 @@ public enum SandboxApp {
         return Joiner.on(",").join(
                 "classpath:/",
                 // Due to different classloader used by Spring we can't reference these in other way:
-                getAndValidatePathFromResource("sandbox/application-" + dbProfileAndStartDbIfNeeded(ctx) + ".yml"),
-                getAndValidatePathFromResource("sandbox/application-test-common.yml"),
+                getAndValidatePathFromResource("sandbox-old/application-" + dbProfileAndStartDbIfNeeded(ctx) + ".yml"),
+                getAndValidatePathFromResource("sandbox-old/application-test-common.yml"),
                 getPrimaryConfigFile()
         );
     }
 
     private String getPrimaryConfigFile() {
-        return getAndValidatePathFromResource("sandbox/application-" + testProfileName() + (useOldProperties ? "-old" : "") + ".yml");
+        return getAndValidatePathFromResource("sandbox-old/application-" + testProfileName() + ".yml");
     }
 
     @SneakyThrows
@@ -347,8 +343,7 @@ public enum SandboxApp {
     // Profiles from spring.profile.active are not always applied, forcing it.
     @SneakyThrows
     public Set<String> extraProfiles() {
-        String resourceName = "sandbox/application-" + testProfileName() + (useOldProperties ? "-old" : "") + ".yml";
-        JsonNode tree = YML.readTree(Resources.getResource(resourceName));
+        JsonNode tree = YML.readTree(Resources.getResource("sandbox-old/application-" + testProfileName() + ".yml"));
         String pointer = "/spring/profiles/active";
         JsonNode activeProfiles = tree.at(pointer);
 
