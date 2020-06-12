8b52e133042644536e30160e499ed08375fe9c8b
electronicpeter <github@electronicpeter.de>
2020-06-12 15:53:57 +0200

    OBG 699 fixed tests

--- fintech-examples/fintech-ui/src/app/bank/payment/confirm/confirm.component.spec.ts
+++ fintech-examples/fintech-ui/src/app/bank/payment/confirm/confirm.component.spec.ts
@@ -3,6 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ConfirmComponent } from './confirm.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ConfirmData } from './confirm.data';
+import { RedirectStruct } from '../../redirect-page/redirect-struct';
+import { ClassSinglePaymentInitiationRequest } from '../../../api/model-classes/ClassSinglePaymentInitiationRequest';
 
 describe('ConfirmComponent', () => {
   let component: ConfirmComponent;
@@ -12,7 +16,35 @@ describe('ConfirmComponent', () => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [ConfirmComponent]
-    }).compileComponents();
+    })
+      .overrideComponent(ConfirmComponent, {
+        set: {
+          providers: [
+            {
+              provide: ActivatedRoute,
+              useValue: {
+                paramMap: {
+                  subscribe(confirmationData: string): string {
+                    const r: RedirectStruct = new RedirectStruct();
+                    r.bankName = 'peter';
+                    r.redirectUrl = 'redirectUrl';
+                    const p: ClassSinglePaymentInitiationRequest = new ClassSinglePaymentInitiationRequest();
+                    p.debitorIban = 'DE80760700240271232400';
+                    p.creditorIban = 'AL90208110080000001039531801';
+                    p.amount = '1.10';
+                    p.name = 'peter';
+                    const c: ConfirmData = new ConfirmData();
+                    c.redirectStruct = r;
+                    c.paymentRequest = p;
+                    return JSON.stringify(c);
+                  }
+                }
+              }
+            }
+          ]
+        }
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,9 +53,7 @@ describe('ConfirmComponent', () => {
     fixture.detectChanges();
   });
 
-  /* TODO Hergie help me
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-    */
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
