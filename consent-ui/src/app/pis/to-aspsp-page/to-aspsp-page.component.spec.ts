cb2a55f2346061d940ced1e786a918e74e0dd46d
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-16 17:29:46 +0200

    #698 select sca method in payment fix

--- consent-ui/src/app/pis/to-aspsp-page/to-aspsp-page.component.spec.ts
+++ consent-ui/src/app/pis/to-aspsp-page/to-aspsp-page.component.spec.ts
@@ -2,6 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ToAspspPageComponent } from './to-aspsp-page.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ActivatedRoute} from "@angular/router";
+import {of} from "rxjs";
+import {StubUtilTests} from "../../ais/common/stub-util-tests";
 
 describe('ToAspspPageComponent', () => {
   let component: ToAspspPageComponent;
@@ -10,7 +15,16 @@ describe('ToAspspPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ToAspspPageComponent],
-      schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            parent: { params: of({ authId: StubUtilTests.AUTH_ID }) }
+          }
+        }
+      ]
     }).compileComponents();
   }));
 
