0e4a5ac36ba5105ddc820eb3c64013243b3d5d56
iiuex <iiu@adorsys.com.ua>
2020-06-17 08:01:46 +0300

    Obg 736: Test and pmd fixed

--- opba-protocols/xs2a-protocol-tests/xs2a-bdd-wiremock/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/wiremock/mocks/WiremockPaymentRequest.java
+++ opba-protocols/xs2a-protocol-tests/xs2a-bdd-wiremock/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/wiremock/mocks/WiremockPaymentRequest.java
@@ -1,20 +1,9 @@
 package de.adorsys.opba.protocol.xs2a.tests.e2e.wiremock.mocks;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
-import de.adorsys.opba.api.security.external.service.RequestSigningService;
 import de.adorsys.opba.protocol.xs2a.tests.e2e.stages.PaymentRequestCommon;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @JGivenStage
 @SuppressWarnings("checkstyle:MethodName") // Jgiven prettifies snake-case names not camelCase
 public class WiremockPaymentRequest<SELF extends WiremockPaymentRequest<SELF>> extends PaymentRequestCommon<SELF> {
-
-    @ExpectedScenarioState
-    private WireMockServer wireMock;
-
-    @Autowired
-    private RequestSigningService requestSigningService;
-
 }
