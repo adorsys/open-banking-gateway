e96ce195262e829457655c26b054773a90cb760c
Veronika Sedlackova <vse@adorsys.de>
2020-06-18 13:49:12 +0200

    obg-276 removed unused imports

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/ValidationRule.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/ValidationRule.java
@@ -14,7 +14,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
