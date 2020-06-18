f523c8c88924d995788421aa4c15153ee1e2d6b7
electronicpeter <github@electronicpeter.de>
2020-06-18 12:09:56 +0200

    OBG 684 fixed ui test

--- fintech-examples/fintech-ui/src/app/bank/settings/settings.component.spec.ts
+++ fintech-examples/fintech-ui/src/app/bank/settings/settings.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -8,7 +11,8 @@ describe('SettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [ SettingsComponent ],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   }));
@@ -19,8 +23,7 @@ describe('SettingsComponent', () => {
     fixture.detectChanges();
   });
 
-  // TODO PETER FIX THIS
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+     expect(component).toBeTruthy();
+  });
 });
