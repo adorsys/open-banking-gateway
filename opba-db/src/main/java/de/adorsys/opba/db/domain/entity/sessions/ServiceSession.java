4f2fa816a5125ae7bb0c1ac918af7856b2ed9a71
Veronika Sedlackova <vse@adorsys.de>
2020-06-18 13:49:12 +0200

    obg-276 removed unused imports

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/sessions/ServiceSession.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/sessions/ServiceSession.java
@@ -21,7 +21,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
