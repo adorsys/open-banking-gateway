699d5a2d85c9164be8f174b4430024d9dbb44bde
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-06-22 15:57:29 +0200

    #775 disable retry

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/config/RetryConfig.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/config/RetryConfig.java
@@ -3,6 +3,7 @@ package de.adorsys.opba.protocol.xs2a.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.RetryOperations;
+import org.springframework.retry.policy.NeverRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 /**
@@ -13,6 +14,8 @@ public class RetryConfig {
 
     @Bean
     public RetryOperations retryOperations() {
-        return new RetryTemplate();
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(new NeverRetryPolicy()); // TODO: re-enable retry after proper error handling implementation
+        return retryTemplate;
     }
 }
