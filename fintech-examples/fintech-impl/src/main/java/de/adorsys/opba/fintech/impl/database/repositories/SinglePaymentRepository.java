7a605ee8926748e926c6629d7f6740ac6e48a539
electronicpeter <github@electronicpeter.de>
2020-06-22 10:32:03 +0200

    OBG 684 removed account and payment cache from fintech server

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/database/repositories/SinglePaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package de.adorsys.opba.fintech.impl.database.repositories;
-
-import de.adorsys.opba.fintech.impl.database.entities.AccountEntity;
-import de.adorsys.opba.fintech.impl.database.entities.SinglePaymentEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SinglePaymentRepository extends CrudRepository<SinglePaymentEntity, Long> {
-    Iterable<SinglePaymentEntity> findByAccountEntity(AccountEntity accountEntity);
-}
