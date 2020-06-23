f3bd9b1cab7a9b93c9502aed9dde8a04cad8920e
electronicpeter <github@electronicpeter.de>
2020-06-23 14:36:07 +0200

    OBG 701 service for status implemented in be

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/controller/FintechSinglePaymentInitiationImpl.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/controller/FintechSinglePaymentInitiationImpl.java
@@ -1,5 +1,6 @@
 package de.adorsys.opba.fintech.impl.controller;
 
+import de.adorsys.opba.fintech.api.model.generated.PaymentInitiationWithStatusResponse;
 import de.adorsys.opba.fintech.api.model.generated.SinglePaymentInitiationRequest;
 import de.adorsys.opba.fintech.api.resource.generated.FintechSinglePaymentInitiationApi;
 import de.adorsys.opba.fintech.impl.service.PaymentService;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -28,7 +30,7 @@ public class FintechSinglePaymentInitiationImpl implements FintechSinglePaymentI
             String fintechRedirectURLNOK,
             String bankId,
             String accountId) {
-        log.info("got payment requrest");
+        log.debug("got initiate payment requrest");
 
         if (!sessionLogicService.isSessionAuthorized()) {
             log.warn("singlePaymentPost failed: user is not authorized!");
@@ -37,4 +39,21 @@ public class FintechSinglePaymentInitiationImpl implements FintechSinglePaymentI
 
         return sessionLogicService.addSessionMaxAgeToHeader(paymentService.initiateSinglePayment(bankId, accountId, body, fintechRedirectURLOK, fintechRedirectURLNOK));
     }
+
+
+    @Override
+    public ResponseEntity<List<PaymentInitiationWithStatusResponse>> retrieveAllSinglePayments(String bankId, String accountId, UUID xRequestID, String xsrfToken) {
+        log.debug("got list all payment requrest");
+
+        if (!sessionLogicService.isSessionAuthorized()) {
+            log.warn("list all payments failed: user is not authorized!");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        return sessionLogicService.addSessionMaxAgeToHeader(paymentService.retrieveAllSinglePayments(bankId, accountId));
+
+
+    }
+
+
 }
--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/database/repositories/ConsentRepository.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/database/repositories/ConsentRepository.java
@@ -5,11 +5,12 @@ import de.adorsys.opba.fintech.impl.database.entities.UserEntity;
 import de.adorsys.opba.fintech.impl.tppclients.ConsentType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ConsentRepository extends CrudRepository<ConsentEntity, Long> {
     Optional<ConsentEntity> findByTppAuthId(String authId);
     Optional<ConsentEntity> findByUserEntityAndBankIdAndConsentTypeAndConsentConfirmed(UserEntity userEntity, String bankId, ConsentType consentType, Boolean consentConfirmed);
-    Optional<ConsentEntity> findByUserEntityAndBankIdAndAccountId(UserEntity userEntity, String bankId, String accountId);
+    List<ConsentEntity> findByUserEntityAndBankIdAndAccountId(UserEntity userEntity, String bankId, String accountId);
 
 }
