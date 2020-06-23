72d19df19aa84da848cdf2ebf1c72f2d7e2fbed5
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-23 14:55:50 +0300

    Updated processes

--- opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/service/consent/authentication/HbciSubmitSelectedMethodAndAskForTanChallenge.java
+++ opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/service/consent/authentication/HbciSubmitSelectedMethodAndAskForTanChallenge.java
@@ -3,6 +3,7 @@ package de.adorsys.opba.protocol.hbci.service.consent.authentication;
 import de.adorsys.multibanking.domain.Bank;
 import de.adorsys.multibanking.domain.BankAccess;
 import de.adorsys.multibanking.domain.BankApiUser;
+import de.adorsys.multibanking.domain.ScaStatus;
 import de.adorsys.multibanking.domain.TanTransportType;
 import de.adorsys.multibanking.domain.request.SelectPsuAuthenticationMethodRequest;
 import de.adorsys.multibanking.domain.response.UpdateAuthResponse;
@@ -11,7 +12,6 @@ import de.adorsys.multibanking.hbci.model.HbciConsent;
 import de.adorsys.opba.protocol.bpmnshared.service.context.ContextUtil;
 import de.adorsys.opba.protocol.bpmnshared.service.exec.ValidatedExecution;
 import de.adorsys.opba.protocol.hbci.context.HbciContext;
-import de.adorsys.opba.protocol.hbci.service.HbciRedirectExecutor;
 import lombok.RequiredArgsConstructor;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.delegate.DelegateExecution;
@@ -20,13 +20,12 @@ import org.springframework.stereotype.Service;
 /**
  * Asks PSU for his SCA challenge result by redirect him to password input page. Suspends process to wait for users' input.
  */
-@Service("hbciSubmitSelectedMethodAndAskForTanChallenge")
+@Service("hbciScaMethodSelected")
 @RequiredArgsConstructor
 public class HbciSubmitSelectedMethodAndAskForTanChallenge extends ValidatedExecution<HbciContext> {
 
     private final OnlineBankingService onlineBankingService;
     private final RuntimeService runtimeService;
-    private final HbciRedirectExecutor redirectExecutor;
 
     @Override
     protected void doRealExecution(DelegateExecution execution, HbciContext context) {
@@ -44,10 +43,11 @@ public class HbciSubmitSelectedMethodAndAskForTanChallenge extends ValidatedExec
         UpdateAuthResponse response = onlineBankingService.getStrongCustomerAuthorisation().selectPsuAuthenticationMethod(request);
         ContextUtil.getAndUpdateContext(
                 execution,
-                (HbciContext ctx) -> ctx.setHbciDialogConsent((HbciConsent) response.getBankApiConsentData())
+                (HbciContext ctx) -> {
+                    ctx.getHbciDialogConsent().setStatus(ScaStatus.SCAMETHODSELECTED);
+                    ctx.setHbciDialogConsent((HbciConsent) response.getBankApiConsentData());
+                }
         );
-
-        redirectExecutor.redirect(execution, context, redir -> redir.getParameters().getReportScaResult());
     }
 
     @Override
