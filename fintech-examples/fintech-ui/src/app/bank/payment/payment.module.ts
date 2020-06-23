1d257a80ff24a7ede2660bb2b159b65057b3d35a
electronicpeter <github@electronicpeter.de>
2020-06-23 12:36:27 +0200

    OBG 701 loa und lot stubs for payment in fintech ui

--- fintech-examples/fintech-ui/src/app/bank/payment/payment.module.ts
+++ fintech-examples/fintech-ui/src/app/bank/payment/payment.module.ts
@@ -6,9 +6,19 @@ import { PaymentComponent } from './payment.component';
 import { InitiateComponent } from './initiate/initiate.component';
 import { ConfirmComponent } from './confirm/confirm.component';
 import { ResultComponent } from './result/result.component';
+import { ListAccountsForPaymentComponent } from './list-accounts-for-payment/list-accounts-for-payment.component';
+import { ListPaymentsComponent } from './list-payments/list-payments.component';
 
 @NgModule({
-  declarations: [PaymentComponent, InitiateComponent, ConfirmComponent, ResultComponent],
+  declarations: [
+    PaymentComponent,
+    InitiateComponent,
+    ConfirmComponent,
+    ResultComponent,
+    ListAccountsForPaymentComponent,
+    ListPaymentsComponent
+  ],
   imports: [CommonModule, ShareModule, PaymentRoutingModule]
 })
-export class PaymentModule {}
+export class PaymentModule {
+}
