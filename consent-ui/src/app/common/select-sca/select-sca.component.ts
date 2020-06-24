4b14cbb499ef05303e897a971cc622646d69e6fe
Vladimir Nechai <vne@adorsys.de>
2020-06-24 19:55:27 +0200

    extract API call for sca component

--- consent-ui/src/app/common/select-sca/select-sca.component.ts
+++ consent-ui/src/app/common/select-sca/select-sca.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ScaUserData } from '../../api';
+import { AuthStateConsentAuthorizationService, ScaUserData, UpdateConsentAuthorizationService } from '../../api';
+import { ApiHeaders } from '../../api/api.headers';
+import { SessionService } from '../session.service';
+import { StubUtil } from '../utils/stub-util';
 
 @Component({
   selector: 'consent-app-select-sca',
@@ -8,20 +11,52 @@ import { ScaUserData } from '../../api';
   styleUrls: ['./select-sca.component.scss']
 })
 export class SelectScaComponent implements OnInit {
-  @Input() scaMethods: ScaUserData[] = [];
-  @Input() selectedMethod = new FormControl();
-  @Output() selectedValue = new EventEmitter<string>();
+  @Input() authorizationSessionId: string;
+  @Input() redirectCode: string;
+  @Output() selectedValue = new EventEmitter<any>();
+  scaMethods: ScaUserData[] = [];
+  selectedMethod = new FormControl();
   scaMethodForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authStateConsentAuthorizationService: AuthStateConsentAuthorizationService,
+    private updateConsentAuthorizationService: UpdateConsentAuthorizationService,
+    private sessionService: SessionService
+  ) {}
 
   ngOnInit() {
+    // init form
     this.scaMethodForm = this.formBuilder.group({
       selectedMethodValue: [this.selectedMethod, Validators.required]
     });
+    this.initSca();
   }
 
   onSubmit(): void {
-    this.selectedValue.emit(this.selectedMethod.value);
+    this.updateConsentAuthorizationService
+      .embeddedUsingPOST(
+        this.authorizationSessionId,
+        StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
+        StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
+        this.redirectCode,
+        { scaAuthenticationData: { SCA_CHALLENGE_ID: this.selectedMethod.value } },
+        'response'
+      )
+      .subscribe(res => this.selectedValue.emit(res));
+  }
+
+  private initSca(): void {
+    this.authStateConsentAuthorizationService
+      .authUsingGET(this.authorizationSessionId, this.redirectCode, 'response')
+      .subscribe(consentAuth => {
+        this.sessionService.setRedirectCode(
+          this.authorizationSessionId,
+          consentAuth.headers.get(ApiHeaders.REDIRECT_CODE)
+        );
+        this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
+        this.scaMethods = consentAuth.body.consentAuth.scaMethods;
+        this.selectedMethod.setValue(this.scaMethods[0].id);
+      });
   }
 }
