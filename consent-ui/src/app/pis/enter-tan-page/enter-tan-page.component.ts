76cf300ab5bc8a17bb1fdf4e5b3d6f904f8669b0
Vladimir Nechai <vne@adorsys.de>
2020-06-24 19:17:09 +0200

    refactor API call from enter-tan.component.ts

--- consent-ui/src/app/pis/enter-tan-page/enter-tan-page.component.ts
+++ consent-ui/src/app/pis/enter-tan-page/enter-tan-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../common/session.service';
 import { UpdateConsentAuthorizationService } from '../../api';
 import { ActivatedRoute } from '@angular/router';
-import { StubUtil } from '../../common/utils/stub-util';
 import { ApiHeaders } from '../../api/api.headers';
 
 @Component({
@@ -12,10 +11,9 @@ import { ApiHeaders } from '../../api/api.headers';
 })
 export class EnterTanPageComponent implements OnInit {
   public static ROUTE = 'sca-result';
-
-  private authorizationSessionId: string;
-  private redirectCode: string;
   wrongSca: boolean;
+  authorizationSessionId: string;
+  redirectCode: string;
 
   constructor(
     private sessionService: SessionService,
@@ -29,21 +27,10 @@ export class EnterTanPageComponent implements OnInit {
     this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
   }
 
-  onSubmit(tan: string): void {
-    this.updateConsentAuthorizationService
-      .embeddedUsingPOST(
-        this.authorizationSessionId,
-        StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
-        StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
-        this.redirectCode,
-        { scaAuthenticationData: { SCA_CHALLENGE_DATA: tan } },
-        'response'
-      )
-      .subscribe(res => {
-        // redirect to the provided location
-        console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
-        this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
-        window.location.href = res.headers.get(ApiHeaders.LOCATION);
-      });
+  onSubmit(res: any): void {
+    // redirect to the provided location
+    console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
+    this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
+    window.location.href = res.headers.get(ApiHeaders.LOCATION);
   }
 }
