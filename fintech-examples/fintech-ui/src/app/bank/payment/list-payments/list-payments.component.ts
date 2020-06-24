407aec910da570517a4c14852ed24b53420a1e54
electronicpeter <github@electronicpeter.de>
2020-06-24 11:05:44 +0200

    OBG 701 old ui hirachy

--- fintech-examples/fintech-ui/src/app/bank/payment/list-payments/list-payments.component.ts
+++ fintech-examples/fintech-ui/src/app/bank/payment/list-payments/list-payments.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FintechSinglePaymentInitiationService } from '../../../api';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-payments',
@@ -7,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPaymentsComponent implements OnInit {
   public static ROUTE = 'accounts';
+  bankId;
+  accountId;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fintechSinglePaymentInitiationService: FintechSinglePaymentInitiationService
+  ) {}
+
 
   ngOnInit() {
+    this.bankId = this.route.snapshot.paramMap.get('bankid');
+    this.accountId = this.route.snapshot.firstChild.paramMap.get('accountid');
+    console.log('lpc bankid:', this.bankId, ' accountId:', this.accountId);
+
+    this.fintechSinglePaymentInitiationService.retrieveAllSinglePayments(this.bankId, this.accountId, '', '', 'response')
+      .pipe(map(response => response))
+      .subscribe(
+        response => {
+          console.log('response status of payment call is ', response.status);
+        }
+      );
   }
 
 }
