fa41070154eb5d885406f558486c497e8a4b8f1a
Vladimir Nechai <vne@adorsys.de>
2020-06-24 19:55:27 +0200

    extract API call for sca component

--- consent-ui/src/app/ais/sca-select-page/sca-select-page.component.ts
+++ consent-ui/src/app/ais/sca-select-page/sca-select-page.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormControl } from '@angular/forms';
-import { UpdateConsentAuthorizationService, ScaUserData } from '../../api';
-import { AuthStateConsentAuthorizationService } from '../../api';
 import { SessionService } from '../../common/session.service';
 import { ApiHeaders } from '../../api/api.headers';
-import { StubUtil } from '../../common/utils/stub-util';
 
 @Component({
   selector: 'consent-app-sca-select-page',
@@ -13,53 +9,21 @@ import { StubUtil } from '../../common/utils/stub-util';
   styleUrls: ['./sca-select-page.component.scss']
 })
 export class ScaSelectPageComponent implements OnInit {
-  authorizationSessionId = '';
-  redirectCode = '';
-  scaMethods: ScaUserData[] = [];
-  selectedMethod = new FormControl();
+  authorizationSessionId: string;
+  redirectCode: string;
 
-  constructor(
-    private sessionService: SessionService,
-    private updateConsentAuthorizationService: UpdateConsentAuthorizationService,
-    private authStateConsentAuthorizationService: AuthStateConsentAuthorizationService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private sessionService: SessionService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.authorizationSessionId = this.route.parent.snapshot.paramMap.get('authId');
     this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
-    this.loadAvailableMethods();
   }
 
-  onSubmit(selectedMethodValue: string): void {
-    this.updateConsentAuthorizationService
-      .embeddedUsingPOST(
-        this.authorizationSessionId,
-        StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
-        StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
-        this.redirectCode,
-        { scaAuthenticationData: { SCA_CHALLENGE_ID: selectedMethodValue } },
-        'response'
-      )
-      .subscribe(res => {
-        // redirect to the provided location
-        console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
-        this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
-        window.location.href = res.headers.get(ApiHeaders.LOCATION);
-      });
-  }
-
-  private loadAvailableMethods(): void {
-    this.authStateConsentAuthorizationService
-      .authUsingGET(this.authorizationSessionId, this.redirectCode, 'response')
-      .subscribe(consentAuth => {
-        this.sessionService.setRedirectCode(
-          this.authorizationSessionId,
-          consentAuth.headers.get(ApiHeaders.REDIRECT_CODE)
-        );
-        this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
-        this.scaMethods = consentAuth.body.consentAuth.scaMethods;
-        this.selectedMethod.setValue(this.scaMethods[0].id);
-      });
+  onSubmit(res: any): void {
+    // responce from the API call is handled in parent component that gives more flexibility
+    console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
+    this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
+    // redirect to the provided location
+    window.location.href = res.headers.get(ApiHeaders.LOCATION);
   }
 }
--- consent-ui/src/app/pis/select-sca-page/select-sca-page.component.ts
+++ consent-ui/src/app/pis/select-sca-page/select-sca-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthStateConsentAuthorizationService, ScaUserData, UpdateConsentAuthorizationService } from '../../api';
-import { FormControl } from '@angular/forms';
 import { SessionService } from '../../common/session.service';
 import { ActivatedRoute } from '@angular/router';
-import { StubUtil } from '../../common/utils/stub-util';
 import { ApiHeaders } from '../../api/api.headers';
 
 @Component({
@@ -14,53 +11,21 @@ import { ApiHeaders } from '../../api/api.headers';
 export class SelectScaPageComponent implements OnInit {
   public static ROUTE = 'select-sca-method';
 
-  authorizationSessionId = '';
-  redirectCode = '';
-  scaMethods: ScaUserData[] = [];
-  selectedMethod = new FormControl();
+  authorizationSessionId: string;
+  redirectCode: string;
 
-  constructor(
-    private sessionService: SessionService,
-    private updateConsentAuthorizationService: UpdateConsentAuthorizationService,
-    private authStateConsentAuthorizationService: AuthStateConsentAuthorizationService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private sessionService: SessionService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.authorizationSessionId = this.route.parent.snapshot.paramMap.get('authId');
     this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
-    this.loadAvailableMethods();
   }
 
-  onSubmit(selectedMethodValue: string): void {
-    this.updateConsentAuthorizationService
-      .embeddedUsingPOST(
-        this.authorizationSessionId,
-        StubUtil.X_REQUEST_ID, // TODO: real values instead of stubs
-        StubUtil.X_XSRF_TOKEN, // TODO: real values instead of stubs
-        this.redirectCode,
-        { scaAuthenticationData: { SCA_CHALLENGE_ID: selectedMethodValue } },
-        'response'
-      )
-      .subscribe(res => {
-        // redirect to the provided location
-        console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
-        this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
-        window.location.href = res.headers.get(ApiHeaders.LOCATION);
-      });
-  }
-
-  private loadAvailableMethods(): void {
-    this.authStateConsentAuthorizationService
-      .authUsingGET(this.authorizationSessionId, this.redirectCode, 'response')
-      .subscribe(consentAuth => {
-        this.sessionService.setRedirectCode(
-          this.authorizationSessionId,
-          consentAuth.headers.get(ApiHeaders.REDIRECT_CODE)
-        );
-        this.redirectCode = this.sessionService.getRedirectCode(this.authorizationSessionId);
-        this.scaMethods = consentAuth.body.consentAuth.scaMethods;
-        this.selectedMethod.setValue(this.scaMethods[0].id);
-      });
+  onSubmit(res: any): void {
+    // responce from the API call is handled in parent component that gives more flexibility
+    console.log('REDIRECTING TO: ' + res.headers.get(ApiHeaders.LOCATION));
+    this.sessionService.setRedirectCode(this.authorizationSessionId, res.headers.get(ApiHeaders.REDIRECT_CODE));
+    // redirect to the provided location
+    window.location.href = res.headers.get(ApiHeaders.LOCATION);
   }
 }
