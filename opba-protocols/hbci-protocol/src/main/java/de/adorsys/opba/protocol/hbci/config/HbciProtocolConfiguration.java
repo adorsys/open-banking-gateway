b26e3484fb7d96d667df373356191f4044cfc83a
Veronika Sedlackova <vse@adorsys.de>
2020-06-24 17:47:57 +0200

    obg-506 replaced context.getAuthorizationSessionIdIfOpened with sessionId in yml

--- opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/config/HbciProtocolConfiguration.java
+++ opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/config/HbciProtocolConfiguration.java
@@ -13,7 +13,7 @@ import static de.adorsys.opba.protocol.hbci.config.ConfigConst.HBCI_PROTOCOL_CON
 /**
  * HBCI URL protocol configuration. Note that all URLs are expanded using {@link de.adorsys.opba.protocol.bpmnshared.service.context.ContextUtil}, so
  * you can use string interpolation like this:
- * http://localhost:8080/v1/consent/#{context.getAuthorizationSessionIdIfOpened()}/fromAspsp/STUB_STATE/ok?redirectCode=#{context.getAspspRedirectCode()}
+ * http://localhost:8080/v1/consent/{sessionId}/fromAspsp/STUB_STATE/ok?redirectCode=#{context.getAspspRedirectCode()}
  * The aforementioned URL will get interpolated using {@link de.adorsys.opba.protocol.hbci.context.HbciContext} functions to i.e.
  * http://localhost:8080/v1/consent/53659eaf-d953-48de-a644-61c3c915d2c6/fromAspsp/STUB_STATE/ok?redirectCode=0174ef07-2e33-4264-9646-3b8965d100a0
  * with data from {@link de.adorsys.opba.protocol.hbci.context.HbciContext} available at the step execution.
