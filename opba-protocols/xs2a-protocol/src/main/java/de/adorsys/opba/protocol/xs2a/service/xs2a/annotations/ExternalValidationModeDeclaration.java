647d280462aabb15b6059fd4bf5bc50bf810bc4e
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-25 10:07:42 +0300

    OBG-303. More flexible validations

---
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/annotations/ExternalValidationModeDeclaration.java
@@ -0,0 +1,18 @@
+package de.adorsys.opba.protocol.xs2a.service.xs2a.annotations;
+
+import de.adorsys.opba.protocol.api.dto.codes.FieldCode;
+import de.adorsys.opba.protocol.xs2a.context.Xs2aContext;
+import de.adorsys.opba.protocol.xs2a.service.xs2a.dto.ValidationMode;
+
+import java.util.Set;
+
+public interface ExternalValidationModeDeclaration {
+
+    default int priority() {
+        return 0;
+    }
+
+    Set<FieldCode> appliesTo();
+    boolean appliesToContext(Xs2aContext context);
+    ValidationMode computeValidationMode(Xs2aContext context);
+}
--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/dto/Xs2aTransactionParameters.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/dto/Xs2aTransactionParameters.java
@@ -4,7 +4,6 @@ import de.adorsys.opba.protocol.bpmnshared.dto.DtoMapper;
 import de.adorsys.opba.protocol.xs2a.context.ais.TransactionListXs2aContext;
 import de.adorsys.opba.protocol.xs2a.service.xs2a.annotations.ContextCode;
 import de.adorsys.opba.protocol.xs2a.service.xs2a.annotations.FrontendCode;
-import de.adorsys.opba.protocol.xs2a.service.xs2a.annotations.ResourceIdConditionProvider;
 import de.adorsys.opba.protocol.xs2a.service.xs2a.annotations.ValidationInfo;
 import de.adorsys.xs2a.adapter.service.RequestParams;
 import lombok.Data;
@@ -21,7 +20,6 @@ import static de.adorsys.opba.protocol.api.dto.codes.FieldCode.DATE_TO;
 import static de.adorsys.opba.protocol.api.dto.codes.TypeCode.STRING;
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.SPRING_KEYWORD;
 import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.XS2A_MAPPERS_PACKAGE;
-import static de.adorsys.opba.protocol.xs2a.service.xs2a.dto.ValidationMode.CONDITIONAL;
 
 /**
  * XS2A transaction list describing parameters.
@@ -33,8 +31,7 @@ public class Xs2aTransactionParameters extends Xs2aWithBalanceParameters {
     /**
      * Transaction booking status - i.e. PENDING/BOOKED.
      */
-    @ValidationInfo(ui = @FrontendCode(STRING), ctx = @ContextCode(BOOKING_STATUS), validationMode = CONDITIONAL,
-            condition = ResourceIdConditionProvider.class)
+    @ValidationInfo(ui = @FrontendCode(STRING), ctx = @ContextCode(BOOKING_STATUS))
     @NotBlank(message = "{no.ctx.bookingStatus}")
     private String bookingStatus;
 
@@ -42,16 +39,14 @@ public class Xs2aTransactionParameters extends Xs2aWithBalanceParameters {
      * Transaction list date from.
      */
     @NotNull(message = "{no.ctx.dateFrom}")
-    @ValidationInfo(ui = @FrontendCode(STRING), ctx = @ContextCode(DATE_FROM), validationMode = CONDITIONAL,
-            condition = ResourceIdConditionProvider.class)
+    @ValidationInfo(ui = @FrontendCode(STRING), ctx = @ContextCode(DATE_FROM))
     private LocalDate dateFrom;
 
     /**
      * Transaction list date to.
      */
     @NotNull(message = "{no.ctx.dateTo}")
-    @ValidationInfo(ui = @FrontendCode(STRING), ctx = @ContextCode(DATE_TO), validationMode = CONDITIONAL,
-            condition = ResourceIdConditionProvider.class)
+    @ValidationInfo(ui = @FrontendCode(STRING), ctx = @ContextCode(DATE_TO))
     private LocalDate dateTo;
 
     // TODO - MapStruct?
