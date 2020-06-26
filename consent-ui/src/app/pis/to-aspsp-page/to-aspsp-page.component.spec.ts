14451b31a232d1ca81bdbf649074dede67f74e51
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-06-26 08:29:44 +0200

    reformate

--- consent-ui/src/app/pis/to-aspsp-page/to-aspsp-page.component.spec.ts
+++ consent-ui/src/app/pis/to-aspsp-page/to-aspsp-page.component.spec.ts
@@ -2,11 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ToAspspPageComponent } from './to-aspsp-page.component';
-import {RouterTestingModule} from "@angular/router/testing";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {ActivatedRoute} from "@angular/router";
-import {of} from "rxjs";
-import {StubUtilTests} from "../../ais/common/stub-util-tests";
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { StubUtilTests } from '../../ais/common/stub-util-tests';
 
 describe('ToAspspPageComponent', () => {
   let component: ToAspspPageComponent;
