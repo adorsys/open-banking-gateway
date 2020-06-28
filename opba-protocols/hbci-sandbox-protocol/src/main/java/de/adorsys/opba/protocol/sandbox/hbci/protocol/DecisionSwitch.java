c625160a598cdff17068424370904b9ab1e63ecd
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-28 22:14:11 +0300

    OBG-693. Implemented decision switches

--- opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/DecisionSwitch.java
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/DecisionSwitch.java
@@ -1,7 +1,45 @@
 package de.adorsys.opba.protocol.sandbox.hbci.protocol;
 
+import com.google.common.base.Strings;
+import de.adorsys.opba.protocol.sandbox.hbci.config.dto.AuthLevel;
+import de.adorsys.opba.protocol.sandbox.hbci.protocol.context.SandboxContext;
 import org.springframework.stereotype.Service;
 
 @Service("decisionSwitch")
 public class DecisionSwitch {
+
+    public boolean isDialogAnonymous(SandboxContext context) {
+        return Strings.isNullOrEmpty(context.getRequestPin())
+                || Operation.DIALOG_INIT_ANON == context.getRequestOperation()
+                || null == context.getUser();
+    }
+
+    public boolean isDialogPinTanOk(SandboxContext context) {
+        return context.getUser().getPin().equals(context.getRequestPin())
+                && context.getUser().getTan().equals(context.getRequestTan());
+    }
+
+    public boolean isDialogPinOk(SandboxContext context) {
+        return context.getUser().getPin().equals(context.getRequestPin());
+    }
+
+    public boolean isDialogInit(SandboxContext context) {
+        return Operation.DIALOG_INIT == context.getRequestOperation();
+    }
+
+    public boolean isCustomMessage(SandboxContext context) {
+        return Operation.CUSTOM_MSG == context.getRequestOperation();
+    }
+
+    public boolean isSynchronization(SandboxContext context) {
+        return Operation.SYNCH == context.getRequestOperation();
+    }
+
+    public boolean isDialogEnd(SandboxContext context) {
+        return Operation.DIALOG_END == context.getRequestOperation();
+    }
+
+    public boolean anonymousBpdSupported(SandboxContext context) {
+        return AuthLevel.ANONYMOUS == context.getBank().getSecurity().getBankParametersData();
+    }
 }
