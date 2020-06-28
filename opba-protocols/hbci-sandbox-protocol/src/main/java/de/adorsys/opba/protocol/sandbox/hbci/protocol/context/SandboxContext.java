12c0f12ae8498f94ab494ecb158259fdf24183e3
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-28 21:54:31 +0300

    OBG-693. Updated configurations settings to include security level

--- opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/context/SandboxContext.java
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/context/SandboxContext.java
@@ -1,5 +1,8 @@
 package de.adorsys.opba.protocol.sandbox.hbci.protocol.context;
 
+import de.adorsys.opba.protocol.sandbox.hbci.config.dto.Account;
+import de.adorsys.opba.protocol.sandbox.hbci.config.dto.Bank;
+import de.adorsys.opba.protocol.sandbox.hbci.config.dto.User;
 import de.adorsys.opba.protocol.sandbox.hbci.protocol.Operation;
 import lombok.Data;
 
@@ -11,8 +14,17 @@ public class SandboxContext {
     private Operation requestOperation;
     private Map<String, String> requestData;
 
+    private Bank bank;
+    private User user;
+    private Account account;
+
     private boolean missingOrWrongTan;
     private boolean missingOrWrongPin;
 
     private String response;
+
+    // commonly used:
+    private String secCheckRef;
+    private String dialogId;
+    private String userId;
 }
