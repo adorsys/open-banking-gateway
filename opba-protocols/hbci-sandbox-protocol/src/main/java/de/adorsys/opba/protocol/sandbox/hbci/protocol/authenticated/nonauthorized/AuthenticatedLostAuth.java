539f66ea0308ed17fff2d6b42a45f213ef85c8fb
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-28 17:44:14 +0300

    OBG-693. Added HBCI templates

---
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/authenticated/nonauthorized/AuthenticatedLostAuth.java
@@ -0,0 +1,7 @@
+package de.adorsys.opba.protocol.sandbox.hbci.protocol.authenticated.nonauthorized;
+
+import org.springframework.stereotype.Service;
+
+@Service("authenticatedLostAuth")
+public class AuthenticatedLostAuth {
+}
