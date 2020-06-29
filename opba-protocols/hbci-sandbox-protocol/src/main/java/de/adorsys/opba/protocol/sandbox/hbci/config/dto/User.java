b6c39ed781c16f3b02b68ecbfbf5b6b526497127
valb3r <valentyn.berezin@adorsys.com.ua>
2020-06-29 12:20:28 +0300

    OBG-693. Added request-response handling

--- opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/config/dto/User.java
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/config/dto/User.java
@@ -5,6 +5,8 @@ import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -25,4 +27,10 @@ public class User {
 
     @NotEmpty
     private Map<@NotBlank String, @NotBlank String> scaMethodsAvailable;
+
+    @NotEmpty
+    private List<@NotNull Account> accounts;
+
+    @NotEmpty
+    private List<@NotNull Transaction> transactions;
 }
--- opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/context/SandboxContext.java
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/context/SandboxContext.java
@@ -1,28 +1,24 @@
 package de.adorsys.opba.protocol.sandbox.hbci.protocol.context;
 
-import de.adorsys.opba.protocol.sandbox.hbci.config.dto.Account;
 import de.adorsys.opba.protocol.sandbox.hbci.config.dto.Bank;
 import de.adorsys.opba.protocol.sandbox.hbci.config.dto.User;
 import de.adorsys.opba.protocol.sandbox.hbci.protocol.Operation;
 import lombok.Data;
 
-import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @Data
 public class SandboxContext {
 
-    private Operation requestOperation;
-    private Map<String, String> requestData;
-    private String requestPin;
-    private String requestTan;
+    private static final Pattern PIN = Pattern.compile("SigTail(?:_\\d+)\\.UserSig(?:_\\d+)\\.pin");
+    private static final Pattern TAN = Pattern.compile("SigTail(?:_\\d+)\\.UserSig(?:_\\d+)\\.tan");
+    private static final String BLZ = "Idn.KIK.blz";
+    private static final String LOGIN = "Idn.customerid";
 
+    private Request request;
     private Bank bank;
     private User user;
-    private List<Account> accounts;
-
-    private boolean missingOrWrongTan;
-    private boolean missingOrWrongPin;
 
     private String response;
 
@@ -30,4 +26,60 @@ public class SandboxContext {
     private String secCheckRef;
     private String dialogId;
     private String userId;
+    private String sysId;
+
+    public Operation getRequestOperation() {
+        return request.getOperation();
+    }
+
+    public Map<String, String> getRequestData() {
+        return request.getData();
+    }
+
+    public boolean isPinOk() {
+        return getUser().getPin().equals(getRequestPin());
+    }
+
+    public boolean isTanOk() {
+        return getUser().getTan().equals(getRequestTan());
+    }
+
+    public String getRequestPin() {
+        if (null == request || null == getRequestData()) {
+            return null;
+        }
+
+        return getRequestData().entrySet().stream()
+                .filter(it -> PIN.matcher(it.getKey()).find())
+                .findFirst()
+                .map(Map.Entry::getValue)
+                .orElse(null);
+    }
+
+    public String getRequestTan() {
+        if (null == request || null == getRequestData()) {
+            return null;
+        }
+
+        return getRequestData().entrySet().stream()
+                .filter(it -> TAN.matcher(it.getKey()).find())
+                .findFirst()
+                .map(Map.Entry::getValue)
+                .orElse(null);
+    }
+
+    public String getRequestBankBlz() {
+        return getRequestData().get(BLZ);
+    }
+
+    public String getRequestUserLogin() {
+        return getRequestData().get(LOGIN);
+    }
+
+    @Data
+    public static class Request {
+
+        private Operation operation;
+        private Map<String, String> data;
+    }
 }
