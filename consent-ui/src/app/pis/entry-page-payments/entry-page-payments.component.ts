4a8f9746f71de90a3e63eae37a21c0e8ae3f4d09
Vladimir Nechai <vne@adorsys.de>
2020-06-29 19:26:14 +0200

    add payment util and pisPayment model

--- consent-ui/src/app/pis/entry-page-payments/entry-page-payments.component.ts
+++ consent-ui/src/app/pis/entry-page-payments/entry-page-payments.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DedicatedAccessComponent } from '../../ais/entry-page/initiation/common/dedicated-access/dedicated-access.component';
 import { PaymentsConsentReviewComponent } from '../payments-consent-review/payments-consent-review.component';
-import { AccountAccessLevel } from '../../ais/common/dto/ais-consent';
-import { Access } from '../../ais/entry-page/initiation/common/initial-consent/consent-account-access-selection.component';
 
 @Component({
   selector: 'consent-app-entry-page-payments',
