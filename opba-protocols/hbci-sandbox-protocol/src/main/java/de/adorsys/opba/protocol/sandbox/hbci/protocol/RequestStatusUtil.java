b04bfb80da0b1e490a3a32f503345e7ba28d067e
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-01 16:00:36 +0300

    OBG-693. Customizable level for accounts

--- opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/RequestStatusUtil.java
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/RequestStatusUtil.java
@@ -10,4 +10,8 @@ public class RequestStatusUtil {
     public boolean isForTransactionListing(Map<String, String> requestData) {
         return "HKKAZ".equals(MapRegexUtil.getDataRegex(requestData, "TAN2Step\\d*\\.ordersegcode"));
     }
+
+    public boolean isForAccountListing(Map<String, String> requestData) {
+        return "HKSPA".equals(MapRegexUtil.getDataRegex(requestData, "TAN2Step\\d*\\.ordersegcode"));
+    }
 }
