3aef6c6b0977b76c538936f99a0a350d41b16753
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-01 15:44:20 +0300

    OBG-693. Customizable level for authorization

--- opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/config/dto/SensitiveAuthLevel.java
+++ opba-protocols/hbci-sandbox-protocol/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/config/dto/SensitiveAuthLevel.java
@@ -1,7 +1,18 @@
 package de.adorsys.opba.protocol.sandbox.hbci.config.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SensitiveAuthLevel {
 
-    AUTHENTICATED,
-    AUTHORIZED
+    AUTHENTICATED(false),
+    AUTHORIZED(true);
+
+    private final boolean needsTan;
+
+    public String needsTanJaNein() {
+        return needsTan ? "J" : "N";
+    }
 }
