c951f1ac07a93a40fd88617f23ce1147e8de4a89
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-02 17:17:13 +0300

    OBG-828. Reorganized hbci-mock banks structure

---
+++ opba-protocols/hbci-protocol-tests/hbci-bdd-sandbox/src/test/java/de/adorsys/opba/protocol/hbci/tests/e2e/sandbox/hbcisteps/HbciServers.java
@@ -0,0 +1,6 @@
+package de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps;
+
+import de.adorsys.opba.protocol.xs2a.tests.e2e.stages.CommonGivenStages;
+
+public class HbciServers<SELF extends HbciServers<SELF>> extends CommonGivenStages<SELF> {
+}
