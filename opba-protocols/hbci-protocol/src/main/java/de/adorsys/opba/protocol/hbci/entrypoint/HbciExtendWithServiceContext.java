fb71aa750cd083f937353731aaaa5de8ee97a559
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-03 19:38:12 +0300

    OBG-828. Reconfigure test for HBCI

--- opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/entrypoint/HbciExtendWithServiceContext.java
+++ opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/entrypoint/HbciExtendWithServiceContext.java
@@ -2,10 +2,12 @@ package de.adorsys.opba.protocol.hbci.entrypoint;
 
 import de.adorsys.opba.protocol.api.dto.context.ServiceContext;
 import de.adorsys.opba.protocol.hbci.context.HbciContext;
+import org.kapott.hbci.manager.HBCIUtils;
 import org.springframework.stereotype.Service;
 
 /**
- * Updates/extends already context with incoming service request by filling request fields.
+ * Updates/extends already context with incoming service request by filling request fields. Also updates bank adapter URL
+ * if needed.
  */
 @Service
 public class HbciExtendWithServiceContext {
@@ -16,6 +18,9 @@ public class HbciExtendWithServiceContext {
         context.setRedirectCodeIfAuthContinued(serviceContext.getFutureRedirectCode().toString());
         context.setAspspRedirectCode(serviceContext.getFutureAspspRedirectCode().toString());
         context.setAuthorizationSessionIdIfOpened(serviceContext.getFutureAuthSessionId().toString());
+
+        HBCIUtils.getBankInfo(serviceContext.getRequestScoped().aspspProfile().getBankCode())
+                .setPinTanAddress(serviceContext.getRequestScoped().aspspProfile().getUrl());
         return context;
     }
 }
