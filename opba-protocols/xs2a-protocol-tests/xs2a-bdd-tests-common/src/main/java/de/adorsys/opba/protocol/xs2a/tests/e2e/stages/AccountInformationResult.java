bf1ea042469c522c40b73f45461b185de97c9669
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-03 20:58:28 +0300

    OBG-828. Done AIS list test on HBCI

--- opba-protocols/xs2a-protocol-tests/xs2a-bdd-tests-common/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/stages/AccountInformationResult.java
+++ opba-protocols/xs2a-protocol-tests/xs2a-bdd-tests-common/src/main/java/de/adorsys/opba/protocol/xs2a/tests/e2e/stages/AccountInformationResult.java
@@ -165,6 +165,29 @@ public class AccountInformationResult<SELF extends AccountInformationResult<SELF
     }
 
     @SneakyThrows
+    public SELF open_banking_can_read_max_musterman_hbci_account_data_using_consent_bound_to_service_session(String bankId) {
+        ExtractableResponse<Response> response = withAccountsHeaders(ANTON_BRUECKNER, bankId, requestSigningService, OperationType.AIS)
+                    .header(SERVICE_SESSION_ID, serviceSessionId)
+                .when()
+                    .get(AIS_ACCOUNTS_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                    .body("accounts[0].iban", equalTo("DE59300000033466865655"))
+                    .body("accounts[0].resourceId", instanceOf(String.class))
+                    .body("accounts[0].currency", equalTo("EUR"))
+                    .body("accounts[0].name", equalTo("Extra-Konto"))
+                    .body("accounts[1].iban", equalTo("DE13300000032278292697"))
+                    .body("accounts[1].resourceId", instanceOf(String.class))
+                    .body("accounts[1].currency", equalTo("EUR"))
+                    .body("accounts[1].name", equalTo("Extra-Konto"))
+                    .body("accounts", hasSize(2))
+                .extract();
+
+        this.responseContent = response.body().asString();
+        return self();
+    }
+
+    @SneakyThrows
     public SELF open_banking_reads_anton_brueckner_transactions_using_consent_bound_to_service_session_data_validated_by_iban(
         String resourceId, LocalDate dateFrom, LocalDate dateTo, String bookingStatus
     ) {
