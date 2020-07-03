a6e7290a72b54106e6837b54a46cc6712ee547e8
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-03 15:21:17 +0300

    OBG-828. Configure test to smart select JSON serializer

--- opba-protocols/hbci-protocol-tests/hbci-bdd-sandbox/src/test/java/de/adorsys/opba/protocol/hbci/tests/e2e/sandbox/HbciSandboxConsentE2EXs2aProtocolTest.java
+++ opba-protocols/hbci-protocol-tests/hbci-bdd-sandbox/src/test/java/de/adorsys/opba/protocol/hbci/tests/e2e/sandbox/HbciSandboxConsentE2EXs2aProtocolTest.java
@@ -1,25 +1,29 @@
 package de.adorsys.opba.protocol.hbci.tests.e2e.sandbox;
 
 import com.tngtech.jgiven.integration.spring.junit5.SpringScenarioTest;
-import de.adorsys.opba.protocol.api.common.Approach;
+import de.adorsys.opba.db.repository.jpa.BankProfileJpaRepository;
+import de.adorsys.opba.protocol.api.dto.ValidationIssue;
+import de.adorsys.opba.protocol.api.dto.result.body.ValidationError;
+import de.adorsys.opba.protocol.bpmnshared.dto.DtoMapper;
+import de.adorsys.opba.protocol.hbci.config.HbciAdapterProperties;
 import de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps.HbciServers;
 import de.adorsys.opba.protocol.sandbox.hbci.HbciServerApplication;
-import de.adorsys.opba.protocol.xs2a.config.protocol.ProtocolUrlsConfiguration;
-import de.adorsys.opba.protocol.xs2a.tests.e2e.JGivenConfig;
 import de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AccountInformationResult;
 import de.adorsys.opba.protocol.xs2a.tests.e2e.wiremock.mocks.WiremockAccountInformationRequest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
 import java.util.UUID;
 
+import static de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.Const.HBCI_SANDBOX_CONFIG;
 import static de.adorsys.opba.protocol.xs2a.tests.TestProfiles.MOCKED_SANDBOX;
 import static de.adorsys.opba.protocol.xs2a.tests.TestProfiles.ONE_TIME_POSTGRES_RAMFS;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -31,9 +35,9 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(classes = {
         HbciProtocolApplication.class,
         HbciServerApplication.class,  // Starting HBCI server within test so that application basically communicates with itself
-        JGivenConfig.class
+        HbciJGivenConfig.class
 }, webEnvironment = RANDOM_PORT)
-@ActiveProfiles(profiles = {ONE_TIME_POSTGRES_RAMFS, MOCKED_SANDBOX})
+@ActiveProfiles(profiles = {ONE_TIME_POSTGRES_RAMFS, MOCKED_SANDBOX, HBCI_SANDBOX_CONFIG})
 class HbciSandboxConsentE2EXs2aProtocolTest extends SpringScenarioTest<HbciServers, WiremockAccountInformationRequest<? extends WiremockAccountInformationRequest<?>>, AccountInformationResult> {
 
     private final String OPBA_LOGIN = UUID.randomUUID().toString();
@@ -43,26 +47,32 @@ class HbciSandboxConsentE2EXs2aProtocolTest extends SpringScenarioTest<HbciServe
     private int port;
 
     @Autowired
-    private ProtocolUrlsConfiguration urlsConfiguration;
+    private HbciAdapterProperties adapterProperties;
+
+    @Autowired
+    private BankProfileJpaRepository bankProfileJpaRepository;
+
+    // TODO: Those dependencied do not need to be mocked, but should be optional
+    // Stubbing out xs2a protocol declared dependencies:
+    @MockBean
+    private DtoMapper<Set<ValidationIssue>, Set<ValidationError>> dtoMapper;
 
     // See https://github.com/spring-projects/spring-boot/issues/14879 for the 'why setting port'
     @BeforeEach
+    @Transactional
     void setBaseUrl() {
-        ProtocolUrlsConfiguration.WebHooks aisUrls = urlsConfiguration.getAis().getWebHooks();
-        aisUrls.setOk(aisUrls.getOk().replaceAll("localhost:\\d+", "localhost:" + port));
-        aisUrls.setNok(aisUrls.getNok().replaceAll("localhost:\\d+", "localhost:" + port));
+        makeHbciAdapterToPointToHbciMockEndpoints();
     }
 
-    @ParameterizedTest
-    @EnumSource(Approach.class)
-    void testAccountsListWithConsentUsingRedirect(Approach expectedApproach) {
+    @Test
+    void testAccountsListWithConsentUsingRedirect() {
+        String bankId = "918d80fa-f7fd-4c9f-a6bd-7a9e12aeee76";
         given()
-                .preferred_sca_approach_selected_for_all_banks_in_opba(expectedApproach)
                 .rest_assured_points_to_opba_server()
                 .user_registered_in_opba_with_credentials(OPBA_LOGIN, OPBA_PASSWORD);
 
         when()
-                .fintech_calls_list_accounts_for_anton_brueckner()
+                .fintech_calls_list_accounts_for_anton_brueckner(bankId)
                 .and()
                 .user_logged_in_into_opba_as_opba_user_with_credentials_using_fintech_supplied_url(OPBA_LOGIN, OPBA_PASSWORD)
                 .and()
@@ -75,5 +85,15 @@ class HbciSandboxConsentE2EXs2aProtocolTest extends SpringScenarioTest<HbciServe
                 .open_banking_has_consent_for_anton_brueckner_account_list()
                 .fintech_calls_consent_activation_for_current_authorization_id()
                 .open_banking_can_read_anton_brueckner_account_data_using_consent_bound_to_service_session();
+    }
+
+    private void makeHbciAdapterToPointToHbciMockEndpoints() {
+        adapterProperties.getAdorsysMockBanksBlz().stream()
+                .flatMap(it -> bankProfileJpaRepository.findByBankBankCode(String.valueOf(it)).stream())
+                .map(it -> {
+                    it.setUrl("http://localhost:" + port + "/hbci-mock/");
+                    return it;
+                })
+                .forEach(it -> bankProfileJpaRepository.save(it));
     }
 }
