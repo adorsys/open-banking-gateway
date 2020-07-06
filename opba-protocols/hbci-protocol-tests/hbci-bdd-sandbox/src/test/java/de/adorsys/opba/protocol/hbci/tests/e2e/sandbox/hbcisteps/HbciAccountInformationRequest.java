82eb29b0bac515a156dca80e5115aec4f4a8f528
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-06 18:58:57 +0300

    OBG-828. Added more tests

--- opba-protocols/hbci-protocol-tests/hbci-bdd-sandbox/src/test/java/de/adorsys/opba/protocol/hbci/tests/e2e/sandbox/hbcisteps/HbciAccountInformationRequest.java
+++ opba-protocols/hbci-protocol-tests/hbci-bdd-sandbox/src/test/java/de/adorsys/opba/protocol/hbci/tests/e2e/sandbox/hbcisteps/HbciAccountInformationRequest.java
@@ -1,15 +1,22 @@
 package de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps;
 
 import com.tngtech.jgiven.integration.spring.JGivenStage;
+import de.adorsys.opba.api.security.external.domain.OperationType;
+import de.adorsys.opba.protocol.xs2a.tests.GetTransactionsQueryParams;
 import de.adorsys.opba.protocol.xs2a.tests.e2e.stages.AccountInformationRequestCommon;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
 
 import static de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps.FixtureConst.BANK_BLZ_20000002_ID;
 import static de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps.FixtureConst.BANK_BLZ_30000003_ID;
 import static de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps.FixtureConst.MAX_MUSTERMAN_BANK_BLZ_20000002_ACCOUNT_ID;
 import static de.adorsys.opba.protocol.hbci.tests.e2e.sandbox.hbcisteps.FixtureConst.MAX_MUSTERMAN_BANK_BLZ_30000003_ACCOUNT_ID;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.AIS_TRANSACTIONS_ENDPOINT;
 import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.AUTHORIZE_CONSENT_ENDPOINT;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.MAX_MUSTERMAN;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.withTransactionsHeaders;
+import static de.adorsys.opba.restapi.shared.HttpHeaders.SERVICE_SESSION_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.LOCATION;
 import static org.springframework.http.HttpStatus.ACCEPTED;
@@ -25,6 +32,10 @@ public class HbciAccountInformationRequest<SELF extends HbciAccountInformationRe
         return fintech_calls_list_accounts_for_anton_brueckner(BANK_BLZ_30000003_ID);
     }
 
+    public SELF fintech_calls_list_accounts_max_musterman_for_blz_20000002() {
+        return fintech_calls_list_accounts_for_max_musterman(BANK_BLZ_20000002_ID);
+    }
+
     public SELF fintech_calls_list_transactions_for_max_musterman_for_blz_30000003() {
         return fintech_calls_list_transactions_for_max_musterman(MAX_MUSTERMAN_BANK_BLZ_30000003_ACCOUNT_ID, BANK_BLZ_30000003_ID);
     }
@@ -32,6 +43,22 @@ public class HbciAccountInformationRequest<SELF extends HbciAccountInformationRe
     public SELF fintech_calls_list_transactions_for_max_musterman_for_blz_20000002() {
         return fintech_calls_list_transactions_for_max_musterman(MAX_MUSTERMAN_BANK_BLZ_20000002_ACCOUNT_ID, BANK_BLZ_20000002_ID);
     }
+
+    public SELF fintech_calls_list_transactions_for_max_musterman_using_already_granted_service_session(String resourceId, String bankId) {
+        ExtractableResponse<Response> response = withTransactionsHeaders(MAX_MUSTERMAN, bankId, requestSigningService, OperationType.AIS, GetTransactionsQueryParams.newEmptyInstance())
+                    .header(SERVICE_SESSION_ID, serviceSessionId)
+                .when()
+                    .get(AIS_TRANSACTIONS_ENDPOINT, resourceId)
+                .then()
+                    .statusCode(HttpStatus.ACCEPTED.value())
+                .extract();
+
+        updateServiceSessionId(response);
+        updateRedirectCode(response);
+        updateNextConsentAuthorizationUrl(response);
+        return self();
+    }
+
 
     public SELF user_anton_brueckner_provided_initial_parameters_to_list_accounts_with_all_accounts_consent() {
         startInitialInternalConsentAuthorization(
