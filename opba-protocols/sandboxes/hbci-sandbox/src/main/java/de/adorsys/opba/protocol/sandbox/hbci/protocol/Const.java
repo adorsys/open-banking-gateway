4b4d027eb1c47b99e9d2ef6a33a3c2457a14c70f
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-08 09:58:20 +0300

    OBG-828. Reorder packages and enable HBCI codecov

---
+++ opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/Const.java
@@ -0,0 +1,14 @@
+package de.adorsys.opba.protocol.sandbox.hbci.protocol;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor") // Lombok generates private ctor.
+public class Const {
+
+    public static final String CONTEXT = "CONTEXT";
+
+    public static final String SEPA_INFO = "GV.SEPAInfo";
+    public static final String TRANSACTIONS = "GV.KUmsZeit";
+    public static final String DIALOG_ID = "MsgHead.dialogid";
+}
