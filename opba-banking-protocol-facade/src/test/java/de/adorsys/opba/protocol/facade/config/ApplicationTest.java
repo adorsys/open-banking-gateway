06b797b955e9e960b56d0e73bbed681b6ff35595
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-07-08 08:56:45 +0200

    #781 fix tests

--- opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/config/ApplicationTest.java
+++ opba-banking-protocol-facade/src/test/java/de/adorsys/opba/protocol/facade/config/ApplicationTest.java
@@ -3,11 +3,17 @@ package de.adorsys.opba.protocol.facade.config;
 import de.adorsys.opba.api.security.internal.EnableTokenBasedApiSecurity;
 import de.adorsys.opba.api.security.internal.config.TppTokenProperties;
 import de.adorsys.opba.db.config.EnableBankingPersistence;
+import de.adorsys.opba.protocol.api.fintechspec.ApiConsumer;
+import de.adorsys.opba.protocol.api.fintechspec.ApiConsumerConfig;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.Collections;
+import java.util.Map;
 
 import static de.adorsys.opba.api.security.internal.config.ConfigConst.API_CONFIG_PREFIX;
 
@@ -29,6 +35,32 @@ public class ApplicationTest {
         @Configuration
         @ConfigurationProperties(API_CONFIG_PREFIX + "security")
         public class TppTokenPropertiesConfig extends TppTokenProperties {
+        }
+
+        @Data
+        @Validated
+        public class AdorsysFintechProfile implements ApiConsumer {
+            private TppTokenPropertiesConfig tppTokenPropertiesConfig;
+
+            @Override
+            public String getName() {
+                return "Adorsys Fintech";
+            }
+
+            @Override
+            public String getPublicKey() {
+                return tppTokenPropertiesConfig.getPublicKey();
+            }
+        }
+
+        @Validated
+        @Configuration
+        public class ApiConsumerConfigImpl implements ApiConsumerConfig {
+
+            @Override
+            public Map<String, ? extends ApiConsumer> getConsumers() {
+                return Collections.singletonMap("MY-SUPER-FINTECH-ID", new AdorsysFintechProfile());
+            }
         }
     }
 }
