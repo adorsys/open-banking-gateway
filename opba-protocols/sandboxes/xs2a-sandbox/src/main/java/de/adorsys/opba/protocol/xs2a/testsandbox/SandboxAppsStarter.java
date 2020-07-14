6c83ba431ef623a6a7538092b5705b38ef811deb
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-14 10:31:18 +0300

    More resilent starter

--- opba-protocols/sandboxes/xs2a-sandbox/src/main/java/de/adorsys/opba/protocol/xs2a/testsandbox/SandboxAppsStarter.java
+++ opba-protocols/sandboxes/xs2a-sandbox/src/main/java/de/adorsys/opba/protocol/xs2a/testsandbox/SandboxAppsStarter.java
@@ -1,14 +1,15 @@
 package de.adorsys.opba.protocol.xs2a.testsandbox;
 
 import com.google.common.collect.ImmutableSet;
-import de.adorsys.opba.protocol.xs2a.testsandbox.internal.SandboxAppExecutor;
 import de.adorsys.opba.protocol.xs2a.testsandbox.internal.SandboxApp;
+import de.adorsys.opba.protocol.xs2a.testsandbox.internal.SandboxAppExecutor;
 import de.adorsys.opba.protocol.xs2a.testsandbox.internal.StarterContext;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Durations;
 
 import java.time.Duration;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -24,13 +25,18 @@ import static org.awaitility.Awaitility.await;
 public class SandboxAppsStarter implements AutoCloseable {
 
     public static final Set<SandboxApp> ALL = ImmutableSet.copyOf(SandboxApp.values());
-
     private final ExecutorService executor;
     private final AtomicBoolean started = new AtomicBoolean();
     private final StarterContext context;
 
     public SandboxAppsStarter() {
         this.context = new StarterContext();
+        this.executor = new SandboxAppExecutor(context);
+    }
+
+    public SandboxAppsStarter(Map<SandboxApp, Integer> dockerPorts) {
+        this.context = new StarterContext();
+        this.context.getDockerPorts().putAll(dockerPorts);
         this.executor = new SandboxAppExecutor(context);
     }
 
@@ -59,6 +65,18 @@ public class SandboxAppsStarter implements AutoCloseable {
         }
     }
 
+    /**
+     * Note that at this moment this function is usable only for docker-container starter, not for JAR-based apps
+     */
+    public void awaitForAllStopped(Duration atMost) {
+        try {
+            await().atMost(atMost).pollDelay(Durations.ONE_SECOND).until(() -> 0 == SandboxApp.countReadyToUse(context));
+        } catch (Throwable ex) {
+            log.error("Sandbox stop failed");
+            throw ex;
+        }
+    }
+
     private void reportSandboxFailedApps() {
         ALL.forEach(it -> {
             if (!it.isReadyToUse(context)) {
@@ -69,6 +87,13 @@ public class SandboxAppsStarter implements AutoCloseable {
 
     public void awaitForAllStarted() {
         awaitForAllStarted(Durations.FIVE_MINUTES);
+    }
+
+    /**
+     * Note that at this moment this function is usable only for docker-container starter, not for JAR-based apps
+     */
+    public void awaitForAllStopped() {
+        awaitForAllStopped(Durations.FIVE_MINUTES);
     }
 
     @Override
