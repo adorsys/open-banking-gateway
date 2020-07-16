aaf92868c1c22f1293b68dd3457020c482e027cc
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-16 09:37:05 +0300

    OBG-845. Initial implementation for anonymous payment access

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/scoped/paymentaccess/PaymentAccessFactory.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/scoped/paymentaccess/PaymentAccessFactory.java
@@ -1,20 +1,17 @@
-package de.adorsys.opba.protocol.facade.services.scoped;
+package de.adorsys.opba.protocol.facade.services.scoped.paymentaccess;
 
 import de.adorsys.opba.db.domain.entity.Bank;
 import de.adorsys.opba.db.domain.entity.fintech.Fintech;
 import de.adorsys.opba.db.domain.entity.psu.Psu;
 import de.adorsys.opba.db.domain.entity.psu.PsuAspspPrvKey;
 import de.adorsys.opba.db.domain.entity.sessions.ServiceSession;
-import de.adorsys.opba.db.repository.jpa.ConsentRepository;
+import de.adorsys.opba.db.repository.jpa.PaymentRepository;
 import de.adorsys.opba.db.repository.jpa.fintech.FintechPsuAspspPrvKeyRepository;
 import de.adorsys.opba.db.repository.jpa.psu.PsuAspspPrvKeyRepository;
 import de.adorsys.opba.protocol.api.services.EncryptionService;
-import de.adorsys.opba.protocol.api.services.scoped.consent.ConsentAccess;
 import de.adorsys.opba.protocol.api.services.scoped.consent.PaymentAccess;
 import de.adorsys.opba.protocol.facade.config.encryption.PsuConsentEncryptionServiceProvider;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.fintech.FintechSecureStorage;
-import de.adorsys.opba.protocol.facade.services.scoped.consentaccess.FintechConsentAccess;
-import de.adorsys.opba.protocol.facade.services.scoped.consentaccess.PsuConsentAccess;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,34 +20,23 @@ import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
-public class ConsentAccessFactory {
+public class PaymentAccessFactory {
 
     private final EntityManager entityManager;
     private final PsuAspspPrvKeyRepository prvKeyRepository;
     private final FintechSecureStorage fintechVault;
     private final PsuConsentEncryptionServiceProvider psuEncryption;
     private final FintechPsuAspspPrvKeyRepository fintechPsuAspspPrvKeyRepository;
-    private final ConsentRepository consentRepository;
-
-    public ConsentAccess consentForPsuAndAspsp(Psu psu, Bank aspsp, ServiceSession session) {
-        PsuAspspPrvKey prvKey = prvKeyRepository.findByPsuIdAndAspspId(psu.getId(), aspsp.getId())
-                .orElseThrow(() -> new IllegalStateException("No public key for: " + psu.getId()));
-        return new PsuConsentAccess(psu, aspsp, psuEncryption.forPublicKey(prvKey.getId(), prvKey.getPubKey().getKey()), session, consentRepository);
-    }
+    private final PaymentRepository paymentRepository;
 
     public PaymentAccess paymentForPsuAndAspsp(Psu psu, Bank aspsp, ServiceSession session) {
         PsuAspspPrvKey prvKey = prvKeyRepository.findByPsuIdAndAspspId(psu.getId(), aspsp.getId())
                 .orElseThrow(() -> new IllegalStateException("No public key for: " + psu.getId()));
-        return null;
+        return new PsuPaymentAccess(psu, aspsp, psuEncryption.forPublicKey(prvKey.getId(), prvKey.getPubKey().getKey()), session, paymentRepository);
     }
 
     public PaymentAccess paymentAnonymousPsuAndAspsp(Bank aspsp, EncryptionService encryptionService, ServiceSession session) {
         return null;
-    }
-    public ConsentAccess consentForFintech(Fintech fintech, ServiceSession session, Supplier<char[]> fintechPassword) {
-        return new FintechConsentAccess(
-                fintech, psuEncryption, fintechPsuAspspPrvKeyRepository, fintechVault, consentRepository, entityManager, session.getId(), fintechPassword
-        );
     }
 
     public PaymentAccess paymentForFintech(Fintech fintech, ServiceSession session, Supplier<char[]> fintechPassword) {
