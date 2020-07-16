ac023eead53f82e11f99a58de1865ec66e03dafd
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-16 10:54:23 +0300

    OBG-845. Random fintech encryption key selection

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/config/encryption/impl/psu/PsuSecureStorage.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/config/encryption/impl/psu/PsuSecureStorage.java
@@ -6,7 +6,7 @@ import de.adorsys.datasafe.types.api.actions.ReadRequest;
 import de.adorsys.datasafe.types.api.actions.WriteRequest;
 import de.adorsys.opba.db.domain.entity.psu.Psu;
 import de.adorsys.opba.db.domain.entity.sessions.AuthSession;
-import de.adorsys.opba.protocol.facade.config.encryption.AuthenticatedPsuEncryptionServiceProvider;
+import de.adorsys.opba.protocol.facade.config.encryption.PsuEncryptionServiceProvider;
 import de.adorsys.opba.protocol.facade.config.encryption.datasafe.BaseDatasafeDbStorageService;
 import de.adorsys.opba.protocol.facade.config.encryption.impl.PairIdPsuAspspTuple;
 import de.adorsys.opba.protocol.facade.services.EncryptionKeySerde;
@@ -30,7 +30,7 @@ public class PsuSecureStorage {
     private final DefaultDatasafeServices datasafeServices;
 
     private final DFSConfig config;
-    private final AuthenticatedPsuEncryptionServiceProvider encryptionServiceProvider;
+    private final PsuEncryptionServiceProvider encryptionServiceProvider;
     private final EncryptionKeySerde serde;
 
     public void registerPsu(Psu psu, Supplier<char[]> password) {
--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/scoped/paymentaccess/AnonymousPsuPaymentAccess.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/scoped/paymentaccess/AnonymousPsuPaymentAccess.java
@@ -1,21 +1,28 @@
 package de.adorsys.opba.protocol.facade.services.scoped.paymentaccess;
 
+import com.google.common.collect.Iterables;
 import de.adorsys.opba.db.domain.entity.Bank;
 import de.adorsys.opba.db.domain.entity.Payment;
+import de.adorsys.opba.db.domain.entity.fintech.Fintech;
+import de.adorsys.opba.db.domain.entity.fintech.FintechPrvKey;
 import de.adorsys.opba.db.domain.entity.sessions.ServiceSession;
 import de.adorsys.opba.db.repository.jpa.PaymentRepository;
 import de.adorsys.opba.protocol.api.services.EncryptionService;
 import de.adorsys.opba.protocol.api.services.scoped.consent.PaymentAccess;
 import de.adorsys.opba.protocol.api.services.scoped.consent.ProtocolFacingPayment;
+import de.adorsys.opba.protocol.facade.config.encryption.PsuEncryptionServiceProvider;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RequiredArgsConstructor
 public class AnonymousPsuPaymentAccess implements PaymentAccess {
 
     private final Bank aspsp;
-    private final EncryptionService encryptionService;
+    private final Fintech fintech;
+    private final PsuEncryptionServiceProvider psuEncryption;
     private final ServiceSession serviceSession;
     private final PaymentRepository paymentRepository;
 
@@ -31,7 +38,7 @@ public class AnonymousPsuPaymentAccess implements PaymentAccess {
                 .aspsp(aspsp)
                 .build();
 
-        return new ProtocolFacingPaymentImpl(newPayment, encryptionService);
+        return new ProtocolFacingPaymentImpl(newPayment, anonymousEncryptionServiceBasedOnRandomKeyFromFintech());
     }
 
     @Override
@@ -47,5 +54,12 @@ public class AnonymousPsuPaymentAccess implements PaymentAccess {
     @Override
     public List<ProtocolFacingPayment> findByCurrentServiceSessionOrderByModifiedDesc() {
         throw new IllegalStateException("Not implemented");
+    }
+
+    private EncryptionService anonymousEncryptionServiceBasedOnRandomKeyFromFintech() {
+        Collection<FintechPrvKey> keys = fintech.getFintechOnlyPrvKeys();
+        // Expecting key amount to be small
+        FintechPrvKey psuAspspKey = Iterables.get(keys, ThreadLocalRandom.current().nextInt(0, keys.size() - 1));
+        return psuEncryption.forPublicKey(psuAspspKey.getId(), psuAspspKey.getPubKey().getKey());
     }
 }
