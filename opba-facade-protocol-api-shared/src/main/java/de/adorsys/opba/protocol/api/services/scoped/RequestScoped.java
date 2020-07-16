38ce1ee062194b5f068298df83c8c5ec28d4d2d1
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-16 08:43:00 +0300

    OBG-845. Initialize payment access supplying chain

--- opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/scoped/RequestScoped.java
+++ opba-facade-protocol-api-shared/src/main/java/de/adorsys/opba/protocol/api/services/scoped/RequestScoped.java
@@ -2,6 +2,7 @@ package de.adorsys.opba.protocol.api.services.scoped;
 
 import de.adorsys.opba.protocol.api.services.scoped.aspsp.UsesCurrentAspspProfile;
 import de.adorsys.opba.protocol.api.services.scoped.consent.UsesConsentAccess;
+import de.adorsys.opba.protocol.api.services.scoped.consent.UsesPaymentAccess;
 import de.adorsys.opba.protocol.api.services.scoped.encryption.UsesEncryptionService;
 import de.adorsys.opba.protocol.api.services.scoped.fintech.UsesCurrentFintechProfile;
 import de.adorsys.opba.protocol.api.services.scoped.transientdata.UsesTransientStorage;
@@ -11,7 +12,7 @@ import de.adorsys.opba.protocol.api.services.scoped.validation.UsesValidation;
  * General services that are required for protocol execution.
  */
 // TODO - consider migration to custom spring scope
-public interface RequestScoped extends UsesEncryptionService, UsesTransientStorage, UsesConsentAccess,
+public interface RequestScoped extends UsesEncryptionService, UsesTransientStorage, UsesConsentAccess, UsesPaymentAccess,
                                         UsesCurrentAspspProfile, UsesValidation, UsesCurrentFintechProfile {
 
     /**
