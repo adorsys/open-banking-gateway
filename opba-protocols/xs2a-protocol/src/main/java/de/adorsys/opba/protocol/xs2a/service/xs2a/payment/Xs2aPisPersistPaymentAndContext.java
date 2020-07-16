724aa198638ed958d8739562d547f77c37477d20
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-16 08:43:00 +0300

    OBG-845. Initialize payment access supplying chain

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/payment/Xs2aPisPersistPaymentAndContext.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/payment/Xs2aPisPersistPaymentAndContext.java
@@ -1,7 +1,7 @@
 package de.adorsys.opba.protocol.xs2a.service.xs2a.payment;
 
 import com.google.common.collect.ImmutableMap;
-import de.adorsys.opba.protocol.api.services.scoped.consent.ProtocolFacingConsent;
+import de.adorsys.opba.protocol.api.services.scoped.consent.ProtocolFacingPayment;
 import de.adorsys.opba.protocol.bpmnshared.config.flowable.FlowableObjectMapper;
 import de.adorsys.opba.protocol.bpmnshared.service.exec.ValidatedExecution;
 import de.adorsys.opba.protocol.xs2a.context.pis.Xs2aPisContext;
@@ -23,15 +23,15 @@ public class Xs2aPisPersistPaymentAndContext extends ValidatedExecution<Xs2aPisC
     @Override
     @SneakyThrows
     protected void doRealExecution(DelegateExecution execution, Xs2aPisContext context) {
-        ProtocolFacingConsent consent = context.consentAccess().findSingleByCurrentServiceSession()
-                .orElseGet(() -> context.consentAccess().createDoNotPersist());
+        ProtocolFacingPayment consent = context.paymentAccess().findSingleByCurrentServiceSession()
+                .orElseGet(() -> context.paymentAccess().createDoNotPersist());
 
-        consent.setConsentId(context.getPaymentId());
-        consent.setConsentContext(
+        consent.setPaymentId(context.getPaymentId());
+        consent.setPaymentContext(
                 mapper.getMapper().writeValueAsString(
                         ImmutableMap.of(context.getClass().getCanonicalName(), context)
                 )
         );
-        context.consentAccess().save(consent);
+        context.paymentAccess().save(consent);
     }
 }
