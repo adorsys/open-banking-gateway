8840c8dc472692f26ceaee232fe807a03a47a78f
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-20 16:40:46 +0300

    OBG-801. Add 'service accounts' feature

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/database/repositories/UserRepository.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/database/repositories/UserRepository.java
@@ -2,7 +2,13 @@ package de.adorsys.opba.fintech.impl.database.repositories;
 
 
 import de.adorsys.opba.fintech.impl.database.entities.UserEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, String> {
+
+    @Modifying
+    @Query("UPDATE UserEntity u SET u.active = false WHERE u.serviceAccount = true")
+    int deactivateAllServiceAccounts();
 }
