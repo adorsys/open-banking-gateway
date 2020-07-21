1ce843965bdaa72d74e354462844ed550d49a0f0
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-21 21:40:12 +0300

    OBG-844. Initialized nimbus based oauth2 approach

---
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/config/GmailOauth2Config.java
@@ -0,0 +1,23 @@
+package de.adorsys.opba.fintech.impl.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+
+@Data
+@Validated
+@Configuration
+@ConfigurationProperties("oauth2.login.gmail")
+public class GmailOauth2Config {
+
+    @NotNull
+    private URI authenticationEndpoint;
+
+    @NotBlank
+    private String scope;
+}
