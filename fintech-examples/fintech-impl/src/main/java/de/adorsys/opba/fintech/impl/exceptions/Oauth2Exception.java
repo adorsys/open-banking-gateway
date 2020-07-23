7e9c1658ae3e40ca8b720a4b0981598f0a224730
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-23 17:52:54 +0300

    OBG-844. Added email and OAuth2 error handlers

---
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/exceptions/Oauth2Exception.java
@@ -0,0 +1,8 @@
+package de.adorsys.opba.fintech.impl.exceptions;
+
+public class Oauth2Exception extends IllegalStateException {
+
+    public Oauth2Exception(String msg) {
+        super(msg);
+    }
+}
