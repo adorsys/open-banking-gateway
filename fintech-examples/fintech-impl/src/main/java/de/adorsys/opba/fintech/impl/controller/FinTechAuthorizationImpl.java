bcdb26abdcc18077cac991b5d1ac2d118bbe3f9b
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-23 17:13:01 +0300

    OBG-844. Moved OAuth2 authorization to service

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/controller/FinTechAuthorizationImpl.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/controller/FinTechAuthorizationImpl.java
@@ -5,21 +5,17 @@ import de.adorsys.opba.fintech.api.model.generated.InlineResponse200;
 import de.adorsys.opba.fintech.api.model.generated.LoginRequest;
 import de.adorsys.opba.fintech.api.model.generated.UserProfile;
 import de.adorsys.opba.fintech.api.resource.generated.FinTechAuthorizationApi;
-import de.adorsys.opba.fintech.impl.config.Oauth2Provider;
 import de.adorsys.opba.fintech.impl.controller.utils.OkOrNotOk;
 import de.adorsys.opba.fintech.impl.controller.utils.RestRequestContext;
 import de.adorsys.opba.fintech.impl.database.entities.ConsentEntity;
 import de.adorsys.opba.fintech.impl.database.entities.LoginEntity;
-import de.adorsys.opba.fintech.impl.database.entities.OauthSessionEntity;
 import de.adorsys.opba.fintech.impl.database.entities.PaymentEntity;
 import de.adorsys.opba.fintech.impl.database.entities.UserEntity;
 import de.adorsys.opba.fintech.impl.database.repositories.ConsentRepository;
 import de.adorsys.opba.fintech.impl.database.repositories.LoginRepository;
-import de.adorsys.opba.fintech.impl.database.repositories.OauthSessionEntityRepository;
 import de.adorsys.opba.fintech.impl.database.repositories.PaymentRepository;
 import de.adorsys.opba.fintech.impl.service.AuthorizeService;
 import de.adorsys.opba.fintech.impl.service.ConsentService;
-import de.adorsys.opba.fintech.impl.service.Oauth2Authenticator;
 import de.adorsys.opba.fintech.impl.service.RedirectHandlerService;
 import de.adorsys.opba.fintech.impl.service.SessionLogicService;
 import lombok.RequiredArgsConstructor;
@@ -30,10 +26,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_REQUEST_ID;
@@ -42,9 +36,6 @@ import static de.adorsys.opba.fintech.impl.tppclients.HeaderFields.X_REQUEST_ID;
 @RestController
 @RequiredArgsConstructor
 public class FinTechAuthorizationImpl implements FinTechAuthorizationApi {
-
-    private final OauthSessionEntityRepository oauthSessions;
-    private final Set<Oauth2Authenticator> authenticators;
 
     private final AuthorizeService authorizeService;
     private final RedirectHandlerService redirectHandlerService;
@@ -58,7 +49,7 @@ public class FinTechAuthorizationImpl implements FinTechAuthorizationApi {
     @Override
     public ResponseEntity<InlineResponse200> loginPOST(LoginRequest loginRequest, UUID xRequestID) {
         log.debug("loginPost is called for {}", loginRequest.getUsername());
-        Optional<UserEntity> optionalUserEntity = authorizeService.login(loginRequest);
+        Optional<UserEntity> optionalUserEntity = authorizeService.loginWithPassword(loginRequest);
         if (!optionalUserEntity.isPresent()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
@@ -74,32 +65,7 @@ public class FinTechAuthorizationImpl implements FinTechAuthorizationApi {
             throw new IllegalStateException(String.format("Resource server returned error: %s for %s", error, state));
         }
 
-        Oauth2Provider provider = Arrays.stream(Oauth2Provider.values())
-                .filter(it -> it.matches(state)).findFirst()
-                .orElseThrow(() -> new IllegalStateException("Unknown state provider: " + state));
-
-        Optional<OauthSessionEntity> session = oauthSessions.findById(state);
-        if (!session.isPresent()) {
-            throw new IllegalStateException("Unauthorized state value: " + state);
-        }
-
-        oauthSessions.delete(session.get());
-
-        Oauth2Authenticator authenticator = authenticators.stream()
-                .filter(it -> provider == it.getProvider())
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("No authenticator for: " + state));
-
-        Optional<String> oauth2UserName = authenticator.authenticatedUserName(code);
-
-        if (!oauth2UserName.isPresent()) {
-            throw new IllegalStateException("Unable to authenticate in Oauth2 resource server: " + state);
-        }
-
-        String username = provider.encode(oauth2UserName.get());
-        UserEntity user = authorizeService.findUser(username)
-                .orElseGet(() -> authorizeService.createUser(username, UUID.randomUUID().toString()));
-
+        UserEntity user = authorizeService.loginWithOAuth2(code, state);
         return performUserLogin(user);
     }
 
