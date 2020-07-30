323db7db6380d6ff01bf1337b0992bf6346b8ace
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-30 19:22:51 +0300

    OBG-859. Added signer / path extractor

--- opba-api-security-filter-signer-generator/src/main/java/de/adorsys/opba/api/security/generator/api/Signer.java
+++ opba-api-security-filter-signer-generator/src/main/java/de/adorsys/opba/api/security/generator/api/Signer.java
@@ -3,5 +3,16 @@ package de.adorsys.opba.api.security.generator.api;
 public interface Signer {
 
     Signer withBasePath(String basePath);
-    PathSignature signFor(String path);
+    RequestSignature signFor(HttpMethod method, String path);
+
+    enum HttpMethod {
+        POST,
+        GET,
+        PUT,
+        PATCH,
+        DELETE,
+        HEAD,
+        OPTIONS,
+        TRACE;
+    }
 }
