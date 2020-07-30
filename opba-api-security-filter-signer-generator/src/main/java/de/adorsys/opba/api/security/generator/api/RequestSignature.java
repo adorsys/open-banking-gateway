8e4ca4a84c290a8313a00e8ccf054e1bd4e01527
valb3r <valentyn.berezin@adorsys.com.ua>
2020-07-30 19:22:51 +0300

    OBG-859. Added signer / path extractor

---
+++ opba-api-security-filter-signer-generator/src/main/java/de/adorsys/opba/api/security/generator/api/RequestSignature.java
@@ -0,0 +1,12 @@
+package de.adorsys.opba.api.security.generator.api;
+
+import java.util.Map;
+
+public interface RequestSignature {
+
+    RequestSignature headers(Map<String, String> headers);
+    RequestSignature query(Map<String, String> queryParams);
+    RequestSignature body(String body);
+
+    String sign();
+}
