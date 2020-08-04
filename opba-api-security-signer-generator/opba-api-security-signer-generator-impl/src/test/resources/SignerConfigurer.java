32e57633db73d4cf5b25a8338bea6624127dec4d
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-04 22:55:27 +0300

    OBG-859. Drop unused YML's

--- opba-api-security-signer-generator/opba-api-security-signer-generator-impl/src/test/resources/SignerConfigurer.java
+++ opba-api-security-signer-generator/opba-api-security-signer-generator-impl/src/test/resources/SignerConfigurer.java
@@ -2,6 +2,6 @@ package de.adorsys.signer.test;
 
 import de.adorsys.opba.api.security.generator.api.GeneratedSigner;
 
-@GeneratedSigner(openApiYamlPath = {"api/tpp_banking_api_ais.yml", "api/tpp_banking_api_pis.yml", "api/tpp_banking_api_bank_search.yml"})
+@GeneratedSigner(openApiYamlPath = {"api/tpp_banking_api_ais.yml"})
 public class SignerConfigurer {
 }
