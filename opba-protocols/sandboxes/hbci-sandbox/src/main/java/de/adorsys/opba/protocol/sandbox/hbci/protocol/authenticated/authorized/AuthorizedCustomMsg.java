a10b052273475807d96d99b3fec4bf848ea6f005
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-05 21:51:56 +0300

    OBG-862. Introduced payment propagation to payment status

--- opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/authenticated/authorized/AuthorizedCustomMsg.java
+++ opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/protocol/authenticated/authorized/AuthorizedCustomMsg.java
@@ -1,5 +1,6 @@
 package de.adorsys.opba.protocol.sandbox.hbci.protocol.authenticated.authorized;
 
+import de.adorsys.opba.protocol.sandbox.hbci.protocol.MapRegexUtil;
 import de.adorsys.opba.protocol.sandbox.hbci.protocol.Operation;
 import de.adorsys.opba.protocol.sandbox.hbci.protocol.RequestStatusUtil;
 import de.adorsys.opba.protocol.sandbox.hbci.protocol.TemplateBasedOperationHandler;
@@ -9,6 +10,7 @@ import de.adorsys.opba.protocol.sandbox.hbci.service.HbciSandboxPaymentService;
 import org.springframework.stereotype.Service;
 
 import static de.adorsys.opba.protocol.sandbox.hbci.protocol.Const.PAYMENT;
+import static de.adorsys.opba.protocol.sandbox.hbci.protocol.Const.PAYMENT_STATUS;
 import static de.adorsys.opba.protocol.sandbox.hbci.protocol.Const.SEPA_INFO;
 import static de.adorsys.opba.protocol.sandbox.hbci.protocol.Const.TRANSACTIONS;
 
@@ -36,8 +38,15 @@ public class AuthorizedCustomMsg extends TemplateBasedOperationHandler {
 
         if (context.getRequestData().keySet().stream().anyMatch(it -> it.startsWith(PAYMENT))
                 || RequestStatusUtil.isForPayment(context.getRequestData())) {
-            paymentService.createPayment(context);
+            paymentService.createPaymentIfNeededAndPossibleFromContext(context);
+            paymentService.acceptPayment(context);
             return "response-templates/authorized/custom-message-payment-response.json";
+        }
+
+        if (context.getRequestData().keySet().stream().anyMatch(it -> it.startsWith(PAYMENT_STATUS))) {
+            String paymentId = MapRegexUtil.getDataRegex(context.getRequestData(), "GV\\.InstantUebSEPAStatus\\d\\.orderid");
+            paymentService.paymentFromDatabaseToContext(context, paymentId);
+            return "response-templates/authorized/custom-message-payment-status.json";
         }
 
         throw new IllegalStateException("Cant't handle message: " + context.getRequestData());
