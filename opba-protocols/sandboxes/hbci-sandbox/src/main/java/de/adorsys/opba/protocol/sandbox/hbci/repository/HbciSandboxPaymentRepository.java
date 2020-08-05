17ea619d84c7a7252aa45ece8467a879a3a215df
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-05 21:51:56 +0300

    OBG-862. Introduced payment propagation to payment status

--- opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/repository/HbciSandboxPaymentRepository.java
+++ opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/repository/HbciSandboxPaymentRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HbciSandboxPaymentRepository extends JpaRepository<HbciSandboxPayment, Long> {
 
+    Optional<HbciSandboxPayment> findByOwnerLoginAndOrderReference(String ownerLogin, String orderReference);
     List<HbciSandboxPayment> findByOwnerLoginOrderByCreatedAtDesc(String ownerLogin);
 }
