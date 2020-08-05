85e565564b521d19d015aabe5337d9ebc54ec5af
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-05 13:45:53 +0300

    OBG-859. Added tests for signature generation and README's

--- opba-api-security-signer-generator/opba-api-security-signer-generator-impl/src/test/resources/DataToSignConfigurer.java
+++ opba-api-security-signer-generator/opba-api-security-signer-generator-impl/src/test/resources/DataToSignConfigurer.java
@@ -3,5 +3,5 @@ package de.adorsys.signer.test;
 import de.adorsys.opba.api.security.generator.api.GeneratedDataToSignNormalizer;
 
 @GeneratedDataToSignNormalizer(openApiYamlPath = {"api/tpp_banking_api_ais.yml"}, signerClassName = "TestSigner")
-public class DataToSigmConfigurer {
+public class DataToSignConfigurer {
 }
