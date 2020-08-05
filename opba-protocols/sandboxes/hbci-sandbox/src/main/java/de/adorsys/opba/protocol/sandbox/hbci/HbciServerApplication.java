9d488473a3cd107189fb846f8f0bb9799e3b553d
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-06 00:37:40 +0300

    OBG-862. Extract JpaAuditing into separate config to avoid collisions

--- opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/HbciServerApplication.java
+++ opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/HbciServerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties
 @EntityScan(basePackages = "de.adorsys.opba.protocol.sandbox.hbci.domain")
