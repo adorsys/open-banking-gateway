aa5d1b1925dfcf396b34cbade3d5ff6ef066f59b
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-05 23:09:40 +0300

    OBG-862. Implemented mapping from payments to transactions

--- opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/HbciServerApplication.java
+++ opba-protocols/sandboxes/hbci-sandbox/src/main/java/de/adorsys/opba/protocol/sandbox/hbci/HbciServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties
 @EntityScan(basePackages = "de.adorsys.opba.protocol.sandbox.hbci.domain")
