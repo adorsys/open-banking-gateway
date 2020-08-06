5b4276fd5f0bee32f0b877ffb0ac0a5547105115
iiuex <iiu@adorsys.com.ua>
2020-08-06 09:11:04 +0300

    OBG 808: Db dependency removed

--- opba-db/src/main/java/de/adorsys/opba/db/repository/jpa/PaymentRepository.java
+++ opba-db/src/main/java/de/adorsys/opba/db/repository/jpa/PaymentRepository.java
@@ -18,7 +18,6 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     List<Payment> findByServiceSessionIdOrderByModifiedAtDesc(UUID serviceSessionId);
     Collection<Payment> findByPsu(Psu owner);
-    Payment findByPsuId(String psuId);
 
     @Modifying
     @Transactional
