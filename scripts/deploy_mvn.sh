c508ffaea6bf72675c65f4dbb9d6b1aa75b49aca
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-25 21:17:47 +0300

    OBG-932. Remodify script

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-echo "$GPG_SECRET_KEY" | base64 --decode | $GPG_EXECUTABLE --import || true
-echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust || true
-mvn --settings scripts/mvn-release-settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
+mvn --settings scripts/mvn-release-settings.xml nexus-staging-maven-plugin:deploy -Prelease -DskipTests -B -U;
