dba633977de85719529da7a5b8025014f5523028
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-26 19:38:39 +0300

    OBG-932. Local maven deploy + promote works (but with double submission)

--- scripts/deploy_mvn_release_candidate.sh
+++ scripts/deploy_mvn_release_candidate.sh
@@ -6,4 +6,4 @@ set -e
 echo "$GPG_SECRET_KEY" | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch --yes || true
 echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust --no-tty --batch --yes || true
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml package gpg:sign deploy -Prelease-candidate -DskipTests -B -U;
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml gitflow-helper:retarget-deploy gitflow-helper:promote-master  deploy -Prelease-candidate -DskipTests -B -U;
