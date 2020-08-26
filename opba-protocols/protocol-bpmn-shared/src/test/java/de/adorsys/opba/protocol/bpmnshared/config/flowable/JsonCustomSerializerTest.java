3efada54be737c38d9cb3d18cebac9c00dc3713a
iiuex <iiu@adorsys.com.ua>
2020-08-26 09:18:12 +0300

    OBG 930: Init commit, serializer return fixed

---
+++ opba-protocols/protocol-bpmn-shared/src/test/java/de/adorsys/opba/protocol/bpmnshared/config/flowable/JsonCustomSerializerTest.java
@@ -0,0 +1,16 @@
+package de.adorsys.opba.protocol.bpmnshared.config.flowable;
+
+public class JsonCustomSerializerTest {
+
+    void serializeLessThanMaxValueTest() {
+
+    }
+
+    void serializeEqualsToMaxValueTest() {
+
+    }
+
+    void serializeMoreThanMaxValueTest() {
+
+    }
+}
---
+++ opba-protocols/protocol-bpmn-shared/src/test/java/de/adorsys/opba/protocol/bpmnshared/config/flowable/LargeJsonCustomSerializerTest.java
@@ -0,0 +1,8 @@
+package de.adorsys.opba.protocol.bpmnshared.config.flowable;
+
+public class LargeJsonCustomSerializerTest {
+
+    void serializeLessThanMaxValueTest() {
+
+    }
+}
