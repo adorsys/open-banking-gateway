b4dab6b20dee943612b93d29bc62ba7cc9ec1e04
Valentyn Berezin <valb3r.ua@gmail.com>
2020-08-27 10:56:58 +0300

    OBG-932. Fixed deployment

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml gitflow-helper:retarget-deploy gitflow-helper:promote-master deploy -Prelease -DskipTests -B -U
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U
