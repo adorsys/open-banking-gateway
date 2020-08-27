cb70e47f289fe163a28d5aac968c4ff1cfc01132
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-27 19:07:18 +0300

    OBG-932. Restore travis values and tests

--- scripts/build_mvn.sh
+++ scripts/build_mvn.sh
@@ -2,9 +2,9 @@
 
 # Run heavy tests daily on cron.
 # In particular it is sandbox-spinning tests that are launched if ENABLE_HEAVY_TESTS==true
-if [[ "$GITHUB_EVENT_NAME" == "cron"* || "$TRAVIS_BRANCH" == *"heavy-tests-enabled"* || "$TRAVIS_PULL_REQUEST_BRANCH" == *"heavy-tests-enabled"* ]]; then
+if [[ "$TRAVIS_EVENT_NAME" == "cron"* || "$TRAVIS_BRANCH" == *"heavy-tests-enabled"* || "$TRAVIS_PULL_REQUEST_BRANCH" == *"heavy-tests-enabled"* ]]; then
     echo "Heavy tests enabled"
     export ENABLE_HEAVY_TESTS="true";
 fi
 
-mvn verify --no-transfer-progress -Djgiven.report.text=false -DskipTests
+mvn verify --no-transfer-progress -Djgiven.report.text=false
