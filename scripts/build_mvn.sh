8c3eb6d352d5221bb4588fa5b3523165b3a45fdc
valb3r <valentyn.berezin@adorsys.com.ua>
2020-08-27 19:15:03 +0300

    OBG-932. Correct travis version

--- scripts/build_mvn.sh
+++ scripts/build_mvn.sh
@@ -2,7 +2,7 @@
 
 # Run heavy tests daily on cron.
 # In particular it is sandbox-spinning tests that are launched if ENABLE_HEAVY_TESTS==true
-if [[ "$TRAVIS_EVENT_NAME" == "cron"* || "$TRAVIS_BRANCH" == *"heavy-tests-enabled"* || "$TRAVIS_PULL_REQUEST_BRANCH" == *"heavy-tests-enabled"* ]]; then
+if [[ "$TRAVIS_EVENT_TYPE" == "cron"* || "$TRAVIS_BRANCH" == *"heavy-tests-enabled"* || "$TRAVIS_PULL_REQUEST_BRANCH" == *"heavy-tests-enabled"* ]]; then
     echo "Heavy tests enabled"
     export ENABLE_HEAVY_TESTS="true";
 fi
