7602d1144bda2e7b79caac0ce6715393d1770273
Hergy Tchuinkou <htc@adorsys.de>
2020-08-27 12:21:47 +0200

    redesigned ais in fintech

--- fintech-examples/fintech-ui/src/app/bank/list-accounts/list-accounts.component.ts
+++ fintech-examples/fintech-ui/src/app/bank/list-accounts/list-accounts.component.ts
@@ -15,12 +15,6 @@ export class ListAccountsComponent implements OnInit {
   accounts: AccountDetails[];
   bankId = '';
   loARetrievalInformation;
-  id = 'dfdfdfd4drrrrr-444rr33-er43';
-  id1 = '4443fdfd4drrrrr-444rr33-er43';
-  iban = 'DE2750010517421134792522';
-  iban1 = 'DE2750010517421134792622';
-  name = 'bob';
-  name1 = 'tom';
 
   constructor(
     private router: Router,
@@ -32,6 +26,10 @@ export class ListAccountsComponent implements OnInit {
   ngOnInit() {
     this.bankId = this.route.snapshot.paramMap.get('bankid');
     this.loadAccount();
+  }
+
+  onSubmit(id: any) {
+    this.router.navigate([id], { relativeTo: this.route });
   }
 
   private loadAccount(): void {
@@ -58,11 +56,8 @@ export class ListAccountsComponent implements OnInit {
           // to be removed when issue https://github.com/adorsys/open-banking-gateway/issues/848 is resolved
           // or Fintech UI refactored
           this.accounts = response.body.accounts;
-          const loa = [];
-          loa.push(new AccountStruct(this.id, this.iban, this.name));
-          loa.push(new AccountStruct(this.id1, this.iban1, this.name1));
-          this.accounts.push(loa.pop());
-          this.accounts.push(loa.pop());
+          let loa = [];
+          this.loadTestAccounts();
           for (const accountDetail of this.accounts) {
             loa.push(new AccountStruct(accountDetail.resourceId, accountDetail.iban, accountDetail.name));
           }
@@ -71,9 +66,17 @@ export class ListAccountsComponent implements OnInit {
     });
   }
 
-  onSubmit(value: boolean, id: string) {
-    if (value) {
-      this.router.navigate([id], { relativeTo: this.route });
-    }
+  loadTestAccounts(): void {
+    const loa = [];
+    const id = 'dfdfdfd4drrrrr-444rr33-er43';
+    const id1 = '4443fdfd4drrrrr-444rr33-er43';
+    const iban = 'DE2750010517421134792522';
+    const iban1 = 'DE2750010517421134792622';
+    const name = 'bob';
+    const name1 = 'tom';
+    loa.push(new AccountStruct(id, iban, name));
+    loa.push(new AccountStruct(id1, iban1, name1));
+    this.accounts.push(loa.pop());
+    this.accounts.push(loa.pop());
   }
 }
