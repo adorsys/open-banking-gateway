623eb8f4f93ea099527a973b0b6c1f9cf9014e2d
electronicpeter <github@electronicpeter.de>
2020-08-28 08:29:30 +0200

    OBG 947 added ctx mapping test

--- opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/service/xs2a/dto/Xs2aWithBalanceParametersFromCtxTest.java
+++ opba-protocols/xs2a-protocol/src/test/java/de/adorsys/opba/protocol/xs2a/service/xs2a/dto/Xs2aWithBalanceParametersFromCtxTest.java
@@ -12,7 +12,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = MapperTestConfig.class)
 public class Xs2aWithBalanceParametersFromCtxTest {
-    public static final String PATH_PREFIX = "mapper-test-fixtures/xs2a_with_balance_parameters_from_";
+    public static final String WITHOUT_BALANCE_PATH_PREFIX = "mapper-test-fixtures/xs2a_without_balance_parameters_from_";
+    public static final String WITH_BALANCE_PATH_PREFIX = "mapper-test-fixtures/xs2a_with_balance_parameters_from_";
 
     @Autowired
     private Xs2aWithBalanceParameters.FromCtx mapper;
@@ -22,10 +23,24 @@ public class Xs2aWithBalanceParametersFromCtxTest {
 
     @Test
     @SneakyThrows
+    public void xs2aWithoutBalanceParametersFromCtxMapperTest() {
+        // Given
+        Xs2aAisContext mappingInput = fixtureProvider.getFromFile(WITHOUT_BALANCE_PATH_PREFIX + "xs2a_consent_input.json", Xs2aAisContext.class);
+        Xs2aWithBalanceParameters expected = fixtureProvider.getFromFile(WITHOUT_BALANCE_PATH_PREFIX + "xs2a_consent_output.json", Xs2aWithBalanceParameters.class);
+
+        // When
+        Xs2aWithBalanceParameters actual = mapper.map(mappingInput);
+
+        // Then
+        assertThat(actual).isEqualToComparingFieldByFieldRecursively(expected);
+    }
+
+    @Test
+    @SneakyThrows
     public void xs2aWithBalanceParametersFromCtxMapperTest() {
         // Given
-        Xs2aAisContext mappingInput = fixtureProvider.getFromFile(PATH_PREFIX + "xs2a_consent_input.json", Xs2aAisContext.class);
-        Xs2aWithBalanceParameters expected = fixtureProvider.getFromFile(PATH_PREFIX + "xs2a_consent_output.json", Xs2aWithBalanceParameters.class);
+        Xs2aAisContext mappingInput = fixtureProvider.getFromFile(WITH_BALANCE_PATH_PREFIX + "xs2a_consent_input.json", Xs2aAisContext.class);
+        Xs2aWithBalanceParameters expected = fixtureProvider.getFromFile(WITH_BALANCE_PATH_PREFIX + "xs2a_consent_output.json", Xs2aWithBalanceParameters.class);
 
         // When
         Xs2aWithBalanceParameters actual = mapper.map(mappingInput);
