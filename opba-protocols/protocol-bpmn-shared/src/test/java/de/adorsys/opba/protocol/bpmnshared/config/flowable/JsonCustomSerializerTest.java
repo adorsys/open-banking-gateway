59d01bbbbc9b7f1278d18280583b5cfc76c927cf
iiuex <iiu@adorsys.com.ua>
2020-08-28 16:32:16 +0300

    OBG 930: Tests refactored, object mapper mocked

--- opba-protocols/protocol-bpmn-shared/src/test/java/de/adorsys/opba/protocol/bpmnshared/config/flowable/JsonCustomSerializerTest.java
+++ opba-protocols/protocol-bpmn-shared/src/test/java/de/adorsys/opba/protocol/bpmnshared/config/flowable/JsonCustomSerializerTest.java
@@ -1,37 +1,45 @@
 package de.adorsys.opba.protocol.bpmnshared.config.flowable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class JsonCustomSerializerTest {
     private static JsonCustomSerializer serializer;
+    private static ObjectMapper mapper;
 
     @BeforeAll
+    @SneakyThrows
     public static void init() {
-        serializer = new JsonCustomSerializer(null, new ObjectMapper(), Arrays.asList("java.lang.String"), 2048);
+        mapper = mock(ObjectMapper.class);
+        when(mapper.writeValueAsString(any())).thenAnswer(inv -> inv.getArgument(0, String.class));
+        serializer = new JsonCustomSerializer(null, mapper, Collections.singletonList(String.class.getCanonicalName()), 2048);
     }
 
     @Test
     void serializeLessThanMaxValueTest() {
-        String input = generateRandomStringOfLength(2045);
+        String input = generateRandomStringOfLength(2047);
         assertThat(serializer.isAbleToStore(input)).isTrue();
     }
 
     @Test
     void serializeEqualsToMaxValueTest() {
-        String input = generateRandomStringOfLength(2046);
+        String input = generateRandomStringOfLength(2048);
         assertThat(serializer.isAbleToStore(input)).isTrue();
     }
 
     @Test
     void serializeMoreThanMaxValueTest() {
-        String input = generateRandomStringOfLength(2047);
+        String input = generateRandomStringOfLength(2049);
         assertThat(serializer.isAbleToStore(input)).isFalse();
     }
 
