3b10eca413bba752cf07fc2bb9320b8fd41e5f01
iiuex <iiu@adorsys.com.ua>
2020-08-28 16:32:16 +0300

    OBG 930: Tests refactored, object mapper mocked

--- opba-protocols/protocol-bpmn-shared/src/test/java/de/adorsys/opba/protocol/bpmnshared/config/flowable/LargeJsonCustomSerializerTest.java
+++ opba-protocols/protocol-bpmn-shared/src/test/java/de/adorsys/opba/protocol/bpmnshared/config/flowable/LargeJsonCustomSerializerTest.java
@@ -1,31 +1,40 @@
 package de.adorsys.opba.protocol.bpmnshared.config.flowable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class LargeJsonCustomSerializerTest {
     private static LargeJsonCustomSerializer serializer;
+    private static ObjectMapper mapper;
 
     @BeforeAll
+    @SneakyThrows
     public static void init() {
-        serializer = new LargeJsonCustomSerializer(null, new ObjectMapper(), Arrays.asList("java.lang.String"), 2048);
+        mapper = mock(ObjectMapper.class);
+        when(mapper.writeValueAsString(any())).thenAnswer(inv -> inv.getArgument(0, String.class));
+        serializer = new LargeJsonCustomSerializer(null, mapper, Collections.singletonList(String.class.getCanonicalName()), 2048);
     }
+
 
     @Test
     void serializeLessThanMinValueTest() {
-        String input = generateRandomStringOfLength(2046);
+        String input = generateRandomStringOfLength(2048);
         assertThat(serializer.isAbleToStore(input)).isFalse();
     }
 
     @Test
     void serializeMoreToMinValueTest() {
-        String input = generateRandomStringOfLength(2047);
+        String input = generateRandomStringOfLength(2049);
         assertThat(serializer.isAbleToStore(input)).isTrue();
     }
 
