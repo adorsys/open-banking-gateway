a742258e625a9cce1682b71aea78810110bf3e6c
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-09-07 17:32:03 +0200

    Fixed timer service test

--- fintech-examples/fintech-ui/src/app/services/timer.service.spec.ts
+++ fintech-examples/fintech-ui/src/app/services/timer.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TimerService } from './timer.service';
 
 describe('TimerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    })
+  );
 
   it('should be created', () => {
     const service: TimerService = TestBed.get(TimerService);
