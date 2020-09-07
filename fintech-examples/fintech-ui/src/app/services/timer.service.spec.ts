1961ca6105b5d6c5f2dc104dfda7e86459d144ff
collins-k <cyrille_08kamgaing@yahoo.fr>
2020-09-07 16:44:03 +0200

    Extracted timer to a serve for better timer management

---
+++ fintech-examples/fintech-ui/src/app/services/timer.service.spec.ts
@@ -0,0 +1,12 @@
+import {TestBed} from '@angular/core/testing';
+
+import {TimerService} from './timer.service';
+
+describe('TimerService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TimerService = TestBed.get(TimerService);
+    expect(service).toBeTruthy();
+  });
+});
