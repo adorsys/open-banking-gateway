a568c33e7031d60ab053eb1cadf161471bd85426
Kamgaing <cyrille_08kamgaing@yahoo.fr>
2020-09-16 11:54:16 +0200

    Fixed to-aspsp test

--- consent-ui/src/app/pis/to-aspsp-page/to-aspsp-page.component.spec.ts
+++ consent-ui/src/app/pis/to-aspsp-page/to-aspsp-page.component.spec.ts
@@ -21,10 +21,13 @@ describe('ToAspspPageComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            parent: { params: of({ authId: StubUtilTests.AUTH_ID }) }
-          }
-        }
-      ]
+            parent: {
+              params: of({ authId: StubUtilTests.AUTH_ID }),
+              queryParams: of({ redirectCode: StubUtilTests.REDIRECT_ID }),
+            },
+          },
+        },
+      ],
     }).compileComponents();
   }));
 
