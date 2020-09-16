a10a685da0c970baf44bd2bccd78744ee5a34d77
iiuex <iiu@adorsys.com.ua>
2020-09-16 11:10:28 +0300

    OBG 797: Mockito fix

--- fintech-examples/fintech-impl/src/test/java/de/adorsys/opba/fintech/impl/service/BankSearchServiceTest.java
+++ fintech-examples/fintech-impl/src/test/java/de/adorsys/opba/fintech/impl/service/BankSearchServiceTest.java
@@ -1,25 +1,22 @@
 package de.adorsys.opba.fintech.impl.service;
 
 import de.adorsys.opba.fintech.api.model.generated.InlineResponse2001;
-import de.adorsys.opba.fintech.impl.config.FintechUiConfig;
 import de.adorsys.opba.fintech.impl.controller.utils.RestRequestContext;
-import de.adorsys.opba.fintech.impl.database.entities.RedirectUrlsEntity;
 import de.adorsys.opba.fintech.impl.tppclients.TppBankSearchClient;
 import de.adorsys.opba.tpp.banksearch.api.model.generated.BankSearchResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockReset;
+import org.springframework.http.ResponseEntity;
 
+import java.util.Collections;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,10 +34,21 @@ class BankSearchServiceTest {
     @Test
     void searchBank() {
         // Given
+        int start = 1;
+        int max = 10;
+        BankSearchResponse bankSearchResponse = new BankSearchResponse().bankDescriptor(Collections.emptyList());
         when(restRequestContext.getRequestId()).thenReturn(UUID.randomUUID().toString());
+        when(tppBankSearchClient.bankSearchGET(
+                any(),
+                eq(INVALID_KEYWORD),
+                any(),
+                any(),
+                any(),
+                eq(1),
+                eq(10))).thenReturn(ResponseEntity.ok().body(bankSearchResponse));
 
         // When
-        InlineResponse2001 actual = bankSearchService.searchBank("INVALID_KEYWORD", 1, 10);
+        InlineResponse2001 actual = bankSearchService.searchBank(INVALID_KEYWORD, 1, 10);
 
         // Then
         assertThat(actual).isNotNull();
