0876afd837a33930e0531c5ce902c78fcc8aee73
valb3r <valentyn.berezin@adorsys.com.ua>
2020-09-18 19:46:44 +0300

    OBG-974. Make checkstyle happy

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/pis/Xs2aPaymentContextLoader.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/entrypoint/pis/Xs2aPaymentContextLoader.java
@@ -1,23 +1,15 @@
 package de.adorsys.opba.protocol.xs2a.entrypoint.pis;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import de.adorsys.opba.protocol.api.services.scoped.consent.ProtocolFacingConsent;
 import de.adorsys.opba.protocol.api.services.scoped.consent.ProtocolFacingPayment;
 import de.adorsys.opba.protocol.bpmnshared.config.flowable.FlowableObjectMapper;
 import de.adorsys.opba.protocol.bpmnshared.config.flowable.FlowableProperties;
-import de.adorsys.opba.protocol.xs2a.context.ais.AccountListXs2aContext;
-import de.adorsys.opba.protocol.xs2a.context.ais.TransactionListXs2aContext;
-import de.adorsys.opba.protocol.xs2a.context.ais.Xs2aAisContext;
 import de.adorsys.opba.protocol.xs2a.context.pis.Xs2aPisContext;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.flowable.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
-import java.util.Optional;
-
-import static de.adorsys.opba.protocol.xs2a.constant.GlobalConst.CONTEXT;
 
 @Service
 @RequiredArgsConstructor
