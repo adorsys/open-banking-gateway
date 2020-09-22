f509b8a0b7061b19feeb770ebc2eba41a309c4bd
rjtch <tchuinkoufongue@gmail.com>
2020-09-22 10:53:04 +0200

    some unit-tests

--- fintech-examples/fintech-ui/src/app/bank-search/bank-search.component.spec.ts
+++ fintech-examples/fintech-ui/src/app/bank-search/bank-search.component.spec.ts
@@ -5,8 +5,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { BankSearchComponent } from './bank-search.component';
 import { SearchComponent } from '../common/search/search.component';
+import { BankDescriptor } from '../api';
 
-describe('BankSearchComponent', () => {
+fdescribe('BankSearchComponent', () => {
   let component: BankSearchComponent;
   let fixture: ComponentFixture<BankSearchComponent>;
 
@@ -25,5 +26,20 @@ describe('BankSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSearch', () => {
+    const keyword = 'adorsys';
+    const mockBankDescriptor: BankDescriptor[] = [];
+
+    spyOn(component, 'onSearch').withArgs(keyword).and.callThrough();
+    expect(component.searchedBanks).toEqual(mockBankDescriptor);
+  });
+
+  it('should call onBankSelect', () => {
+    const mockBank: BankDescriptor = {};
+
+    spyOn(component, 'onBankSelect').withArgs(mockBank).and.callThrough();
+    expect(component.selectedBank).toEqual(mockBank.uuid);
   });
 });
