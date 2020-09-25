a9809f724d6c343829502a499a7358631b0c3f55
valb3r <valentyn.berezin@adorsys.com.ua>
2020-09-25 13:04:53 +0300

    OBG-1027. Initial POM structure for the exporter

---
+++ firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/FireflyexporterApplication.java
@@ -0,0 +1,13 @@
+package de.adorsys.opba.fireflyexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireflyexporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireflyexporterApplication.class, args);
+	}
+
+}
---
+++ firefly-exporter/src/test/java/de/adorsys/opba/fireflyexporter/FireflyexporterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adorsys.opba.fireflyexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireflyexporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
