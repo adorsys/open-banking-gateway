ed71f4271b179061f490582812d7bbe2be063774
valb3r <valentyn.berezin@adorsys.com.ua>
2020-09-25 15:47:39 +0300

    OBG-1027. Added OPBA api security config

--- firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/config/FireFlyConfig.java
+++ firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/config/FireFlyConfig.java
@@ -3,16 +3,17 @@ package de.adorsys.opba.fireflyexporter.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.net.URL;
 
 @Data
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "firefly")
 public class FireFlyConfig {
 
     @NotNull
-    private URL fireFlyUrl;
+    private URL url;
 }
