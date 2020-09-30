d041108eac81118533e56e04791260cfb15b8cce
electronicpeter <github@electronicpeter.de>
2020-09-30 13:05:18 +0200

    OBG 1034 update

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/controller/FinTechAccountInformationImpl.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/controller/FinTechAccountInformationImpl.java
@@ -1,24 +1,59 @@
 package de.adorsys.opba.fintech.impl.controller;
 
-import de.adorsys.opba.fintech.api.resource.generated.FintechRetrieveConsentApi;
-import de.adorsys.opba.fintech.impl.service.ConsentRetrievalService;
+import de.adorsys.opba.fintech.api.model.generated.AccountList;
+import de.adorsys.opba.fintech.api.model.generated.TransactionsResponse;
+import de.adorsys.opba.fintech.api.resource.generated.FinTechAccountInformationApi;
+import de.adorsys.opba.fintech.impl.controller.utils.LoARetrievalInformation;
+import de.adorsys.opba.fintech.impl.controller.utils.LoTRetrievalInformation;
+import de.adorsys.opba.fintech.impl.database.entities.SessionEntity;
+import de.adorsys.opba.fintech.impl.service.AccountService;
+import de.adorsys.opba.fintech.impl.service.SessionLogicService;
+import de.adorsys.opba.fintech.impl.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@Profile("CONSENT_RETRIEVAL")
-public class FinTechAccountInformationImpl implements FintechRetrieveConsentApi {
-    private final ConsentRetrievalService consentRetrievalService;
+public class FinTechAccountInformationImpl implements FinTechAccountInformationApi {
 
-    public ResponseEntity<Object> retrieveConsent(String userid, String password) {
-        ConsentRetrievalService.ConsentRetrievalResult consentRetrievalResult = consentRetrievalService.get(userid, password);
-        log.info("found:{}", consentRetrievalResult.toString());
-        return new ResponseEntity<>(consentRetrievalResult, HttpStatus.OK);
+    private final SessionLogicService sessionLogicService;
+    private final AccountService accountService;
+    private final TransactionService transactionService;
+
+    @Override
+    public ResponseEntity<AccountList> aisAccountsGET(String bankId, UUID xRequestID, String xsrfToken,
+                                                      String fintechRedirectURLOK, String fintechRedirectURLNOK,
+                                                      String loARetrievalInformation, Boolean withBalance) {
+        if (!sessionLogicService.isSessionAuthorized()) {
+            log.warn("aisAccountsGET failed: user is not authorized!");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        SessionEntity sessionEntity = sessionLogicService.getSession();
+        return sessionLogicService.addSessionMaxAgeToHeader(accountService.listAccounts(sessionEntity,
+            fintechRedirectURLOK, fintechRedirectURLNOK, bankId, LoARetrievalInformation.valueOf(loARetrievalInformation), withBalance));
+    }
+
+    @Override
+    public ResponseEntity<TransactionsResponse> aisTransactionsGET(String bankId, String accountId, UUID xRequestID,
+                                                                   String xsrfToken, String fintechRedirectURLOK, String fintechRedirectURLNOK,
+                                                                   String loTRetrievalInformation,
+                                                                   LocalDate dateFrom, LocalDate dateTo,
+                                                                   String entryReferenceFrom, String bookingStatus, Boolean deltaList) {
+        if (!sessionLogicService.isSessionAuthorized()) {
+            log.warn("aisTransactionsGET failed: user is not authorized!");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        SessionEntity sessionEntity = sessionLogicService.getSession();
+        return sessionLogicService.addSessionMaxAgeToHeader(
+            transactionService.listTransactions(sessionEntity, fintechRedirectURLOK, fintechRedirectURLNOK,
+                bankId, accountId, dateFrom, dateTo, entryReferenceFrom, bookingStatus, deltaList, LoTRetrievalInformation.valueOf(loTRetrievalInformation)));
     }
 }
