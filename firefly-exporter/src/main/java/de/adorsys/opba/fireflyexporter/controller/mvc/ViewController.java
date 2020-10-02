1abaa89f40073a9a779a3bdcaa1e5085c586bf2a
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-02 19:31:49 +0300

    OBG-1026. Retrieve bank name as well

--- firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/controller/mvc/ViewController.java
+++ firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/controller/mvc/ViewController.java
@@ -1,12 +1,17 @@
 package de.adorsys.opba.fireflyexporter.controller.mvc;
 
+import de.adorsys.opba.fireflyexporter.client.TppBankSearchClient;
 import de.adorsys.opba.fireflyexporter.config.ApiConfig;
 import de.adorsys.opba.fireflyexporter.service.ConsentService;
+import de.adorsys.opba.tpp.banksearch.api.model.generated.BankProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,6 +19,7 @@ public class ViewController {
 
     private final ApiConfig apiConfig;
     private final ConsentService consentService;
+    private final TppBankSearchClient bankSearch;
 
     @GetMapping("/")
     public String fireFlyUploader(Model model) {
@@ -23,6 +29,8 @@ public class ViewController {
     @GetMapping("/consent-confirmed")
     public String confirmConsent(@RequestParam("redirectCode") String redirectCode, Model model) {
         String bankId = consentService.confirmConsentAndGetBankId(redirectCode);
+        ResponseEntity<BankProfileResponse> bankProfile = bankSearch.bankProfileGET(UUID.randomUUID(), bankId, null, null, null);
+        model.addAttribute("bankName", bankProfile.getBody().getBankProfileDescriptor().getBankName());
         model.addAttribute("bankId", bankId);
         return showFireflyUploaderScreen(model);
     }
