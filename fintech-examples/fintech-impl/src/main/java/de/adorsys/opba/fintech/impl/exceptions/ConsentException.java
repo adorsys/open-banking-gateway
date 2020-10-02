849c04a951ac8e6ea5d613bbeacc193c887701a8
electronicpeter <github@electronicpeter.de>
2020-10-02 14:54:39 +0200

    OBG 1028 ConsentException with error message

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/exceptions/ConsentException.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/exceptions/ConsentException.java
@@ -3,7 +3,6 @@ package de.adorsys.opba.fintech.impl.exceptions;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class ConsentException extends RuntimeException {
     public ConsentException(int httpResponseCode, String xErrorCode) {
