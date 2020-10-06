3dd7fcec206ce749610c17aac1f1c8cd338d007b
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-06 15:41:12 +0300

    Release test

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U;
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml nexus-staging:close -Prelease -DskipTests -B -U;
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy nexus-staging:release -Prelease -DskipTests -B -U;
