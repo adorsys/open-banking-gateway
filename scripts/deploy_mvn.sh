566a3d19e31e6c232d17803ef216e7492e08cfda
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-06 15:11:58 +0300

    Reorder deployment

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U;
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml nexus-staging:close -Prelease -DskipTests -B -U;
