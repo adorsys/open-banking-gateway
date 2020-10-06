3752fb3645ac5db57edc649d7c28ea85cbb932cf
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-06 19:20:13 +0300

    Provide signing too

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -5,4 +5,5 @@ set -e
 echo "$GPG_SECRET_KEY" | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch --yes || true
 echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust --no-tty --batch --yes || true
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml gitflow-helper:promote-master gpg:sign nexus-staging:release -Prelease -DskipTests -B -U
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml gitflow-helper:promote-master gpg:sign -Prelease -DskipTests -B -U
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy nexus-staging:release -Prelease -DskipTests -B -U
