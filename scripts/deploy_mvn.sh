e0d2880a9ad2a459c797572453bf45435a8236e6
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-06 18:52:51 +0300

    Modify promotion

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy nexus-staging:release -Prelease -DskipTests -B -U;
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml gitflow-helper:promote-master gpg:sign nexus-staging:release -Prelease -DskipTests -B -U
