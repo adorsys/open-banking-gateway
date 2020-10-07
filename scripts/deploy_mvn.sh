9079740f344347594af28c95513c29d09cb52065
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-07 14:34:37 +0300

    Added custom plugin version for deployment

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -8,4 +8,4 @@ echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust -
 # Install custom plugin version to avoid catalog.txt deployment
 mvn install:install-file -Dfile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT.jar -DpomFile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT-pom.xml || exit 1
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy nexus-staging:release -Prelease -DskipTests -B -U
