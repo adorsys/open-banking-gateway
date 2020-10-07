1d53cd6b0842c947b90b39469fb5012344d8a61f
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-07 14:27:41 +0300

    Added custom plugin version for deployment

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -5,5 +5,7 @@ set -e
 echo "$GPG_SECRET_KEY" | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch --yes || true
 echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust --no-tty --batch --yes || true
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml gitflow-helper:promote-master gpg:sign -Prelease -DskipTests -B -U
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy nexus-staging:release -Prelease -DskipTests -B -U
+# Install custom plugin version to avoid catalog.txt deployment
+mvn install:install-file -Dfile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT.jar -DpomFile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT-pom.xml || exit 1
+
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U
