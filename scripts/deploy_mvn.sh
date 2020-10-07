1dbd92917f6fb112c0664213efab2b5c6d50156a
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-07 14:41:03 +0300

    Remove unnecessary creds

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# These are production secrets
-echo "$GPG_SECRET_KEY" | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch --yes || true
-echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust --no-tty --batch --yes || true
-
 # Install custom plugin version to avoid catalog.txt deployment
 mvn install:install-file -Dfile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT.jar -DpomFile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT-pom.xml || exit 1
 
