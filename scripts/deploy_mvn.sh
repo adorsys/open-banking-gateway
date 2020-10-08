d7e413574f84515ee7408bbb60385e342a269ee3
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2020-10-08 15:39:59 +0300

    Remove explicit nexus (#1075)

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -4,4 +4,4 @@ set -e
 # Install custom plugin version to avoid catalog.txt deployment
 mvn install:install-file -Dfile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT.jar -DpomFile=scripts/plugins/gitflow-helper-maven-plugin-3.0.1-SKIP-CATALOG-SNAPSHOT-pom.xml || exit 1
 
-mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy nexus-staging:release -Prelease -DskipTests -B -U
+mvn --no-transfer-progress --settings scripts/mvn-release-settings.xml deploy -Prelease -DskipTests -B -U
