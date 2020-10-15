687c15d0fc7714527821bd52b353c1b2b749fa65
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2020-10-15 13:11:21 +0300

    Feature/obg 1086 finalize travis migration (#1087)
    * OBG-1086. Restructure Travis's scripts
    
    * OBG-1086. Updated scripts and dropped Travis
    
    * OBG-1086. Change github pusher

--- scripts/build_mvn.sh
+++ scripts/build_mvn.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-# Run heavy tests daily on cron.
-# In particular it is sandbox-spinning tests that are launched if ENABLE_HEAVY_TESTS==true
-if [[ "$TRAVIS_EVENT_TYPE" == "cron"* || "$TRAVIS_BRANCH" == *"heavy-tests-enabled"* || "$TRAVIS_PULL_REQUEST_BRANCH" == *"heavy-tests-enabled"* ]]; then
-    echo "Heavy tests enabled"
-    export ENABLE_HEAVY_TESTS="true";
-fi
-
-mvn verify --no-transfer-progress -Djgiven.report.text=false
+mvn verify --no-transfer-progress -Djgiven.report.text=false || exit 1
--- scripts/validate_dev_deploy.sh
+++ scripts/validate_dev_deploy.sh
@@ -7,15 +7,15 @@ if [[ "$DISABLE_SMOKE_TESTS"  == "true" ]]; then
   exit 0
 fi
 
-if [[ $TRAVIS_REPO_SLUG != "adorsys/open-banking-gateway"
-    || $TRAVIS_PULL_REQUEST != "false"
+if [[ $GITHUB_REPOSITORY != "adorsys/open-banking-gateway"
+    || $GITHUB_EVENT_NAME == "pull_request"
     || -z "$TRAVIS_COMMIT" ]];
 then
   echo "ERROR: Deployment validation not allowed"
   exit 1
 fi
 
-COMMIT="$TRAVIS_COMMIT"
+COMMIT="$GITHUB_SHA"
 OPBA_URL=https://obg-dev-openbankinggateway.cloud.adorsys.de
 
 function fail() {
@@ -32,6 +32,6 @@ echo "Run smoke tests"
 # Run smoke tests:
 export ENABLE_SMOKE_TESTS=true
 echo "Run API smoke tests"
-./mvnw test --no-transfer-progress -Djgiven.report.text=false -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.OpbaApiSmokeE2ETest
+./mvnw test --no-transfer-progress -Djgiven.report.text=false -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.OpbaApiSmokeE2ETest || exit 1
 echo "Run Consent UI and API smoke tests"
-./mvnw test --no-transfer-progress -Djgiven.report.text=false -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.OpbaApiWithConsentUiSmokeE2ETest
+./mvnw test --no-transfer-progress -Djgiven.report.text=false -DfailIfNoTests=false -Dtest=de.adorsys.opba.smoketests.OpbaApiWithConsentUiSmokeE2ETest || exit 1
