c36a9c8a0d0987935fc076b73c6e6a709a64bf31
iiuex <iiu@adorsys.com.ua>
2020-10-27 10:48:49 +0200

    OBG 809: pmd fix

--- opba-db/src/main/java/de/adorsys/opba/db/domain/entity/DatasafeConfig.java
+++ opba-db/src/main/java/de/adorsys/opba/db/domain/entity/DatasafeConfig.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
