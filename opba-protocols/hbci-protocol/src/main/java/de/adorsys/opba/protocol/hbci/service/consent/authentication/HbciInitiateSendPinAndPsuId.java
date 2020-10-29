9106b1fbb756d52570b253d6cb12a2a205d4b8eb
electronicpeter <github@electronicpeter.de>
2020-10-29 10:27:28 +0100

    Revert "OBG 901 sink pin error"
    This reverts commit c002d12d

--- opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/service/consent/authentication/HbciInitiateSendPinAndPsuId.java
+++ opba-protocols/hbci-protocol/src/main/java/de/adorsys/opba/protocol/hbci/service/consent/authentication/HbciInitiateSendPinAndPsuId.java
@@ -11,7 +11,6 @@ import de.adorsys.opba.protocol.bpmnshared.service.context.ContextUtil;
 import de.adorsys.opba.protocol.bpmnshared.service.exec.ValidatedExecution;
 import de.adorsys.opba.protocol.hbci.context.HbciContext;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.kapott.hbci.manager.HBCIProduct;
 import org.springframework.stereotype.Service;
@@ -21,60 +20,39 @@ import java.util.stream.Collectors;
 
 @Service("hbciInitiateSendPinAndPsuId")
 @RequiredArgsConstructor
-@Slf4j
 public class HbciInitiateSendPinAndPsuId extends ValidatedExecution<HbciContext> {
 
     private final Optional<HBCIProduct> product;
     private final OnlineBankingService onlineBankingService;
-    private final HbciAuthorizationPossibleErrorHandler errorSink;
-
 
     @Override
     protected void doRealExecution(DelegateExecution execution, HbciContext context) {
+        HbciConsent consent = context.getHbciDialogConsent();
+        if (null == consent) {
+            consent = new HbciConsent();
+            consent.setHbciProduct(product.orElse(null));
+            consent.setCredentials(Credentials.builder()
+                    .userId(context.getPsuId())
+                    .pin(context.getPsuPin())
+                    .build()
+            );
+        }
 
-        errorSink.handlePossibleAuthorizationError(
-                () -> {
-                    context.setWrongAuthCredentials(false);
-                    HbciConsent consent = context.getHbciDialogConsent();
-                    if (null == consent) {
-                        consent = new HbciConsent();
-                        consent.setHbciProduct(product.orElse(null));
-                        consent.setCredentials(Credentials.builder()
-                                .userId(context.getPsuId())
-                                .pin(context.getPsuPin())
-                                .build()
-                        );
-                    }
+        UpdatePsuAuthenticationRequest request = new UpdatePsuAuthenticationRequest();
+        request.setCredentials(consent.getCredentials());
+        request.setBankApiConsentData(consent);
+        request.setBank(context.getBank());
 
-                    UpdatePsuAuthenticationRequest request = new UpdatePsuAuthenticationRequest();
-                    request.setCredentials(consent.getCredentials());
-                    request.setBankApiConsentData(consent);
-                    request.setBank(context.getBank());
+        UpdateAuthResponse response =
+                onlineBankingService.getStrongCustomerAuthorisation().updatePsuAuthentication(request);
 
-                    UpdateAuthResponse response = onlineBankingService.getStrongCustomerAuthorisation().updatePsuAuthentication(request);
+        if (handleScaChallengeRequired(execution, response)) {
+            return;
+        }
 
-                    if (handleScaChallengeRequired(execution, response)) {
-                        return;
-                    }
-
-                    ContextUtil.getAndUpdateContext(
-                            execution,
-                            (HbciContext ctx) -> ctx.setHbciDialogConsent((HbciConsent) response.getBankApiConsentData())
-                    );
-                },
-                ex -> aisOnWrongPassword(execution)
-        );
-
-    }
-
-    private void aisOnWrongPassword(DelegateExecution execution) {
         ContextUtil.getAndUpdateContext(
                 execution,
-                (HbciContext ctx) -> {
-                    log.warn("Request {} of {} has provided incorrect password", ctx.getRequestId(), ctx.getSagaId());
-                    log.warn("Hi Maksym, though wrong pin is set here, ui looks bad....");
-                    ctx.setWrongAuthCredentials(true);
-                }
+                (HbciContext ctx) -> ctx.setHbciDialogConsent((HbciConsent) response.getBankApiConsentData())
         );
     }
 
