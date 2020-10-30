333dd600ee45099a40f1259099d6cde340d8095e
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-30 18:34:32 +0200

    OBG-1129. Update schemas

--- opba-protocols/sandboxes/xs2a-sandbox/src/main/resources/sandbox/prepare-postgres.sql
+++ opba-protocols/sandboxes/xs2a-sandbox/src/main/resources/sandbox/prepare-postgres.sql
@@ -1,8 +1,10 @@
 CREATE SCHEMA ledgers;
 CREATE SCHEMA consent;
+CREATE SCHEMA tpp;
 
 CREATE ROLE sandboxrole;
 GRANT ALL ON SCHEMA consent TO sandboxrole;
+GRANT ALL ON SCHEMA tpp TO sandboxrole;
 
 /* Ledgers is has its own database */
 CREATE USER testledgersapp WITH ENCRYPTED PASSWORD 'testledgersapp';
@@ -20,6 +22,8 @@ ALTER DEFAULT PRIVILEGES FOR USER testonlinebanking IN SCHEMA consent GRANT ALL 
 ALTER DEFAULT PRIVILEGES FOR USER testonlinebanking IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
 ALTER DEFAULT PRIVILEGES FOR USER testtpprest IN SCHEMA consent GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
 ALTER DEFAULT PRIVILEGES FOR USER testtpprest IN SCHEMA consent GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER testtpprest IN SCHEMA tpp GRANT ALL PRIVILEGES ON TABLES TO sandboxrole;
+ALTER DEFAULT PRIVILEGES FOR USER testtpprest IN SCHEMA tpp GRANT ALL PRIVILEGES ON SEQUENCES TO sandboxrole;
 
 /* Access to Large objects in DB (by default is off) */
 ALTER DATABASE sandbox_apps SET lo_compat_privileges=on;
