3e75c882c92d7b73c3182df96851d0725e498511
valb3r <valentyn.berezin@adorsys.com.ua>
2020-11-02 14:07:02 +0200

    OBG-1111. Use latest consent

--- firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/service/FireFlyTransactionExporter.java
+++ firefly-exporter/src/main/java/de/adorsys/opba/fireflyexporter/service/FireFlyTransactionExporter.java
@@ -123,7 +123,7 @@ public class FireFlyTransactionExporter {
                 null,
                 null,
                 bankId,
-                consentRepository.findFirstByBankIdOrderByModifiedAt(bankId).map(BankConsent::getConsentId).orElse(null),
+                consentRepository.findFirstByBankIdOrderByModifiedAtDesc(bankId).map(BankConsent::getConsentId).orElse(null),
                 from,
                 to,
                 null,
