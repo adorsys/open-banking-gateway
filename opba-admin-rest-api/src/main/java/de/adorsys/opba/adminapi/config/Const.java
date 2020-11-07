00ab178512033b859fc934f10b0c7ef228ce0191
valb3r <valentyn.berezin@adorsys.com.ua>
2020-11-07 18:13:18 +0200

    OBG-1116. Added security filter for admin API and basic auth scheme

---
+++ opba-admin-rest-api/src/main/java/de/adorsys/opba/adminapi/config/Const.java
@@ -0,0 +1,10 @@
+package de.adorsys.opba.adminapi.config;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor") //Checkstyle doesn't recognise Lombok
+public class Const {
+
+    public static final String DISABLED_ON_NO_ADMIN_API = "!no-admin-api";
+}
