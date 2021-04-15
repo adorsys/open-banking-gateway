65eb48463c24a2bda0a6ced756e577823b1960e4
Valentyn Berezin <vbe@adorsys.com.ua>
2021-04-15 19:55:00 +0300

    Use smartanalytics snapshot

--- opba-analytics/analytics-smartanalytics/src/main/java/de/adorsys/opba/smartanalytics/SmartAnalyticsAnalyzerConfig.java
+++ opba-analytics/analytics-smartanalytics/src/main/java/de/adorsys/opba/smartanalytics/SmartAnalyticsAnalyzerConfig.java
@@ -1,10 +1,33 @@
 package de.adorsys.opba.smartanalytics;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
+@Slf4j
 @Configuration
-@ComponentScan("de.adorsys.opba.smartanalytics")
+@ComponentScan(
+        basePackages = {
+                "de.adorsys.opba.smartanalytics",
+                "de.adorsys.smartanalytics"
+        },
+        // Disabling SmartAnalytics HATEOAS due to missing issues
+        excludeFilters = {@ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = {"de\\.adorsys\\.smartanalytics\\.config\\..*"}
+        ), @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = {"de\\.adorsys\\.smartanalytics\\.exception\\..*"}
+        ), @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = {"de\\.adorsys\\.smartanalytics\\.web\\.ServerInfoController.*"}
+        ), @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = {"de\\.adorsys\\.smartanalytics\\.web\\..*"}
+        )
+        }
+)
 public class SmartAnalyticsAnalyzerConfig {
 }
 
