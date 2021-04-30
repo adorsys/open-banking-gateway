d1d1b4cff63e60ab2aac846ccb2b493ef4ff3e36
Valentyn Berezin <vbe@adorsys.com.ua>
2021-04-30 11:58:18 +0300

    Add custom INTEG deployer

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# log disk space to understand why heavy tests fail
-while true; do df -h; sleep 10; done &
-while true; do du -hs .; sleep 15; done &
-
 echo "Building and testing JAVA backend"
 bash ./scripts/build_mvn.sh || exit 1
 
