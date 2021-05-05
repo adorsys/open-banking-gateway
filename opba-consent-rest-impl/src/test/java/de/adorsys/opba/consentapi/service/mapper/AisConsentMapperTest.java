13187d3ecf16b3c492f0a969e07b19d2944c4025
Valentyn Berezin <vbe@adorsys.com.ua>
2021-05-05 17:29:24 +0300

    Remove unused imports

--- opba-consent-rest-impl/src/test/java/de/adorsys/opba/consentapi/service/mapper/AisConsentMapperTest.java
+++ opba-consent-rest-impl/src/test/java/de/adorsys/opba/consentapi/service/mapper/AisConsentMapperTest.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 
 import java.time.LocalDate;
 
