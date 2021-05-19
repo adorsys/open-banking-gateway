c9e2ed9362c65626360b289a798b1e98a49ce34e
Valentyn Berezin <vbe@adorsys.com.ua>
2021-05-19 18:51:24 +0300

    Added comment

--- opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/consent/Xs2aPersistConsentAndContext.java
+++ opba-protocols/xs2a-protocol/src/main/java/de/adorsys/opba/protocol/xs2a/service/xs2a/consent/Xs2aPersistConsentAndContext.java
@@ -30,6 +30,7 @@ public class Xs2aPersistConsentAndContext extends ValidatedExecution<Xs2aContext
         ProtocolFacingConsent consent = context.consentAccess().findSingleByCurrentServiceSession()
                 .orElseGet(() -> context.consentAccess().createDoNotPersist());
 
+        // ING special condition
         if (null == context.getConsentId()) {
             context.setConsentAcquired(true);
         }
