9ec94907b443a1194ac5409f22e90b8fa4dc494d
Valentyn Berezin <vbe@adorsys.com.ua>
2021-05-31 12:26:31 +0300

    Make checkstyle happy

--- opba-protocols/opba-protocol-testing-helper/src/main/java/de/adorsys/opba/helpers/protocol/testing/controller/ProtocolTestingController.java
+++ opba-protocols/opba-protocol-testing-helper/src/main/java/de/adorsys/opba/helpers/protocol/testing/controller/ProtocolTestingController.java
@@ -1,6 +1,5 @@
 package de.adorsys.opba.helpers.protocol.testing.controller;
 
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.opba.helpers.protocol.testing.service.MapBasedAspspRepository;
@@ -21,7 +20,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
--- opba-protocols/opba-protocol-testing-helper/src/main/java/de/adorsys/opba/helpers/protocol/testing/service/MapBasedRequestScopedServicesProvider.java
+++ opba-protocols/opba-protocol-testing-helper/src/main/java/de/adorsys/opba/helpers/protocol/testing/service/MapBasedRequestScopedServicesProvider.java
@@ -20,7 +20,11 @@ import lombok.Setter;
 import lombok.experimental.Delegate;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
