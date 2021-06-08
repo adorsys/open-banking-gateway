0182cf34664ba5c6462bfe15a62d8d0c17238e9d
Valentyn Berezin <vbe@adorsys.com.ua>
2021-06-08 19:20:05 +0300

    Added comment

--- opba-embedded-starter/src/main/java/de/adorsys/opba/starter/config/extensions/OpbaFinapiExtensionConfig.java
+++ opba-embedded-starter/src/main/java/de/adorsys/opba/starter/config/extensions/OpbaFinapiExtensionConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-// Might require setting 'spring.devtools.restart.enabled' to 'false' (env. var or property) due to Classloader issues
+// Might require setting 'spring.devtools.restart.enabled' to 'false' (system property) due to Classloader issues
 @Configuration
 @ConditionalOnClass(name = "de.adorsys.opba.protocol.finapi.webform.EnableFinApiWebformProtocol")
 @ComponentScan(basePackages = {"de.adorsys.opba.protocol.finapi"})
