4056e81f50a672a1ba61c2803c8f1b15093f1b7e
Valentyn Berezin <vbe@adorsys.com.ua>
2021-06-10 17:04:29 +0300

    Propagate OnLogin action support to upper layers

--- opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/authorization/OnLoginService.java
+++ opba-banking-protocol-facade/src/main/java/de/adorsys/opba/protocol/facade/services/authorization/OnLoginService.java
@@ -3,6 +3,8 @@ package de.adorsys.opba.protocol.facade.services.authorization;
 import de.adorsys.opba.protocol.api.authorization.OnLogin;
 import de.adorsys.opba.protocol.api.dto.request.authorization.OnLoginRequest;
 import de.adorsys.opba.protocol.api.dto.result.body.UpdateAuthBody;
+import de.adorsys.opba.protocol.facade.dto.result.torest.FacadeResult;
+import de.adorsys.opba.protocol.facade.exceptions.NoProtocolRegisteredException;
 import de.adorsys.opba.protocol.facade.services.FacadeService;
 import de.adorsys.opba.protocol.facade.services.ProtocolResultHandler;
 import de.adorsys.opba.protocol.facade.services.ProtocolSelector;
@@ -12,8 +14,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
-import static de.adorsys.opba.protocol.api.common.ProtocolAction.GET_AUTHORIZATION_STATE;
+import static de.adorsys.opba.protocol.api.common.ProtocolAction.ON_LOGIN;
 import static de.adorsys.opba.protocol.facade.services.context.ServiceContextProviderForFintech.FINTECH_CONTEXT_PROVIDER;
 
 @Service
@@ -25,6 +28,15 @@ public class OnLoginService extends FacadeService<OnLoginRequest, UpdateAuthBody
             @Qualifier(FINTECH_CONTEXT_PROVIDER) ServiceContextProvider provider,
             ProtocolResultHandler handler,
             TransactionTemplate txTemplate) {
-        super(GET_AUTHORIZATION_STATE, actionProviders, selector, provider, handler, txTemplate);
+        super(ON_LOGIN, actionProviders, selector, provider, handler, txTemplate);
+    }
+
+    @Override
+    public CompletableFuture<FacadeResult<UpdateAuthBody>> execute(OnLoginRequest onLoginRequest) {
+        try {
+            return super.execute(onLoginRequest);
+        } catch (NoProtocolRegisteredException ex) {
+            return CompletableFuture.completedFuture(null);
+        }
     }
 }
