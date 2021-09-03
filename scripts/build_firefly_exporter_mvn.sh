22be529d2259f8c31e286f5237702c21d43dfdde
valb3r <valb3r.ua@gmail.com>
2021-09-03 17:51:50 +0300

    Force using wrapper instead of maven

--- scripts/build_firefly_exporter_mvn.sh
+++ scripts/build_firefly_exporter_mvn.sh
@@ -3,12 +3,12 @@
 # Build FireFly-Exporter separately, as it is not fulltime-deployment part
 echo "Building FireFly-exporter"
 cd firefly-exporter
-./mvnw verify --no-transfer-progress
+../mvnw verify --no-transfer-progress
 
 if [[ "$MVN_TESTS_DISABLED" == "true" ]]; then
   echo "Maven tests are disabled, only building FireFly exporter"
-  ./mvnw verify --no-transfer-progress -DskipTests
+  ../mvnw verify --no-transfer-progress -DskipTests
 else
   echo "Building and testing FireFly exporter"
-  ./mvnw verify --no-transfer-progress
+  ../mvnw verify --no-transfer-progress
 fi
