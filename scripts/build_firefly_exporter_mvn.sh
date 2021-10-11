dd3fbf60abadd4a3b9a9686b86cdad25843f9192
valb3r <valb3r.ua@gmail.com>
2021-10-11 19:53:56 +0300

    Fixed issue with FireFly build

--- scripts/build_firefly_exporter_mvn.sh
+++ scripts/build_firefly_exporter_mvn.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Build FireFly-Exporter separately, as it is not fulltime-deployment part
+echo "Installing dependencies for FireFly Exporter"
+./mvnw install --no-transfer-progress -DskipTests=true -Dmaven.javadoc.skip=true -B -V || exit 1
+
 echo "Building FireFly-exporter"
 cd firefly-exporter
 ../mvnw verify --no-transfer-progress
