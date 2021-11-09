b46126008c3d7dcf1065bd8c67a5156a6cc4340b
valb3r <valb3r.ua@gmail.com>
2021-11-09 11:24:40 +0200

    Fixed test expectation and advice

--- opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/controller/BankingGenericControllerAdvice.java
+++ opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/controller/BankingGenericControllerAdvice.java
@@ -22,6 +22,11 @@ public class BankingGenericControllerAdvice {
                     .build();
         }
 
+        if (ex instanceof IllegalArgumentException) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(List.of(ex.getMessage()));
+        }
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .build();
