c6a303e27081f3b7eba4387ec9a533bd8acf1f74
valb3r <valb3r.ua@gmail.com>
2021-11-09 10:21:11 +0200

    Added test

---
+++ opba-banking-rest-impl/src/main/java/de/adorsys/opba/tppbankingapi/controller/BankingGenericControllerAdvice.java
@@ -0,0 +1,29 @@
+package de.adorsys.opba.tppbankingapi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.security.UnrecoverableKeyException;
+import java.util.List;
+
+@ControllerAdvice(basePackageClasses = {TppBankingApiAisController.class})
+@Slf4j
+public class BankingGenericControllerAdvice {
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<List<String>> handleException(Exception ex) {
+        log.error("Unhandled exception: {}", ex.getMessage(), ex);
+        if (ex.getCause() instanceof UnrecoverableKeyException) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .build();
+    }
+}
---
+++ opba-embedded-starter-tests/src/test/java/de/adorsys/opba/starter/FintechPasswordApplyTest.java
@@ -0,0 +1,103 @@
+package de.adorsys.opba.starter;
+
+import de.adorsys.opba.api.security.external.service.RequestSigningService;
+import de.adorsys.opba.api.security.requestsigner.OpenBankingDataToSignProvider;
+import de.adorsys.opba.protocol.xs2a.entrypoint.ais.Xs2aListAccountsEntrypoint;
+import de.adorsys.opba.protocol.xs2a.tests.e2e.stages.CommonGivenStages;
+import de.adorsys.opba.starter.config.FintechRequestSigningTestConfig;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.BANK_PROFILE_ID;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.FINTECH_ID;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.FINTECH_REDIRECT_URL_NOK;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.FINTECH_REDIRECT_URL_OK;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.FINTECH_USER_ID;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.SERVICE_SESSION_PASSWORD;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.X_REQUEST_ID;
+import static de.adorsys.opba.protocol.xs2a.tests.HeaderNames.X_TIMESTAMP_UTC;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.AIS_ACCOUNTS_ENDPOINT;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.ANTON_BRUECKNER;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.DEFAULT_FINTECH_ID;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.FINTECH_REDIR_NOK;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.FINTECH_REDIR_OK;
+import static de.adorsys.opba.protocol.xs2a.tests.e2e.stages.StagesCommonUtil.SANDBOX_BANK_PROFILE_ID;
+import static de.adorsys.opba.restapi.shared.HttpHeaders.SERVICE_SESSION_ID;
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.RedirectConfig.redirectConfig;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * This is a very basic test to ensure application starts up and components are bundled properly.
+ * Protocols are tested in their own packages exhaustively.
+ */
+@ActiveProfiles("test")
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@SpringBootTest(classes = {OpenBankingEmbeddedApplication.class, FintechRequestSigningTestConfig.class}, webEnvironment = RANDOM_PORT)
+class FintechPasswordApplyTest {
+
+    @SpyBean
+    private Xs2aListAccountsEntrypoint xs2aListAccountsEntrypoint;
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Autowired
+    private RequestSigningService signingService;
+
+    @BeforeEach
+    void setupRestAssured() {
+        RestAssured.baseURI = "http://localhost:" + serverPort;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        config = config().redirect(redirectConfig().followRedirects(false));
+        RestAssured.replaceFiltersWith(new CommonGivenStages.RequestSigner(signingService, new OpenBankingDataToSignProvider()));
+    }
+
+    @Test
+    void testFintechPasswordIsValidated() {
+        xs2aAccountList(HttpStatus.ACCEPTED, "qwerty");
+        xs2aAccountList(HttpStatus.ACCEPTED, "qwerty");
+        xs2aAccountList(HttpStatus.UNAUTHORIZED, "not-qwerty");
+
+        verify(xs2aListAccountsEntrypoint, times(2)).execute(any());
+    }
+
+    private void xs2aAccountList(HttpStatus expected, String fintechPassword) {
+        headersWithoutIpAddress(ANTON_BRUECKNER, SANDBOX_BANK_PROFILE_ID, UUID.randomUUID(), Instant.now(), fintechPassword)
+                    .header(SERVICE_SESSION_ID, UUID.randomUUID().toString())
+                    .header(SERVICE_SESSION_PASSWORD, fintechPassword)
+                .when()
+                    .get(AIS_ACCOUNTS_ENDPOINT)
+                .then()
+                    .statusCode(expected.value());
+    }
+
+    private static RequestSpecification headersWithoutIpAddress(String fintechUserId, String bankProfileId, UUID xRequestId, Instant xTimestampUtc, String fintechPassword) {
+        return RestAssured
+                .given()
+                .header(BANK_PROFILE_ID, bankProfileId)
+                .header(FINTECH_REDIRECT_URL_OK, FINTECH_REDIR_OK)
+                .header(FINTECH_REDIRECT_URL_NOK, FINTECH_REDIR_NOK)
+                .header(SERVICE_SESSION_PASSWORD, fintechPassword)
+                .header(FINTECH_USER_ID, fintechUserId)
+                .header(FINTECH_ID, DEFAULT_FINTECH_ID)
+                .header(X_REQUEST_ID, xRequestId.toString())
+                .header(X_TIMESTAMP_UTC, xTimestampUtc.toString());
+    }
+}
