dc3b2942f5ac5deaaee2acfa9f68cb188a3195b0
tsehelnyk <58398447+tsehelnyk@users.noreply.github.com>
2021-11-12 11:42:44 +0200

    Added currency selector to consentUI (dedicated consent flow) Fixed bug in consentUI when creating dedicated consent with several accounts (displaying an incorrect list of accounts)

--- consent-ui/src/app/ais/common/constant/constant.ts
+++ consent-ui/src/app/ais/common/constant/constant.ts
@@ -1,3 +1,5 @@
 export const DATA_PATTERN = '\\d{4}-\\d{2}-\\d{2}';
 // The consent can be used max times per day
 export const MAX_FREQUENCY_PER_DAY = 999;
+
+export const DEFAULT_CURRENCY = 'EUR';
