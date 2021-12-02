415e510e4998656dc4a1b43b7856a932119f232a
valb3r <valb3r.ua@gmail.com>
2021-12-02 08:04:20 +0200

    OPEN-148. Make checkstyle happy

--- opba-embedded-starter-tests/src/test/java/de/adorsys/opba/starter/redirectcode/OpenBankingRedirectCodeTest.java
+++ opba-embedded-starter-tests/src/test/java/de/adorsys/opba/starter/redirectcode/OpenBankingRedirectCodeTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
