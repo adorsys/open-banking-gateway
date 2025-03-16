eb295ed196b2341fb1f5064b36fc5f8bc1125ae3
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2025-03-16 20:20:01 +0100

    fix RequestNormalizerGenerationTest test

--- opba-api-security-signer-generator/opba-api-security-signer-generator-impl/src/test/resources/expected/ExpectedDataToSignProvider.java
+++ opba-api-security-signer-generator/opba-api-security-signer-generator-impl/src/test/resources/expected/ExpectedDataToSignProvider.java
@@ -4,14 +4,11 @@ import de.adorsys.opba.api.security.generator.api.DataToSignProvider;
 import de.adorsys.opba.api.security.generator.api.MatcherUtil;
 import de.adorsys.opba.api.security.generator.api.RequestDataToSignNormalizer;
 import de.adorsys.opba.api.security.generator.api.RequestToSign;
+import jakarta.annotation.Generated;
 import java.lang.Override;
 import java.lang.String;
-import jakarta.annotation.Generated;
 
-@Generated(
-        value = "de.adorsys.opba.api.security.generator.normalizer.DataToSignProviderGenerator",
-        comments = "This class provides request signature canonicalization classes (convert Request to String to sign)"
-)
+@Generated(value = "de.adorsys.opba.api.security.generator.normalizer.DataToSignProviderGenerator", comments = "This class provides request signature canonicalization classes (convert Request to String to sign)")
 public class TestSigner implements DataToSignProvider {
     private final String basePath;
 
@@ -43,6 +40,6 @@ public class TestSigner implements DataToSignProvider {
         if (DataToSignProvider.HttpMethod.GET == toSign.getMethod() && MatcherUtil.matches("/v1/banking/ais/accounts/{account-id}/transactions", computedPath)) {
             return new GetTransactions();
         }
-        throw new IllegalStateException(String.format("Can't create signer for %s %s (full path: %s)", toSign.getMethod(), computedPath, toSign.getPath()));
+        throw new IllegalStateException(String.format("Can\'t create signer for %s %s (full path: %s)", toSign.getMethod(), computedPath, toSign.getPath()));
     }
 }
